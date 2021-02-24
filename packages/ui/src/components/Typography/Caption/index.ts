39a99a2ea0e34a9e62233ee7e72463441b308a5a
Белоусова Алиса Дмитриевна <alisabelousova007@gmail.com>
2021-02-24 13:58:42 +0300

    feat(ui): Добавлено правило переноса строк для типографики

--- packages/ui/src/components/Typography/Caption/index.ts
+++ packages/ui/src/components/Typography/Caption/index.ts
@@ -1,4 +1,4 @@
 import styled from 'styled-components';
 import { caption } from '@salutejs/plasma-tokens';
 
-export const Caption = styled.div(caption);
+export const Caption = styled.div({ overflowWrap: 'break-word', hyphens: 'auto', ...caption });
