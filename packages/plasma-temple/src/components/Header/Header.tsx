44cc0f2785fe72e805d4fbdd51c9d4fafae4010f
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-03-04 09:41:57 +0300

    feat(plasma-template): set header title for an active gallery

--- packages/plasma-temple/src/components/Header/Header.tsx
+++ packages/plasma-temple/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { mediaQuery } from '@salutejs/ui/utils';
 import { Header as UIKitHeader } from '@salutejs/ui/components/Header/Header';
 
+export const headerPaddingYM = 1.125;
+
 export const Header = styled(UIKitHeader)`
-    ${mediaQuery('M')`
-        padding-top: 1.125rem;
-        padding-bottom: 1.125rem;
-    `}
+    ${mediaQuery('M')(css`
+        padding-top: ${headerPaddingYM}rem;
+        padding-bottom: ${headerPaddingYM}rem;
+    `)}
 `;
