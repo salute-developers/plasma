acb3a226cd475736321e196a8f1b85eccbd43605
Fanil Zubairov <fannisco@gmail.com>
2021-04-01 11:44:09 +0200

    feat: Moved Grid from ui to plasma-core, Grid for plasma-web

--- packages/plasma-core/src/components/Grid/index.ts
+++ packages/plasma-core/src/components/Grid/index.ts
@@ -1,5 +1,4 @@
-export { Row } from '@salutejs/plasma-core/components/Grid';
-
 export { Container } from './Container';
 export { Col } from './Col';
 export type { ColProps } from './Col';
+export { Row } from './Row';
