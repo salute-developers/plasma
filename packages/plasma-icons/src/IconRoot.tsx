769a85adfedf01592b74a73e620150b8a3a6f99e
Fanil Zubairov <fannisco@gmail.com>
2021-04-05 12:36:34 +0200

    feat(plasma-icons): Using plasma-core's tokens

--- packages/plasma-icons/src/IconRoot.tsx
+++ packages/plasma-icons/src/IconRoot.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { primary } from '@salutejs/plasma-tokens';
+import { primary } from '@salutejs/plasma-core/tokens';
 
 const sizeMap = {
     xs: 1, // 16px
