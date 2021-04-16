f3221ba8551404f07b5e50fa8c3b50ec305b4857
Fanil Zubairov <fannisco@gmail.com>
2021-04-16 14:10:17 +0200

    fix(plasma-ui, plasma-web): Badge: correct reexport & const creation

--- packages/plasma-web/src/components/Badge/index.ts
+++ packages/plasma-web/src/components/Badge/index.ts
@@ -1,2 +1,4 @@
-export { Badge, badgeSizes, badgeRootSizes, badgeViews } from '@salutejs/plasma-core/components/Badge';
+export { badgeSizes, badgeRootSizes, badgeViews } from '@salutejs/plasma-core/components/Badge';
 export type { BadgeProps, BadgeSize, BadgeView } from '@salutejs/plasma-core/components/Badge';
+
+export { Badge } from './Badge';
