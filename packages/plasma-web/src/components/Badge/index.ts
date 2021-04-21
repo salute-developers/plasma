7af219a7892008b4dfab07f65cdc5966417a7b7b
Fanil Zubairov <fannisco@gmail.com>
2021-04-21 14:52:57 +0200

    fix(plasma-web, plasma-core): Badge views & radius

--- packages/plasma-web/src/components/Badge/index.ts
+++ packages/plasma-web/src/components/Badge/index.ts
@@ -1,4 +1,5 @@
-export { badgeSizes, badgeRootSizes, badgeViews } from '@salutejs/plasma-core/components/Badge';
-export type { BadgeProps, BadgeSize, BadgeView } from '@salutejs/plasma-core/components/Badge';
+export { badgeSizes } from '@salutejs/plasma-core/components/Badge';
+export type { BadgeSize } from '@salutejs/plasma-core/components/Badge';
 
-export { Badge } from './Badge';
+export { Badge, badgeViews } from './Badge';
+export type { BadgeProps, BadgeView } from './Badge';
