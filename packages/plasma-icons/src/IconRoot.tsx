7ad003d8998b5d64b4cb2e263ddd57f95919aa9a
VladislavPetyukevich <pet.vladislav@gmail.com>
2021-05-25 15:25:26 +0300

    fix: import es module from core in icons

--- packages/plasma-icons/src/IconRoot.tsx
+++ packages/plasma-icons/src/IconRoot.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { primary } from '@salutejs/plasma-core/tokens';
+import { primary } from '@salutejs/plasma-core';
 
 const sizeMap = {
     xs: 1, // 16px
