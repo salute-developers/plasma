1269548df352fa2eec9b4588b1c8e8000d00e3a6
Fanil Zubairov <fannisco@gmail.com>
2021-06-08 17:35:20 +0200

    feat(plasma-core, plasma-ui, plasma-web): Reassembled Field

---
+++ packages/plasma-ui/src/components/Field/Field.tsx
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components';
+import type { TextFieldProps } from '@salutejs/plasma-core';
+import { surfaceLiquid01, surfaceLiquid02 } from '@salutejs/plasma-tokens';
+
+export const FieldInput = styled.input<Pick<TextFieldProps, '$isFocused'>>`
+    background: ${surfaceLiquid01};
+    border-color: transparent;
+    border-radius: 1rem;
+
+    &:focus:not(:read-only) {
+        background-color: ${surfaceLiquid02};
+    }
+
+    ${({ $isFocused }) =>
+        $isFocused &&
+        css`
+            background-color: ${surfaceLiquid02};
+        `}
+`;
--- packages/plasma-ui/src/components/TextField/TextField.tsx
+++ packages/plasma-ui/src/components/TextField/TextField.tsx
@@ -1,142 +1,26 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-import { caption, accent, surfaceLiquid01, surfaceLiquid02, primary, secondary } from '@salutejs/plasma-tokens';
-import { TextFieldRoot, TextFieldHelper } from '@salutejs/plasma-core';
-import type { TextFieldProps as BaseTextFieldProps } from '@salutejs/plasma-core';
+import {
+    TextFieldRoot,
+    TextFieldInput,
+    TextFieldPlaceholder,
+    TextFieldContent,
+    TextFieldHelper,
+    TextFieldProps as BaseProps,
+} from '@salutejs/plasma-core';
 
-export interface TextFieldProps
-    extends BaseTextFieldProps,
-        Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange' | 'onFocus' | 'onBlur'> {
-    /**
-     * Слот для контента слева.
-     */
-    contentLeft?: React.ReactElement;
-}
+import { FieldInput } from '../Field/Field';
 
-interface StyledRootProps {
-    isContentLeft?: boolean;
-    isContentRight?: boolean;
-}
-interface StyledLabelProps {
-    isValue?: boolean;
-}
-
-const StyledInputWrapper = styled.label`
-    position: relative;
-    display: block;
-    cursor: text;
-`;
-const StyledInput = styled.input`
-    box-sizing: border-box;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-
-    padding: 1.625rem 1rem 0.625rem;
-    width: 100%;
-    height: 3.5rem;
-
-    background-color: ${surfaceLiquid01};
-    border: 0 none;
-    border-radius: 1rem;
-    color: ${primary};
-    caret-color: ${accent};
-
-    font: inherit;
-    line-height: inherit;
-    letter-spacing: inherit;
-
-    transition-duration: 0.1s;
-    transition-property: background-color, color;
-    transition-timing-function: ease-in-out;
-
-    &:focus {
-        background-color: ${surfaceLiquid02};
-        outline: none;
-    }
-
-    &:disabled {
-        color: ${secondary};
-    }
-`;
-const StyledLabel = styled.span<StyledLabelProps>`
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-
-    position: absolute;
-    top: 1.125rem;
-    left: 1rem;
-    right: 1rem;
-
-    transition: all 0.1s ease-in-out;
-
-    /* stylelint-disable-next-line selector-nested-pattern, selector-type-no-unknown */
-    ${StyledInput}:focus ~ & {
-        ${caption};
-
-        top: 0.375rem;
-    }
-
-    ${({ isValue }) =>
-        isValue &&
-        css`
-            ${caption};
-
-            top: 0.375rem;
-        `}
-`;
-const StyledContent = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    margin: 0 1rem;
-
-    display: flex;
-    align-items: center;
-
-    color: ${secondary};
-
-    /* stylelint-disable-next-line */
-    ${StyledInput} ~ & {
-        left: auto;
-        right: 0;
-    }
-`;
-const StyledRoot = styled(TextFieldRoot)<StyledRootProps>`
-    color: ${secondary};
-
-    ${({ isContentLeft }) =>
-        isContentLeft &&
-        css`
-            & ${StyledInput} {
-                padding-left: 3.5rem;
-            }
-            & ${StyledLabel} {
-                left: 3.5rem;
-            }
-        `}
-
-    ${({ isContentRight }) =>
-        isContentRight &&
-        css`
-            & ${StyledInput} {
-                padding-right: 3.5rem;
-            }
-            & ${StyledLabel} {
-                right: 3.5rem;
-            }
-        `}
-`;
+export interface TextFieldProps extends BaseProps {}
 
 /**
- * Компонент ввода с подписью (caption) сверху.
+ * Поле ввода текста.
  */
 export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
     (
         {
+            size = 'l',
             value,
+            placeholder,
             label,
             helperText,
             disabled,
@@ -146,39 +30,44 @@ export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
             onChange,
             onFocus,
             onBlur,
-            className,
-            id,
             style,
-            type = 'text',
+            id,
+            className,
             ...rest
         },
         ref,
-    ) => (
-        <StyledRoot
-            status={status}
-            disabled={disabled}
-            isContentLeft={!!contentLeft}
-            isContentRight={!!contentRight}
-            className={className}
-            style={style}
-            id={id}
-        >
-            <StyledInputWrapper>
-                {contentLeft && <StyledContent>{contentLeft}</StyledContent>}
-                <StyledInput
+    ) => {
+        return (
+            <TextFieldRoot
+                $size={size}
+                $disabled={disabled}
+                $isContentLeft={!!contentLeft}
+                $isContentRight={!!contentRight}
+                $isValue={!!value}
+                $isHelper={!!helperText}
+                status={status}
+                className={className}
+                style={style}
+            >
+                {contentLeft && <TextFieldContent pos="left">{contentLeft}</TextFieldContent>}
+                {(label || placeholder) && (
+                    <TextFieldPlaceholder htmlFor={id}>{label || placeholder}</TextFieldPlaceholder>
+                )}
+                <FieldInput
                     ref={ref}
+                    id={id}
+                    as={TextFieldInput}
                     value={value}
-                    type={type}
                     disabled={disabled}
+                    status={status}
                     onChange={onChange}
                     onFocus={onFocus}
                     onBlur={onBlur}
                     {...rest}
                 />
-                {label && <StyledLabel isValue={!!value}>{label}</StyledLabel>}
-                {contentRight && <StyledContent>{contentRight}</StyledContent>}
-            </StyledInputWrapper>
-            {helperText && <TextFieldHelper status={status}>{helperText}</TextFieldHelper>}
-        </StyledRoot>
-    ),
+                {contentRight && <TextFieldContent pos="right">{contentRight}</TextFieldContent>}
+                {helperText && <TextFieldHelper status={status}>{helperText}</TextFieldHelper>}
+            </TextFieldRoot>
+        );
+    },
 );
