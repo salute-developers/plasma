819d371dc4659980e312189a408d3445d7f5b7d5
Fanil Zubairov <fannisco@gmail.com>
2021-07-16 15:40:02 +0200

    chore: Plasma Main App

---
+++ website/plasma-website/components/Badge.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Badge as BaseBadge } from '@salutejs/plasma-b2c';
+import { whitePrimary, buttonPrimary } from '@salutejs/plasma-tokens-b2c';
+
+export const Badge = styled(BaseBadge).attrs(() => ({ size: 's', view: 'primary' }))`
+    margin-left: 0.5rem;
+    padding: 0.25rem;
+
+    background-color: ${buttonPrimary};
+    color: ${whitePrimary};
+    border-radius: 0.375rem;
+`;
