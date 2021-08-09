0a5deedde8a510431ef096133d538622af1219fb
Fanil Zubairov <fannisco@gmail.com>
2021-08-09 21:39:33 +0200

    feat(plasma-ui): Add mixin reexports from core

--- packages/plasma-ui/src/mixins/index.ts
+++ packages/plasma-ui/src/mixins/index.ts
@@ -1,3 +1,19 @@
+export {
+    addFocus,
+    syntheticFocus,
+    applyBlur,
+    applyDisabled,
+    applyEllipsis,
+    applyHyphens,
+    applyMaxLines,
+    applyNoSelect,
+    applyRoundness,
+    radiuses,
+    applyView,
+    views,
+    applyScrollSnap,
+    applySpacing,
+} from '@salutejs/plasma-core';
 export type {
     FocusProps,
     OutlinedProps,
