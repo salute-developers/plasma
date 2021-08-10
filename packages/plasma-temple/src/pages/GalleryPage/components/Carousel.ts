26f7c5900a53820a6afe7ac434fa7f2218bd8fdc
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-08-10 18:52:03 +0300

    feat(plasma-temple): error page layout
    fix(plasma-temple): other stories

--- packages/plasma-temple/src/pages/GalleryPage/components/Carousel.ts
+++ packages/plasma-temple/src/pages/GalleryPage/components/Carousel.ts
@@ -3,6 +3,8 @@ import { Carousel, CarouselProps as BaseCarouselProps } from '@salutejs/plasma-u
 
 export type CarouselProps = Omit<BaseCarouselProps, 'detectAvtive' | 'detectThreshold'>;
 
+export type { CarouselProps } from '@salutejs/plasma-ui';
+
 export const CommonCarousel = styled(Carousel)`
     padding-right: var(--plasma-grid-margin);
 `;
