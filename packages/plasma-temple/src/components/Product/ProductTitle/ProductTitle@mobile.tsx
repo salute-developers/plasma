358220859d9bb242b4b5333566491b8253ec0790
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-08-20 14:26:12 +0300

    feat(plasma-temple): Empty cart mobile version

---
+++ packages/plasma-temple/src/components/Product/ProductTitle/ProductTitle@mobile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Footnote1, Headline2, TextBox } from '@salutejs/plasma-ui';
+import { secondary } from '@salutejs/plasma-tokens';
+
+import { ProductTitleProps } from './types';
+
+const StyledSubtitle = styled(Footnote1)`
+    color: ${secondary};
+`;
+
+export const ProductTitleMobile: React.FC<ProductTitleProps> = ({ title, subtitle }) => (
+    <TextBox>
+        <Headline2>{title}</Headline2>
+        <StyledSubtitle>{subtitle}</StyledSubtitle>
+    </TextBox>
+);
