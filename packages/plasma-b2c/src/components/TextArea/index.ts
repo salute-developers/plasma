72d73a06f0c4c4be2ab339e9b9b2703b13dbe547
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2021-09-27 23:31:58 +0300

    feat(plasma-b2c): TextArea for b2c added

--- packages/plasma-b2c/src/components/TextArea/index.ts
+++ packages/plasma-b2c/src/components/TextArea/index.ts
@@ -1,2 +1,2 @@
-export { TextArea } from '@salutejs/plasma-web';
-export type { TextAreaProps } from '@salutejs/plasma-web';
+export { TextArea } from './TextArea';
+export type { TextAreaProps } from './TextArea';
