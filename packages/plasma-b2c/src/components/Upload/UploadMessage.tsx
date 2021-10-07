d5c5785edeedc816f8952bb8a29cc34fd417e44d
Artem Neretin <hawktrike@gmail.com>
2021-10-07 19:15:40 +0700

    feat(plasma-b2c): Added `PreviewGallery` component

--- packages/plasma-b2c/src/components/Upload/UploadMessage.tsx
+++ packages/plasma-b2c/src/components/Upload/UploadMessage.tsx
@@ -3,7 +3,7 @@ import { success, critical } from '@salutejs/plasma-core';
 
 import { Caption } from '../Typography';
 
-import { Status } from './types';
+import { StatusType } from './types';
 
 const statuses = {
     error: {
@@ -14,7 +14,7 @@ const statuses = {
     },
 };
 
-export const UploadMessage = styled(Caption)<{ status?: Status }>`
+export const UploadMessage = styled(Caption)<{ status?: StatusType }>`
     margin-top: 0.25rem;
     text-align: center;
 
