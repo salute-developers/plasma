9100702e5832c5cb6562335024040c8e3e441cad
Artem Neretin <hawktrike@gmail.com>
2021-10-18 19:31:52 +0700

    chore(plasma-b2c): Add global style for `a` tag in storybook

--- packages/plasma-b2c/.storybook/preview.js
+++ packages/plasma-b2c/.storybook/preview.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 import { addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { b2c } from '@salutejs/plasma-tokens-b2c/typo';
+import { link, linkHover, linkActive } from '@salutejs/plasma-tokens-web';
 import { light, dark } from '@salutejs/plasma-tokens-b2c/themes';
 import { standard as standardTypo, compatible as compatibleTypo } from '@salutejs/plasma-typo';
 
@@ -12,6 +13,17 @@ import { ToastProvider } from '../src/components/Toast';
 const DocumentStyle = createGlobalStyle`
     html:root {
         min-height: 100vh;
+    }
+    a {
+        color: ${link};
+        text-decoration: underline;
+
+        &:hover {
+            color: ${linkHover};
+        }
+        &:active {
+            color: ${linkActive};
+        }
     }
 `;
 /* stylelint-enable */
