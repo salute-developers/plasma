baada3cec1b8624523147db99df797450405c538
Fanil Zubairov <fannisco@gmail.com>
2021-10-30 13:10:26 +0200

    feat(plasma-website): Plasma icons service

---
+++ website/plasma-website/components/roster/SearchForm.tsx
@@ -0,0 +1,39 @@
+import { memo, useCallback, useRef } from 'react';
+import styled from 'styled-components';
+import { TextField, TextFieldProps, Button } from '@salutejs/plasma-b2c';
+import { IconSearch } from '@salutejs/plasma-icons';
+import { tertiary } from '@salutejs/plasma-tokens-b2c';
+
+interface SearchFormProps {
+    onInput: TextFieldProps['onInput'];
+}
+
+const StyledForm = styled.form``;
+const StyledButton = styled(Button)`
+    cursor: text;
+`;
+
+export const SearchForm = memo<SearchFormProps>(({ onInput }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const onIconClick = useCallback(() => inputRef.current?.focus(), []);
+
+    return (
+        <StyledForm>
+            <TextField
+                ref={inputRef}
+                placeholder="Search"
+                size="m"
+                onInput={onInput}
+                contentLeft={
+                    <StyledButton
+                        type="button"
+                        view="clear"
+                        shiftLeft
+                        contentLeft={<IconSearch color={tertiary} />}
+                        onClick={onIconClick}
+                    />
+                }
+            />
+        </StyledForm>
+    );
+});
