51756729ed3c244af4e0cb306419604a190bf642
Artem Neretin <hawktrike@gmail.com>
2021-11-01 11:42:20 +0700

    feat(plasma-docs-ui): Add `CodeSandbox` component

--- website/plasma-web-docs/src/theme/ReactLiveScope/index.tsx
+++ website/plasma-web-docs/src/theme/ReactLiveScope/index.tsx
@@ -3,11 +3,13 @@ import * as Icons from '@salutejs/plasma-icons';
 import * as Tokens from '@salutejs/plasma-tokens-web';
 import * as Web from '@salutejs/plasma-web';
 import { Filler } from '@salutejs/plasma-docs-ui';
+import styled from 'styled-components';
 
 // Add react-live imports you need here
 const ReactLiveScope = {
     React,
     Filler,
+    styled,
     ...Icons,
     ...Tokens,
     ...Web,
