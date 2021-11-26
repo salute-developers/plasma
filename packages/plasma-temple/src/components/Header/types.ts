60b986e2eb21d9a48e54ab44f85f871ce9b86137
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-11-26 11:10:04 +0300

    feat(plasma-temple): add support NeuHeader Component

--- packages/plasma-temple/src/components/Header/types.ts
+++ packages/plasma-temple/src/components/Header/types.ts
@@ -1,6 +1,8 @@
-import { HeaderProps } from '@salutejs/plasma-ui/components/Header/Header';
+import { NeuHeaderProps as NewHeaderProps } from '@salutejs/plasma-ui/components/Header/NeuHeader';
+import { HeaderProps as OldHeaderProps } from '@salutejs/plasma-ui';
 
-export type { HeaderProps };
+export type HeaderProps = NewHeaderProps | OldHeaderProps;
+export type { NewHeaderProps, OldHeaderProps };
 
 export interface ComponentPropsWithHeader {
     header?: HeaderProps;
