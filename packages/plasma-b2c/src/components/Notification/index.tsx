31e6f48cec8c2f47f997d6d0d8bc1b546c9798fd
Fanil Zubairov <fannisco@gmail.com>
2021-12-22 17:20:50 +0200

    feat(plasma-web, plasma-b2c): Added possibility to specify id for `Notification`

--- packages/plasma-b2c/src/components/Notification/index.tsx
+++ packages/plasma-b2c/src/components/Notification/index.tsx
@@ -1,2 +1,2 @@
 export { NotificationsProvider } from '@salutejs/plasma-web';
-export { addNotification } from '@salutejs/plasma-web';
+export { addNotification, closeNotification } from '@salutejs/plasma-web';
