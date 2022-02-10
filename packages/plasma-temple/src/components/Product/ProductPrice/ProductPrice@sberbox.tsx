d657d76349664675982395454fccc85cc6bacc8e
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2022-02-10 12:42:12 +0300

    feat(plasma-temple): Product component

--- packages/plasma-temple/src/components/Product/ProductPrice/ProductPrice@sberbox.tsx
+++ packages/plasma-temple/src/components/Product/ProductPrice/ProductPrice@sberbox.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Headline2 } from '@salutejs/plasma-ui';
 
-import { ProductPriceProps } from './ProductPrice';
 import { ProductPriceCommon } from './ProductPrice@common';
+import { ProductPriceProps } from './types';
 
 export const ProductPriceSberBox: React.FC<ProductPriceProps> = (props) => (
     <Headline2>
