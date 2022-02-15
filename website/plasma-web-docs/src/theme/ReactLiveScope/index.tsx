ff0525aef6f4b27a074f7dee40cdac621e89047b
Fanil Zubairov <fannisco@gmail.com>
2022-02-15 13:51:03 +0200

    feat(plasma-web-docs): Using b2b tokens / switch light/dark

--- website/plasma-web-docs/src/theme/ReactLiveScope/index.tsx
+++ website/plasma-web-docs/src/theme/ReactLiveScope/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as Icons from '@salutejs/plasma-icons';
-import * as Tokens from '@salutejs/plasma-tokens-web';
+import * as Tokens from '@salutejs/plasma-tokens-b2b';
 import * as Web from '@salutejs/plasma-web';
 import { Filler } from '@salutejs/plasma-docs-ui';
 import styled from 'styled-components';
