4c92d0d2ae49e645754470bb721ed9462a85fb05
Fanil Zubairov <fannisco@gmail.com>
2022-02-17 11:43:06 +0200

    fix(plasma-web): `Modal` background set to `backgroundPrimary` (01) token

--- packages/plasma-web/src/components/Modal/ModalView.tsx
+++ packages/plasma-web/src/components/Modal/ModalView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { surfaceCard } from '@salutejs/plasma-core';
+import { backgroundPrimary } from '@salutejs/plasma-core';
 import { IconClose } from '@salutejs/plasma-icons';
 
 import { Button } from '../Button';
@@ -23,7 +23,7 @@ const StyledBody = styled.div`
     max-width: 100%;
 
     border-radius: 1.25rem;
-    background-color: ${surfaceCard};
+    background-color: ${backgroundPrimary};
 `;
 const StyledContent = styled.div`
     padding: 2rem;
