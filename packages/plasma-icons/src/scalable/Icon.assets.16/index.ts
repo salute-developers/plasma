52a3f2a98752f3588007f7aa670c28699c7454df
neretinaa <hawktrike@gmail.com>
2022-03-29 17:28:50 +0700

    feat(plasma-icons): Add other icons for `24 dp` and  `16 dp`

--- packages/plasma-icons/src/scalable/Icon.assets.16/index.ts
+++ packages/plasma-icons/src/scalable/Icon.assets.16/index.ts
@@ -34,6 +34,20 @@ import { Star } from './Star';
 import { Add } from './Add';
 import { Profile } from './Profile';
 import { Work } from './Work';
+import { Temperature } from './Temperature';
+import { Humidity } from './Humidity';
+import { Motion } from './Motion';
+import { BatteryCharge } from './BatteryCharge';
+import { Battery100 } from './Battery100';
+import { Battery80 } from './Battery80';
+import { Battery60 } from './Battery60';
+import { Battery40 } from './Battery40';
+import { Battery20 } from './Battery20';
+import { BatteryEmpty } from './BatteryEmpty';
+import { DoorClosed } from './DoorClosed';
+import { DoorOpen } from './DoorOpen';
+import { Smoke } from './Smoke';
+import { Flood } from './Flood';
 
 export const iconSet16 = {
     video: Video,
@@ -72,6 +86,20 @@ export const iconSet16 = {
     add: Add,
     profile: Profile,
     work: Work,
+    temperature: Temperature,
+    humidity: Humidity,
+    motion: Motion,
+    batteryCharge: BatteryCharge,
+    battery100: Battery100,
+    battery80: Battery80,
+    battery60: Battery60,
+    battery40: Battery40,
+    battery20: Battery20,
+    batteryEmpty: BatteryEmpty,
+    doorClosed: DoorClosed,
+    doorOpen: DoorOpen,
+    smoke: Smoke,
+    flood: Flood,
 };
 
 export type IconSet16 = keyof typeof iconSet16;
