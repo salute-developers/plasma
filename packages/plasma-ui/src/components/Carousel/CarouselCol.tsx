f39894058599b4e6a0536cf76c3f34229f44198a
Arseny Kruglikov <kruglikov.arseny@gmail.com>
2022-06-02 17:13:03 +0300

    feat(plasma-ui): support of the withUseVirtual in the CarouselCol

--- packages/plasma-ui/src/components/Carousel/CarouselCol.tsx
+++ packages/plasma-ui/src/components/Carousel/CarouselCol.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import styled from 'styled-components';
-import { applyScrollSnap, ScrollSnapProps, CarouselItemProps } from '@salutejs/plasma-core';
+import styled, { css } from 'styled-components';
+import { applyScrollSnap, ScrollSnapProps, CarouselItemProps, VirtualizationProps } from '@salutejs/plasma-core';
 
 import { Col, ColProps } from '../Grid';
 
-const StyledCol = styled(Col)<ScrollSnapProps>`
+const StyledCol = styled(Col)<ScrollSnapProps & VirtualizationProps>`
+    ${({ withUseVirtual }) =>
+        withUseVirtual &&
+        css`
+            position: absolute;
+        `}
+
     ${applyScrollSnap};
 `;
 
-export interface CarouselColProps extends ColProps, CarouselItemProps, React.HTMLAttributes<HTMLDivElement> {}
+export type CarouselColProps = ColProps & CarouselItemProps & React.HTMLAttributes<HTMLDivElement>;
 
 /**
  * Элемент списка. В рамках интерфейса элемент наследуется от ``Col`` и ``CarouselItem``.
  * Используется для каруселей с сеткой.
  */
-export const CarouselCol: React.FC<CarouselColProps> = ({ children, ...rest }) => {
+export const CarouselCol: React.FC<CarouselColProps> = ({
+    virtualLeft,
+    virtualTop,
+    withUseVirtual,
+    children,
+    ...rest
+}) => {
     return (
-        <StyledCol type="calc" role="group" aria-roledescription="slide" {...rest}>
+        <StyledCol
+            type="calc"
+            role="group"
+            aria-roledescription="slide"
+            withUseVirtual={withUseVirtual}
+            style={withUseVirtual ? { transform: `translate(${virtualLeft ?? 0}px, ${virtualTop ?? 0}px)` } : undefined}
+            {...rest}
+        >
             {children}
         </StyledCol>
     );
