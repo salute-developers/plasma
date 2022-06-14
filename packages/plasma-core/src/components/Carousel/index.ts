29b4b15b9430eda99210ccf0ce0fe3186556aed1
Arseny Kruglikov <kruglikov.arseny@gmail.com>
2022-06-14 14:00:38 +0300

    test(plasma-ui): fix export of CarouselItemVirtual, update contributing.md

--- packages/plasma-core/src/components/Carousel/index.ts
+++ packages/plasma-core/src/components/Carousel/index.ts
@@ -2,7 +2,7 @@ export { CarouselGridWrapper } from './Carousel';
 export { Carousel } from './Carousel';
 export { CarouselTrack } from './Carousel';
 
-export { CarouselItem } from './CarouselItem';
+export { CarouselItem, CarouselItemVirtual } from './CarouselItem';
 export type { CarouselItemProps, CarouselItemVirtualProps } from './CarouselItem';
 
 export { useCarousel } from './hooks';
