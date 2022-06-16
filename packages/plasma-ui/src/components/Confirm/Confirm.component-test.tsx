2b8050a6d0a2173d1671ef6f08fc5d9f7019d4bb
Vasiliy Loginevskiy <yeti.or@gmail.com>
2022-06-17 00:18:12 +0300

    test(plasma-ui): add Confirm tests

---
+++ packages/plasma-ui/src/components/Confirm/Confirm.component-test.tsx
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import { mount, CypressTestDecorator, getComponent, PadMe, SpaceMe } from '@salutejs/plasma-cy-utils';
+
+describe('Confirm', () => {
+    const Confirm = getComponent('Confirm');
+
+    const title = 'Использовать максимум возможностей?';
+    const subtitle = 'Возможно всё.';
+    const approveText = 'Да';
+    const dismissText = 'Нет';
+
+    const props = {
+        title,
+        subtitle,
+        approveText,
+        dismissText,
+    };
+
+    it('basic', () => {
+        mount(
+            <CypressTestDecorator>
+                <Confirm visible view="primary" {...props} />
+            </CypressTestDecorator>,
+        );
+        cy.matchImageSnapshot();
+    });
+
+    ['primary', undefined, 'secondary', 'success', 'warning', 'critical'].forEach((view) => {
+        it(`_view_${view}`, () => {
+            mount(
+                <CypressTestDecorator>
+                    <Confirm visible view={view} {...props} />
+                </CypressTestDecorator>,
+            );
+            cy.matchImageSnapshot();
+        });
+    });
+
+    describe('__button', () => {
+        it('_reverse', () => {
+            mount(
+                <CypressTestDecorator>
+                    <Confirm reverseButtons visible view="primary" {...props} />
+                </CypressTestDecorator>,
+            );
+            cy.matchImageSnapshot();
+        });
+
+        it('_one', () => {
+            mount(
+                <CypressTestDecorator>
+                    <Confirm reverseButtons visible view="primary" {...{ ...props, dismissText: null }} />
+                </CypressTestDecorator>,
+            );
+            cy.matchImageSnapshot();
+        });
+    });
+
+    describe('_visible', () => {
+        const Body1 = getComponent('Body1');
+
+        it('_yes', () => {
+            mount(
+                <CypressTestDecorator>
+                    <Confirm visible view="primary" {...props} />
+                    <Body1 mt={12}>
+                        I have a dream that one day this nation will rise up and live out the true meaning of its creed.
+                        We hold these truths to be self-evident that all men are created equal. I have a dream that one
+                        day on the red hills of Georgia the sons of former slaves and the sons of former slaveowners
+                        will be able to sit down together at the table of brotherhood. I have a dream that one day even
+                        the state of Mississippi, a state sweltering with the heat of injustice, sweltering with the
+                        heat of oppression, will be transformed into an oasis of freedom and justice. I have a dream...
+                    </Body1>
+                </CypressTestDecorator>,
+            );
+            cy.matchImageSnapshot();
+        });
+
+        it('_no', () => {
+            mount(
+                <CypressTestDecorator>
+                    <Confirm view="primary" {...props} />
+                    <Body1 mt={12}>
+                        I have a dream that one day this nation will rise up and live out the true meaning of its creed.
+                        We hold these truths to be self-evident that all men are created equal. I have a dream that one
+                        day on the red hills of Georgia the sons of former slaves and the sons of former slaveowners
+                        will be able to sit down together at the table of brotherhood. I have a dream that one day even
+                        the state of Mississippi, a state sweltering with the heat of injustice, sweltering with the
+                        heat of oppression, will be transformed into an oasis of freedom and justice. I have a dream...
+                    </Body1>
+                </CypressTestDecorator>,
+            );
+            cy.matchImageSnapshot();
+        });
+    });
+
+    describe(':events', () => {
+        const Body1 = getComponent('Body1');
+        const Button = getComponent('Button');
+
+        const Interactive = ({ onApproveAction, onDismissAction }) => {
+            const [visible, setVisible] = useState(false);
+
+            const onDismiss = () => {
+                onDismissAction();
+                setVisible(false);
+            };
+
+            const onApprove = () => {
+                onApproveAction();
+                setVisible(false);
+            };
+
+            return (
+                <>
+                    <Button id="show" text="show" onClick={() => setVisible(true)} />
+                    <Confirm
+                        id="confirm"
+                        visible={visible}
+                        view="primary"
+                        onApprove={onApprove}
+                        onDismiss={onDismiss}
+                        {...props}
+                    />
+                    <Body1 mt={12}>
+                        I have a dream that one day this nation will rise up and live out the true meaning of its creed.
+                        We hold these truths to be self-evident that all men are created equal. I have a dream that one
+                        day on the red hills of Georgia the sons of former slaves and the sons of former slaveowners
+                        will be able to sit down together at the table of brotherhood. I have a dream that one day even
+                        the state of Mississippi, a state sweltering with the heat of injustice, sweltering with the
+                        heat of oppression, will be transformed into an oasis of freedom and justice. I have a dream...
+                    </Body1>
+                </>
+            );
+        };
+
+        it('onApprove', () => {
+            const onApproveAction = cy.spy().as('onApproveAction');
+            mount(
+                <CypressTestDecorator>
+                    <Interactive onApproveAction={onApproveAction} />
+                </CypressTestDecorator>,
+            );
+
+            cy.get('#show').click();
+
+            cy.contains(approveText).click();
+            cy.get('@onApproveAction').should('have.been.calledOnce');
+        });
+
+        it('onDismiss', () => {
+            const onDismissAction = cy.spy().as('onDismissAction');
+            mount(
+                <CypressTestDecorator>
+                    <Interactive onDismissAction={onDismissAction} />
+                </CypressTestDecorator>,
+            );
+
+            cy.get('#show').click();
+
+            cy.contains(dismissText).click();
+            cy.get('@onDismissAction').should('have.been.calledOnce');
+
+            cy.get('#show').click();
+
+            cy.get('#confirm > :first-child').click();
+            cy.get('@onDismissAction').should('have.been.calledTwice');
+        });
+    });
+});
