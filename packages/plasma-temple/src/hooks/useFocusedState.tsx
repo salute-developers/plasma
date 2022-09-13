32d273766f56b9a8bfcac53dee27e8048feda927
Maksim Sviridov <maksim.v.sviridov@gmail.com>
2022-09-13 18:43:02 +0300

    fix(plasma-temple): prevent `focusin` event

--- packages/plasma-temple/src/hooks/useFocusedState.tsx
+++ packages/plasma-temple/src/hooks/useFocusedState.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 export const useFocusedState = <T extends HTMLElement>(ref: React.RefObject<T>): boolean => {
-    const [focused, setFocused] = React.useState(false);
+    const [focused, setFocused] = useState(false);
 
-    const onFocus = React.useCallback(() => {
-        setFocused(true);
-    }, []);
-
-    const onBlur = React.useCallback(() => {
-        setFocused(false);
-    }, []);
-
-    React.useEffect(() => {
-        if (ref.current) {
-            const element = ref.current;
-
-            element.addEventListener('focusin', onFocus);
-            element.addEventListener('focusout', onBlur);
-
-            return () => {
-                element.removeEventListener('focusin', onFocus);
-                element.removeEventListener('focusout', onBlur);
-            };
+    useEffect(() => {
+        if (!ref.current) {
+            return;
         }
 
-        return undefined;
-    }, [onBlur, onFocus, ref]);
+        const handleFocus = (event: FocusEvent) => {
+            event.preventDefault();
+            setFocused(true);
+        };
+
+        const handleBlur = () => {
+            setFocused(false);
+        };
+
+        const element = ref.current;
+
+        element.addEventListener('focusin', handleFocus);
+        element.addEventListener('focusout', handleBlur);
+
+        return () => {
+            element.removeEventListener('focusin', handleFocus);
+            element.removeEventListener('focusout', handleBlur);
+        };
+    }, [ref]);
 
     return focused;
 };
