b88f72cabeee621d1aa077ee0767256fbef9076d
Alex Lobyntsev <yakutoc@gmail.com>
2022-10-04 16:30:12 +0700

    feat(plasma-ui): add useThemeContext hook

--- packages/plasma-ui/src/hooks/index.ts
+++ packages/plasma-ui/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useDebouncedFunction, useForkRef } from '@salutejs/plasma-core';
 
 export { useRemoteListener } from './useRemoteListener';
+export { useThemeContext } from './useThemeContext';
