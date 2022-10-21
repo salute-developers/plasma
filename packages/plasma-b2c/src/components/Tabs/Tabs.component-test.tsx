bbe3ba7c516dbde5d6de2209a6484edf0e13a59b
Alex Lobyntsev <yakutoc@gmail.com>
2022-10-21 16:25:44 +0700

    feat(plasma-b2c): handling tab focus by cypress tests

--- packages/plasma-b2c/src/components/Tabs/Tabs.component-test.tsx
+++ packages/plasma-b2c/src/components/Tabs/Tabs.component-test.tsx
@@ -7,6 +7,9 @@ describe('plasma-b2c: Tabs', () => {
     const Tabs = getComponent('Tabs');
     const TabItem = getComponent('TabItem');
 
+    const withAutoFocus = getComponent('withAutoFocus');
+    const TabAutoFocus = withAutoFocus(TabItem);
+
     it('_size', () => {
         mount(
             <CypressTestDecorator>
@@ -58,6 +61,69 @@ describe('plasma-b2c: Tabs', () => {
                 </Tabs>
             </CypressTestDecorator>,
         );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('Handle tab focus: autoFocus', () => {
+        mount(
+            <CypressTestDecorator>
+                <Tabs>
+                    {items.map((item, i) => (
+                        <TabAutoFocus key={i} isActive={i === 1} autoFocus={i === 1}>
+                            {item.label}
+                        </TabAutoFocus>
+                    ))}
+                </Tabs>
+            </CypressTestDecorator>,
+        );
+
+        cy.get('div > div:nth-child(2)').focus();
+        cy.matchImageSnapshot();
+    });
+
+    it('Handle tab focus: pilled', () => {
+        mount(
+            <CypressTestDecorator>
+                <Tabs pilled>
+                    {items.map((item, i) => (
+                        <TabAutoFocus key={i} isActive={i === 1} autoFocus={i === 1}>
+                            {item.label}
+                        </TabAutoFocus>
+                    ))}
+                </Tabs>
+            </CypressTestDecorator>,
+        );
+
+        cy.get('div > div:nth-child(2)').focus();
+        cy.matchImageSnapshot();
+    });
+
+    // INFO: Этот тест должен обязательно идти после проверки на autoFocus,
+    // иначе фокус в предыдущих двух не будет работать.
+    it('Handle tab focus: default', () => {
+        function StubComponent() {
+            const [index, setIndex] = React.useState(1);
+
+            return (
+                <Tabs>
+                    {items.map((item, i) => (
+                        <TabItem key={i} tabIndex={0} isActive={i === index} onClick={() => setIndex(i)}>
+                            {item.label}
+                        </TabItem>
+                    ))}
+                </Tabs>
+            );
+        }
+
+        mount(
+            <CypressTestDecorator>
+                <StubComponent />
+            </CypressTestDecorator>,
+        );
+
+        cy.get('div > div:nth-child(2)').focus();
+        cy.get('div > div:nth-child(1)').last().focus().click({ force: true });
 
         cy.matchImageSnapshot();
     });
