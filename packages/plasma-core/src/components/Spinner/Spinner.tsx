94ab4dfe0b1510309366f8b5783d8a251ab51d02
Alex Lobyntsev <yakutoc@gmail.com>
2022-10-25 14:51:50 +0700

    feat(plasma-core): handle "Spinner" size

--- packages/plasma-core/src/components/Spinner/Spinner.tsx
+++ packages/plasma-core/src/components/Spinner/Spinner.tsx
@@ -27,17 +27,31 @@ const StyledRoot = styled.div<{ $size: string }>`
     animation: ${rotateAnimation} 1s linear infinite;
 `;
 
+const scalingPixelBasis = 16;
+
 export interface SpinnerProps extends React.HTMLAttributes<HTMLDivElement> {
     size?: number | string;
     color?: string;
+    deviceScale?: number;
 }
+
+const sizeFormatting = (size: number | string, deviceScale = 1): string => {
+    if (typeof size === 'number' || (!size.endsWith('rem') && !size.endsWith('px'))) {
+        return `${Number(size) / (scalingPixelBasis * deviceScale)}rem`;
+    }
+
+    return size;
+};
 
 /**
  * Компонент для отображения индикатора загрузки.
  */
-export const Spinner: React.FC<SpinnerProps> = ({ id, size = 56, color = accent, ...rest }) => {
+export const Spinner: React.FC<SpinnerProps> = (props) => {
+    const { id, size = 56, color = accent, deviceScale, ...rest } = props;
+    const currentSize = sizeFormatting(size, deviceScale);
+
     return (
-        <StyledRoot id={id} $size={typeof size === 'number' ? `${size}px` : size} {...rest}>
+        <StyledRoot id={id} $size={currentSize} {...rest}>
             <SpinnerSvg id={id} width={size} height={size} color={color} />
         </StyledRoot>
     );
