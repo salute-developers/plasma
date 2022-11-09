5d2439b2896fdcc6226c208dcbb91946ca6a60e6
Alex Lobyntsev <yakutoc@gmail.com>
2022-11-09 16:08:53 +0700

    feat(plasma-b2c): processing slider keyboard support by cypress

--- packages/plasma-b2c/src/components/Slider/Slider.component-test.tsx
+++ packages/plasma-b2c/src/components/Slider/Slider.component-test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { mount, CypressTestDecorator, getComponent, PadMe, Portal } from '@salutejs/plasma-cy-utils';
 
 const noop = () => {};
@@ -119,5 +119,139 @@ describe('plasma-b2c: Slider', () => {
         dragAndDrop(cy.root().get('div > div + div').last(), { clientX: 450, clientY: 15 });
 
         cy.matchImageSnapshot();
+    });
+
+    describe('Keyboard support', () => {
+        const props = {
+            max: 100,
+            min: 0,
+        };
+
+        const ariaValueNow = 'aria-valuenow';
+        const ariaValueMax = 'aria-valuemax';
+        const ariaValueMin = 'aria-valuemin';
+        const defaultSliderValue = 20;
+
+        function StubComponent({ value = defaultSliderValue }: { value?: number | number[] }) {
+            const [state, setState] = useState(value);
+
+            const onChangeCommittedHandle = (values) => {
+                setState(values);
+            };
+
+            return <Slider onChangeCommitted={onChangeCommittedHandle} value={state} {...props} />;
+        }
+
+        it('increase value by ArrowRight, ArrowUp', () => {
+            const countRepeat = 2;
+
+            mount(
+                <CypressTestDecorator>
+                    <StubComponent />
+                </CypressTestDecorator>,
+            );
+
+            cy.get(sliderThumbSelector)
+                .focus()
+                .should('have.attr', ariaValueNow, defaultSliderValue)
+                .type('{rightArrow}'.repeat(countRepeat))
+                .should('have.attr', ariaValueNow, defaultSliderValue + countRepeat)
+                .type('{upArrow}'.repeat(countRepeat))
+                .should('have.attr', ariaValueNow, defaultSliderValue + countRepeat * 2);
+        });
+
+        it('decrease value by ArrowLeft, ArrowDown', () => {
+            const countRepeat = 2;
+
+            mount(
+                <CypressTestDecorator>
+                    <StubComponent />
+                </CypressTestDecorator>,
+            );
+
+            cy.get(sliderThumbSelector)
+                .focus()
+                .should('have.attr', ariaValueNow, defaultSliderValue)
+                .type('{leftArrow}'.repeat(countRepeat))
+                .should('have.attr', ariaValueNow, defaultSliderValue - countRepeat)
+                .type('{downArrow}'.repeat(countRepeat))
+                .should('have.attr', ariaValueNow, defaultSliderValue - countRepeat * 2);
+        });
+
+        it('change value by Home, End', () => {
+            mount(
+                <CypressTestDecorator>
+                    <StubComponent />
+                </CypressTestDecorator>,
+            );
+
+            cy.get(sliderThumbSelector)
+                .focus()
+                .should('have.attr', ariaValueNow, defaultSliderValue)
+                .type('{end}')
+                .should('have.attr', ariaValueNow, props.max)
+                .type('{home}')
+                .should('have.attr', ariaValueNow, props.min);
+        });
+
+        it('processing multiple steps by PageUp, PageDown', () => {
+            mount(
+                <CypressTestDecorator>
+                    <StubComponent />
+                </CypressTestDecorator>,
+            );
+
+            const multipleSteps = props.max * 0.1;
+
+            cy.get(sliderThumbSelector)
+                .focus()
+                .should('have.attr', ariaValueNow, defaultSliderValue)
+                .type('{pageUp}')
+                .should('have.attr', ariaValueNow, defaultSliderValue + multipleSteps)
+                .type('{pageDown}')
+                .should('have.attr', ariaValueNow, defaultSliderValue);
+        });
+
+        it('Double slider', () => {
+            mount(
+                <CypressTestDecorator>
+                    <StubComponent value={[25, 75]} />
+                </CypressTestDecorator>,
+            );
+
+            cy.get(sliderThumbSelector)
+                .first()
+                .focus()
+                .should('have.attr', ariaValueNow, 25)
+                .should('have.attr', ariaValueMin, props.min)
+                .type('{end}')
+                .should('have.attr', ariaValueMax, 75)
+                .type('{home}')
+                .should('have.attr', ariaValueNow, props.min)
+                .type('{rightArrow}'.repeat(10));
+
+            cy.get(sliderThumbSelector)
+                .last()
+                .focus()
+                .type('{end}')
+                .should('have.attr', ariaValueMax, props.max)
+                .type('{home}')
+                .invoke('attr', ariaValueNow)
+                .then((state) => {
+                    // INFO: Проверяем, что максимальное значение первого слайдера ограничено текущем значением второго
+                    cy.get(sliderThumbSelector).first().should('have.attr', ariaValueMax, state);
+                });
+
+            // INFO: Проверяем, что минимальное значение второго слайдера ограничено текущем значением первого
+            cy.get(sliderThumbSelector)
+                .last()
+                .focus()
+                .invoke('attr', ariaValueMin)
+                .then((state) => {
+                    cy.get(sliderThumbSelector).first().should('have.attr', ariaValueNow, state);
+                });
+
+            cy.matchImageSnapshot();
+        });
     });
 });
