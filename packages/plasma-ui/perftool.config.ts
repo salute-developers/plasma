37431a5e2b548f3b701400c3e2648c6be3461ddc
Artem Khaydarov <akhdrv@gmail.com>
2022-12-27 16:30:44 +0300

    chore(plasma-ui): update perftool script, config

--- packages/plasma-ui/perftool.config.ts
+++ packages/plasma-ui/perftool.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from '@salutejs/perftool';
 
 const config: Config = {
-    jobs: 3,
-    retries: 10,
+    jobs: 2,
+    retries: 50,
     include: ['src/**/*.perftest.tsx'],
-    outputFilePath: 'perftest/plasma-ui/result.json',
+    outputFilePath: 'perftest/result.json',
+    displayIntermediateCalculations: false,
+    failOnSignificantChanges: false,
     modifyWebpackConfig: function (config) {
         const babelLoaderOpts = config.module.rules.find(
             (rule) => typeof rule === 'object' && rule.loader === 'babel-loader',
