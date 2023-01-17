c36b8eaebc29cd23829fb30c832f8500cdb154bc
Artem Khaydarov <akhdrv@gmail.com>
2023-01-18 01:09:14 +0300

    chore(plasma-ui): add ProductCard perftest

---
+++ packages/plasma-ui/src/components/ProductCard/ProductCard.perftest.tsx
@@ -0,0 +1,29 @@
+import React, { useMemo, useState } from 'react';
+
+import { ProductCard } from '.';
+
+export const Default = () => {
+    const [quantity, setQuantity] = useState(1);
+
+    const badge = useMemo(() => <div>−20%</div>, []);
+    const media = useMemo(() => <div style={{ width: '12.25rem', height: '12.25rem' }} />, []);
+
+    return (
+        <div
+            style={{
+                width: '12.25rem',
+            }}
+        >
+            <ProductCard
+                badge={badge}
+                media={media}
+                text="Беконайзер с сыром, зеленью, большой котлетой, яйцом и соусом"
+                price={80}
+                oldPrice={100}
+                quantityMax={10}
+                quantity={quantity}
+                onQuantityChange={setQuantity}
+            />
+        </div>
+    );
+};
