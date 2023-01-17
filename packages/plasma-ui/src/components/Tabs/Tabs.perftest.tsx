7229a8ef5ee9d9b23e6525f54db12a5578ee55d7
Artem Khaydarov <akhdrv@gmail.com>
2023-01-18 01:24:53 +0300

    chore(plasma-ui): add Stepper and Tabs perftests

---
+++ packages/plasma-ui/src/components/Tabs/Tabs.perftest.tsx
@@ -0,0 +1,87 @@
+import React, { useMemo } from 'react';
+import styled from 'styled-components';
+
+import { Tabs, TabItem } from '.';
+
+const StorybookTabsWrapper = styled.div`
+    margin-top: 0.5rem;
+`;
+
+const defaultProps = {
+    itemsNumber: 4,
+    size: 'l' as 'l',
+    view: 'secondary' as 'secondary',
+    stretch: true,
+    pilled: false,
+    scaleOnInteraction: true,
+    outlined: false,
+    disabled: false,
+    text: 'Label',
+};
+
+const icon = <div style={{ width: 10, height: 10 }} />;
+
+export const Default = () => {
+    const { itemsNumber, size, view, stretch, pilled, scaleOnInteraction, text, disabled, outlined } = defaultProps;
+
+    const id = 'tabs-example';
+    const [index, setIndex] = React.useState(0);
+
+    const items = useMemo(() => {
+        type elem = {
+            label: string;
+            onClick: () => void;
+            onFocus: () => void;
+            onBlur: () => void;
+        };
+
+        const res: Array<elem> = [];
+        for (let i = 0; i < itemsNumber; i++) {
+            res.push({
+                label: `${text} ${i} `,
+                onFocus: () => {},
+                onBlur: () => {},
+                onClick: () => {
+                    !disabled && setIndex(i);
+                },
+            });
+        }
+        return res;
+    }, [itemsNumber, text]);
+
+    const tabIndex = disabled ? -1 : 0;
+
+    return (
+        <StorybookTabsWrapper>
+            <Tabs
+                id={id}
+                size={size}
+                view={view}
+                stretch={stretch}
+                pilled={pilled}
+                scaleOnPress={scaleOnInteraction}
+                outlined={!disabled && outlined}
+                disabled={disabled}
+                index={index}
+                animated
+                forwardedAs="ul"
+            >
+                {items.map((elem, i) => (
+                    <TabItem
+                        key={`item:${i}`}
+                        forwardedAs="li"
+                        isActive={i === index}
+                        aria-controls={id}
+                        tabIndex={tabIndex}
+                        contentLeft={icon}
+                        onClick={elem.onClick}
+                        onFocus={elem.onFocus}
+                        onBlur={elem.onBlur}
+                    >
+                        {elem.label}
+                    </TabItem>
+                ))}
+            </Tabs>
+        </StorybookTabsWrapper>
+    );
+};
