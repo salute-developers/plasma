4f2ab03db89ac09970a29ddefbe8ca5c4d48d752
Neretin Artem <hawktrike@gmail.com>
2023-01-31 19:06:48 +0600

    feat: Add ignore component-test files for babel config

--- .babelrc.js
+++ .babelrc.js
@@ -14,6 +14,8 @@ const plugins = [
 ];
 const ignore = ['**/*.d.ts', '**/helpers', '**/__helpers', '**/*.stories.tsx'];
 
+!process.env.INCLUDE_TEST_FILES && ignore.push('**/*.component-test.tsx');
+
 process.env.COVERAGE && plugins.push(['babel-plugin-istanbul']);
 
 module.exports = {
