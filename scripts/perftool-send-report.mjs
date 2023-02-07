9e16856090bfbbab1869eb4c1373644d8a7de316
Ekaterina Anishkina <anishkina@gmail.com>
2023-02-07 20:20:25 +0300

    feat(perftool): try to fix

--- scripts/perftool-send-report.mjs
+++ scripts/perftool-send-report.mjs
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 
 /* eslint-disable no-console */
-
 import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
+import uuid from 'uuid';
 import fetch from 'node-fetch';
 import fsPromises from 'fs/promises';
-import { createCommand, createArgument } from 'commander';
 
 function getValue(metric) {
     switch (typeof metric) {
@@ -31,20 +29,19 @@ function getValue(metric) {
 
 const URL = 'https://metrics.prom.third-party-app.sberdevices.ru/create-canvas-app-metrics';
 
-const cli = createCommand('perftool-sendReport');
-
-cli.addArgument(createArgument('<commitSHA>', 'The commit SHA that triggered the workflow.'));
-
-async function start() {
+async function perftoolSendReport() {
     const message = 'Sending report';
     console.log(`${message}...`);
+
+    const commithash = process.env.GITHUB_SHA;
+    console.log(`commithash = ${commithash}...`);
 
     const data = await fsPromises
         .readFile(path.resolve('./perftest/comparison.json'), { encoding: 'utf-8' })
         .then(JSON.parse);
 
-    const sessionId = uuidv4();
-    const [commithash] = cli.processedArgs;
+    const sessionId = uuid.v4();
+
     const body = [];
     const subjectIdList = Object.keys(data);
     for (const subjectId of subjectIdList) {
@@ -62,7 +59,7 @@ async function start() {
                     key: metricId,
                     ...getValue(metric),
                     // TODO: fix any
-                    metadata: JSON.stringify(metric?.change || {}),
+                    metadata: JSON.stringify(metric.change || {}),
                 };
                 body.push(item);
             }
@@ -79,7 +76,7 @@ async function start() {
     }
 }
 
-await start()
+perftoolSendReport()
     .then(() => process.exit())
     .catch((err) => {
         console.error(err.name, err.message, err.stack);
