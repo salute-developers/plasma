25bbb0447985cfe0a7e112a57f0cf86aaa690a43
Ekaterina Anishkina <anishkina@gmail.com>
2023-02-07 20:20:25 +0300

    feat(perftool): try to fix

--- scripts/perftool-send-report.js
+++ scripts/perftool-send-report.js
@@ -1,85 +1,106 @@
 #!/usr/bin/env node
+/* eslint-disable no-continue */
 
-/* eslint-disable no-console */
+/* eslint-disable no-console,  @typescript-eslint/no-var-requires */
+const path = require('path');
+const uuid = require('uuid');
+const fetch = require('node-fetch');
+const fsPromises = require('fs/promises');
 
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-import fetch from 'node-fetch';
-import fsPromises from 'fs/promises';
-import { createCommand, createArgument } from 'commander';
+// function getValue(metric) {
+//     switch (typeof metric) {
+//         case 'boolean':
+//             return {
+//                 valueBool: metric,
+//             };
+//         case 'number':
+//             return {
+//                 valueNum: metric,
+//             };
+//         case 'string':
+//             return {
+//                 valueStr: metric,
+//             };
+//         default:
+//             return {
+//                 valueStr: JSON.stringify(metric),
+//             };
+//     }
+// }
 
-function getValue(metric) {
-    switch (typeof metric) {
-        case 'boolean':
-            return {
-                valueBool: metric,
-            };
-        case 'number':
-            return {
-                valueNum: metric,
-            };
-        case 'string':
-            return {
-                valueStr: metric,
-            };
-        default:
-            return {
-                valueStr: JSON.stringify(metric),
-            };
+const getMetadata = (data) => {
+    if (!data) {
+        return JSON.stringify({});
     }
-}
+
+    let result;
+
+    if (Array.isArray(data)) {
+        result = data.reduce((acc, v, idx) => {
+            acc[idx] = String(v);
+            return acc;
+        }, {});
+    } else {
+        for (const key of Object.keys(data)) {
+            result[key] = String(data[key]);
+        }
+    }
+
+    return JSON.stringify(result);
+};
 
 const URL = 'https://metrics.prom.third-party-app.sberdevices.ru/create-canvas-app-metrics';
 
-const cli = createCommand('perftool-sendReport');
+async function perftoolSendReport() {
+    console.log('Sending report...');
 
-cli.addArgument(createArgument('<commitSHA>', 'The commit SHA that triggered the workflow.'));
+    const commithash = process.env.GITHUB_SHA;
+    console.log(`commithash = ${commithash}`);
 
-async function start() {
-    const message = 'Sending report';
-    console.log(`${message}...`);
-
-    const data = await fsPromises
-        .readFile(path.resolve('./perftest/comparison.json'), { encoding: 'utf-8' })
+    const { staticTaskChange, result } = await fsPromises
+        .readFile(path.resolve('./packages/plasma-ui/perftest/comparison.json'), { encoding: 'utf-8' })
         .then(JSON.parse);
 
-    const sessionId = uuidv4();
-    const [commithash] = cli.processedArgs;
+    const data = {
+        staticTaskChange,
+        result,
+    };
+    console.log('data = ', JSON.stringify(data));
+
     const body = [];
-    const subjectIdList = Object.keys(data);
-    for (const subjectId of subjectIdList) {
-        const taskIdList = Object.keys(data[subjectId]);
-        for (const taskId of taskIdList) {
-            const task = data[subjectId][taskId];
-            const metricIdList = Object.keys(task);
-            for (const metricId of metricIdList) {
-                const metric = task[metricId];
-                const item = {
-                    hostname: `perftool_${commithash}`,
-                    sessionId,
-                    path: subjectId,
-                    ua: taskId,
-                    key: metricId,
-                    ...getValue(metric),
-                    // TODO: fix any
-                    metadata: JSON.stringify(metric?.change || {}),
-                };
-                body.push(item);
+    const sessionId = uuid.v4();
+    // e.g. componentId = src/components/Carousel/Carousel.perftest.tsx#CarouselLiteBasic
+    for (const componentId of Object.keys(data)) {
+        // e.g. taskId = rerender | render
+        for (const taskId of Object.keys(data[componentId])) {
+            for (const metricId of ['median', 'mean']) {
+                const metric = data[componentId][taskId][metricId];
+                for (const typeId of ['old', 'news', 'change']) {
+                    const item = {
+                        ua: 'perftool',
+                        hostname: commithash,
+                        sessionId,
+                        path: `${componentId}_${taskId}`,
+                        key: `${metricId}_${typeId}`,
+                        metadata: getMetadata(metric[typeId]),
+                    };
+                    body.push(item);
+                }
             }
         }
     }
-    console.log(message);
-
+    console.log('body = ', JSON.stringify(body));
+    const message = 'Sent report';
     const response = await fetch(URL, { body: JSON.stringify(body), method: 'POST' });
     if (!response.ok) {
         const errorInfo = await response.json();
         console.error(`${message} with error: ${errorInfo}`);
     } else {
-        console.info(`${message} with success status: ${response.status}`);
+        console.info(`${message} with success: ${response.status}`);
     }
 }
 
-await start()
+perftoolSendReport()
     .then(() => process.exit())
     .catch((err) => {
         console.error(err.name, err.message, err.stack);
