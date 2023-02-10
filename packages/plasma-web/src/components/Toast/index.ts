8cec2cf35a52a0c633f6b4cf311ba3248084064c
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-10 11:19:46 +0700

    feat(plasma-web): add onHide action in storybook

--- packages/plasma-web/src/components/Toast/index.ts
+++ packages/plasma-web/src/components/Toast/index.ts
@@ -1,2 +1,2 @@
 export { Toast, ToastProvider, useToast } from '@salutejs/plasma-core';
-export type { ToastProps } from '@salutejs/plasma-core';
+export type { ToastProps, ToastPosition } from '@salutejs/plasma-core';
