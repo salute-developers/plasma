489b220b3017ddcbec21dac04e96382c5307b197
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-14 17:34:21 +0700

    fix(plasma-b2c): use CSF for Grid, Link, Skeleton
    Component Story Format (CSF)

--- packages/plasma-b2c/src/components/Link/Link.stories.tsx
+++ packages/plasma-b2c/src/components/Link/Link.stories.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { InSpacingDecorator } from '@salutejs/plasma-sb-utils';
+import { Meta } from '@storybook/react';
 
 import { P1 } from '../Typography';
 
 import { Link } from '.';
+
+export default {
+    title: 'Content/Link',
+    decorators: [InSpacingDecorator],
+} as Meta;
 
 const props = { href: '/', target: '__blank' };
 
