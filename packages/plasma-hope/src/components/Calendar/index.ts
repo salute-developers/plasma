6a37ceec0bf9736d4711ec76a556f3120f4e8ab4
Vasiliy Loginevskiy <yeti.or@gmail.com>
2023-02-16 11:41:06 +0300

    fix(plasma-hope): export CalendarStateType

--- packages/plasma-hope/src/components/Calendar/index.ts
+++ packages/plasma-hope/src/components/Calendar/index.ts
@@ -1,7 +1,7 @@
-export { CalendarBase, CalendarStateType } from './CalendarBase';
+export { CalendarBase } from './CalendarBase';
 export { CalendarDouble } from './CalendarDouble';
 export { Calendar, CalendarBaseRange, CalendarDoubleRange } from './Calendar';
 
-export type { CalendarBaseProps } from './CalendarBase';
+export type { CalendarBaseProps, CalendarStateType } from './CalendarBase';
 export type { CalendarDoubleProps } from './CalendarDouble';
 export type { CalendarProps } from './Calendar';
