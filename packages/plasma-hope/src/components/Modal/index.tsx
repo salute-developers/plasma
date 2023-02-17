73d2988ab6bac2a6b236927f9424a03fa7dbbacd
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-17 12:36:35 +0700

    feat(plasma-hope): export ModalViewProps as part API component

--- packages/plasma-hope/src/components/Modal/index.tsx
+++ packages/plasma-hope/src/components/Modal/index.tsx
@@ -2,3 +2,4 @@ export { ModalsProvider } from './ModalsContext';
 export { Modal } from './Modal';
 export { ModalView } from './ModalView';
 export type { ModalProps } from './Modal';
+export type { ModalViewProps } from './ModalView';
