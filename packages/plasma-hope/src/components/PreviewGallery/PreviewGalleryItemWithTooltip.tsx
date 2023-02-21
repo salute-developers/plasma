fdbd9f452f4f06a232aab7f2acedce3c378b9b40
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-21 16:31:08 +0700

    feat(plasma-hope): add "PreviewGallery" component

---
+++ packages/plasma-hope/src/components/PreviewGallery/PreviewGalleryItemWithTooltip.tsx
@@ -0,0 +1,45 @@
+import React, { PropsWithChildren, useCallback, useState, FC } from 'react';
+import styled from 'styled-components';
+
+import { Tooltip } from '../Tooltip';
+
+import { TooltipItem } from './types';
+
+const StyledTooltip = styled(Tooltip)`
+    display: block;
+`;
+
+export interface PreviewGalleryItemWithTooltipProps {
+    tooltip?: TooltipItem;
+}
+
+export const PreviewGalleryItemWithTooltip: FC<PropsWithChildren<PreviewGalleryItemWithTooltipProps>> = ({
+    children,
+    tooltip,
+}) => {
+    const [visible, setVisible] = useState(false);
+
+    const onMouseOver = useCallback(() => {
+        setVisible(true);
+    }, []);
+
+    const onMouseOut = useCallback(() => {
+        setVisible(false);
+    }, []);
+
+    return tooltip ? (
+        <StyledTooltip
+            onDismiss={onMouseOut}
+            onMouseOver={onMouseOver}
+            onFocus={onMouseOver}
+            onMouseOut={onMouseOut}
+            onBlur={onMouseOut}
+            isVisible={visible}
+            {...tooltip}
+        >
+            {children}
+        </StyledTooltip>
+    ) : (
+        <>{children}</>
+    );
+};
