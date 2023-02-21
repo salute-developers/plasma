a8496d87f9279688e4da330e04bf4e38cd75947d
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-21 17:20:13 +0700

    feat(plasma-hope): add "Upload" component

---
+++ packages/plasma-hope/src/components/Upload/UploadMessage.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import { success, critical } from '@salutejs/plasma-core';
+
+import { Caption } from '../Typography';
+
+import { StatusType } from './types';
+
+const statuses = {
+    error: {
+        color: critical,
+    },
+    success: {
+        color: success,
+    },
+};
+
+// NOTE: don't know how to fix ts error TS2742
+type uploadMessage = FC<{ status?: StatusType }>;
+
+export const UploadMessage: uploadMessage = styled(Caption)<{ status?: StatusType }>`
+    margin-top: 0.25rem;
+    text-align: center;
+
+    ${({ status }) => status && statuses[status]}
+`;
