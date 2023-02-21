76623a9ed26197622028e3a2f057674d1c0a8446
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-21 17:45:27 +0700

    feat(plasma-web): re-export "Upload" component

--- packages/plasma-web/src/index.ts
+++ packages/plasma-web/src/index.ts
@@ -28,6 +28,7 @@ export * from './components/TextField';
 export * from './components/Toast';
 export * from './components/Tooltip';
 export * from './components/Typography';
+export * from './components/Upload';
 export * from './hocs';
 export * from './hooks';
 export * from './mixins';
