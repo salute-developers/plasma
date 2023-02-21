ead9c37478229e8af8535c1a69b9c463cb8aaf98
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-21 17:45:27 +0700

    feat(plasma-web): re-export "Upload" component

---
+++ packages/plasma-web/src/components/Upload/index.ts
@@ -0,0 +1,6 @@
+export { Upload } from '@salutejs/plasma-hope';
+export { UploadProgress } from '@salutejs/plasma-hope';
+export { defaultValidate } from '@salutejs/plasma-hope';
+export type { UploadProps } from '@salutejs/plasma-hope';
+export type { UploadProgressProps } from '@salutejs/plasma-hope';
+export type { ValidationResult } from '@salutejs/plasma-hope';
