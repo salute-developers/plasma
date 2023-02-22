be044038464706b5e1f008ff299cfbb5e79850bc
Alex Lobyntsev <yakutoc@gmail.com>
2023-02-22 16:48:51 +0700

    feat(plasma-web): re-export SliderThumb from plasma-hope

--- packages/plasma-web/src/components/Slider/Slider.tsx
+++ packages/plasma-web/src/components/Slider/Slider.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Slider as SliderCore, SliderProps } from '@salutejs/plasma-core';
-import {
-    accent as borderColor,
-    white as thumbBackgroundColor,
-    surfaceLiquid03 as color,
-    buttonAccent as fillColor,
-} from '@salutejs/plasma-tokens-web';
+import styled from 'styled-components';
+import { SliderCore, SliderProps, SliderSettings } from '@salutejs/plasma-core';
+import { SliderThumb } from '@salutejs/plasma-hope';
+import { accent, white, surfaceLiquid03, buttonAccent as fillColor } from '@salutejs/plasma-tokens-web';
 
-const sliderSettings = {
-    thumbBackgroundColor,
-    borderColor,
-    color,
+const sliderSettings: SliderSettings = {
+    backgroundColor: surfaceLiquid03,
     fillColor,
 };
 
+const StyledThumb = styled(SliderThumb)`
+    border-color: ${surfaceLiquid03};
+
+    background-color: ${white};
+
+    color: ${accent};
+`;
+
 export const Slider = (props: SliderProps) => {
-    return <SliderCore {...props} settings={sliderSettings} />;
+    return <SliderCore {...props} settings={sliderSettings} thumb={StyledThumb} />;
 };
