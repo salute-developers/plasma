75a592fdf7f6e2c44a1c3ace38ca795018bef6ec
Vasiliy Loginevskiy <yeti.or@gmail.com>
2023-03-03 12:52:53 +0300

    fix(plasma-core): fix IS_REACT_18

--- packages/plasma-core/src/utils/react.ts
+++ packages/plasma-core/src/utils/react.ts
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const { flushSync } = ReactDOM as any;
 
-export const IS_REACT_18 = typeof flushSync === 'function';
+export const IS_REACT_18 = ReactDOM.version.split('.')[0] === '18';
 
 export function safeFlushSync(fn: () => void): void {
     if (!IS_REACT_18) {
