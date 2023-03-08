68d8b56c60fc80bbac9b3f66ef2f08e112517d91
Alexander Lobyntsev <alexander.lobyntsev@virtualhealth.com>
2023-03-08 13:11:27 +0700

    feat(plasma-web): processing new typography in story

--- packages/plasma-web/src/components/Typography/Typography.stories.tsx
+++ packages/plasma-web/src/components/Typography/Typography.stories.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Story, Meta } from '@storybook/react';
+import { InSpacingDecorator, disableProps } from '@salutejs/plasma-sb-utils';
 import { secondary } from '@salutejs/plasma-core';
 
+import type { SpacingProps } from '../../mixins';
+
 import {
+    DsplL,
+    DsplM,
+    DsplS,
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    BodyL,
+    BodyM,
+    BodyS,
+    BodyXS,
+    BodyXXS,
+    TextL,
+    TextM,
+    TextS,
+    TextXS,
     Headline1,
     Headline2,
     Headline3,
@@ -19,6 +40,103 @@ import {
     Underline,
 } from '.';
 
+export default {
+    title: 'Content/Typography',
+    component: DsplL,
+    argTypes: {
+        ...disableProps(['ref', 'theme', 'as', 'forwardedAs']),
+    },
+    decorators: [InSpacingDecorator],
+} as Meta;
+
+export const Dspl: Story<SpacingProps> = (props) => (
+    <>
+        <DsplL bold={false} {...props}>
+            DsplL
+        </DsplL>
+        <DsplL {...props}>DsplL Bold</DsplL>
+        <DsplM bold={false} {...props}>
+            DsplM
+        </DsplM>
+        <DsplM {...props}>DsplM Bold</DsplM>
+        <DsplS bold={false} {...props}>
+            DsplS
+        </DsplS>
+        <DsplS {...props}>DsplS Bold</DsplS>
+    </>
+);
+
+export const H: Story<SpacingProps> = (props) => (
+    <>
+        <H1 bold={false} {...props}>
+            H1
+        </H1>
+        <H1 {...props}>H1 Bold</H1>
+        <H2 bold={false} {...props}>
+            H2
+        </H2>
+        <H2 {...props}>H2 Bold</H2>
+        <H3 bold={false} {...props}>
+            H3
+        </H3>
+        <H3 {...props}>H3 Bold</H3>
+        <H4 bold={false} {...props}>
+            H4
+        </H4>
+        <H4 {...props}>H4 Bold</H4>
+        <H5 bold={false} {...props}>
+            H5
+        </H5>
+        <H5 {...props}>H5 Bold</H5>
+    </>
+);
+
+export const Body: Story<SpacingProps> = (props) => (
+    <>
+        <BodyL {...props}>BodyL</BodyL>
+        <BodyL bold {...props}>
+            BodyL Bold
+        </BodyL>
+        <BodyM {...props}>BodyM</BodyM>
+        <BodyM bold {...props}>
+            BodyM Bold
+        </BodyM>
+        <BodyS {...props}>BodyS</BodyS>
+        <BodyS bold {...props}>
+            BodyS Bold
+        </BodyS>
+        <BodyXS {...props}>BodyXS</BodyXS>
+        <BodyXS bold {...props}>
+            BodyXS Bold
+        </BodyXS>
+        <BodyXXS {...props}>BodyXXS</BodyXXS>
+        <BodyXXS bold {...props}>
+            BodyXXS Bold
+        </BodyXXS>
+    </>
+);
+
+export const Text: Story<SpacingProps> = (props) => (
+    <>
+        <TextL {...props}>TextL</TextL>
+        <TextL bold {...props}>
+            TextL Bold
+        </TextL>
+        <TextM {...props}>TextM</TextM>
+        <TextM bold {...props}>
+            TextM Bold
+        </TextM>
+        <TextS {...props}>TextS</TextS>
+        <TextS bold {...props}>
+            TextS Bold
+        </TextS>
+        <TextXS {...props}>TextXS</TextXS>
+        <TextXS bold {...props}>
+            TextXS Bold
+        </TextXS>
+    </>
+);
+
 const Section = styled.section`
     margin-bottom: 2.625rem;
 
@@ -30,12 +148,13 @@ const Section = styled.section`
         margin-bottom: 0;
     }
 `;
+
 const Note = styled(Footnote1)`
     font-weight: normal;
     color: ${secondary};
 `;
 
-export const Default = () => (
+export const Old = () => (
     <>
         <Section>
             <Headline1>Headline 1</Headline1>
