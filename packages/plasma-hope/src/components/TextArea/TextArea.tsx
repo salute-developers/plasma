fdfc2a9d4c336edbd992efe351e6653131329ebc
Neretin Artem <hawktrike@gmail.com>
2023-03-13 12:35:17 +0600

    feat(plasma-hope): Add TextArea union from plasma-web and plasma-b2c

--- packages/plasma-hope/src/components/TextArea/TextArea.tsx
+++ packages/plasma-hope/src/components/TextArea/TextArea.tsx
@@ -1,120 +1,39 @@
-import React, { forwardRef, useState, useMemo, createRef } from 'react';
-import styled from 'styled-components';
-import {
-    FieldRoot,
-    FieldContent,
-    FieldHelper,
-    TextArea as BaseArea,
-    TextFieldHelper,
-    useResizeObserver,
-} from '@salutejs/plasma-core';
-import type { TextAreaProps as BaseProps } from '@salutejs/plasma-core';
+import React, { createRef, forwardRef, useMemo, useState } from 'react';
 
-import { applyInputStyles } from '../Field';
+import { useResizeObserver } from '../../hooks';
 
-export interface TextAreaProps extends BaseProps {
-    /**
-     * @deprecated не используется в компоненте.
-     */
-    helperBlock?: React.ReactElement;
-    /**
-     * @deprecated свойство устарело, необходимо использовать `leftHelper`.
-     */
-    helperText?: string;
-    /**
-     * Вспомогательный текст снизу слева для поля ввода.
-     */
-    leftHelper?: string;
-    /**
-     * Вспомогательный текст снизу справа для поля ввода.
-     */
-    rightHelper?: string;
-}
+import { TextAreaWeb } from './views/web/TextAreaWeb';
+import { TextAreaB2C } from './views/b2c/TextAreaB2C';
+import type { TextAreaProps as TextAreaPropsBase } from './types';
 
-const StyledTextArea = styled(BaseArea)`
-    -webkit-appearance: none; /* для отображение рамок на iOS */
+const componentMap = {
+    web: TextAreaWeb,
+    b2c: TextAreaB2C,
+};
 
-    ${applyInputStyles}
-`;
+const ROOT_FONT_SIZE = 16;
 
-const StyledFieldHelpers = styled.div<{ width: number }>`
-    display: flex;
-    justify-content: space-between;
+export type TextAreaProps = TextAreaPropsBase & {
+    design: 'b2c' | 'web';
+};
 
-    width: ${({ width }) => width}px;
-`;
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>((props, innerRef) => {
+    const { rightHelper, leftHelper, label, placeholder, design } = props;
 
-const StyledFieldContentWrapper = styled.div<{ width: number }>`
-    position: absolute;
-
-    width: ${({ width }) => width}px;
-`;
-
-const StyledFieldRightHelper = styled(FieldHelper)`
-    margin-left: auto;
-`;
-
-/**
- * Поле ввода многострочного текста.
- */
-// eslint-disable-next-line prefer-arrow-callback
-export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(function TextArea(
-    {
-        id,
-        disabled,
-        status,
-        label,
-        placeholder,
-        contentRight,
-        helperText,
-        style,
-        className,
-        leftHelper,
-        rightHelper,
-        ...rest
-    },
-    outerRef,
-) {
     const [width, setWidth] = useState(0);
-    const ref = useMemo(() => (outerRef && 'current' in outerRef ? outerRef : createRef<HTMLTextAreaElement>()), [
-        outerRef,
-    ]);
 
-    useResizeObserver(ref, (currentElement) => {
-        const { width: elementWidth } = currentElement.getBoundingClientRect();
-
-        setWidth(elementWidth);
-    });
-
+    const outerRef = innerRef && 'current' in innerRef ? innerRef : createRef<HTMLTextAreaElement>();
+    const hasHelper = Boolean(leftHelper || rightHelper);
     const placeLabel = (label || placeholder) as string | undefined;
 
-    return (
-        <FieldRoot
-            $disabled={disabled}
-            $isContentRight={Boolean(contentRight)}
-            $isHelper={Boolean(leftHelper || helperText)}
-            status={status}
-            style={style}
-            className={className}
-        >
-            <StyledFieldContentWrapper width={width}>
-                {contentRight && <FieldContent pos="right">{contentRight}</FieldContent>}
-            </StyledFieldContentWrapper>
-            <StyledTextArea
-                ref={ref}
-                id={id}
-                placeholder={placeLabel}
-                disabled={disabled}
-                status={status}
-                aria-describedby={id ? `${id}-helper` : undefined}
-                {...rest}
-            />
-            <StyledFieldHelpers width={width} id={id ? `${id}-helper` : undefined}>
-                {(leftHelper || helperText) && (
-                    <FieldHelper as={TextFieldHelper}>{leftHelper || helperText}</FieldHelper>
-                )}
-                {rightHelper && <StyledFieldRightHelper>{rightHelper}</StyledFieldRightHelper>}
-            </StyledFieldHelpers>
-        </FieldRoot>
-    );
+    useResizeObserver(outerRef, (currentElement) => {
+        const { width: elementWidth } = currentElement.getBoundingClientRect();
+        setWidth(elementWidth / ROOT_FONT_SIZE);
+    });
+
+    const commonProps = { outerRef, hasHelper, placeLabel, width };
+
+    const TextAreaView = useMemo(() => componentMap[design], [design]);
+
+    return <TextAreaView {...commonProps} {...props} />;
 });
---
+++ packages/plasma-hope/src/components/TextArea/views/web/TextAreaWeb.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FieldRoot, FieldContent, FieldHelper, TextArea as BaseArea } from '@salutejs/plasma-core';
+
+import { applyInputStyles } from '../../../Field';
+import type { TextAreaPropsCommon } from '../../types';
+
+const StyledTextArea = styled(BaseArea)`
+    -webkit-appearance: none; /* для отображение рамок на iOS */
+
+    ${applyInputStyles}
+`;
+
+const StyledFieldHelpers = styled.div<{ width: number }>`
+    display: flex;
+    justify-content: space-between;
+
+    width: ${({ width }) => width}rem;
+`;
+
+const StyledFieldContentWrapper = styled.div<{ width: number }>`
+    position: absolute;
+
+    width: ${({ width }) => width}rem;
+`;
+
+const StyledFieldRightHelper = styled(FieldHelper)`
+    margin-left: auto;
+`;
+
+/**
+ * Поле ввода многострочного текста.
+ */
+// eslint-disable-next-line prefer-arrow-callback
+export const TextAreaWeb = ({
+    id,
+    disabled,
+    status,
+    contentRight,
+    helperText,
+    style,
+    className,
+    leftHelper,
+    rightHelper,
+    hasHelper,
+    placeLabel,
+    outerRef,
+    width,
+    ...rest
+}: TextAreaPropsCommon) => {
+    return (
+        <FieldRoot
+            $disabled={disabled}
+            $isHelper={hasHelper || Boolean(helperText)}
+            status={status}
+            style={style}
+            className={className}
+        >
+            {contentRight && (
+                <StyledFieldContentWrapper width={width}>
+                    <FieldContent pos="right">{contentRight}</FieldContent>
+                </StyledFieldContentWrapper>
+            )}
+            <StyledTextArea
+                ref={outerRef}
+                id={id}
+                placeholder={placeLabel}
+                disabled={disabled}
+                status={status}
+                aria-describedby={id ? `${id}-helper` : undefined}
+                {...rest}
+            />
+            {(hasHelper || helperText) && (
+                <StyledFieldHelpers width={width} id={id ? `${id}-helper` : undefined}>
+                    {(leftHelper || helperText) && <FieldHelper>{leftHelper || helperText}</FieldHelper>}
+                    {rightHelper && <StyledFieldRightHelper>{rightHelper}</StyledFieldRightHelper>}
+                </StyledFieldHelpers>
+            )}
+        </FieldRoot>
+    );
+};
