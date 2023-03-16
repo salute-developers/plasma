8c265aecc9d024c8719e7fd050a8ad2704e468d1
Artem Khaydarov <akhdrv@gmail.com>
2023-03-16 15:23:50 +0300

    ci: perftool for whole repo, fail on bad changes

--- perftool.config.ts
+++ perftool.config.ts
@@ -16,25 +16,27 @@ const config: Config = {
             failOnSignificantChanges: false,
         },
     },
-    include: ['src/**/*.perftest.tsx'],
+    include: ['packages/**/*.perftest.tsx'],
+    exclude: ['**/node_modules/**'],
     outputFilePath: 'perftest/result.json',
     displayIntermediateCalculations: false,
-    failOnSignificantChanges: false,
+    failOnSignificantChanges: true,
     stabilizers: ['staticTask'],
     absoluteError: 1,
-    modifyWebpackConfig: function (config) {
+    modifyWebpackConfig(config) {
         const babelLoaderOpts = config.module?.rules?.find(
             (rule) => typeof rule === 'object' && rule.loader === 'babel-loader',
         );
 
-        if (typeof babelLoaderOpts == 'object' && typeof babelLoaderOpts?.options === 'object') {
+        if (typeof babelLoaderOpts === 'object' && typeof babelLoaderOpts?.options === 'object') {
             babelLoaderOpts?.options?.plugins?.push('babel-plugin-styled-components');
+            babelLoaderOpts?.options?.plugins?.push(['const-enum', { transform: 'constObject' }]);
         }
 
         (config.resolve = config.resolve || {}).alias = {
-            react: '/node_modules/react',
-            'react-dom': '/node_modules/react-dom',
-            'styled-components': '/node_modules/styled-components',
+            react: '/packages/plasma-ui/node_modules/react',
+            'react-dom': '/packages/plasma-ui/node_modules/react-dom',
+            'styled-components': '/packages/plasma-ui/node_modules/styled-components',
         };
 
         return config;
