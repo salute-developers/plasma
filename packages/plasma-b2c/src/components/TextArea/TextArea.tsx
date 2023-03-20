75a7623fbe51f663de1cef43a91d7a42bb43168f
Neretin Artem <hawktrike@gmail.com>
2023-03-20 14:45:58 +0600

    feat(plasma-b2c): Add import TextField from plasma-hope

--- packages/plasma-b2c/src/components/TextArea/TextArea.tsx
+++ packages/plasma-b2c/src/components/TextArea/TextArea.tsx
@@ -2,6 +2,9 @@ import React, { forwardRef } from 'react';
 import { TextArea as TextAreaHope } from '@salutejs/plasma-hope';
 import type { TextAreaProps } from '@salutejs/plasma-hope';
 
+/**
+ * Поле ввода многострочного текста.
+ */
 export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>((props, ref) => (
     <TextAreaHope design="b2c" {...props} ref={ref} />
 ));
