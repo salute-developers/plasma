1d301d1d605fd752a00b892ec990f1c03cc3174c
Neretin Artem <hawktrike@gmail.com>
2023-03-20 14:44:51 +0600

    feat(plasma-web): Add import TextField from plasma-hope

--- packages/plasma-web/src/components/TextField/TextField.tsx
+++ packages/plasma-web/src/components/TextField/TextField.tsx
@@ -1,66 +1,10 @@
-import React, { forwardRef, KeyboardEvent } from 'react';
-
-import { Field } from '../Field';
-import type { FieldProps } from '../Field';
-import { Input } from '../Input';
-import type { InputProps } from '../Input';
-
-export interface TextFieldProps
-    extends Pick<FieldProps, 'contentLeft' | 'contentRight' | 'helperText'>,
-        Omit<InputProps, 'hasContentLeft' | 'hasContentRight'> {
-    /**
-     * Callback по нажатию Enter
-     */
-    onSearch?: (value: string, event?: KeyboardEvent<HTMLInputElement>) => void;
-}
+import React, { forwardRef } from 'react';
+import { TextField as TextFieldHope } from '@salutejs/plasma-hope';
+import type { TextFieldProps } from '@salutejs/plasma-hope';
 
 /**
  * Поле ввода текста.
  */
-// eslint-disable-next-line prefer-arrow-callback
-export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(function TextField(
-    {
-        id,
-        size,
-        disabled,
-        status,
-        label,
-        animatedHint,
-        contentLeft,
-        contentRight,
-        helperText,
-        className,
-        style,
-        ...rest
-    },
-    ref,
-) {
-    return (
-        <Field
-            id={id}
-            disabled={disabled}
-            label={animatedHint !== 'label' ? label : undefined}
-            helperText={helperText}
-            contentLeft={contentLeft}
-            contentRight={contentRight}
-            status={status}
-            className={className}
-            style={style}
-        >
-            <Input
-                {...rest}
-                ref={ref}
-                id={id}
-                disabled={disabled}
-                size={size}
-                status={status}
-                label={label}
-                animatedHint={animatedHint}
-                hasContentLeft={Boolean(contentLeft)}
-                hasContentRight={Boolean(contentRight)}
-                aria-labelledby={id ? `${id}-label` : undefined}
-                aria-describedby={id ? `${id}-helpertext` : undefined}
-            />
-        </Field>
-    );
-});
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => (
+    <TextFieldHope design="web" {...props} ref={ref} />
+));
