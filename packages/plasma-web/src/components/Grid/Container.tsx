5d38b1638ac2e36f1f8883683e9b64ff3e5bbf69
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-22 19:11:26 +0700

    feat(plasma-web): re-export "Grid" component from plasma-hope

---
+++ packages/plasma-web/src/components/Grid/Container.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Container as ContainerHope } from '@salutejs/plasma-hope';
+
+/**
+ * Блок с полями по бокам для размещения контента по вертикали.
+ * Блок нельзя вкладывать сам в себя или дальше по дереву.
+ */
+export const Container = styled(ContainerHope).attrs({ design: 'web' })``;
