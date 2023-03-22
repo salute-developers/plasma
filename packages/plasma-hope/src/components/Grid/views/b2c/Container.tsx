f85819bc17bd00ee924df18bb167f1dbcbf99109
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-22 19:10:52 +0700

    feat(plasma-hope): add "Grid" component

---
+++ packages/plasma-hope/src/components/Grid/views/b2c/Container.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import { Container as BaseContainer } from '../web/Container';
+
+export const Container = styled(BaseContainer)`
+    max-width: 125rem;
+`;
---
+++ packages/plasma-hope/src/components/Grid/views/web/Container.tsx
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components';
+
+import { gridGutters, gridMargins, gridSizes, mediaQuery } from '../../../../utils';
+
+const deviceScale = 1;
+const sidesCount = 2;
+
+export const Container = styled.div`
+    margin: 0 auto;
+
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
+
+    width: 100%;
+    padding-left: var(--plasma-grid-margin);
+    padding-right: var(--plasma-grid-margin);
+
+    max-width: 90rem;
+
+    ${() =>
+        gridSizes.map((breakpoint) =>
+            mediaQuery(
+                breakpoint,
+                deviceScale,
+            )(css`
+                --plasma-grid-margin: ${gridMargins[breakpoint]}rem;
+                --plasma-grid-gutter: ${gridGutters[breakpoint] / sidesCount}rem;
+            `),
+        )}
+`;
