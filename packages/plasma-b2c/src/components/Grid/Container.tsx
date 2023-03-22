e05cfe67c2d8303767ec8fddebb96b609243b3a8
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-22 19:11:43 +0700

    feat(plasma-b2c): re-export "Grid" component from plasma-hope

--- packages/plasma-b2c/src/components/Grid/Container.tsx
+++ packages/plasma-b2c/src/components/Grid/Container.tsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
-import { Container as BaseContainer } from '@salutejs/plasma-hope';
+import { Container as ContainerHope } from '@salutejs/plasma-hope';
 
 /**
  * Блок с полями по бокам для размещения контента по вертикали.
  * Блок нельзя вкладывать сам в себя или дальше по дереву.
  */
-export const Container = styled(BaseContainer)`
-    max-width: 125rem;
-`;
+export const Container = styled(ContainerHope).attrs({ design: 'b2c' })``;
