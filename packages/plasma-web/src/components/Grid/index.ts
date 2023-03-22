5ef789cd719be10c01530c12ba77326fa1e9bf01
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-22 19:11:26 +0700

    feat(plasma-web): re-export "Grid" component from plasma-hope

--- packages/plasma-web/src/components/Grid/index.ts
+++ packages/plasma-web/src/components/Grid/index.ts
@@ -1,4 +1,4 @@
 export { Row, Col, sizes, offsets } from '@salutejs/plasma-hope';
 export type { ColProps, ColCount, ColSizeProps, ColOffsetProps } from '@salutejs/plasma-hope';
 
-export { Container } from '@salutejs/plasma-hope';
+export { Container } from './Container';
