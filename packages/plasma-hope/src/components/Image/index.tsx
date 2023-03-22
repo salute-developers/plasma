6ce8ab54bd0da852ddeb4b3392390d4ea10996f7
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-22 16:54:07 +0700

    feat(plasma-hope): add "Image" component

--- packages/plasma-hope/src/components/Image/index.tsx
+++ packages/plasma-hope/src/components/Image/index.tsx
@@ -1,4 +1,4 @@
 export type { ImageBaseProps, Ratio } from '@salutejs/plasma-core';
+export type { ImageProps } from './types';
 
 export { Image } from './Image';
-export type { ImageProps } from './Image';
