4801671c75cd9c6817d14e7d29ddd1b35c1e44f1
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-22 16:57:23 +0700

    feat(plasma-b2c): re-export "Image" component from plasma-hope

--- packages/plasma-b2c/src/components/Image/index.tsx
+++ packages/plasma-b2c/src/components/Image/index.tsx
@@ -1,2 +1,2 @@
-export { Image } from '@salutejs/plasma-core';
-export type { ImageProps, ImageBaseProps, Ratio } from '@salutejs/plasma-core';
+export { Image } from '@salutejs/plasma-hope';
+export type { ImageProps, ImageBaseProps, Ratio } from '@salutejs/plasma-hope';
