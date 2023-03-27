0cf04abe453c0762409c9fe16dd0e55b1728025a
Neretin Artem <hawktrike@gmail.com>
2023-03-27 23:21:36 +0600

    fix(plasmas-hope): Fix `Container` component for rest props in Grid

--- packages/plasma-hope/src/components/Grid/Container.tsx
+++ packages/plasma-hope/src/components/Grid/Container.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, FC, PropsWithChildren } from 'react';
+import React, { useMemo, PropsWithChildren, forwardRef, HTMLAttributes } from 'react';
 
 import { Container as ContainerWeb } from './views/web/Container';
 import { Container as ContainerB2C } from './views/b2c/Container';
@@ -8,13 +8,22 @@ const componentMap = {
     b2c: ContainerB2C,
 };
 
+export type ContainerProps = HTMLAttributes<HTMLDivElement> & {
+    design: 'b2c' | 'web';
+};
+
 /**
  * Блок с полями по бокам для размещения контента по вертикали.
  * Блок нельзя вкладывать сам в себя или дальше по дереву.
  */
-export const Container: FC<PropsWithChildren<{ design: 'b2c' | 'web' }>> = (props): JSX.Element => {
-    const { design, children } = props;
-    const ContainerView = useMemo(() => componentMap[design], [design]);
+export const Container = forwardRef<HTMLDivElement, PropsWithChildren<ContainerProps>>(
+    ({ design, children, ...rest }, ref) => {
+        const ContainerView = useMemo(() => componentMap[design], [design]);
 
-    return <ContainerView> {children} </ContainerView>;
-};
+        return (
+            <ContainerView {...rest} ref={ref}>
+                {children}
+            </ContainerView>
+        );
+    },
+);
