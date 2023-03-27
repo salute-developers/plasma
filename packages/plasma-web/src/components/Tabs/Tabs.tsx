117a9950b2d7adde967e5d9fc828664005efe631
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-27 12:23:24 +0700

    feat(plasma-web): re-export "Tabs" component from plasma-hope

--- packages/plasma-web/src/components/Tabs/Tabs.tsx
+++ packages/plasma-web/src/components/Tabs/Tabs.tsx
@@ -1,15 +1,8 @@
-import styled from 'styled-components';
-import { Tabs as BaseTabs } from '@salutejs/plasma-core';
-import type { TabsProps as BaseTabsProps } from '@salutejs/plasma-core';
+import { Tabs as TabsHope } from '@salutejs/plasma-hope';
 
-export interface TabsProps extends BaseTabsProps {}
+const TabsBase = TabsHope({ design: 'web' });
 
 /**
  * Контейнер вкладок, основной компонент для пользовательской сборки вкладок.
  */
-export const Tabs = styled(BaseTabs)<TabsProps>`
-    --tabs-margin: 1rem;
-    --tab-focus-border-size: 0rem;
-
-    gap: 0;
-`;
+export const Tabs = TabsBase;
--- packages/plasma-web/src/components/Tabs/index.ts
+++ packages/plasma-web/src/components/Tabs/index.ts
@@ -1,8 +1,7 @@
 export { TabsController } from './TabsController';
-export type { TabsControllerProps } from './TabsController';
+export type { TabsControllerProps } from '@salutejs/plasma-hope';
+export type { TabsProps } from '@salutejs/plasma-hope';
+export type { TabItemProps } from '@salutejs/plasma-hope';
 
 export { Tabs } from './Tabs';
-export type { TabsProps } from './Tabs';
-
 export { TabItem } from './TabItem';
-export type { TabItemProps } from './TabItem';
