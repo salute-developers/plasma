b756034d202ec4f6d26240010816b02a80cf0a75
Neretin Artem <hawktrike@gmail.com>
2023-03-28 22:53:54 +0600

    fix(plasma-website): Fix by design review

--- website/plasma-website/components/roster/Main.tsx
+++ website/plasma-website/components/roster/Main.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Container } from '@salutejs/plasma-b2c';
 
-export const Main = styled(Container).attrs(() => ({ as: 'main' }))`
+export const Main = styled(Container).attrs(() => ({ forwardedAs: 'main' }))`
     padding-top: 3.75rem;
     padding-bottom: 3.75rem;
 `;
