f4875f9e7d02002f5a475419e07a1bd982723bb6
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-29 16:17:55 +0700

    feat(plasma-web): re-export "Select" component

--- packages/plasma-web/src/components/Select/Select.tsx
+++ packages/plasma-web/src/components/Select/Select.tsx
@@ -1,27 +1,13 @@
-import React, { forwardRef, ReactElement, RefAttributes } from 'react';
-import styled from 'styled-components';
-import { applyDropdownListCssProperties } from '@salutejs/plasma-hope';
+import React, { forwardRef } from 'react';
+import { Select as SelectHope } from '@salutejs/plasma-hope';
 import type { SelectProps, SelectRefElement } from '@salutejs/plasma-hope';
 
-import { SelectView } from './SelectView';
-import { withSingleSelect } from './withSingleSelect';
-import { withMultiSelect } from './withMultiSelect';
-
-const StyledSelectView = styled(SelectView)`
-    ${applyDropdownListCssProperties};
-`;
-
-const SingleSelect = withSingleSelect(StyledSelectView);
-const MultiSelect = withMultiSelect(StyledSelectView);
+const SelectBase = SelectHope({ design: 'web' });
 
 /**
  * Выпадающий список для использования в формах.
  * Поддерживает выбор одного или нескольких значений.
  */
-// eslint-disable-next-line prefer-arrow-callback
-export const Select = forwardRef<SelectRefElement, SelectProps>(function Select(props, ref) {
-    if (props.multiselect) {
-        return <MultiSelect ref={ref} {...props} />;
-    }
-    return <SingleSelect ref={ref} {...props} />;
-}) as <T>(props: SelectProps<T> & RefAttributes<SelectRefElement>) => ReactElement;
+export const Select = forwardRef<SelectRefElement, SelectProps>((props, ref) => {
+    return <SelectBase {...props} ref={ref} />;
+});
