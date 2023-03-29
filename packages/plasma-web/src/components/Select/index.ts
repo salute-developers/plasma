1dd1b073f1d51c96854f7fc6c9235fcbc28cd344
Alex Lobyntsev <yakutoc@gmail.com>
2023-03-29 16:17:55 +0700

    feat(plasma-web): re-export "Select" component

--- packages/plasma-web/src/components/Select/index.ts
+++ packages/plasma-web/src/components/Select/index.ts
@@ -1,17 +1,2 @@
 export { Select } from './Select';
-export type { SelectProps, SelectRefElement } from '@salutejs/plasma-hope';
-
-export { SelectButton } from './SelectButton';
-export type { SelectButtonProps } from './SelectButton';
-
-export { SelectDropdown } from './SelectDropdown';
-export type { SelectDropdownProps } from './SelectDropdown';
-
-export { SelectView } from './SelectView';
-export type { SelectViewProps } from './SelectView';
-
-export { withMultiSelect } from './withMultiSelect';
-export type { MultiSelectProps } from './withMultiSelect';
-
-export { withSingleSelect } from './withSingleSelect';
-export type { SingleSelectProps } from './withSingleSelect';
+export type { SelectProps } from '@salutejs/plasma-hope';
