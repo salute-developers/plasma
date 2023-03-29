22837b8fdabfe3ecceb65012dc4a0292889f2c3b
Neretin Artem <hawktrike@gmail.com>
2023-03-29 12:54:25 +0600

    feat(plasma-b2c): Add import Button from plasma-hope

--- packages/plasma-b2c/src/components/Button/Button.tsx
+++ packages/plasma-b2c/src/components/Button/Button.tsx
@@ -1,40 +1,11 @@
-import styled from 'styled-components';
-import { createButton, ButtonRoot, getButtonSizesMixin, buttonSizes } from '@salutejs/plasma-core';
-import type {
-    ButtonProps as BaseProps,
-    ButtonContentProps,
-    ButtonSizeProps,
-    ButtonViewProps,
-} from '@salutejs/plasma-core';
-import { buttonViews, ButtonView } from '@salutejs/plasma-hope';
-import { bodySBold, bodyLBold, bodyMBold } from '@salutejs/plasma-typo';
-
-export type ButtonProps = BaseProps &
-    ButtonContentProps &
-    Partial<ButtonSizeProps> &
-    Partial<ButtonViewProps<ButtonView>>;
-
-const buttonTypography = {
-    l: bodyLBold,
-    m: bodyMBold,
-    s: bodySBold,
-};
-
-const applySizes = getButtonSizesMixin(buttonSizes, buttonTypography);
-
-const StyledButtonRoot = styled(ButtonRoot)<Partial<ButtonSizeProps> & Partial<ButtonViewProps<ButtonView>>>`
-    ${applySizes}
-    ${({ view }) => buttonViews[view]}
-`;
+import React, { forwardRef } from 'react';
+import { Button as ButtonHope } from '@salutejs/plasma-hope';
+import type { ButtonProps } from '@salutejs/plasma-hope';
 
 /**
  * Кнопка.
  * Поддерживает несколько режимов отображения (`view`) и размеров (`size`).
  */
-export const Button = createButton<HTMLButtonElement, ButtonProps>(StyledButtonRoot);
-
-Button.defaultProps = {
-    size: 'm',
-    view: 'secondary',
-    pin: 'square-square',
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => (
+    <ButtonHope design="b2c" {...props} ref={ref} />
+));
