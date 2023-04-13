b15ebb204c048ff3a4f0c1147b66c4f6ecf8ac3a
Neretin Artem <neretinartem@Neretins-MBP.netis>
2023-04-13 19:35:11 +0600

    feat(plasma-ui): Add swipe for whole sheet

--- packages/plasma-ui/src/components/Sheet/useSheetSwipe.ts
+++ packages/plasma-ui/src/components/Sheet/useSheetSwipe.ts
@@ -1,56 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const SWIPE_THRESHOLD = 0.2;
 
 export const useSheetSwipe = (args: {
     contentWrapperRef: React.RefObject<HTMLDivElement>;
+    contentRef: React.RefObject<HTMLDivElement>;
     handleRef: React.RefObject<HTMLDivElement>;
     onClose: () => void;
 }) => {
-    const { contentWrapperRef, handleRef, onClose } = args;
+    const { contentWrapperRef, contentRef, handleRef, onClose } = args;
+    const [isTopScroll, setIsTopScroll] = useState(true);
 
-    React.useEffect(() => {
-        if (!handleRef.current || !contentWrapperRef.current) {
+    useEffect(() => {
+        const contentWrapperEl = contentWrapperRef.current;
+        const handleEl = handleRef.current;
+        const contentEl = contentRef.current;
+
+        const hasScroll = contentEl && contentEl.scrollHeight > contentEl.clientHeight;
+        const triggerElement = !isTopScroll && hasScroll ? handleEl : contentWrapperEl;
+
+        if (!triggerElement || !contentEl || !contentWrapperEl) {
             return;
         }
-        const handleEl = handleRef.current;
-        const contentWrapperEl = contentWrapperRef.current;
 
-        const curtainHeight = contentWrapperEl.offsetHeight;
+        let startY = 0;
+        let currentY = 0;
 
-        let startY: number;
-        let currentY: number;
+        contentWrapperEl.style.willChange = 'transform';
 
         const onTouchStart = (event: TouchEvent) => {
             startY = event.changedTouches[0].clientY;
             currentY = startY;
+
             contentWrapperEl.style.transition = 'none';
         };
 
         const onTouchMove = (event: TouchEvent) => {
             const { clientY } = event.changedTouches[0];
             currentY = Math.max(startY, clientY);
-            contentWrapperEl.style.transform = `translateY(${currentY - startY}px)`;
+            const offsetY = currentY - startY;
+
+            contentWrapperEl.style.transform = `translateY(${offsetY}px)`;
         };
 
         const onTouchEnd = (event: TouchEvent) => {
+            const curtainHeight = contentWrapperEl.offsetHeight;
             const endY = event.changedTouches[0].clientY;
             const offsetY = endY - startY;
 
             contentWrapperEl.style.transform = '';
             contentWrapperEl.style.transition = '';
 
-            if (offsetY / curtainHeight > 0.2) {
+            if (offsetY / curtainHeight > SWIPE_THRESHOLD) {
                 onClose();
             }
         };
 
-        handleEl.addEventListener('touchstart', onTouchStart);
-        handleEl.addEventListener('touchmove', onTouchMove);
-        handleEl.addEventListener('touchend', onTouchEnd);
+        const onScroll = () => {
+            setIsTopScroll(contentEl.scrollTop <= 0);
+        };
+
+        triggerElement.addEventListener('touchstart', onTouchStart);
+        triggerElement.addEventListener('touchmove', onTouchMove);
+        triggerElement.addEventListener('touchend', onTouchEnd);
+        contentEl.addEventListener('scroll', onScroll);
 
         return () => {
-            handleEl.removeEventListener('touchstart', onTouchStart);
-            handleEl.removeEventListener('touchmove', onTouchMove);
-            handleEl.removeEventListener('touchend', onTouchEnd);
+            triggerElement.removeEventListener('touchstart', onTouchStart);
+            triggerElement.removeEventListener('touchmove', onTouchMove);
+            triggerElement.removeEventListener('touchend', onTouchEnd);
+            contentEl.removeEventListener('scroll', onScroll);
         };
-    }, []);
+    }, [isTopScroll]);
 };
