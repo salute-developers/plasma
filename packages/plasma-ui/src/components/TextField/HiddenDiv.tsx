e06927d3cb3bc8e71e200ff5f061c57a6450f962
Alex Lobyntsev <yakutoc@gmail.com>
2023-04-17 19:59:43 +0700

    feat(plasma-ui): handling trailing symbols

---
+++ packages/plasma-ui/src/components/TextField/HiddenDiv.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const HiddenDiv = styled.div`
+    position: fixed;
+
+    display: inline-block;
+    width: auto;
+
+    letter-spacing: inherit;
+    font-weight: inherit;
+    font-family: inherit;
+
+    white-space: pre;
+    visibility: hidden;
+    overflow: auto;
+`;
