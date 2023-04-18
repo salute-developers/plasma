15b4fc760da387ff66e66d8059c6f44e541a1e10
Neretin Artem <neretinartem@Neretins-MBP.netis>
2023-04-18 13:58:57 +0600

    feat(plasma-tokens): Remove new schema generate

--- packages/plasma-tokens/lib/themeBuilder/themeTokenGetters/overlay/index.ts
+++
@@ -1,8 +0,0 @@
-import { OverlayName, TokensGetterFn } from '../../types';
-import { getOverlayHardTokens } from './getOverlayHardTokens';
-import { getOverlaySoftTokens } from './getOverlaySoftTokens';
-
-export const overlayTokenGetters: Record<OverlayName, TokensGetterFn> = {
-    overlaySoft: getOverlaySoftTokens,
-    overlayHard: getOverlayHardTokens,
-};
