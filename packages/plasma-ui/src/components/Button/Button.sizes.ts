5fa319ade09983b1057f4b2596f29b3284819095
Neretin Artem <neretinartem@Neretins-MBP.netis>
2023-04-18 13:42:21 +0600

    feat(plasma-ui): Add size tokens to `Button` component

---
+++ packages/plasma-ui/src/components/Button/Button.sizes.ts
@@ -0,0 +1,26 @@
+import { css } from 'styled-components';
+
+export const sizes = css`
+    --plasma-outline-size: 0.125rem;
+    --plasma-button-l-height: 3.5rem;
+    --plasma-button-l-padding-y: 1rem;
+    --plasma-button-l-padding-x: 1.625rem;
+    --plasma-button-l-padding-content-x: 1.625rem;
+    --plasma-button-l-padding-stretch-x: 1.25rem;
+    --plasma-button-l-radius: 1rem;
+    --plasma-button-l-radius-circle: 1.75rem;
+    --plasma-button-m-height: 3rem;
+    --plasma-button-m-padding-y: 0.75rem;
+    --plasma-button-m-padding-x: 1.375rem;
+    --plasma-button-m-padding-content-x: 1.5rem;
+    --plasma-button-m-padding-stretch-x: 1.25rem;
+    --plasma-button-m-radius: 0.75rem;
+    --plasma-button-m-radius-circle: 1.5rem;
+    --plasma-button-s-height: 2.5rem;
+    --plasma-button-s-padding-y: 0.5rem;
+    --plasma-button-s-padding-x: 1.125rem;
+    --plasma-button-s-padding-content-x: 1.25rem;
+    --plasma-button-s-padding-stretch-x: 1.25rem;
+    --plasma-button-s-radius: 0.75rem;
+    --plasma-button-s-radius-circle: 1.25rem;
+`;
