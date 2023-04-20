efea62b7bd1603d54ac29a62d267e5f8cc86852f
Neretin Artem <neretinartem@Neretins-MBP.netis>
2023-04-20 18:30:05 +0600

    fix(plasma-tokens): Change default value for deprecated prop in generateFile

--- utils/plasma-tokens-utils/src/generators/generateFile.ts
+++ utils/plasma-tokens-utils/src/generators/generateFile.ts
@@ -3,7 +3,7 @@ import { ROBO_COMMENT } from '../constants';
 /**
  * Создает дескриптор на файл с определенным именем и контентом.
  */
-export const generateFile = (name: string, content: string | object, deprecated?: string) => {
+export const generateFile = (name: string, content: string | object, deprecated = '') => {
     if (typeof content !== 'string') {
         content = JSON.stringify(content, null, 4);
     }
