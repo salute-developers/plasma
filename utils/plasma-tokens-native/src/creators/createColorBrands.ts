7dc206ee88f1244479c0266fb900b29322200398
Neretin Artem <neretinartem@Neretins-MBP.netis>
2023-04-30 00:34:52 +0600

    feat(plasma-tokens-native): Update generation native shemes

--- utils/plasma-tokens-native/src/creators/createColorBrands.ts
+++ utils/plasma-tokens-native/src/creators/createColorBrands.ts
@@ -2,17 +2,32 @@ import { writeGeneratedToFS } from '@salutejs/plasma-tokens-utils';
 
 import type { ThemeTokenDataGroups } from '../types';
 
-export const createColorBrands = (dir: string, themesColorTokenGroups: Record<string, ThemeTokenDataGroups>) =>
+export const createColorBrands = (
+    dir: string,
+    themesColorTokenGroups: Record<string, Record<string, ThemeTokenDataGroups>>,
+) =>
     writeGeneratedToFS(
         dir,
-        Object.entries(themesColorTokenGroups).map(([key, value]) => ({
-            file: `${key}.json`,
-            content: JSON.stringify(
-                {
-                    color: value,
-                },
-                null,
-                2,
-            ),
-        })),
+        Object.entries(themesColorTokenGroups).map(([key, value]) => {
+            const color = Object.entries(value).reduce((acc, [themeName, tokens]) => {
+                return {
+                    ...acc,
+                    [themeName]: {
+                        ...tokens.color,
+                    },
+                };
+            }, {});
+
+            return {
+                file: `${key}.json`,
+                content: JSON.stringify(
+                    {
+                        color,
+                    },
+                    null,
+                    2,
+                ),
+            };
+        }),
+        '',
     );
--- utils/plasma-tokens-native/src/creators/createShadows.ts
+++ utils/plasma-tokens-native/src/creators/createShadows.ts
@@ -13,4 +13,5 @@ export const createShadows = (dir: string, shadowTokens: Record<string, any>) =>
                 2,
             ),
         })),
+        '',
     );
