8277f606205f135bc3b8f3d17be318ebb10683d6
Neretin Artem <neretinartem@Neretins-MacBook-Pro.local>
2023-06-06 13:18:54 +0600

    feat(plasma-tokens-native): Add generate complex gradient for iOS

--- utils/plasma-tokens-native/src/generator/transformers/gradientSwift.ts
+++ utils/plasma-tokens-native/src/generator/transformers/gradientSwift.ts
@@ -1,4 +1,7 @@
 import type { TransformedToken } from 'style-dictionary';
+import { getHEXAColor } from '@salutejs/plasma-tokens-utils';
+
+import { Point } from '../../types';
 
 const hexAToRGBA = (hex: string) => ({
     r: Number(`0x${hex[1]}${hex[2]}`),
@@ -9,23 +12,44 @@ const hexAToRGBA = (hex: string) => ({
 
 const getNormalizeValue = (value: number) => Number((value / 255).toFixed(3));
 
-const getGradientStop = (color: string) => {
-    const colors = hexAToRGBA(color);
+const getUIColor = (color: string) => {
+    const hexColor = getHEXAColor(color);
+    const colors = hexAToRGBA(hexColor);
     const [red, green, blue, alpha] = Object.values(colors).map(getNormalizeValue);
 
     return `UIColor(red: ${red}, green: ${green}, blue: ${blue}, alpha: ${alpha})`;
 };
 
-const getGradientStops = (colors: Array<string>) => [getGradientStop(colors[0]), getGradientStop(colors[1])];
+const getGradientStops = (colors: Array<string>) => [getUIColor(colors[0]), getUIColor(colors[1])];
 
-const getPointX = (deg: number) => Number(parseFloat(`${Math.sin((deg * Math.PI) / 180)}`).toFixed(2));
+const getVectorPoints = (startPoint: Point, endPoint: Point) => ({
+    startPoint: `CGPoint(x: ${startPoint.x}, y: ${startPoint.y})`,
+    endPoint: `CGPoint(x: ${endPoint.x}, y: ${endPoint.y})`,
+});
 
-const getPointY = (deg: number) => Number(parseFloat(`${Math.cos((deg * Math.PI) / 180)}`).toFixed(2));
+const getGradient = (value: any) => {
+    const { type, colors, locations, startPoint: rawStartPoint, endPoint: rawEndPoint } = value;
+    const [startColor, endColor] = getGradientStops(colors);
+    const { startPoint, endPoint } = getVectorPoints(rawStartPoint, rawEndPoint);
 
-const getCoord = (pointValue: number, sign: number) => parseFloat(`${(pointValue * sign + 1) / 2}`).toFixed(2);
+    return {
+        type,
+        startColor,
+        endColor,
+        locations,
+        startPoint,
+        endPoint,
+    };
+};
 
-const getVectorPoints = (angle: string) => {
+// TODO: Убрать после того, как переделаем схемы на новый формат градиентов
+const getLinearVectorPointsLegacy = (angle: string) => {
     const degree = Number(angle);
+
+    const getCoord = (pointValue: number, sign: number) =>
+        Number(parseFloat(`${(pointValue * sign + 1) / 2}`).toFixed(2));
+    const getPointX = (deg: number) => Number(parseFloat(`${Math.sin((deg * Math.PI) / 180)}`).toFixed(2));
+    const getPointY = (deg: number) => Number(parseFloat(`${Math.cos((deg * Math.PI) / 180)}`).toFixed(2));
 
     const startPoint = {
         x: getCoord(getPointX(degree), -1),
@@ -37,27 +61,55 @@ const getVectorPoints = (angle: string) => {
     };
 
     return {
-        startPoint: `UnitPoint(x: ${startPoint.x}, y: ${startPoint.y})`,
-        endPoint: `UnitPoint(x: ${endPoint.x}, y: ${endPoint.y})`,
+        startPoint: `CGPoint(x: ${startPoint.x}, y: ${startPoint.y})`,
+        endPoint: `CGPoint(x: ${endPoint.x}, y: ${endPoint.y})`,
+    };
+};
+// TODO: Убрать после того, как переделаем схемы на новый формат градиентов
+const getLinearGradientLegacy = (value: any) => {
+    const { angle, colors, locations = ['0', '100'] } = value;
+    const [startColor, endColor] = getGradientStops(colors);
+    const { startPoint, endPoint } = getLinearVectorPointsLegacy(angle);
+
+    return {
+        type: '.linear',
+        startColor,
+        endColor,
+        locations: locations.map((item: string) => Number((Number(item) / 100).toFixed(2))),
+        startPoint,
+        endPoint,
     };
 };
 
 export const gradientSwiftTransformer = (prop: TransformedToken) => {
-    const { linearGradient } = prop.original.value;
+    if (Array.isArray(prop.original.value)) {
+        return {
+            gradient: prop.original.value.reverse().map((item) => {
+                if (item.swift) {
+                    return getGradient(item.swift);
+                }
 
-    if (!linearGradient || linearGradient.colors.length < 2) {
-        return prop.value;
+                if (item.backgroundColor) {
+                    return {
+                        backgroundColor: getUIColor(getHEXAColor(item.backgroundColor)),
+                    };
+                }
+
+                return 'undefined';
+            }),
+        };
     }
 
-    const { colors, angle } = prop.original.value.linearGradient;
+    const { linearGradient, swift } = prop.original.value;
 
-    const [startColor, endColor] = getGradientStops(colors);
-    const { startPoint, endPoint } = getVectorPoints(angle);
+    if (swift && swift.colors.length > 1) {
+        return { gradient: getGradient(swift) };
+    }
 
-    return {
-        startColor,
-        endColor,
-        startPoint,
-        endPoint,
-    };
+    // TODO: Убрать после того, как переделаем схемы на новый формат градиентов
+    if (linearGradient && linearGradient.colors.length > 1) {
+        return { gradient: getLinearGradientLegacy(linearGradient) };
+    }
+
+    return prop.value;
 };
