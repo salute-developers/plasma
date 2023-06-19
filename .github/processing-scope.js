816997bc956a8f05fa36a76786ebab05e38ea514
Alexander Lobyntsev <yakutoc@gmail.com>
2023-06-19 16:10:58 +0700

    ci: change detection as js script
    use actions/github-script@v6 + pure js

---
+++ .github/processing-scope.js
@@ -0,0 +1,20 @@
+const CYPRESS_SCOPE = [
+    '@salutejs/plasma-web',
+    '@salutejs/plasma-b2c',
+    '@salutejs/plasma-ui',
+    '@salutejs/plasma-temple',
+];
+
+module.exports = () => {
+    const { CHANGED_STATE } = process.env;
+
+    if (!CHANGED_STATE) {
+        return { SCOPE: [], HAS_SCOPE: false };
+    }
+
+    const scope = CHANGED_STATE.split(',')
+        .filter((item) => CYPRESS_SCOPE.includes(item))
+        .map((item) => `"${item.split('-')[1]}"`);
+
+    return { SCOPE: JSON.stringify(scope), HAS_SCOPE: !!scope.length };
+};
