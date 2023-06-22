3f36e5fb2cc4c717c3706837af4c2a5f57ff424f
Alexander Lobyntsev <yakutoc@gmail.com>
2023-06-22 12:53:24 +0700

    ci: processing scope wip

--- .github/processing-scope.js
+++ .github/processing-scope.js
@@ -12,9 +12,48 @@ module.exports = () => {
         return { SCOPE: [], HAS_SCOPE: false };
     }
 
-    const scope = CHANGED_STATE.split(',')
-        .filter((item) => CYPRESS_SCOPE.includes(item))
-        .map((item) => `"${item.split('-')[1]}"`);
+    const packagesList = CHANGED_STATE.split(',');
 
-    return { SCOPE: JSON.stringify(scope), HAS_SCOPE: Boolean(scope.length) };
+    console.log(packagesList);
+
+    const HAS_PLASMA_UI_DOCS = packagesList.includes('@salutejs/plasma-ui-docs');
+    const HAS_PLASMA_WEB_DOCS = packagesList.includes('@salutejs/plasma-web-docs');
+    const HAS_PLASMA_TEMPLE_DOCS = packagesList.includes('@salutejs/plasma-temple-docs');
+    const HAS_PLASMA_WEBSITE = packagesList.includes('@salutejs/plasma-website');
+
+    const HAS_PLASMA_UI = packagesList.includes('@salutejs/plasma-ui');
+    const HAS_PLASMA_WEB = packagesList.includes('@salutejs/plasma-web');
+    const HAS_PLASMA_B2C = packagesList.includes('@salutejs/plasma-b2c');
+    const HAS_PLASMA_TEMPLE = packagesList.includes('@salutejs/plasma-temple');
+
+    const HAS_DOCUMENTATION_CHANGED =
+        HAS_PLASMA_UI_DOCS || HAS_PLASMA_WEB_DOCS || HAS_PLASMA_TEMPLE_DOCS || HAS_PLASMA_WEBSITE;
+
+    console.log(
+        HAS_DOCUMENTATION_CHANGED,
+        HAS_PLASMA_UI_DOCS,
+        HAS_PLASMA_WEB_DOCS,
+        HAS_PLASMA_TEMPLE_DOCS,
+        HAS_PLASMA_WEBSITE,
+        HAS_PLASMA_UI,
+        HAS_PLASMA_WEB,
+        HAS_PLASMA_B2C,
+        HAS_PLASMA_TEMPLE,
+    );
+
+    const scope = packagesList.filter((item) => CYPRESS_SCOPE.includes(item)).map((item) => `"${item.split('-')[1]}"`);
+
+    return {
+        SCOPE: JSON.stringify(scope),
+        HAS_SCOPE: Boolean(scope.length),
+        HAS_DOCUMENTATION_CHANGED,
+        HAS_PLASMA_UI_DOCS,
+        HAS_PLASMA_WEB_DOCS,
+        HAS_PLASMA_TEMPLE_DOCS,
+        HAS_PLASMA_WEBSITE,
+        HAS_PLASMA_UI,
+        HAS_PLASMA_WEB,
+        HAS_PLASMA_B2C,
+        HAS_PLASMA_TEMPLE,
+    };
 };
