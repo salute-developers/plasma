58186a8d16520508f7cf7db3f450632b0fde938b
Neretin Artem <hawktrike@gmail.com>
2023-07-08 15:03:04 +0600

    fix(plasma-theme-builder): Remove auto theme changes

--- website/plasma-theme-builder/src/GlobalStyle.tsx
+++ website/plasma-theme-builder/src/GlobalStyle.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { plasma } from '@salutejs/plasma-typo';
-import { light, dark } from '@salutejs/plasma-tokens-b2c/themes';
+import { dark } from '@salutejs/plasma-tokens-b2c/themes';
 import { text } from '@salutejs/plasma-tokens-b2c';
 import { b2c } from '@salutejs/plasma-tokens-b2c/typo';
 import type { ThemeMode } from '@salutejs/plasma-tokens-utils';
@@ -23,27 +23,17 @@ const DocumentStyle = createGlobalStyle`
     }
 `;
 
-const LightThemeStyle = createGlobalStyle(light);
 const DarkThemeStyle = createGlobalStyle(dark);
 const TypoStyle = createGlobalStyle(plasma);
 const OldTypoStyle = createGlobalStyle(b2c);
 
-const matchMediaWithColorScheme = window.matchMedia('(prefers-color-scheme: dark');
-
-const getSystemTheme = () => (matchMediaWithColorScheme.matches ? 'dark' : 'light');
-
 export const GlobalStyle = () => {
-    const [theme, setTheme] = useState<ThemeMode>(getSystemTheme());
-
-    matchMediaWithColorScheme.addEventListener('change', ({ matches }) => {
-        setTheme(matches ? 'dark' : 'light');
-    });
+    const theme: ThemeMode = 'dark';
 
     return (
         <>
             <DocumentStyle />
             {theme === 'dark' && <DarkThemeStyle />}
-            {theme === 'light' && <LightThemeStyle />}
             <TypoStyle />
             <OldTypoStyle />
         </>
