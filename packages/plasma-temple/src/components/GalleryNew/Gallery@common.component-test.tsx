a5c6b14caea18d56005bc3cb288092fa36e17abc
Neretin Artem <hawktrike@gmail.com>
2023-07-27 00:22:37 +0600

    feat(plasma-temple): Add some threshold fallbacks

--- packages/plasma-temple/src/components/GalleryNew/Gallery@common.component-test.tsx
+++ packages/plasma-temple/src/components/GalleryNew/Gallery@common.component-test.tsx
@@ -5,38 +5,59 @@ import { generateWrapper, galleries } from './Gallery@helpers.component-test';
 describe('Gallery', () => {
     it('render single gallery', () => {
         generateWrapper({ items: galleries[0] });
-        cy.matchImageSnapshot();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.05,
+            failureThresholdType: 'percent',
+        });
     });
 
     it('render multi gallery', () => {
         generateWrapper();
-        cy.matchImageSnapshot();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.05,
+            failureThresholdType: 'percent',
+        });
     });
 
     it('single gallery with initial state', () => {
         generateWrapper({ initialState: 5, items: galleries[0] });
-        cy.matchImageSnapshot();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.05,
+            failureThresholdType: 'percent',
+        });
     });
 
     it('multi gallery with initial state', () => {
         generateWrapper({ initialState: { activeGallery: 1, activeCards: [3, 5] } });
-        cy.matchImageSnapshot();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.1,
+            failureThresholdType: 'percent',
+        });
     });
 
     it('with autofocus', () => {
         generateWrapper({ autoFocus: true });
-        cy.matchImageSnapshot();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.05,
+            failureThresholdType: 'percent',
+        });
     });
 
     it('voice navigation', () => {
         generateWrapper({ autoFocus: true });
         cy.sendNavigateAction([navigate.RIGHT, navigate.DOWN, navigate.RIGHT, navigate.RIGHT, navigate.UP]);
-        cy.matchImageSnapshot();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.05,
+            failureThresholdType: 'percent',
+        });
     });
 
     it('external navigation', () => {
         generateWrapper();
         cy.get('[data-cy="goto-gallery-1"]').click();
-        cy.matchImageSnapshot();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.05,
+            failureThresholdType: 'percent',
+        });
     });
 });
