39d90a46a9ae3256d6b4c8baba35f0cf0c3b0055
Vasiliy Loginevskiy <yeti.or@gmail.com>
2023-08-31 00:59:07 +0300

    feat(plasma-new-hope): add plasma_b2c-old theme

--- packages/plasma-new-hope/src/components/Button/_view/base.ts
+++ packages/plasma-new-hope/src/components/Button/_view/base.ts
@@ -8,18 +8,21 @@ export const base = css`
     background-color: var(--plasma-button-bg-color);
 
     :hover {
-        color: var(--plasma-button-color-hover);
+        color: var(--plasma-button-color-hover, var(--plasma-button-color));
         background-color: var(--plasma-button-bg-color-hover, var(--plasma-button-bg-color));
     }
 
     :active {
-        color: var(--plasma-button-color-active);
+        color: var(--plasma-button-color-active, var(--plasma-button-color));
         background-color: var(--plasma-button-bg-color-active, var(--plasma-button-bg-color));
 
         /* TODO: */
         scale: 1.02;
     }
 `;
+
+// TODO: should we move defaults for token --plasma-button-color-hover default => --plasma-button-color
+// from code to plasma-mapping ??
 
 // TODO: background-image: gradients
 
