97219039daac35d5a3ed1c65a7688b3099b895da
Neretin Artem <hawktrike@gmail.com>
2023-09-13 21:54:41 +0600

    feat(plasma-web): Replace `Link` from plasma-new-hope

--- packages/plasma-web/src/components/Link/Link.stories.tsx
+++ packages/plasma-web/src/components/Link/Link.stories.tsx
@@ -1,13 +1,60 @@
 import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { InSpacingDecorator } from '@salutejs/plasma-sb-utils';
 
 import { P1 } from '../Typography';
 
 import { Link } from '.';
 
-const props = { href: '/', target: '__blank' };
+const views = ['primary'] as const;
 
-export const Default = () => (
+export default {
+    title: 'Content/Link',
+    decorators: [InSpacingDecorator],
+    argTypes: {
+        text: {
+            control: {
+                type: 'text',
+            },
+        },
+        href: {
+            control: {
+                type: 'text',
+            },
+        },
+        view: {
+            options: views,
+            control: {
+                type: 'select',
+            },
+        },
+    },
+} as Meta;
+
+type LinkStoryProps = {
+    text: string;
+    href: string;
+    view: typeof views[number];
+    disabled?: boolean;
+    focused?: boolean;
+};
+
+const args: LinkStoryProps = {
+    text: 'приложение',
+    href: 'https://google.com',
+    view: 'primary',
+    disabled: false,
+    focused: true,
+};
+
+export const Default: Story<LinkStoryProps> = ({ text, ...props }) => (
     <P1>
-        Скачайте <Link {...props}>приложение</Link>.
+        Скачайте{' '}
+        <Link target="_blank" {...props}>
+            {text}
+        </Link>
+        .
     </P1>
 );
+
+Default.args = args;
