bc1f4254987b1bf0c3bafd8e24d1fadfc9e67112
Neretin Artem <hawktrike@gmail.com>
2023-09-13 21:03:14 +0600

    feat(plasma-new-hope): Add `Link` component with new design and tokens themes

---
+++ packages/plasma-new-hope/src/examples/themes/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+import plasma_b2c from './plasma_b2c.module.css';
+import plasma_web from './plasma_web.module.css';
+
+export type ThemeType = { light: string; dark: string };
+
+export const themes: Record<string, ThemeType> = {
+    plasma_b2c,
+    plasma_web,
+};
