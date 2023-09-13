390ed3bdec05766e606ce385f2e14022d099fcca
Neretin Artem <hawktrike@gmail.com>
2023-09-13 21:55:09 +0600

    feat(plasma-hope): Remove `Link` component

--- packages/plasma-hope/src/index.ts
+++ packages/plasma-hope/src/index.ts
@@ -11,7 +11,6 @@ export * from './components/ElasticGrid';
 export * from './components/Dropdown';
 export * from './components/Grid';
 export * from './components/Image';
-export * from './components/Link';
 export * from './components/List';
 export * from './components/Modal';
 export * from './components/Notification';
