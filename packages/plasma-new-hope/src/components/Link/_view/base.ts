f4ee17309dc5a194dfff6d1b645504c01d824984
Neretin Artem <hawktrike@gmail.com>
2023-09-13 13:23:36 +0600

    feat(plasma-new-hope): Add core `Link` component

---
+++ packages/plasma-new-hope/src/components/Link/_view/base.ts
@@ -0,0 +1,37 @@
+import { css } from '@linaria/core';
+
+const getColor = (cssVar: string) => `
+    color: var(${cssVar}, var(--plasma-link-color));
+
+    ::before {
+        opacity: 1;
+    }
+`;
+
+export const base = css`
+    color: var(--plasma-link-color);
+
+    ::before {
+        border-bottom: var(--plasma-link-underline-border) solid currentColor;
+    }
+
+    &:hover {
+        ${getColor('--plasma-link-color-hover')};
+    }
+
+    &:active {
+        ${getColor('--plasma-link-color-active')};
+    }
+
+    &:visited {
+        ${getColor('--plasma-link-color-visited')};
+    }
+
+    &:visited:hover {
+        ${getColor('--plasma-link-color-visited-hover')};
+    }
+
+    &:visited:active {
+        ${getColor('--plasma-link-color-visited-active')};
+    }
+`;
