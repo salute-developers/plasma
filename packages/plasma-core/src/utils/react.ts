7a8b46b37ec38a56edeeae3936fec62a3ec5d4c3
Neretin Artem <hawktrike@gmail.com>
2023-09-20 21:11:07 +0600

    feat(plasma-new-hope,plasma-core): Add core `Spinner` component

--- packages/plasma-core/src/utils/react.ts
+++ packages/plasma-core/src/utils/react.ts
@@ -1,8 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+
+// NOTE: для next.js важно, чтобы путь до хука был полный, т.к. есть проблемы с ре-экспортом
+import { useUniqId } from '../hooks/useUniqId';
 
 // TODO: https://github.com/salute-developers/plasma/issues/233
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const { flushSync } = ReactDOM as any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const { useId } = React as any;
 
 export const IS_REACT_18 = ReactDOM.version.split('.')[0] === '18';
 
@@ -13,3 +19,5 @@ export function safeFlushSync(fn: () => void): void {
 
     return flushSync(fn);
 }
+
+export const safeUseId: () => string = !IS_REACT_18 ? useUniqId : useId;
