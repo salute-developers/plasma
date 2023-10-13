3a3790dd5211e7a17f8e5a84100e96d7a35c1a80
Alexander Lobyntsev <yakutoc@gmail.com>
2023-10-13 14:11:01 +0700

    chore(plasma-b2c): migrate to CSF 3 [Editable]

--- packages/plasma-b2c/src/components/Editable/Editable.stories.tsx
+++ packages/plasma-b2c/src/components/Editable/Editable.stories.tsx
@@ -1,27 +1,14 @@
-import React, { useState } from 'react';
-import { Story, Meta } from '@storybook/react';
+import React, { useState, useCallback } from 'react';
+import type { StoryObj, Meta } from '@storybook/react';
 import { InSpacingDecorator, disableProps } from '@salutejs/plasma-sb-utils';
-import { useCallback } from '@storybook/client-api';
 import { IconEdit, IconSize } from '@salutejs/plasma-icons';
 
 import * as typography from '../Typography';
 
-import { Editable, EditableProps } from '.';
+import { Editable } from '.';
+import type { EditableProps } from '.';
 
-const propsToDisable = [
-    'ref',
-    'theme',
-    'as',
-    'forwardedAs',
-    'textComponent',
-    'icon',
-    'onChange',
-    'onBlur',
-    'onPaste',
-    'value',
-];
-
-export default {
+const meta: Meta<EditablePropsExtend> = {
     title: 'Controls/Editable',
     component: Editable,
     argTypes: {
@@ -33,18 +20,31 @@ export default {
             options: Object.keys(typography),
             control: { type: 'select' },
         },
-        ...disableProps(propsToDisable),
+        ...disableProps([
+            'ref',
+            'theme',
+            'as',
+            'forwardedAs',
+            'textComponent',
+            'icon',
+            'onChange',
+            'onBlur',
+            'onPaste',
+            'value',
+        ]),
     },
     decorators: [InSpacingDecorator],
-} as Meta;
+};
 
-interface ExtraProps {
+export default meta;
+
+type EditablePropsExtend = EditableProps & {
     iconSize: IconSize;
     componentName: keyof typeof typography;
     defaultValue: string;
-}
+};
 
-export const Default: Story<EditableProps & ExtraProps> = ({ defaultValue, componentName, iconSize, ...rest }) => {
+const StoryDefault = ({ defaultValue, componentName, iconSize, ...rest }: EditablePropsExtend) => {
     const [value, setValue] = useState<string>(defaultValue);
 
     const handleChange = useCallback((e) => {
@@ -55,16 +55,19 @@ export const Default: Story<EditableProps & ExtraProps> = ({ defaultValue, compo
         <Editable
             icon={<IconEdit size={iconSize} color="inherit" />}
             textComponent={typography[componentName]}
-            value={value}
+            value={defaultValue}
             onChange={handleChange}
             {...rest}
         />
     );
 };
 
-Default.args = {
-    iconSize: 's',
-    componentName: 'Headline1',
-    defaultValue: 'Document 1',
-    placeholder: 'Компонент с возможностью редактирования текста',
+export const Default: StoryObj<EditablePropsExtend> = {
+    args: {
+        iconSize: 's',
+        componentName: 'Headline1',
+        defaultValue: 'Document 1',
+        placeholder: 'Компонент с возможностью редактирования текста',
+    },
+    render: (args) => <StoryDefault {...args} />,
 };
