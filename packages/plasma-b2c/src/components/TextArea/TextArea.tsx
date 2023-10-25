e5c0ea7732f6d44b1dc9a79dc2d97226e5cbe01c
Neretin Artem <hawktrike@gmail.com>
2023-10-25 11:30:59 +0600

    feat(plasma-b2c): Replace `TextArea` from plasma-new-hope

--- packages/plasma-b2c/src/components/TextArea/TextArea.tsx
+++ packages/plasma-b2c/src/components/TextArea/TextArea.tsx
@@ -1,12 +1,15 @@
-import React, { forwardRef } from 'react';
-import { TextArea as TextAreaHope } from '@salutejs/plasma-hope';
-import type { TextAreaProps } from '@salutejs/plasma-hope';
+import { textAreaConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
 
-const TextAreaBase = TextAreaHope({ design: 'b2c' });
+import { config } from './TextArea.config';
+
+import type { TextAreaProps } from '.';
+
+const mergedConfig = mergeConfig(textAreaConfig, config);
+const TextAreaComponent = component(mergedConfig) as React.ForwardRefExoticComponent<
+    TextAreaProps & React.RefAttributes<HTMLTextAreaElement>
+>;
 
 /**
  * Поле ввода многострочного текста.
  */
-export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>((props, ref) => (
-    <TextAreaBase {...props} ref={ref} />
-));
+export const TextArea = TextAreaComponent;
