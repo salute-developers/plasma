e0a9a78d20b2eea9b2b7391e063bafdd0d5f3502
Alexander Lobyntsev <yakutoc@gmail.com>
2023-11-14 16:22:49 +0700

    ci: revert HAS_PLASMA_WEBSITE flag for build documentation

--- .github/processing-scope.js
+++ .github/processing-scope.js
@@ -42,8 +42,10 @@ module.exports = () => {
     const HAS_PLASMA_TEMPLE = packagesList.includes('@salutejs/plasma-temple');
     const HAS_PLASMA_HOPE = packagesList.includes('@salutejs/plasma-new-hope');
     const HAS_PLASMA_ASDK = packagesList.includes('@salutejs/plasma-asdk');
+    const HAS_PLASMA_WEBSITE = packagesList.includes('@salutejs/plasma-website');
 
-    const HAS_DOCUMENTATION_CHANGED = HAS_PLASMA_UI_DOCS || HAS_PLASMA_WEB_DOCS || HAS_PLASMA_TEMPLE_DOCS;
+    const HAS_DOCUMENTATION_CHANGED =
+        HAS_PLASMA_WEBSITE || HAS_PLASMA_UI_DOCS || HAS_PLASMA_WEB_DOCS || HAS_PLASMA_TEMPLE_DOCS;
 
     /**
      * List short packages name
