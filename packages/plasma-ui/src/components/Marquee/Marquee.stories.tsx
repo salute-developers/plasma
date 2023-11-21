3804491c65ec31a5b6cbca20d65bd19cee58a2ed
Alexander Lobyntsev <yakutoc@gmail.com>
2023-11-21 13:57:42 +0700

    chore(plasma-ui): migrate to CSF 3 [Marquee]

--- packages/plasma-ui/src/components/Marquee/Marquee.stories.tsx
+++ packages/plasma-ui/src/components/Marquee/Marquee.stories.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import type { StoryObj, Meta } from '@storybook/react';
 
 import { InSpacing } from '../../helpers/StoryDecorators';
 import { Footnote1 } from '../Typography';
 
 import { Marquee } from '.';
 
-export default {
+const meta: Meta = {
     title: 'Content/Marquee',
     component: Marquee,
     decorators: [InSpacing],
 };
+
+export default meta;
 
 const MarqueeWrapper = styled.div`
     width: 18rem;
     overflow: hidden;
 `;
 
-export const Default = () => {
-    return (
-        <MarqueeWrapper>
-            <Marquee>
-                <Footnote1>Очень очень длинный текст бегущей строки</Footnote1>
-            </Marquee>
-        </MarqueeWrapper>
-    );
+export const Default: StoryObj = {
+    render: () => {
+        return (
+            <MarqueeWrapper>
+                <Marquee>
+                    <Footnote1>Очень очень длинный текст бегущей строки</Footnote1>
+                </Marquee>
+            </MarqueeWrapper>
+        );
+    },
 };
 
-export const Basic = () => {
-    return (
-        <MarqueeWrapper>
-            <Marquee>Очень очень длинный текст бегущей строки</Marquee>
-        </MarqueeWrapper>
-    );
-};
-
-Basic.parameters = {
-    chromatic: {
-        disable: true,
+export const Basic: StoryObj = {
+    render: () => {
+        return (
+            <MarqueeWrapper>
+                <Marquee>Очень очень длинный текст бегущей строки</Marquee>
+            </MarqueeWrapper>
+        );
     },
 };
