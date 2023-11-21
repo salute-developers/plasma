3023a945d55713cc25721251c64e13083f07084d
Alexander Lobyntsev <yakutoc@gmail.com>
2023-11-21 12:40:02 +0700

    chore(plasma-ui): migrate to CSF 3 [Card]

--- packages/plasma-ui/src/components/Card/Card.stories.tsx
+++ packages/plasma-ui/src/components/Card/Card.stories.tsx
@@ -1,23 +1,20 @@
 import React, { useMemo } from 'react';
-import { Story, Meta } from '@storybook/react';
+import type { StoryObj, Meta } from '@storybook/react';
 
 import { InSpacingDecorator, IconPlaceholder } from '../../helpers';
 import { Button } from '../Button';
 import { Cell, CellIcon, CellListItem, CellDisclosure } from '../Cell';
 import { TextBox, TextBoxTitle, TextBoxSubTitle, TextBoxBiggerTitle, TextBoxBigTitle } from '../TextBox';
 
-import { Card, CardProps } from './Card';
+import { Card } from './Card';
 import { CardBody } from './CardBody';
 import { CardContent } from './CardContent';
 import { CardMedia } from './CardMedia';
 
+import type { CardProps } from '.';
 import { CardParagraph1 } from '.';
 
-const longText = `Канадский актёр, кинопродюсер, и музыкант. Наиболее известен своими ролями в киносериях «Матрица» и
-«Джон Уик», а также в фильмах «На гребне волны», «Мой личный штат Айдахо», «Дракула», «Скорость»,
-«Джонни-мнемоник», «Адвокат дьявола», «Константин: Повелитель тьмы» и «Короли улиц».`;
-
-export default {
+const meta: Meta<CardProps> = {
     title: 'Content/Card',
     decorators: [InSpacingDecorator],
     parameters: {
@@ -25,119 +22,136 @@ export default {
             disable: false,
         },
     },
-} as Meta;
+};
 
-interface BasicValueProps extends CardProps {
+export default meta;
+
+const longText = `Канадский актёр, кинопродюсер, и музыкант. Наиболее известен своими ролями в киносериях «Матрица» и
+«Джон Уик», а также в фильмах «На гребне волны», «Мой личный штат Айдахо», «Дракула», «Скорость»,
+«Джонни-мнемоник», «Адвокат дьявола», «Константин: Повелитель тьмы» и «Короли улиц».`;
+
+type BasicValueProps = CardProps & {
     cover: boolean;
     coverGradient: boolean;
     subTitle: string;
     title: string;
     description: string;
-}
-
-const basicCardStyle = { width: '22.5rem' };
-const basicButtonStyle = { marginTop: '1em' };
-
-export const BasicValue: Story<BasicValueProps> = ({
-    outlined,
-    coverGradient,
-    scaleOnFocus,
-    cover,
-    subTitle,
-    title,
-    description,
-}) => {
-    return (
-        <Card style={basicCardStyle} tabIndex={0} outlined={outlined} scaleOnFocus={scaleOnFocus}>
-            <CardBody>
-                <CardMedia
-                    src="./images/320_320_0.jpg"
-                    placeholder="./images/320_320_1.jpg"
-                    ratio={cover ? '1 / 1' : '16 / 9'}
-                />
-                <CardContent cover={cover as true} coverGradient={cover && coverGradient}>
-                    <TextBox>
-                        <TextBoxBigTitle>{subTitle}</TextBoxBigTitle>
-                        <TextBoxBiggerTitle>{title}</TextBoxBiggerTitle>
-                        <TextBoxSubTitle>{description}</TextBoxSubTitle>
-                    </TextBox>
-                    <Button
-                        text="Label"
-                        view="primary"
-                        size="s"
-                        scaleOnInteraction={false}
-                        outlined={false}
-                        stretch
-                        style={basicButtonStyle}
-                        tabIndex={-1}
-                    />
-                </CardContent>
-            </CardBody>
-        </Card>
-    );
 };
 
-BasicValue.args = {
-    cover: false,
-    coverGradient: true,
-    outlined: true,
-    scaleOnFocus: true,
-    subTitle: 'Потребительский кредит',
-    title: 'до 230 000 ₽',
-    description: 'На 18 месяцев, ставка 13,9%',
-};
-
-interface BasicDefaultProps {
+type BasicPropsDefault = {
     cover: boolean;
     title: string;
     footer1: string;
     footer2: string;
     description: string;
-}
-
-const defaultCardStyle = { width: '20rem' };
-const defaultParagraphStyle = { marginTop: '0.75rem' };
-
-export const BasicDefault: Story<BasicDefaultProps> = ({ cover, title, footer1, footer2, description }) => {
-    return (
-        <Card style={defaultCardStyle}>
-            <CardBody>
-                <CardMedia
-                    src="./images/320_320_2.jpg"
-                    placeholder="./images/320_320_3.jpg"
-                    ratio={cover ? '1 / 1' : '16 / 9'}
-                />
-                <CardContent cover={cover}>
-                    <TextBox>
-                        <TextBoxBigTitle>{title}</TextBoxBigTitle>
-                        <TextBoxSubTitle>{footer1}</TextBoxSubTitle>
-                        <CardParagraph1 style={defaultParagraphStyle} lines={4}>
-                            {description}
-                        </CardParagraph1>
-                        <TextBoxSubTitle>{footer2}</TextBoxSubTitle>
-                    </TextBox>
-                </CardContent>
-            </CardBody>
-        </Card>
-    );
 };
 
-BasicDefault.args = {
-    cover: false,
-    title: 'Киану Ривз',
-    footer1: 'Актёр',
-    footer2: 'ru.wikipedia.org',
-    description: longText,
-};
-
-interface ListCardProps {
+type ListCardProps = {
     title: string;
     subtitle: string;
-}
+};
+
+type ListAndHeaderProps = {
+    listItems: number;
+    header: string;
+    details: string;
+    title: string;
+    subtitle: string;
+};
+
+type FastAnswerProps = {
+    header: string;
+    details: string;
+    value: string;
+    description: string;
+};
+
+const basicCardStyle = { width: '22.5rem' };
+
+const basicButtonStyle = { marginTop: '1em' };
+
+export const BasicValue: StoryObj<BasicValueProps> = {
+    args: {
+        cover: false,
+        coverGradient: true,
+        outlined: true,
+        scaleOnFocus: true,
+        subTitle: 'Потребительский кредит',
+        title: 'до 230 000 ₽',
+        description: 'На 18 месяцев, ставка 13,9%',
+    },
+    render: ({ outlined, coverGradient, scaleOnFocus, cover, subTitle, title, description }) => {
+        return (
+            <Card style={basicCardStyle} tabIndex={0} outlined={outlined} scaleOnFocus={scaleOnFocus}>
+                <CardBody>
+                    <CardMedia
+                        src="./images/320_320_0.jpg"
+                        placeholder="./images/320_320_1.jpg"
+                        ratio={cover ? '1 / 1' : '16 / 9'}
+                    />
+                    <CardContent cover={cover as true} coverGradient={cover && coverGradient}>
+                        <TextBox>
+                            <TextBoxBigTitle>{subTitle}</TextBoxBigTitle>
+                            <TextBoxBiggerTitle>{title}</TextBoxBiggerTitle>
+                            <TextBoxSubTitle>{description}</TextBoxSubTitle>
+                        </TextBox>
+                        <Button
+                            text="Label"
+                            view="primary"
+                            size="s"
+                            scaleOnInteraction={false}
+                            outlined={false}
+                            stretch
+                            style={basicButtonStyle}
+                            tabIndex={-1}
+                        />
+                    </CardContent>
+                </CardBody>
+            </Card>
+        );
+    },
+};
+
+const defaultCardStyle = { width: '20rem' };
+
+const defaultParagraphStyle = { marginTop: '0.75rem' };
+
+export const BasicDefault: StoryObj<BasicPropsDefault> = {
+    args: {
+        cover: false,
+        title: 'Киану Ривз',
+        footer1: 'Актёр',
+        footer2: 'ru.wikipedia.org',
+        description: longText,
+    },
+    render: ({ cover, title, footer1, footer2, description }) => {
+        return (
+            <Card style={defaultCardStyle}>
+                <CardBody>
+                    <CardMedia
+                        src="./images/320_320_2.jpg"
+                        placeholder="./images/320_320_3.jpg"
+                        ratio={cover ? '1 / 1' : '16 / 9'}
+                    />
+                    <CardContent cover={cover}>
+                        <TextBox>
+                            <TextBoxBigTitle>{title}</TextBoxBigTitle>
+                            <TextBoxSubTitle>{footer1}</TextBoxSubTitle>
+                            <CardParagraph1 style={defaultParagraphStyle} lines={4}>
+                                {description}
+                            </CardParagraph1>
+                            <TextBoxSubTitle>{footer2}</TextBoxSubTitle>
+                        </TextBox>
+                    </CardContent>
+                </CardBody>
+            </Card>
+        );
+    },
+};
 
 const listCardStyle = { width: '20rem' };
 
-export const ListCard: Story<ListCardProps> = ({ title, subtitle }) => {
+const StoryListCard = ({ title, subtitle }: ListCardProps) => {
     const [contentLeft, content, contentRight] = useMemo(
         () => [
             <CellIcon>
@@ -161,22 +175,17 @@ export const ListCard: Story<ListCardProps> = ({ title, subtitle }) => {
     );
 };
 
-ListCard.args = {
-    title: 'Title',
-    subtitle: 'Subtitle',
+export const ListCard: StoryObj<ListCardProps> = {
+    args: {
+        title: 'Title',
+        subtitle: 'Subtitle',
+    },
+    render: (args) => <StoryListCard {...args} />,
 };
-
-interface ListAndHeaderProps {
-    listItems: number;
-    header: string;
-    details: string;
-    title: string;
-    subtitle: string;
-}
 
 const listAndHeaderCardStyle = { width: '20rem' };
 
-export const ListAndHeader: Story<ListAndHeaderProps> = ({ listItems, header, details, title, subtitle }) => {
+const StoryListAndHeader = ({ listItems, header, details, title, subtitle }: ListAndHeaderProps) => {
     const cellList = useMemo(() => {
         const items = Array(listItems).fill(0);
         return items.map((_, i) => (
@@ -218,24 +227,20 @@ export const ListAndHeader: Story<ListAndHeaderProps> = ({ listItems, header, de
     );
 };
 
-ListAndHeader.args = {
-    listItems: 3,
-    header: 'Название раздела',
-    details: 'Detail',
-    title: 'Title',
-    subtitle: 'Subtitle',
+export const ListAndHeader: StoryObj<ListAndHeaderProps> = {
+    args: {
+        listItems: 3,
+        header: 'Название раздела',
+        details: 'Detail',
+        title: 'Title',
+        subtitle: 'Subtitle',
+    },
+    render: (args) => <StoryListAndHeader {...args} />,
 };
-
-interface FastAnswerProps {
-    header: string;
-    details: string;
-    value: string;
-    description: string;
-}
 
 const fastAnswerCardStyle = { width: '20rem' };
 
-export const FastAnswer: Story<FastAnswerProps> = ({ header, details, value, description }) => {
+const StoryFastAnswer = ({ header, details, value, description }: FastAnswerProps) => {
     const cardContent = useMemo(
         () => (
             <CardContent compact>
@@ -266,9 +271,12 @@ export const FastAnswer: Story<FastAnswerProps> = ({ header, details, value, des
     return <Card style={fastAnswerCardStyle}>{cardContent}</Card>;
 };
 
-FastAnswer.args = {
-    header: 'Название раздела',
-    details: 'Detail',
-    value: 'Value',
-    description: 'Description',
+export const FastAnswer: StoryObj<FastAnswerProps> = {
+    args: {
+        header: 'Название раздела',
+        details: 'Detail',
+        value: 'Value',
+        description: 'Description',
+    },
+    render: (args) => <StoryFastAnswer {...args} />,
 };
