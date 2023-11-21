b6b2adf97e1712ecd8416de1806bcb234cf3d2fa
Alexander Lobyntsev <yakutoc@gmail.com>
2023-11-21 13:53:44 +0700

    chore(plasma-ui): migrate to CSF 3 [Image]

--- packages/plasma-ui/src/components/Image/Image.stories.tsx
+++ packages/plasma-ui/src/components/Image/Image.stories.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import type { StoryObj, Meta } from '@storybook/react';
 
 import { disableProps } from '../../helpers';
 import { InSpacing } from '../../helpers/StoryDecorators';
 
-import { Image, ImageProps, Ratio } from '.';
+import { Image } from '.';
+import type { ImageProps, Ratio } from '.';
 
 const bases = ['div', 'img'];
 
-const propsToDisable = ['view', 'height', 'customRatio'];
-
 const ratios = ['1/1', '3/4', '4/3', '9/16', '16/9', '1/2', '2/1'];
 
-export default {
+const meta: Meta<ImageProps> = {
     title: 'Content/Image',
     component: Image,
     decorators: [InSpacing],
@@ -29,23 +28,26 @@ export default {
                 type: 'select',
             },
         },
-        ...disableProps(propsToDisable),
+        ...disableProps(['view', 'height', 'customRatio']),
     },
-} as Meta;
+};
 
-export const Default: Story<ImageProps & { ratio: Ratio }> = ({ base, ratio }) => (
-    <div style={{ maxWidth: '10rem' }}>
-        <Image
-            src="./images/320_320_9.jpg"
-            ratio={ratio}
-            base={base}
-            alt="картинка для примера фоном"
-            style={{ position: 'relative' }}
-        />
-    </div>
-);
+export default meta;
 
-Default.args = {
-    base: 'div',
-    ratio: '1/1',
+export const Default: StoryObj<ImageProps & { ratio: Ratio }> = {
+    args: {
+        base: 'div',
+        ratio: '1/1',
+    },
+    render: ({ base, ratio }) => (
+        <div style={{ maxWidth: '10rem' }}>
+            <Image
+                src="./images/320_320_9.jpg"
+                ratio={ratio}
+                base={base}
+                alt="картинка для примера фоном"
+                style={{ position: 'relative' }}
+            />
+        </div>
+    ),
 };
