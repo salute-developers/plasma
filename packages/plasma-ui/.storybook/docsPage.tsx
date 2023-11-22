79133c3ee803204d6f8e5e1f8da313075fda235b
Alexander Lobyntsev <yakutoc@gmail.com>
2023-11-22 13:26:57 +0700

    chore(plasma-ui): add decorators: Theme, Toast, Docs

---
+++ packages/plasma-ui/.storybook/docsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title, Subtitle, Description, Primary, Controls } from '@storybook/blocks';
+
+export const docsPage = () => (
+    <>
+        <Title />
+        <Subtitle />
+        <Description />
+        <Primary />
+        <Controls />
+    </>
+);
