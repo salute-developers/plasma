9c71d98f45345b99e44f9ce3a485e0118ee1dab7
Vasiliy Loginevskiy <yeti.or@gmail.com>
2023-12-01 01:45:46 +0300

    refactor(plasma-new-hope): sds: rewrite Switch

--- packages/plasma-new-hope/src/examples/sds_engineer/components/Switch/Switch.config.ts
+++ packages/plasma-new-hope/src/examples/sds_engineer/components/Switch/Switch.config.ts
@@ -1,84 +1,88 @@
 import { css } from '@linaria/core';
 
 import { switchTokens } from '../../../../components/Switch';
+import { labelPosition_After } from '../../../../components/Switch/_label-position/after';
 
 export const config = {
     defaults: {
         view: 'default',
         size: 's',
+        focused: 'true',
+        labelPosition: 'after',
     },
     variations: {
         size: {
             xs: css`
-                ${switchTokens.fontFamily}: var(--plasma-typo-body-m-font-family);
-                ${switchTokens.fontStyle}: var(--plasma-typo-body-m-font-style);
-                ${switchTokens.fontWeight}: var(--plasma-typo-caption-font-weight);
-                ${switchTokens.lineHeight}: var(--plasma-typo-body-s-line-height);
+                ${switchTokens.fontFamily}: var(--plasma-typo-body-s-font-family);
+                ${switchTokens.fontStyle}: var(--plasma-typo-body-s-font-style);
+                ${switchTokens.fontWeight}: var(--plasma-typo-body-s-bold-font-weight);
+                ${switchTokens.lineHeight}: var(--plasma-typo-body-s-bold-line-height);
                 ${switchTokens.fontSize}: var(--plasma-typo-body-s-bold-font-size);
-                ${switchTokens.letterSpacing}: 0;
-                ${switchTokens.switchTriggerWidth}: 1.75rem;
-                ${switchTokens.switchTriggerHeight}: 1rem;
-                ${switchTokens.switchEllipseSize}: 0.85rem;
-                ${switchTokens.switchEllipseMargin}: 0.05rem;
-                ${switchTokens.switchEllipseBoxShadowSize}: 0.25rem;
-                ${switchTokens.switchTriggerBorderRadius}: 0.975rem;
+                ${switchTokens.letterSpacing}: var(--plasma-typo-body-s-letter-spacing);
+                ${switchTokens.trackWidth}: 1.75rem;
+                ${switchTokens.trackHeight}: 1rem;
+                ${switchTokens.trackBorderRadius}: calc(var(${switchTokens.trackHeight}) / 2);
+                ${switchTokens.thumbSize}: 0.85rem;
+                ${switchTokens.thumbOffset}: 0.05rem;
+                ${switchTokens.thumbBorderRadius}: calc(var(${switchTokens.thumbSize}) / 2);
+                ${switchTokens.thumbPressScale}: 1.25;
+                ${switchTokens.labelOffset}: 0.5rem;
             `,
             s: css`
                 ${switchTokens.fontFamily}: var(--plasma-typo-body-m-font-family);
                 ${switchTokens.fontStyle}: var(--plasma-typo-body-m-font-style);
-                ${switchTokens.fontWeight}: var(--plasma-typo-caption-font-weight);
-                ${switchTokens.lineHeight}: var(--plasma-typo-body-m-line-height);
-                ${switchTokens.fontSize}: var(--plasma-typo-body-m-font-size);
-                ${switchTokens.letterSpacing}: 0;
-                ${switchTokens.switchTriggerWidth}: 1.75rem;
-                ${switchTokens.switchTriggerHeight}: 1rem;
-                ${switchTokens.switchEllipseSize}: 0.875rem;
-                ${switchTokens.switchEllipseMargin}: 0.05rem;
-                ${switchTokens.switchEllipseBoxShadowSize}: 0.25rem;
-                ${switchTokens.switchTriggerBorderRadius}: 0.975rem;
+                ${switchTokens.fontWeight}: var(--plasma-typo-body-m-bold-font-weight);
+                ${switchTokens.lineHeight}: var(--plasma-typo-body-m-bold-line-height);
+                ${switchTokens.fontSize}: var(--plasma-typo-body-m-bold-font-size);
+                ${switchTokens.letterSpacing}: var(--plasma-typo-body-m-letter-spacing);
+                ${switchTokens.trackWidth}: 1.75rem;
+                ${switchTokens.trackHeight}: 1rem;
+                ${switchTokens.trackBorderRadius}: calc(var(${switchTokens.trackHeight}) / 2);
+                ${switchTokens.thumbSize}: 0.875rem;
+                ${switchTokens.thumbOffset}: 0.05rem;
+                ${switchTokens.thumbBorderRadius}: calc(var(${switchTokens.thumbSize}) / 2);
+                ${switchTokens.thumbPressScale}: 1.25;
+                ${switchTokens.labelOffset}: 0.5rem;
             `,
             m: css`
                 ${switchTokens.fontFamily}: var(--plasma-typo-body-m-font-family);
                 ${switchTokens.fontStyle}: var(--plasma-typo-body-m-font-style);
-                ${switchTokens.fontWeight}: var(--plasma-typo-caption-font-weight);
-                ${switchTokens.lineHeight}: var(--plasma-typo-body-m-line-height);
-                ${switchTokens.fontSize}: var(--plasma-typo-body-m-font-size);
-                ${switchTokens.letterSpacing}: 0;
-                ${switchTokens.switchTriggerWidth}: 1.75rem;
-                ${switchTokens.switchTriggerHeight}: 1rem;
-                ${switchTokens.switchEllipseSize}: 0.875rem;
-                ${switchTokens.switchEllipseMargin}: 0.05rem;
-                ${switchTokens.switchEllipseBoxShadowSize}: 0.25rem;
-                ${switchTokens.switchTriggerBorderRadius}: 0.975rem;
+                ${switchTokens.fontWeight}: var(--plasma-typo-body-m-bold-font-weight);
+                ${switchTokens.lineHeight}: var(--plasma-typo-body-m-bold-line-height);
+                ${switchTokens.fontSize}: var(--plasma-typo-body-m-bold-font-size);
+                ${switchTokens.letterSpacing}: var(--plasma-typo-body-m-letter-spacing);
+                ${switchTokens.trackWidth}: 1.75rem;
+                ${switchTokens.trackHeight}: 1rem;
+                ${switchTokens.trackBorderRadius}: calc(var(${switchTokens.trackHeight}) / 2);
+                ${switchTokens.thumbSize}: 0.875rem;
+                ${switchTokens.thumbOffset}: 0.05rem;
+                ${switchTokens.thumbBorderRadius}: calc(var(${switchTokens.thumbSize}) / 2);
+                ${switchTokens.thumbPressScale}: 1.25;
+                ${switchTokens.labelOffset}: 0.5rem;
             `,
         },
         view: {
             default: css`
-                ${switchTokens.switchTriggerBackground}: var(--switch-bg);
-                ${switchTokens.switchTriggerBackgroundHover}: var(--switch-bg-hover);
-                ${switchTokens.switchTriggerBackgroundChecked}: var(--switch-active-bg);
-                ${switchTokens.switchTriggerBackgroundCheckedHover}: var(--switch-active-bg-hover);
-                ${switchTokens.switchEllipseBackground}: var(--switch-ellipse-bg);
-                ${switchTokens.switchEllipseBoxShadow}: 0 0 var(${switchTokens.switchEllipseBoxShadowSize}) 0 var(--switch-ellipse-shadow-off);
-                ${switchTokens.switchEllipseBoxShadowChecked}: 0 0 var(${switchTokens.switchEllipseBoxShadowSize}) 0 var(--switch-ellipse-shadow-on);
-                ${switchTokens.flexDirection}: row;
-                ${switchTokens.justifyContent}: flex-start;
-                ${switchTokens.fontColor}: var(--text-primary);
+                ${switchTokens.trackBackgroundColorOn}: rgb(71, 212, 95);
+                ${switchTokens.trackBackgroundColorOnHover}: rgb(22, 200, 62);
+                ${switchTokens.trackBackgroundColorOff}: rgba(30, 131, 198, 0.11);
+                ${switchTokens.trackBackgroundColorOffHover}: rgba(30, 131, 198, 0.25);
+                ${switchTokens.thumbBackgroundColor}: var(--on-dark-surface-solid-default);
+                ${switchTokens.thumbBoxShadowOn}: 0 0 0.25rem 0 rgba(1, 111, 0, 0.71);
+                ${switchTokens.thumbBoxShadow}: 0 0 0.25rem 0 rgba(70, 106, 251, 0.71);
             `,
+        },
+        labelPosition: {
+            after: labelPosition_After,
         },
         disabled: {
             true: css`
-                ${switchTokens.switchEllipseBoxShadowDisabled}: 0 0 var(${switchTokens.switchEllipseBoxShadowSize}) 0 var(--switch-ellipse-shadow-off-disabled);
-                ${switchTokens.switchTriggerBackgroundDisabled}: var(--surface-solid-secondary);
-                ${switchTokens.switchTriggerBackgroundCheckedDisabled}: var(--surface-solid-secondary);
-                ${switchTokens.switchTriggerBackgroundCheckedDisabledHover}: var(--surface-solid-secondary);
-                ${switchTokens.switchFontColorDisabled}: var(--text-secondary);
-                ${switchTokens.switchOpacityDisabled}: 1;
+                ${switchTokens.disabledOpacity}: 0.4;
             `,
         },
         focused: {
             true: css`
-                ${switchTokens.switchFocusColor}: var(--switch-focus-color);
+                ${switchTokens.trackFocusColor}: rgb(22, 200, 62);
             `,
         },
     },
--- packages/plasma-new-hope/src/examples/sds_engineer/components/Switch/Switch.stories.tsx
+++ packages/plasma-new-hope/src/examples/sds_engineer/components/Switch/Switch.stories.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React, { useState } from 'react';
+import { styled } from '@linaria/react';
 import { action } from '@storybook/addon-actions';
+import type { ComponentProps } from 'react';
+import type { StoryObj, Meta } from '@storybook/react';
 
 import { switchConfig } from '../../../../components/Switch';
 import { mergeConfig } from '../../../../engines';
@@ -9,38 +11,51 @@ import { WithTheme, argTypesFromConfig } from '../../../_helpers';
 import { config } from './Switch.config';
 import { Switch } from './Switch';
 
-export default {
+type SwitchProps = ComponentProps<typeof Switch>;
+
+const meta: Meta<SwitchProps> = {
     title: 'sds_engineer/Switch',
     decorators: [WithTheme],
     component: Switch,
-} as ComponentMeta<typeof Switch>;
+    argTypes: argTypesFromConfig(mergeConfig(switchConfig, config), ['view', 'focused']),
+    args: {
+        label: 'Label',
+    },
+};
 
-const mergedConfig = mergeConfig(switchConfig, config);
+export default meta;
+
+const StyledWrapper = styled.div`
+    width: 13.75rem;
+`;
 
 const onChange = action('onChange');
 const onFocus = action('onFocus');
 const onBlur = action('onBlur');
 
-export const Default: ComponentStory<typeof Switch> = ({ ...rest }) => {
-    const [checked, setChecked] = React.useState(true);
+const StoryDefault = (args: SwitchProps) => {
+    const value = 'yes';
+    const name = 'agree';
+    const [checked, setChecked] = useState(true);
+
     return (
-        <Switch
-            checked={checked}
-            onChange={(event) => {
-                setChecked(event.target.checked);
-                onChange(event);
-            }}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            {...rest}
-        />
+        <StyledWrapper>
+            <Switch
+                value={value}
+                name={name}
+                checked={checked}
+                onChange={(event) => {
+                    setChecked(event.target.checked);
+                    onChange(event);
+                }}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                {...args}
+            />
+        </StyledWrapper>
     );
 };
 
-Default.argTypes = argTypesFromConfig(mergedConfig);
-
-Default.args = {
-    label: 'Label',
-    disabled: false,
-    focused: true,
+export const Default: StoryObj<SwitchProps> = {
+    render: (args) => <StoryDefault {...args} />,
 };
