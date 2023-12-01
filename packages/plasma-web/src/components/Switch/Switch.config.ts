01d7ff75e7753755e3a448bbf2b7206c34db2c17
Vasiliy Loginevskiy <yeti.or@gmail.com>
2023-12-01 04:03:33 +0300

    feat(plasma-web): Replace  from plasma-new-hope

--- packages/plasma-web/src/components/Switch/Switch.config.ts
+++ packages/plasma-web/src/components/Switch/Switch.config.ts
@@ -3,12 +3,12 @@ import { css, switchTokens } from '@salutejs/plasma-new-hope/styled-components';
 export const config = {
     defaults: {
         view: 'default',
-        size: 's',
+        size: 'm',
         focused: 'true',
     },
     variations: {
         size: {
-            s: css`
+            m: css`
                 ${switchTokens.fontFamily}: var(--plasma-typo-body1-font-family);
                 ${switchTokens.fontStyle}: var(--plasma-typo-body1-font-style);
                 ${switchTokens.fontWeight}: var(--plasma-typo-body1-font-weight);
@@ -16,41 +16,31 @@ export const config = {
                 ${switchTokens.lineHeight}: var(--plasma-typo-body1-line-height);
                 ${switchTokens.fontSize}: var(--plasma-typo-body1-font-size);
                 ${switchTokens.letterSpacing}: var(--plasma-typo-body1-letter-spacing);
-                ${switchTokens.switchTriggerWidth}: 2.75rem;
-                ${switchTokens.switchTriggerHeight}: 1.75rem;
-                ${switchTokens.switchEllipseSize}: 1.5rem;
-                ${switchTokens.switchEllipseMargin}: 0.125rem;
-                ${switchTokens.switchEllipseBoxShadowSize}: 0.25rem;
-                ${switchTokens.switchTriggerBorderRadius}: 1rem;
+                ${switchTokens.trackWidth}: 2.75rem;
+                ${switchTokens.trackHeight}: 1.75rem;
+                ${switchTokens.trackBorderRadius}: calc(var(${switchTokens.trackHeight}) / 2);
+                ${switchTokens.thumbSize}: 1.5rem;
+                ${switchTokens.thumbBorderRadius}: calc(var(${switchTokens.thumbSize}) / 2);
+                ${switchTokens.thumbOffset}: 0.125rem;
+                ${switchTokens.thumbPressScale}: 1.25;
             `,
         },
         view: {
             default: css`
-              ${switchTokens.switchTriggerBackground}: var(--plasma-colors-tertiary);
-              ${switchTokens.switchTriggerBackgroundHover}: var(--plasma-colors-tertiary);
-              ${switchTokens.switchTriggerBackgroundChecked}: var(--plasma-colors-accent);
-              ${switchTokens.switchTriggerBackgroundCheckedHover}: var(--plasma-colors-accent);
-              ${switchTokens.switchEllipseBackground}: var(--plasma-colors-white);
-              ${switchTokens.switchEllipseBoxShadow}: 0 0 var(${switchTokens.switchEllipseBoxShadowSize}) 0 var(--switch-ellipse-shadow-off);
-              ${switchTokens.switchEllipseBoxShadowChecked}: 0 0 var(${switchTokens.switchEllipseBoxShadowSize}) 0 var(--switch-ellipse-shadow-on);
-              ${switchTokens.flexDirection}: row-reverse;
-              ${switchTokens.justifyContent}: space-between;
-              ${switchTokens.fontColor}: var(--text-primary);
+                ${switchTokens.trackBackgroundColorOn}: var(--plasma-colors-accent);
+                ${switchTokens.trackBackgroundColorOff}: var(--plasma-colors-tertiary);
+                ${switchTokens.thumbBackgroundColor}: var(--plasma-colors-white);
+                ${switchTokens.thumbBoxShadow}: 0 1px 1px rgba(0, 0, 0, 0.11);
             `,
         },
         disabled: {
             true: css`
-                ${switchTokens.switchEllipseBoxShadowDisabled}: 0 0 var(${switchTokens.switchEllipseBoxShadowSize}) 0 var(--switch-ellipse-shadow-off-disabled);
-                ${switchTokens.switchTriggerBackgroundDisabled}: var(--plasma-colors-tertiary);
-                ${switchTokens.switchTriggerBackgroundCheckedDisabled}: var(--plasma-colors-accent);
-                ${switchTokens.switchTriggerBackgroundCheckedDisabledHover}: var(--plasma-colors-accent);
-                ${switchTokens.switchFontColorDisabled}: var(--text-secondary);
-                ${switchTokens.switchOpacityDisabled}: 0.4;
+                ${switchTokens.disabledOpacity}: 0.4;
             `,
         },
         focused: {
             true: css`
-                ${switchTokens.switchFocusColor}: var(--plasma-colors-accent);
+                ${switchTokens.trackFocusColor}: var(--plasma-colors-button-focused);
             `,
         },
     },
