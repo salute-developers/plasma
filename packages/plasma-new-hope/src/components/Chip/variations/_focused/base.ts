ba6ff01b759add4d95d5ebadf5a250bf7f87128d
Krivonos Aleksandr <yzn-yfv@mail.ru>
2023-12-04 19:58:44 +0300

    feat(plasma-new-hope): replace local addFocus on general in chip

--- packages/plasma-new-hope/src/components/Chip/variations/_focused/base.ts
+++ packages/plasma-new-hope/src/components/Chip/variations/_focused/base.ts
@@ -1,6 +1,7 @@
 import { css } from '@linaria/core';
 
 import { tokens } from '../../Chip.tokens';
+import { addFocus } from '../../../../mixins';
 
 export const base = css`
     --plasma_private-chip-outline-size: var(${tokens.outlineSize}, 0.125rem);
@@ -9,21 +10,10 @@ export const base = css`
         outline: none;
     }
 
-    ::before {
-        content: '';
-        position: absolute;
-        display: block;
-        box-sizing: content-box;
-        pointer-events: none;
-
-        inset: calc(-1 * var(--plasma_private-chip-outline-size));
-        border: var(--plasma_private-chip-outline-size) solid transparent;
-        border-radius: calc(var(${tokens.borderRadius}) + var(--plasma_private-chip-outline-size));
-
-        transition: box-shadow 0.2s ease-in-out 0s;
-    }
-
-    :focus-visible::before {
-        box-shadow: 0 0 0 var(--plasma_private-chip-outline-size) var(${tokens.focusColor});
-    }
+    ${addFocus({
+        outlineOffset: 'calc(-1 * var(--plasma_private-chip-outline-size))',
+        outlineSize: 'var(--plasma_private-chip-outline-size)',
+        outlineRadius: `calc(var(${tokens.borderRadius}) + var(--plasma_private-chip-outline-size))`,
+        outlineColor: `var(${tokens.focusColor})`,
+    })}
 `;
