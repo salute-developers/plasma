de7607ef355fbd1dbfbe9bde2dafde33fe069474
Krivonos Aleksandr <yzn-yfv@mail.ru>
2023-12-08 12:08:48 +0300

    feat(plasma-new-hope): remove media-query from root

--- packages/plasma-new-hope/src/components/Header/HeaderRoot/HeaderRoot.styles.ts
+++ packages/plasma-new-hope/src/components/Header/HeaderRoot/HeaderRoot.styles.ts
@@ -1,30 +1,8 @@
 import { styled } from '@linaria/react';
 
-import { mediaQuery } from '../../../mixins';
 import { classes, tokens } from '../Header.tokens';
 
 import type { StyledHeaderRootProps } from './HeaderRoot.types';
-
-export const sizes = {
-    /* sberBox */
-    l: `
-        ${tokens.headerHeight}: 2.25rem; /* 36px */
-        ${tokens.headerPaddingTop}: 1.875rem; /* 30px */
-        ${tokens.headerPaddingBottom}: 0.875rem; /* 14px */
-    `,
-    /* sberPortal */
-    m: `
-        ${tokens.headerHeight}: 2.25rem; /* 36px */
-        ${tokens.headerPaddingTop}: 1.625rem; /* 26px */
-        ${tokens.headerPaddingBottom}: 0.625rem; /* 10px */
-    `,
-    /* mobile */
-    s: `
-        ${tokens.headerHeight}: 2.25rem; /* 36px */
-        ${tokens.headerPaddingTop}: 0.375rem; /* 6px */
-        ${tokens.headerPaddingBottom}: 0.375rem; /* 6px */
-    `,
-};
 
 export const StyledHeaderRoot = styled.header<StyledHeaderRootProps>`
     box-sizing: content-box;
@@ -47,13 +25,6 @@ export const StyledHeaderRoot = styled.header<StyledHeaderRootProps>`
             );
             background-image: linear-gradient(180deg, var(--header_private-gradient-color), rgba(0, 0, 0, 0));
         }
-    };
-
-    &.${String(classes.headerWithDefaultSizes)} {
-        ${mediaQuery('S', 1)(String(sizes.s))}
-        ${mediaQuery('M')(String(sizes.m))}
-        ${mediaQuery('L')(String(sizes.l))}
-        ${mediaQuery('XL')(String(sizes.l))}
     }
 `;
 
