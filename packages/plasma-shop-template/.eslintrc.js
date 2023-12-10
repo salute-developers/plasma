2209d18d8209c9d93427e17f4e916566bab7bc62
Neretin Artem <hawktrike@gmail.com>
2023-12-10 21:42:21 +0600

    chore(plasma-shop-temple): Remove package

--- packages/plasma-shop-template/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ['../../.eslintrc.js'],
-    rules: {
-        camelcase: 'off',
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-        'react/require-default-props': 'off',
-    },
-};
