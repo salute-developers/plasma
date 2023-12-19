bf9622890a1ca44ad7c061e2787d41db76167c56
Krivonos Aleksandr <yzn-yfv@mail.ru>
2023-12-19 10:10:45 +0300

    feat(plasma-web): add Segment component with tests

---
+++ packages/plasma-web/src/components/Segment/SegmentItem.tsx
@@ -0,0 +1,10 @@
+import { segmentItemConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
+
+import { config } from './Segment.config';
+
+const mergedSegmentItemConfig = mergeConfig(segmentItemConfig, config);
+
+/**
+ * Компонент сегмента.
+ */
+export const SegmentItem = component(mergedSegmentItemConfig);
