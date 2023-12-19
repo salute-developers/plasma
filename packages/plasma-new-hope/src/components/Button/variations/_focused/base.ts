bc3aca4c8a423c908dd3778c60e0bdaf789fdc63
Krivonos Aleksandr <yzn-yfv@mail.ru>
2023-12-19 17:21:42 +0300

    feat(plasma-new-hope): implement addFocus mixin in button

--- packages/plasma-new-hope/src/components/Button/variations/_focused/base.ts
+++ packages/plasma-new-hope/src/components/Button/variations/_focused/base.ts
@@ -1,6 +1,7 @@
 import { css } from '@linaria/core';
 
 import { tokens } from '../../Button.tokens';
+import { addFocus } from '../../../../mixins';
 
 // TODO: focus-visible #711
 
@@ -9,26 +10,10 @@ export const base = css`
         outline: none;
     }
 
-    ::before {
-        content: '';
-        position: absolute;
-        display: block;
-        box-sizing: content-box;
-        pointer-events: none;
-
-        inset: calc(-1 * var(--plasma_private-btn-outline-size));
-        border: var(--plasma_private-btn-outline-size) solid transparent;
-        border-radius: var(--plasma_private-btn-br);
-
-        /* NOTE: 
-            --plasma_private-btn-outline-size is defined in _size 
-            --plasma_private-btn-br is defined in Button.styles.ts 
-        */
-
-        transition: box-shadow 0.2s ease-in-out 0s;
-    }
-
-    :focus-visible::before {
-        box-shadow: 0 0 0 var(--plasma_private-btn-outline-size) var(${tokens.buttonFocusColor});
-    }
+    ${addFocus({
+        outlineOffset: 'calc(-1 * var(--plasma_private-btn-outline-size))',
+        outlineSize: 'var(--plasma_private-btn-outline-size)',
+        outlineRadius: 'var(--plasma_private-btn-br)',
+        outlineColor: `var(${tokens.buttonFocusColor})`,
+    })}
 `;
