4e23a1397679033786334ea919547f1a0d1bb331
Vasiliy Loginevskiy <yeti.or@gmail.com>
2023-12-21 23:49:12 +0300

    build(plasma-b2c): add css build

---
+++ packages/plasma-b2c/scripts/copy-linaria-components.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Example: Calendar
+components=$(grep -R plasma-new-hope src/components | cut -d / -f 3 | sort -u)
+
+mkdir -p src-css/components/
+touch src-css/index.ts
+touch src-css/index.d.ts
+for component in $components; do 
+    if [[ "$component" != "Switch" ]]; then
+        cp -R src/components/$component src-css/components/;
+        grep $component src/index.ts >> src-css/index.ts
+        echo "export * from '../components/$component';" >> css/index.d.ts;
+
+    fi
+done;
+
+# remove unused tests
+rm -rf src-css/components/**/*.component-test.tsx
+rm -rf src-css/components/**/*.perftest.tsx
+rm -rf src-css/components/**/*.stories.tsx
+
+# plasma-new-hope/styled-components
+files=$(find src-css/components -name '*.ts' -or -name '*.tsx');
+
+for file in $files; do
+    echo $file;
+done;
+
+# plasma-new-hope/styled-components => plasma-new-hope
+perl -p -i -e "s/\/styled-components//g" $files
+
