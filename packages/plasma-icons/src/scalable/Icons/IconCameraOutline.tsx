0274be072354ef26b56d0584fc59d9b7e93d588b
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2023-12-26 22:19:02 +0300

    feat(plasma-icons): remove old icons scalable

--- packages/plasma-icons/src/scalable/Icons/IconCameraOutline.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { IconRoot, IconProps } from '../IconRoot';
-
-export const IconCameraOutline: React.FC<IconProps> = ({ size = 's', color, className }) => {
-    return <IconRoot className={className} size={size} color={color} iconName="cameraOutline" />;
-};
--- packages/plasma-icons/src/scalable/Icons/IconDisclosureRightOutline.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { IconRoot, IconProps } from '../IconRoot';
-
-export const IconDisclosureRightOutline: React.FC<IconProps> = ({ size = 's', color, className }) => {
-    return <IconRoot className={className} size={size} color={color} iconName="disclosureRightOutline" />;
-};
