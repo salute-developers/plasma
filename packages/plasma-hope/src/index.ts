bc846575e04427f8a8d048d8c0fbf7d4d20aa9cc
Krivonos Aleksandr <yzn-yfv@mail.ru>
2023-12-26 17:13:36 +0300

    feat(plasma-hope): remove skeleton component and mixin

--- packages/plasma-hope/src/index.ts
+++ packages/plasma-hope/src/index.ts
@@ -17,7 +17,6 @@ export * from './components/Price';
 export * from './components/Progress';
 export * from './components/PreviewGallery';
 export * from './components/Select';
-export * from './components/Skeleton';
 export * from './components/SSRProvider';
 export * from './components/Switch';
 export * from './components/Slider';
