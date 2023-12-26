3a3fd63a95a373d4ff467106b063e993868a1780
kayman233 <46107741+kayman233@users.noreply.github.com>
2023-12-26 23:00:52 +0300

    feat(plasma-icons): Add icons `B`

---
+++ packages/plasma-icons/src/scalable/Icon.assets.24/Brightness0Outline.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { IconProps } from '../IconRoot';
+
+export const Brightness0Outline: React.FC<IconProps> = (props) => (
+    <svg width="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12ZM12 8.5C10.067 8.5 8.5 10.067 8.5 12C8.5 13.933 10.067 15.5 12 15.5C13.933 15.5 15.5 13.933 15.5 12C15.5 10.067 13.933 8.5 12 8.5Z"
+            fill="currentColor"
+        />
+    </svg>
+);
