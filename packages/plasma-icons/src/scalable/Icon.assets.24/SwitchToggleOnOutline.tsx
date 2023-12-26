c61fda4fb321b448ade97d9c42faa7397e7d048e
kayman233 <46107741+kayman233@users.noreply.github.com>
2023-12-26 23:24:09 +0300

    feat(plasma-icons): Add icons `C2`

---
+++ packages/plasma-icons/src/scalable/Icon.assets.24/SwitchToggleOnOutline.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { IconProps } from '../IconRoot';
+
+export const SwitchToggleOnOutline: React.FC<IconProps> = (props) => (
+    <svg width="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            d="M16 15C17.6569 15 19 13.6569 19 12C19 10.3431 17.6569 9 16 9C14.3431 9 13 10.3431 13 12C13 13.6569 14.3431 15 16 15Z"
+            fill="currentColor"
+        />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M8 6C4.68629 6 2 8.68629 2 12C2 15.3137 4.68629 18 8 18H16C19.3137 18 22 15.3137 22 12C22 8.68629 19.3137 6 16 6H8ZM3.5 12C3.5 9.51472 5.51472 7.5 8 7.5H16C18.4853 7.5 20.5 9.51472 20.5 12C20.5 14.4853 18.4853 16.5 16 16.5H8C5.51472 16.5 3.5 14.4853 3.5 12Z"
+            fill="currentColor"
+        />
+    </svg>
+);
