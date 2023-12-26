ce87abafae795061ae21edee87b009f8a41cc4cb
kayman233 <46107741+kayman233@users.noreply.github.com>
2023-12-26 23:00:52 +0300

    feat(plasma-icons): Add icons `B`

---
+++ packages/plasma-icons/src/scalable/Icons/IconBoxOutline.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { IconRoot, IconProps } from '../IconRoot';
+
+export const IconBoxOutline: React.FC<IconProps> = ({ size = 's', color, className }) => {
+    return <IconRoot className={className} size={size} color={color} iconName="boxOutline" />;
+};
