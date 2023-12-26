b8f86bb0b30430add615ccc54efd708a2a6a61e9
kayman233 <46107741+kayman233@users.noreply.github.com>
2023-12-27 00:56:46 +0300

    feat(plasma-icons): Add icons `S1`

---
+++ packages/plasma-icons/src/scalable/Icon.assets.24/SizeMaximize.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { IconProps } from '../IconRoot';
+
+export const SizeMaximize: React.FC<IconProps> = (props) => (
+    <svg width="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12 3.75C12 3.33579 12.3358 3 12.75 3H20.25C20.6642 3 21 3.33579 21 3.75V11.25C21 11.6642 20.6642 12 20.25 12C19.8358 12 19.5 11.6642 19.5 11.25V5.56066L5.56066 19.5H11.25C11.6642 19.5 12 19.8358 12 20.25C12 20.6642 11.6642 21 11.25 21H3.75C3.33579 21 3 20.6642 3 20.25V12.75C3 12.3358 3.33579 12 3.75 12C4.16421 12 4.5 12.3358 4.5 12.75V18.4393L18.4393 4.5H12.75C12.3358 4.5 12 4.16421 12 3.75Z"
+            fill="currentColor"
+        />
+    </svg>
+);
