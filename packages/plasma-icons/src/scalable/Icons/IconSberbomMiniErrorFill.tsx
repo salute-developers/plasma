8ed40fcb685f6cb1798ac8c0926f7a5a027d20dd
kayman233 <46107741+kayman233@users.noreply.github.com>
2023-12-26 23:35:40 +0300

    feat(plasma-icons): Add icons `D`

---
+++ packages/plasma-icons/src/scalable/Icons/IconSberbomMiniErrorFill.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { IconRoot, IconProps } from '../IconRoot';
+
+export const IconSberbomMiniErrorFill: React.FC<IconProps> = ({ size = 's', color, className }) => {
+    return <IconRoot className={className} size={size} color={color} iconName="sberbomMiniErrorFill" />;
+};
