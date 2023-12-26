b27d3de48cee5c04f6e8f25c90ef9e2e508329c1
kayman233 <46107741+kayman233@users.noreply.github.com>
2023-12-27 00:22:54 +0300

    feat(plasma-icons): Add icons `M2`

---
+++ packages/plasma-icons/src/scalable/Icon.assets.24/NoteFill.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { IconProps } from '../IconRoot';
+
+export const NoteFill: React.FC<IconProps> = (props) => (
+    <svg width="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M11.3295 2.12895C11.5356 1.98943 11.7975 1.96122 12.0285 2.05365L18.5856 4.67648C19.4399 5.01817 20 5.84552 20 6.76555V11C20 11.2489 19.8766 11.4815 19.6705 11.6211C19.4644 11.7606 19.2025 11.7888 18.9715 11.6964L12.5 9.10778V17.75C12.5 20.0972 10.5972 22 8.25 22C5.90279 22 4 20.0972 4 17.75C4 15.4028 5.90279 13.5 8.25 13.5C9.29875 13.5 10.2588 13.8799 11 14.5095V2.75C11 2.50114 11.1234 2.26848 11.3295 2.12895Z"
+            fill="currentColor"
+        />
+    </svg>
+);
