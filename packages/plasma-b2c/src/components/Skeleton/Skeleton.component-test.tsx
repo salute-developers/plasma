5ba3b7307e26123f2d6ae202775e745186e28b4c
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-01-09 10:58:56 +0300

    feat(plasma-b2c): add new typography sizes for Skeleton with snapshots

--- packages/plasma-b2c/src/components/Skeleton/Skeleton.component-test.tsx
+++ packages/plasma-b2c/src/components/Skeleton/Skeleton.component-test.tsx
@@ -23,6 +23,23 @@ const textSizes = [
     'button2',
     'caption',
     'underline',
+    'bodyL',
+    'bodyM',
+    'bodyS',
+    'bodyXS',
+    'bodyXXS',
+    'dsplL',
+    'dsplM',
+    'dsplS',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'textL',
+    'textM',
+    'textS',
+    'textXS',
 ];
 const roundnessKeys = Object.keys(radiuses).map((r) => Number(r));
 
