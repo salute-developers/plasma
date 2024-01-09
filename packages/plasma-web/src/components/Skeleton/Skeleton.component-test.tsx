6068a9034fba4bd6841b0e34d35e387d8e7d3258
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-01-09 11:15:10 +0300

    feat(plasma-web): new typography sizes for Skeleton with snapshots

--- packages/plasma-web/src/components/Skeleton/Skeleton.component-test.tsx
+++ packages/plasma-web/src/components/Skeleton/Skeleton.component-test.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import type { FC } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { standard as standardTypo } from '@salutejs/plasma-typo';
 import { mount, CypressTestDecorator, getComponent, PadMe, SpaceMe, withNoAnimation } from '@salutejs/plasma-cy-utils';
 
 import { radiuses } from '../..';
@@ -23,41 +26,67 @@ const textSizes = [
     'button2',
     'caption',
     'underline',
+    'bodyL',
+    'bodyM',
+    'bodyS',
+    'bodyXS',
+    'bodyXXS',
+    'dsplL',
+    'dsplM',
+    'dsplS',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'textL',
+    'textM',
+    'textS',
+    'textXS',
 ];
 const roundnessKeys = Object.keys(radiuses).map((r) => Number(r));
 
+const StandardTypoStyle = createGlobalStyle(standardTypo);
+
 describe('plasma-core: Skeleton', () => {
+    const CypressTestDecoratorWithTypo: FC = ({ children }) => (
+        <CypressTestDecorator>
+            <StandardTypoStyle />
+            {children}
+        </CypressTestDecorator>
+    );
+
     describe('LineSkeleton', () => {
         const LineSkeleton = getComponent('LineSkeleton');
         const Line = withNoAnimation(LineSkeleton);
 
         it('simple', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Line />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it('_size', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     {textSizes.map((size) => (
                         <Line key={size} size={size} />
                     ))}
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it('_roundness', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     {roundnessKeys.map((roundness) => (
                         <Line key={roundness} roundness={roundness} />
                     ))}
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
@@ -69,36 +98,36 @@ describe('plasma-core: Skeleton', () => {
 
         it('simple', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Rect width="12rem" height="8rem" />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it('_size', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Rect width="12rem" height="8rem" />
                     <SpaceMe />
                     <Rect width={128} height={128} />
                     <SpaceMe />
                     <Rect height="8rem" />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it('_roundness', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     {roundnessKeys.map((roundness) => (
                         <>
                             <Rect key={roundness} width="12rem" height="2rem" roundness={roundness} />
                             <PadMe key={roundness} />
                         </>
                     ))}
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
@@ -110,65 +139,65 @@ describe('plasma-core: Skeleton', () => {
 
         it('simple', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Text size="body1" />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it('__lines', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Text size="body1" lines={0} />
                     <Text size="body1" lines={1} />
                     <Text size="body1" lines={2} />
                     <Text size="body1" lines={3} />
                     <Text size="body1" lines={4} />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
         it('__lines:20', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Text size="body1" lines={20} />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it('_size', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     {textSizes.map((size) => (
                         <Text key={size} size={size} />
                     ))}
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it('_roundness', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     {roundnessKeys.map((roundness) => (
                         <>
                             <Text size="body1" key={roundness} roundness={roundness} />
                             <PadMe key={roundness} />
                         </>
                     ))}
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
 
         it(':width', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Text size="body1" lines={3} width={50} />
                     <Text size="body1" lines={3} width="2rem" />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
@@ -181,11 +210,11 @@ describe('plasma-core: Skeleton', () => {
 
         it('simple', () => {
             mount(
-                <CypressTestDecorator>
+                <CypressTestDecoratorWithTypo>
                     <Btn view="primary" text="Loading" skeleton />
                     <PadMe />
                     <Btn view="primary" text="Done" />
-                </CypressTestDecorator>,
+                </CypressTestDecoratorWithTypo>,
             );
             cy.matchImageSnapshot();
         });
