42494670faa4d23add51fb90a0e80a593498f784
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-01-12 13:42:25 +0300

    feat(plasma-b2c/web/core): add Badge & QuantityBadge to packages

--- packages/plasma-b2c/src/components/Badge/Badge.stories.tsx
+++ packages/plasma-b2c/src/components/Badge/Badge.stories.tsx
@@ -1,45 +1,78 @@
 import React from 'react';
-import type { StoryObj, Meta } from '@storybook/react';
-import { IconSettings } from '@salutejs/plasma-icons';
 import { disableProps, InSpacingDecorator } from '@salutejs/plasma-sb-utils';
+import type { StoryObj, Meta } from '@storybook/react';
 
-import { Badge, badgeSizes, badgeViews, BadgeSize, BadgeView } from '.';
-import type { BadgeProps } from '.';
+import { Badge, QuantityBadge } from './Badge';
 
-const sizeKeys = Object.keys(badgeSizes) as BadgeSize[];
-const viewKeys = Object.keys(badgeViews) as BadgeView[];
-
-const meta: Meta<BadgeProps> = {
+const meta: Meta<typeof Badge> = {
     title: 'Content/Badge',
-    component: Badge,
     decorators: [InSpacingDecorator],
     argTypes: {
         size: {
-            options: sizeKeys,
-            control: {
-                type: 'inline-radio',
-            },
-        },
-        view: {
-            options: viewKeys,
+            options: ['s', 'm', 'l'],
             control: {
                 type: 'select',
             },
         },
-        ...disableProps(['contentLeft', 'circled', 'theme', 'as', 'forwardedAs']),
+        view: {
+            options: ['primary', 'accent', 'positive', 'warning', 'negative', 'dark', 'light'],
+            control: {
+                type: 'select',
+            },
+        },
+        ...disableProps(['contentLeft', 'contentRight']),
     },
 };
 
 export default meta;
 
-export const Default: StoryObj<BadgeProps & { enableIcon: boolean }> = {
+type Story = StoryObj<typeof Badge>;
+type StoryQuantityBadge = StoryObj<typeof QuantityBadge>;
+
+const BellIcon = (props) => (
+    <svg width="100%" viewBox="0 0 24 24" fill="none" {...props}>
+        <path
+            d="M11.501 21.28c1.088 0 1.978-.889 1.978-1.977H9.524c0 1.088.88 1.978 1.977 1.978zm5.933-5.932v-4.944c0-3.035-1.622-5.576-4.45-6.248v-.673c0-.82-.662-1.483-1.483-1.483-.82 0-1.483.662-1.483 1.483v.672c-2.838.673-4.45 3.204-4.45 6.25v4.943l-1.275 1.276c-.623.623-.188 1.69.692 1.69h13.022c.88 0 1.325-1.067.702-1.69l-1.275-1.276z"
+            fill="currentColor"
+        />
+    </svg>
+);
+
+export const Default: Story = {
     args: {
-        text: 'Badge',
-        size: 'l',
+        text: 'Hello',
         view: 'primary',
-        enableIcon: false,
+        size: 'm',
+        pilled: false,
+        transparent: false,
     },
-    render: ({ enableIcon, ...rest }) => (
-        <Badge contentLeft={enableIcon && <IconSettings color="inherit" size="xs" />} {...rest} />
-    ),
+};
+
+export const WithIcon: Story = {
+    args: { ...Default.args },
+    render: (args) => <Badge contentLeft={<BellIcon width="1rem" height="1rem" />} {...args} />,
+};
+
+export const Quantity: StoryQuantityBadge = {
+    argTypes: {
+        size: {
+            options: ['xs', 'xxs'],
+            control: {
+                type: 'select',
+            },
+        },
+        view: {
+            options: ['default', 'accent', 'positive', 'warning', 'negative', 'dark', 'light'],
+            control: {
+                type: 'select',
+            },
+        },
+        ...disableProps(['contentLeft', 'contentRight']),
+    },
+    args: {
+        text: '123',
+        view: 'default',
+        size: 'xs',
+    },
+    render: (args) => <QuantityBadge {...args} />,
 };
