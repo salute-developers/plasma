b3deec9b7d89c199c8d2bf739b38afbe0d453cab
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-01-16 12:52:04 +0300

    feat(plasma-web/b2c/new-hope): add Image in new architecture

--- packages/plasma-new-hope/src/components/Image/Image.tsx
+++ packages/plasma-new-hope/src/components/Image/Image.tsx
@@ -3,25 +3,45 @@ import type { HTMLAttributes } from 'react';
 
 import type { RootProps } from '../../engines';
 
-import { StyledDivImg, StyledImg, StyledRoot } from './Image.styles';
+import { StyledDivImg, StyledImg, base } from './Image.styles';
 import type { ImageProps } from './Image.types';
+import { ratios } from './Image.utils';
 
 /**
  * Компонент для отображения картинок.
  */
 export const imageRoot = (Root: RootProps<HTMLDivElement, HTMLAttributes<HTMLDivElement>>) =>
     forwardRef<HTMLDivElement, ImageProps>(
-        ({ src, srcSet, sizes, ratio, customRatio, alt, width, height, base = 'img', ...rest }, outerRootRef) => {
+        (
+            { src, srcSet, sizes, ratio, customRatio, alt, width, height, base = 'img', style, ...rest },
+            outerRootRef,
+        ) => {
+            const widthToken = {
+                '--plasma_private-image-width': width || '100%',
+            };
+            const heightToken = {
+                '--plasma_private-image-height': height || '0',
+            };
             const rootCustomRatio = customRatio ? `${customRatio}%` : 'unset';
+            const ratioToken = {
+                '--plasma_private-image-pading-bottom': ratio ? `${ratios[ratio]}%` : rootCustomRatio,
+            };
 
             return (
-                <Root ref={outerRootRef}>
-                    <StyledRoot $ratio={ratio} $customRatio={rootCustomRatio} $width={width} $height={height} {...rest}>
-                        {base === 'img' && <StyledImg src={src} srcSet={srcSet} sizes={sizes} alt={alt} />}
-                        {base === 'div' && (
-                            <StyledDivImg style={{ backgroundImage: `url(${src})` }} role="img" aria-label={alt} />
-                        )}
-                    </StyledRoot>
+                <Root
+                    ref={outerRootRef}
+                    style={{
+                        ...style,
+                        ...widthToken,
+                        ...heightToken,
+                        ...ratioToken,
+                    }}
+                    {...rest}
+                >
+                    {base === 'img' && <StyledImg src={src} srcSet={srcSet} sizes={sizes} alt={alt} />}
+                    {base === 'div' && (
+                        <StyledDivImg style={{ backgroundImage: `url(${src})` }} role="img" aria-label={alt} />
+                    )}
                 </Root>
             );
         },
@@ -31,7 +51,7 @@ export const imageConfig = {
     name: 'Image',
     tag: 'div',
     layout: imageRoot,
-    base: '',
+    base,
     variations: {},
     defaults: {},
 };
--- packages/plasma-web/src/components/Image/index.tsx
+++
@@ -1,2 +0,0 @@
-export type { ImageBaseProps, Ratio, ImageProps } from '@salutejs/plasma-hope';
-export { Image } from '@salutejs/plasma-hope';
