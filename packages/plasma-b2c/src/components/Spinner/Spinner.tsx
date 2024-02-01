faa2c1fbfd1f7364f73b11f8a8b2e56a5d8e58da
Dima Shuhayeu <shuga2704@gmail.com>
2024-02-01 23:35:18 +0300

    feat(plasma-b2c, plasma-web): Wrapped Spinner up in styled only in SC build

--- packages/plasma-b2c/src/components/Spinner/Spinner.tsx
+++ packages/plasma-b2c/src/components/Spinner/Spinner.tsx
@@ -1,13 +1,15 @@
 import { spinnerConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
+import styled from 'styled-components';
+import { FunctionComponent } from 'react';
 
 import { config } from './Spinner.config';
 
 import type { SpinnerProps } from '.';
 
 const mergedConfig = mergeConfig(spinnerConfig, config);
-const SpinnerComponent = component(mergedConfig) as React.FunctionComponent<SpinnerProps>;
+const SpinnerComponent = component(mergedConfig) as FunctionComponent<SpinnerProps>;
 
 /**
  * Компонент для отображения индикатора загрузки.
  */
-export const Spinner = SpinnerComponent;
+export const Spinner = styled(SpinnerComponent)``;
