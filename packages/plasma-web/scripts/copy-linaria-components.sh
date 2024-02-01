fb1a11223312c76d9b4935aa25b082142987a9d9
Dima Shuhayeu <shuga2704@gmail.com>
2024-02-01 23:35:18 +0300

    feat(plasma-b2c, plasma-web): Wrapped Spinner up in styled only in SC build

--- packages/plasma-web/scripts/copy-linaria-components.sh
+++ packages/plasma-web/scripts/copy-linaria-components.sh
@@ -6,7 +6,7 @@ components=$(grep -R plasma-new-hope src/components | cut -d / -f 3 | sort -u)
 mkdir -p src-css/components/
 touch src-css/index.ts
 touch src-css/index.d.ts
-for component in $components; do 
+for component in $components; do
     if [[ "$component" != "Switch" ]]; then
         cp -R src/components/$component src-css/components/;
         grep $component src/index.ts >> src-css/index.ts
@@ -30,3 +30,6 @@ done;
 # plasma-new-hope/styled-components => plasma-new-hope
 perl -p -i -e "s/\/styled-components//g" $files
 
+#TODO: #1024 удалить обертку styled в спиннере
+perl -p -i -e "s/import styled from 'styled-components';//g" src-css/components/Spinner/Spinner.tsx
+perl -p -i -e "s/styled\(SpinnerComponent\)\`\`/SpinnerComponent/g" src-css/components/Spinner/Spinner.tsx
