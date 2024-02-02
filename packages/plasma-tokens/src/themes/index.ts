114f73eb0294e1b880918b1f79ef1ccf187fba09
Krivonos Aleksandr <40370966+TitanKuzmich@users.noreply.github.com>
2024-02-02 10:44:28 +0300

    feat(plasma-tokens): Add theme sdds_srvc

--- packages/plasma-tokens/src/themes/index.ts
+++ packages/plasma-tokens/src/themes/index.ts
@@ -24,6 +24,8 @@ export { sberonline__dark, darkSberonline } from './sberonline__dark';
 export { sberonline__light, lightSberonline } from './sberonline__light';
 export { sberprime__dark, darkSberprime } from './sberprime__dark';
 export { sberprime__light, lightSberprime } from './sberprime__light';
+export { sdds_srvc__dark, darkSdds_srvc } from './sdds_srvc__dark';
+export { sdds_srvc__light, lightSdds_srvc } from './sdds_srvc__light';
 export { stylesSalute__dark, darkStylesSalute } from './stylesSalute__dark';
 export { stylesSalute__light, lightStylesSalute } from './stylesSalute__light';
 export { salutejs_sber__dark, darkSber } from './salutejs_sber__dark';
