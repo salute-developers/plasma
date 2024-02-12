ad91483ffa0aed0c24d61ebecf348530bbb00940
Dima Shuhayeu <shuga2704@gmail.com>
2024-02-12 16:23:41 +0300

    feat(plasma-rsc): Added Typography

---
+++ packages/plasma-rsc/src/components/Typography/Text/Text.tsx
@@ -0,0 +1,23 @@
+import { base as sizeCSS } from '../variations/_size/base';
+import type { FontProps } from '../Typography.types';
+import { base } from '../Typography.styles';
+import { typographyRootCompose } from '../TypographyRoot';
+
+export type TextProps = FontProps;
+
+export const textRoot = typographyRootCompose();
+
+export const textConfig = {
+    name: 'Text',
+    tag: 'div',
+    layout: textRoot,
+    base,
+    variations: {
+        size: {
+            css: sizeCSS,
+        },
+    },
+    defaults: {
+        size: 'm',
+    },
+};
