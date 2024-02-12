d685f616eb32b84d6156e72773b92c8f579a87f4
Dima Shuhayeu <shuga2704@gmail.com>
2024-02-12 17:39:46 +0300

    feat(plasma-rsc): Added Spinner

--- packages/plasma-rsc/src/index.ts
+++ packages/plasma-rsc/src/index.ts
@@ -2,3 +2,4 @@ export * from './components/Button';
 export * from './components/Checkbox';
 export * from './components/Skeleton';
 export * from './components/Typography';
+export * from './components/Spinner';
