44e2383cd584b458b97262874ad44325586ab79b
Ilya Bychek <illja.by4ek@gmail.com>
2024-03-06 12:05:18 +0300

    feat(new-hope): Cell tokens and types

---
+++ packages/plasma-new-hope/src/components/Cell/Cell.tokens.ts
@@ -0,0 +1,21 @@
+export const classes = {
+    cellStretch: 'button-stretch',
+    autoStretching: 'button-stretching-auto',
+    filledStretching: 'button-stretching-filled',
+    fixedStretching: 'button-stretching-fixed',
+
+    cellTarget: 'cell-target',
+    cellContent: 'cell-content',
+    cellLeftContent: 'cell-left-content',
+    cellRightContent: 'cell-right-content',
+};
+
+export const tokens = {
+    cellColor: '--plasma-cell-color',
+    cellBackgroundColor: '--plasma-cell-background-color',
+
+    cellLeftContentHeight: '--plasma-cell-left-height',
+    cellRightContentHeight: '--plasma-cell-right-height',
+    cellHeight: '--plasma-cell-height',
+    cellWidth: '--plasma-cell-width',
+};
