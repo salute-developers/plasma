d03d8749550c1fa4700a1f9a9ec11634505bf7be
Dima Shuhayeu <shuga2704@gmail.com>
2024-03-07 11:51:29 +0300

    feat(plasma-b2c, web): Dropdown added

--- packages/plasma-new-hope/src/components/Popover/Popover.tsx
+++ packages/plasma-new-hope/src/components/Popover/Popover.tsx
@@ -20,246 +20,268 @@ export const POPOVER_PORTAL_ID = 'plasma-popover-root';
  * и вызова по клику либо ховеру.
  */
 export const popoverRoot = (Root: RootProps<HTMLDivElement, PopoverProps>) =>
-    forwardRef<HTMLDivElement, PopoverProps>(
-        (
-            {
-                target,
-                children,
-                isOpen,
-                trigger,
-                hasArrow,
-                frame = 'document',
-                className,
-                placement = 'auto',
-                offset = [0, 0],
-                zIndex,
-                isFocusTrapped = true,
-                closeOnOverlayClick = true,
-                closeOnEsc = true,
-                preventOverflow = true,
-                usePortal = false,
-                view,
-                onToggle,
-                ...rest
+    forwardRef<HTMLDivElement, PopoverProps>((props, outerRootRef) => {
+        const {
+            target,
+            children,
+            isOpen,
+            trigger = 'click',
+            hasArrow,
+            frame = 'document',
+            className,
+            placement = 'auto',
+            offset = [0, 0],
+            zIndex,
+            isFocusTrapped = true,
+            closeOnEsc = true,
+            preventOverflow = true,
+            usePortal = false,
+            view,
+            onToggle,
+            ...rest
+        } = props;
+
+        let closeOnOverlayClick = true;
+        let closeOnBeyondTargetHover = true;
+
+        if (props.trigger === 'click' && props.closeOnOverlayClick === false) {
+            closeOnOverlayClick = false;
+        }
+        if (props.trigger === 'hover' && props.closeOnBeyondTargetHover === false) {
+            closeOnBeyondTargetHover = false;
+        }
+
+        const rootRef = useRef<HTMLDivElement | null>(null);
+        const popoverRef = useRef<HTMLDivElement | null>(null);
+        const handleRef = useForkRef<HTMLDivElement>(rootRef, outerRootRef);
+        const portalRef = useRef<HTMLElement | null>(null);
+
+        const trapRef = useFocusTrap(isOpen && isFocusTrapped);
+
+        const popoverForkRef = useForkRef<HTMLDivElement>(popoverRef, trapRef);
+
+        const [arrowElement, setArrowElement] = useState<HTMLSpanElement | null>(null);
+
+        const [, forceRender] = useState(false);
+
+        const isAutoArray = Array.isArray(placement);
+        const isAuto = isAutoArray || (placement as PopoverPlacement).startsWith('auto');
+
+        const initialStyles = {
+            visibility: isOpen ? 'visible' : 'hidden',
+            opacity: isOpen ? 1 : 0,
+        } as CSSProperties;
+
+        const { styles, attributes, forceUpdate } = usePopper(rootRef.current, popoverRef.current, {
+            // TODO: #1121
+            // eslint-disable-next-line no-nested-ternary
+            placement: isAutoArray
+                ? placement[0]?.endsWith('start')
+                    ? 'auto-start'
+                    : 'auto'
+                : (placement as PopoverPlacement),
+            modifiers: [
+                {
+                    name: 'preventOverflow',
+                    options: {
+                        mainAxis: preventOverflow,
+                    },
+                },
+                { name: 'offset', options: { offset: [offset[0], offset[1]] } },
+                {
+                    name: 'flip',
+                    enabled: isAuto,
+                    options: {
+                        allowedAutoPlacements: isAutoArray ? (placement as PopoverPlacement[]) : [],
+                    },
+                },
+                {
+                    name: 'arrow',
+                    options: {
+                        element: arrowElement,
+                    },
+                },
+            ],
+        });
+
+        const onEscape = useCallback(
+            (event: KeyboardEvent) => {
+                if (isOpen && closeOnEsc && event.keyCode === ESCAPE_KEYCODE) {
+                    onToggle?.(false, event);
+                }
             },
-            outerRootRef,
-        ) => {
-            const rootRef = useRef<HTMLDivElement | null>(null);
-            const popoverRef = useRef<HTMLDivElement | null>(null);
-            const handleRef = useForkRef<HTMLDivElement>(rootRef, outerRootRef);
-            const portalRef = useRef<HTMLElement | null>(null);
+            [closeOnEsc, isOpen, onToggle],
+        );
 
-            const trapRef = useFocusTrap(isOpen && isFocusTrapped);
+        const onDocumentClick = useCallback(
+            (event: MouseEvent) => {
+                if (isOpen && closeOnOverlayClick && onToggle) {
+                    const targetIsRoot = event.target === rootRef.current;
+                    const rootHasTarget = rootRef.current?.contains(event.target as Element);
 
-            const popoverForkRef = useForkRef<HTMLDivElement>(popoverRef, trapRef);
-
-            const [arrowElement, setArrowElement] = useState<HTMLSpanElement | null>(null);
-
-            const [, forceRender] = useState(false);
-
-            const isAutoArray = Array.isArray(placement);
-            const isAuto = isAutoArray || (placement as PopoverPlacement).startsWith('auto');
-
-            const initialStyles = {
-                visibility: isOpen ? 'visible' : 'hidden',
-                opacity: isOpen ? 1 : 0,
-            } as CSSProperties;
-
-            const { styles, attributes, forceUpdate } = usePopper(rootRef.current, popoverRef.current, {
-                placement: isAutoArray ? 'auto' : (placement as PopoverPlacement),
-                modifiers: [
-                    {
-                        name: 'preventOverflow',
-                        options: {
-                            mainAxis: preventOverflow,
-                        },
-                    },
-                    { name: 'offset', options: { offset: [offset[0], offset[1]] } },
-                    {
-                        name: 'flip',
-                        enabled: isAuto,
-                        options: {
-                            allowedAutoPlacements: isAutoArray ? (placement as PopoverPlacement[]) : [],
-                        },
-                    },
-                    {
-                        name: 'arrow',
-                        options: {
-                            element: arrowElement,
-                        },
-                    },
-                ],
-            });
-
-            const onEscape = useCallback(
-                (event: KeyboardEvent) => {
-                    if (isOpen && closeOnEsc && event.keyCode === ESCAPE_KEYCODE) {
-                        onToggle?.(false, event);
-                    }
-                },
-                [closeOnEsc, isOpen, onToggle],
-            );
-
-            const onDocumentClick = useCallback(
-                (event: MouseEvent) => {
-                    if (isOpen && closeOnOverlayClick && onToggle) {
-                        const targetIsRoot = event.target === rootRef.current;
-                        const rootHasTarget = rootRef.current?.contains(event.target as Element);
-
-                        if (!targetIsRoot && !rootHasTarget) {
-                            onToggle(false, event);
-                        }
-                    }
-                },
-                [closeOnOverlayClick, isOpen, onToggle],
-            );
-
-            const onClick = useCallback<React.MouseEventHandler>(
-                (event) => {
-                    if (trigger === 'click') {
-                        const targetIsPopover = event.target === popoverRef.current;
-                        const rootHasTarget = popoverRef.current?.contains(event.target as Element);
-
-                        if (!targetIsPopover && !rootHasTarget) {
-                            onToggle?.(!isOpen, event);
-                        }
-                    }
-                },
-                [trigger, isOpen, onToggle],
-            );
-
-            const onMouseEnter = useCallback<React.MouseEventHandler>(
-                (event) => {
-                    if (trigger === 'hover') {
-                        onToggle?.(true, event);
-                    }
-                },
-                [trigger, onToggle],
-            );
-
-            const onMouseLeave = useCallback<React.MouseEventHandler>(
-                (event) => {
-                    if (trigger === 'hover') {
-                        onToggle?.(false, event);
-                    }
-                },
-                [trigger, onToggle],
-            );
-
-            const onFocus = useCallback<React.FocusEventHandler>(
-                (event) => {
-                    if (trigger === 'hover') {
-                        onToggle?.(true, event);
-                    }
-                },
-                [trigger, onToggle],
-            );
-
-            const onBlur = useCallback<React.FocusEventHandler>(
-                (event) => {
-                    if (trigger === 'hover') {
-                        onToggle?.(false, event);
-                    }
-                },
-                [trigger, onToggle],
-            );
-
-            useEffect(() => {
-                document.addEventListener('click', onDocumentClick);
-                return () => document.removeEventListener('click', onDocumentClick);
-            }, [closeOnOverlayClick, isOpen, onToggle]);
-
-            useEffect(() => {
-                window.addEventListener('keydown', onEscape);
-                return () => window.removeEventListener('keydown', onEscape);
-            }, [closeOnEsc, isOpen, onToggle]);
-
-            useEffect(() => {
-                let portal = document.getElementById(POPOVER_PORTAL_ID);
-
-                if (typeof frame !== 'string' && frame && frame.current) {
-                    portal = frame.current;
-                }
-
-                if (!usePortal) {
-                    portal = rootRef.current;
-                }
-
-                if (!portal) {
-                    portal = document.createElement('div');
-                    portal.setAttribute('id', POPOVER_PORTAL_ID);
-
-                    if (typeof frame === 'string' && frame !== 'document') {
-                        document.getElementById(frame)?.appendChild(portal);
-                    } else {
-                        document.body.appendChild(portal);
+                    if (!targetIsRoot && !rootHasTarget) {
+                        onToggle(false, event);
                     }
                 }
+            },
+            [closeOnOverlayClick, isOpen, onToggle],
+        );
 
-                portalRef.current = portal;
+        const onClick = useCallback<React.MouseEventHandler>(
+            (event) => {
+                if (trigger === 'click') {
+                    const targetIsPopover = event.target === popoverRef.current;
+                    const rootHasTarget = popoverRef.current?.contains(event.target as Element);
 
-                /**
-                 * Изменение стейта нужно для того, чтобы Popup
-                 * отобразился после записи DOM элемента в portalRef.current
-                 */
-                forceRender(true);
-            }, []);
-
-            useEffect(() => {
-                if (!isOpen || !forceUpdate) {
-                    return;
+                    if (!targetIsPopover && !rootHasTarget) {
+                        onToggle?.(!isOpen, event);
+                    }
                 }
+            },
+            [trigger, isOpen, onToggle],
+        );
 
-                /*
-                 * INFO: Метод forceUpdate содержит в себе flushSync и приводит
-                 * к повторному рендеру компонента, который уже находится в процессе рендера.
-                 * Данный хак, нужен для того, чтобы это поведение избежать и перенаправить
-                 * вызов метода в очередь микрозадач.
-                 */
-                Promise.resolve().then(forceUpdate);
-            }, [isOpen, children, forceUpdate]);
+        const onMouseEnter = useCallback<React.MouseEventHandler>(
+            (event) => {
+                if (trigger === 'hover') {
+                    onToggle?.(true, event);
+                }
+            },
+            [trigger, onToggle],
+        );
 
-            return (
-                <StyledWrapper className={classes.wrapper}>
-                    <StyledRoot
-                        ref={handleRef}
-                        onClick={onClick}
-                        onMouseEnter={onMouseEnter}
-                        onMouseLeave={onMouseLeave}
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                        className={cx(className, classes.target)}
-                    >
-                        {target}
-                    </StyledRoot>
-                    {children &&
-                        portalRef.current &&
-                        ReactDOM.createPortal(
-                            <Root view={view} className={className} {...rest}>
-                                <StyledPopover
-                                    {...attributes.popper}
-                                    className={classes.root}
-                                    ref={popoverForkRef}
-                                    style={{ ...styles.popper, ...initialStyles }}
-                                    zIndex={zIndex}
-                                >
-                                    {hasArrow && (
-                                        <StyledArrow
-                                            className={classes.arrow}
-                                            ref={setArrowElement}
-                                            style={styles.arrow}
-                                            {...attributes.arrow}
-                                        />
-                                    )}
-                                    {children}
-                                </StyledPopover>
-                            </Root>,
-                            portalRef.current,
-                        )}
-                </StyledWrapper>
-            );
-        },
-    );
+        const onMouseLeave = useCallback<React.MouseEventHandler>(
+            (event) => {
+                if (trigger === 'hover') {
+                    onToggle?.(false, event);
+                }
+            },
+            [trigger, onToggle],
+        );
+
+        const onFocus = useCallback<React.FocusEventHandler>(
+            (event) => {
+                if (trigger === 'hover') {
+                    onToggle?.(true, event);
+                }
+            },
+            [trigger, onToggle],
+        );
+
+        const onBlur = useCallback<React.FocusEventHandler>(
+            (event) => {
+                if (trigger === 'hover') {
+                    onToggle?.(false, event);
+                }
+            },
+            [trigger, onToggle],
+        );
+
+        useEffect(() => {
+            document.addEventListener('click', onDocumentClick);
+            return () => document.removeEventListener('click', onDocumentClick);
+        }, [closeOnOverlayClick, isOpen, onToggle]);
+
+        useEffect(() => {
+            window.addEventListener('keydown', onEscape);
+            return () => window.removeEventListener('keydown', onEscape);
+        }, [closeOnEsc, isOpen, onToggle]);
+
+        useEffect(() => {
+            let portal = document.getElementById(POPOVER_PORTAL_ID);
+
+            if (typeof frame !== 'string' && frame && frame.current) {
+                portal = frame.current;
+            }
+
+            if (!usePortal) {
+                portal = rootRef.current;
+            }
+
+            if (!portal) {
+                portal = document.createElement('div');
+                portal.setAttribute('id', POPOVER_PORTAL_ID);
+
+                if (typeof frame === 'string' && frame !== 'document') {
+                    document.getElementById(frame)?.appendChild(portal);
+                } else {
+                    document.body.appendChild(portal);
+                }
+            }
+
+            portalRef.current = portal;
+
+            /**
+             * Изменение стейта нужно для того, чтобы Popup
+             * отобразился после записи DOM элемента в portalRef.current
+             */
+            forceRender(true);
+        }, []);
+
+        useEffect(() => {
+            if (!isOpen || !forceUpdate) {
+                return;
+            }
+
+            /*
+             * INFO: Метод forceUpdate содержит в себе flushSync и приводит
+             * к повторному рендеру компонента, который уже находится в процессе рендера.
+             * Данный хак, нужен для того, чтобы это поведение избежать и перенаправить
+             * вызов метода в очередь микрозадач.
+             */
+            Promise.resolve().then(forceUpdate);
+        }, [isOpen, children, forceUpdate]);
+
+        return (
+            <StyledWrapper className={classes.wrapper}>
+                <StyledRoot
+                    ref={handleRef}
+                    onClick={onClick}
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    className={cx(className, classes.target)}
+                >
+                    {target}
+                </StyledRoot>
+                {children &&
+                    portalRef.current &&
+                    ReactDOM.createPortal(
+                        <Root
+                            view={view}
+                            className={className}
+                            {...rest}
+                            onMouseEnter={closeOnBeyondTargetHover ? undefined : onMouseEnter}
+                            onMouseLeave={closeOnBeyondTargetHover ? undefined : onMouseEnter}
+                        >
+                            <StyledPopover
+                                {...attributes.popper}
+                                className={classes.root}
+                                ref={popoverForkRef}
+                                style={{
+                                    ...styles.popper,
+                                    ...{ display: isOpen ? 'block' : 'none' },
+                                    ...initialStyles,
+                                }}
+                                zIndex={zIndex}
+                            >
+                                {hasArrow && (
+                                    <StyledArrow
+                                        className={classes.arrow}
+                                        ref={setArrowElement}
+                                        style={styles.arrow}
+                                        {...attributes.arrow}
+                                    />
+                                )}
+                                {children}
+                            </StyledPopover>
+                        </Root>,
+                        portalRef.current,
+                    )}
+            </StyledWrapper>
+        );
+    });
 
 export const popoverConfig = {
     name: 'Popover',
---
+++ packages/plasma-web/src/components/Dropdown/components/DropdownItem.tsx
@@ -0,0 +1,62 @@
+import React, { forwardRef, SyntheticEvent } from 'react';
+import { DropdownItemProps } from '@salutejs/plasma-hope';
+import { component, dropdownItemConfig, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
+import styled from 'styled-components';
+import { IconDone } from '@salutejs/plasma-icons';
+import { accent } from '@salutejs/plasma-core';
+
+const mergedItemConfig = mergeConfig(dropdownItemConfig);
+export const DropdownNewHopeItem = component(mergedItemConfig);
+
+const StyledCheck = styled(IconDone)`
+    margin-left: auto;
+    pointer-events: none;
+    margin-right: -0.5rem;
+`;
+
+export const DropdownItem = forwardRef<HTMLDivElement, DropdownItemProps>((props, ref) => {
+    const {
+        value,
+        label,
+        color,
+        contentLeft,
+        isActive,
+        isHovered,
+        index,
+        onClick,
+        onHover,
+        onFocus,
+        items,
+        isDisabled,
+        style,
+        ...rest
+    } = props;
+
+    const handleClick = (e: SyntheticEvent<Element, Event>) => {
+        if (onClick) {
+            onClick({ value, label, color, contentLeft }, e);
+        }
+    };
+
+    const handleHover = () => {
+        if (onHover) {
+            onHover(index);
+        }
+    };
+
+    return (
+        <DropdownNewHopeItem
+            {...(rest as any)}
+            value={value}
+            text={label}
+            ref={ref}
+            style={{ ...style, color }}
+            contentLeft={contentLeft}
+            contentRight={isActive ? <StyledCheck size="s" color={accent} /> : undefined}
+            onClick={handleClick}
+            onMouseOver={handleHover}
+            onFocus={onFocus}
+            disabled={isDisabled}
+        />
+    );
+});
