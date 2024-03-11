4be572887240d5423099405072c2805d30306e56
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-03-11 20:56:49 +0300

    feat(sdds-serv): add grid component

--- packages/sdds-serv/src/index.ts
+++ packages/sdds-serv/src/index.ts
@@ -4,6 +4,7 @@ export * from './components/Badge';
 export * from './components/Button';
 export * from './components/ButtonGroup';
 export * from './components/Drawer';
+export * from './components/Grid';
 export * from './components/Checkbox';
 export * from './components/Combobox';
 export * from './components/Counter';
