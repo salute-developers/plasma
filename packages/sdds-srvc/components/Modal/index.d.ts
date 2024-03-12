9aa749f3e1312f558b9bbcb2800cc44a2285eb0e
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-03-12 12:45:39 +0300

    chore: remove sdds-srvc dir

--- packages/sdds-srvc/components/Modal/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Modal } from './Modal';
-export { modalClasses } from '@salutejs/plasma-new-hope/styled-components';
-export type { ModalProps } from '@salutejs/plasma-new-hope/styled-components';
--- packages/sdds-srvc/es/components/Radiobox/Radiobox.js
+++
@@ -1,9 +0,0 @@
-import { radioboxConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
-import { config } from './Radiobox.config';
-var mergedConfig = /*#__PURE__*/mergeConfig(radioboxConfig, config);
-var RadioboxComponent = /*#__PURE__*/component(mergedConfig);
-/**
- * Переключатель, или *радиокнопка*.
- */
-export var Radiobox = RadioboxComponent;
-export { RadioGroup } from '@salutejs/plasma-new-hope/styled-components';
