7299bca8a3ff9d3780215234cebe7c9948f27a03
Neretin Artem <hawktrike@gmail.com>
2024-03-15 18:25:16 +0500

    feat(plasma-theme-builder): Update tokens creators for `overlay` group

--- website/plasma-theme-builder/src/builder/themeTokenGetters/overlay/getOverlayHardTokens.ts
+++ website/plasma-theme-builder/src/builder/themeTokenGetters/overlay/getOverlayHardTokens.ts
@@ -1,54 +1,29 @@
-import { alphenColor } from '@salutejs/plasma-tokens-utils';
-import type { TokensByType } from '@salutejs/plasma-tokens-utils';
+import type { ThemeConfig, TokensByType } from '@salutejs/plasma-tokens-utils';
 
-import { baseColors } from '../../../types';
+import { getGreyTokenData, tokensCreator } from '../../../utils';
 
-const comment: Record<keyof TokensByType, string> = {
-    default: 'Цвет фона паранжи темный',
-    onDark: 'Цвет фона паранжи темный на темном фоне',
-    onLight: 'Цвет фона паранжи темный на светлом фоне',
-    inverse: 'Инвертированный цвет фона паранжи темный',
-};
+export const getOverlayHardTokens = (config: ThemeConfig) => {
+    const { grayscale } = config;
 
-export const getOverlayHardTokens = () => {
-    const value = alphenColor(baseColors.black.value, -0.1);
+    const opacityValue = 0.96;
 
-    return {
-        dark: {
-            default: {
-                value,
-                comment: comment.default,
-            },
-            onDark: {
-                value,
-                comment: comment.onDark,
-            },
-            onLight: {
-                value,
-                comment: comment.onLight,
-            },
-            inverse: {
-                value,
-                comment: comment.inverse,
-            },
-        },
-        light: {
-            default: {
-                value,
-                comment: comment.default,
-            },
-            onDark: {
-                value,
-                comment: comment.onDark,
-            },
-            onLight: {
-                value,
-                comment: comment.onLight,
-            },
-            inverse: {
-                value,
-                comment: comment.inverse,
-            },
-        },
+    const darkValue = getGreyTokenData({
+        saturation: 1000,
+        grayscale: grayscale.dark,
+        opacity: opacityValue,
+    });
+    const lightValue = getGreyTokenData({
+        saturation: 50,
+        grayscale: grayscale.light,
+        opacity: opacityValue,
+    });
+
+    const comment: Record<keyof TokensByType, string> = {
+        default: 'Цвет фона паранжи темный',
+        onDark: 'Цвет фона паранжи темный на темном фоне',
+        onLight: 'Цвет фона паранжи темный на светлом фоне',
+        inverse: 'Инвертированный цвет фона паранжи темный',
     };
+
+    return tokensCreator({ darkValue, lightValue, comment });
 };
