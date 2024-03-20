1e2d26a4d250462b7034ea39a8eed2e678caefc7
Neretin Artem <hawktrike@gmail.com>
2024-03-20 22:12:38 +0500

    feat(plasma-theme-builder): Add formulas for calcultae tokens state value

---
+++ website/plasma-theme-builder/src/types/formulas.ts
@@ -0,0 +1,48 @@
+interface Operation {
+    hover: (value: number) => number;
+    active: (value: number) => number;
+}
+
+interface ConditionHSL {
+    lightness: [number, number];
+    hue: [number, number];
+    saturation: [number, number];
+}
+
+interface ConditionAlpha {
+    alpha: [number, number];
+}
+
+interface SolidCondition {
+    condition: ConditionHSL;
+    operation: Operation;
+}
+
+interface TransparentCondition {
+    condition: ConditionAlpha;
+    operation: Operation;
+}
+
+interface SolidMode {
+    light: Array<SolidCondition>;
+    dark: Array<SolidCondition>;
+}
+
+interface TransparentMode {
+    light: Array<TransparentCondition>;
+    dark: Array<TransparentCondition>;
+}
+
+interface OpacityMode {
+    solid: SolidMode;
+    transparent: TransparentMode;
+}
+
+export interface Formula {
+    stroke: OpacityMode;
+    fill: OpacityMode;
+}
+
+export type FormulaMode = keyof Formula;
+
+export type OperationKind = keyof Operation;
