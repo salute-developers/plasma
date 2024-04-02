c48d054ef5bae6947ee1f39077590f5ec81f386c
Alexander Lobyntsev <yakutoc@gmail.com>
2024-04-02 14:38:49 +0700

    chore: delete unnecessary package "Caldera"

--- packages/caldera/.storybook/decoratorToast.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { Decorator } from '@storybook/react';
-import { ToastProvider } from '@salutejs/plasma-core';
-
-export const withToast: Decorator = (Story) => (
-    <ToastProvider>
-        <Story />
-    </ToastProvider>
-);
--- packages/caldera/src/components/SSRProvider/index.ts
+++
@@ -1,1 +0,0 @@
-export { SSRProvider } from '@salutejs/plasma-new-hope/styled-components';
