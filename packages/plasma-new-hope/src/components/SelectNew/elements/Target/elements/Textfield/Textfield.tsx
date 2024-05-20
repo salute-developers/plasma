4ae4ea8f9b65fe8df4e3b73a1d06b7858a9a1aa0
Dima Shugaev <shuga2704@gmail.com>
2024-05-20 22:32:32 +0300

    feat(plasma-new-hope): part 12

--- packages/plasma-new-hope/src/components/SelectNew/elements/Target/elements/Textfield/Textfield.tsx
+++ packages/plasma-new-hope/src/components/SelectNew/elements/Target/elements/Textfield/Textfield.tsx
@@ -6,9 +6,9 @@ import { classes } from '../../../../SelectNew.tokens';
 import { Chip } from './elements/Chip/Chip';
 import { StyledButton, StyledArrow, Wrapper, ChipWrapper, TextfieldWrapper } from './Textfield.styles';
 
-const getLabel = (value, targetView, multiselect, valueToItemMap) => {
+const getLabel = (value, targetView, multiselect, valueToItemMap, onChipClick) => {
     if (!value || !value.length) {
-        return <Chip text="Ничего не выбрано" />;
+        return <Chip text="Ничего не выбрано" onClick={(e) => e.stopPropagation()} />;
     }
 
     if (!multiselect) {
@@ -17,25 +17,31 @@ const getLabel = (value, targetView, multiselect, valueToItemMap) => {
 
     switch (targetView) {
         case 'amount': {
-            return <Chip text={`Выбрано: ${value.length}`} />;
+            return <Chip text={`Выбрано: ${value.length}`} onClick={(e) => e.stopPropagation()} />;
         }
 
         default: {
             return value.map((value) => (
-                <Chip text={valueToItemMap.get(value)?.secondaryLabel || valueToItemMap.get(value)?.label} />
+                <Chip
+                    text={valueToItemMap.get(value)?.secondaryLabel || valueToItemMap.get(value)?.label}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        onChipClick(value);
+                    }}
+                />
             ));
         }
     }
 };
 
-export const Textfield: React.FC<any> = ({ opened, value, targetView, multiselect, valueToItemMap }) => {
+export const Textfield: React.FC<any> = ({ opened, value, targetView, multiselect, valueToItemMap, onChipClick }) => {
     const withArrowInverse = opened ? classes.arrowInverse : undefined;
 
     return (
         <TextfieldWrapper opened={opened}>
             <StyledButton stretching="filled" className={classes.textfieldTarget}>
                 <Wrapper>
-                    <ChipWrapper>{getLabel(value, targetView, multiselect, valueToItemMap)}</ChipWrapper>
+                    <ChipWrapper>{getLabel(value, targetView, multiselect, valueToItemMap, onChipClick)}</ChipWrapper>
 
                     <StyledArrow size="s" color="inherit" className={cx(classes.selectTargetArrow, withArrowInverse)} />
                 </Wrapper>
