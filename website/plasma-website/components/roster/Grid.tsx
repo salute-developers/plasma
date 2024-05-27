e1e9604061a07bcc6587c91786ca8e9acc51f7ac
Alexander Lobyntsev <yakutoc@gmail.com>
2024-05-27 19:09:16 +0700

    chore(website): refactoring Grid element

--- website/plasma-website/components/roster/Grid.tsx
+++ website/plasma-website/components/roster/Grid.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Grid = styled.div`
     display: grid;
-    grid-template-columns: repeat(8, calc(12.5% - 0.5rem + 0.5rem / 8));
-    gap: 0.5rem;
+    grid-template-columns: repeat(auto-fit, 3.75rem);
+    gap: 0.25rem;
+
+    box-sizing: border-box;
 `;
