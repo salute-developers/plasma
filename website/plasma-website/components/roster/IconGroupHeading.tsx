b01a91a90ac84786da79618c1a1408bb6c16b574
Alexander Lobyntsev <yakutoc@gmail.com>
2024-05-27 19:04:53 +0700

    chore(website): add IconGroupHeading element

---
+++ website/plasma-website/components/roster/IconGroupHeading.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import type { IconGroup } from '../../types';
+
+type IconGroupHeadingProps = IconGroup & { count: number };
+
+const StyledIconGroupHeading = styled.div`
+    display: flex;
+    column-gap: 0.5rem;
+    margin-bottom: 1rem;
+`;
+
+const StyledCommonTitle = styled.span`
+    font-size: 0.75rem;
+    line-height: 0.875rem;
+`;
+
+const StyledTitle = styled(StyledCommonTitle)`
+    color: rgba(255, 255, 255, 0.56);
+`;
+
+const StyledSubtitle = styled(StyledCommonTitle)`
+    color: rgba(255, 255, 255, 0.28);
+`;
+
+export const IconGroupHeading = ({ title, subtitle, count }: IconGroupHeadingProps) => {
+    return (
+        <StyledIconGroupHeading>
+            <StyledTitle>{title}</StyledTitle>
+            <StyledSubtitle>{subtitle}</StyledSubtitle>
+            <StyledSubtitle>{count}</StyledSubtitle>
+        </StyledIconGroupHeading>
+    );
+};
