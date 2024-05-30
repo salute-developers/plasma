ae4275d917c42c2ed7b108e9318369af2660f101
Alexander Lobyntsev <yakutoc@gmail.com>
2024-05-30 12:38:56 +0700

    chore(website): add IconClipboard element

--- website/plasma-website/components/roster/IconClipboard.tsx
+++ website/plasma-website/components/roster/IconClipboard.tsx
@@ -2,13 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 import { IconCopyOutline } from '@salutejs/plasma-icons';
 
-const StyledIconClipboard = styled.div`
+const StyledClipboardIcon = styled(IconCopyOutline)`
     margin-left: -1.375rem;
     margin-right: 0.313rem;
     color: rgba(255, 255, 255, 0.28);
+    visibility: hidden;
+    opacity: 0;
+
+    transition: visibility 120ms ease-in, opacity 120ms ease-in;
 `;
 
-export const IconClipboard = ({ source }: { source: string }) => {
+const StyledClipboard = styled.div`
+    display: inline-flex;
+    flex-direction: column;
+    row-gap: 0.5rem;
+
+    cursor: pointer;
+
+    &:hover {
+        ${StyledClipboardIcon} {
+            visibility: visible;
+            opacity: 1;
+        }
+    }
+
+    &:last-child {
+        margin-top: 1.25rem;
+    }
+`;
+
+const StyledHeading = styled.span`
+    font-size: 0.75rem;
+    line-height: 0.875rem;
+    color: rgba(255, 255, 255, 0.8);
+`;
+
+const StyledTitle = styled(StyledHeading)`
+    color: rgba(255, 255, 255, 0.28);
+`;
+
+const StyledSource = styled.div`
+    display: inline-flex;
+`;
+
+export const IconClipboard = ({ source, title }: { source: string; title: string }) => {
     const copyToClipboard = async () => {
         try {
             await navigator.clipboard.writeText(source);
@@ -19,8 +56,12 @@ export const IconClipboard = ({ source }: { source: string }) => {
     };
 
     return (
-        <StyledIconClipboard onClick={copyToClipboard}>
-            <IconCopyOutline size="xs" color="inherit" />
-        </StyledIconClipboard>
+        <StyledClipboard onClick={copyToClipboard}>
+            <StyledTitle>{title}</StyledTitle>
+            <StyledSource>
+                <StyledClipboardIcon size="xs" color="inherit" />
+                <StyledHeading>{source}</StyledHeading>
+            </StyledSource>
+        </StyledClipboard>
     );
 };
