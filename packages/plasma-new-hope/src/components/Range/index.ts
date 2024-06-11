ee6e1213b880f2111fcf08d103291542ac05623a
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-06-11 16:11:51 +0300

    feat(plasma-new-hope): fix Range LeftContent margin

--- packages/plasma-new-hope/src/components/Range/index.ts
+++ packages/plasma-new-hope/src/components/Range/index.ts
@@ -1,3 +1,3 @@
 export { rangeRoot, rangeConfig } from './Range';
 export { tokens as rangeTokens } from './Range.tokens';
-export type { RangeProps } from './Range.types';
+export type { RangeProps, RangeInputRefs } from './Range.types';
