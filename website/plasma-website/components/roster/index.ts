6396bb5d0b14e22a91a7af68a751ba7ca1968f76
Alexander Lobyntsev <yakutoc@gmail.com>
2024-06-13 13:56:10 +0700

    chore(plasma-website): delete unnecessary code

--- website/plasma-website/components/roster/index.ts
+++ website/plasma-website/components/roster/index.ts
@@ -1,6 +1,5 @@
 export { FlatButton } from './FlatButton';
 export { Header } from './Header';
-export { IconForm } from './IconForm';
 export { IconsList } from './IconsList';
 export { Main } from './Main';
 export { Panel } from './Panel';
