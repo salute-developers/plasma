47fde46aa494c3af06271a8a628bef0adb95309b
Ilya Bychek <illja.by4ek@gmail.com>
2024-06-27 20:30:10 +0300

    feat(website): add animations

--- website/plasma-website/components/colors/GradientScroll.tsx
+++ website/plasma-website/components/colors/GradientScroll.tsx
@@ -1,50 +1,51 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
+const GradientScrollContainer = styled.div`
     position: fixed;
     top: 7rem;
     height: calc(100vh - 200px);
     left: 0;
     width: 4rem;
     padding-left: 1.875rem;
-    box-sizing : border-box;
+    box-sizing: border-box;
     display: flex;
     align-items: center;
     z-index: 3;
 `;
 
-const ContainerScroll  = styled.div`
+const GradientScrollContainerScroll = styled.div`
     width: 2.125rem;
     height: 100%;
     position: relative;
 `;
 
-const AllScroll = styled.div<{colors: string[]}>`
-    background: white;
+const GradientScrollHeight = styled.div<{ colors: string[] }>`
     width: 0.25rem;
     height: 100%;
     position: absolute;
     top: 0;
     left: 0;
     border-radius: 0.125rem;
+    /* stylelint-disable */
     background: linear-gradient(
         180deg,
-        ${({colors}) => colors.map((color: string, index: number)  => `${color} ${(100 / colors.length) * index}%`).join(',')}
+        ${({ colors }) =>
+            colors.map((color: string, index: number) => `${color} ${(100 / colors.length) * index}%`).join(',')}
     );
+    /* stylelint-enable */
 `;
 
-const Scroll = styled.div<{height: number, top: number}>`
-    background: rgba(255,255,255, 0.56);
+const GradientScrollScroll = styled.div<{ height: number; top: number }>`
+    background: rgba(255, 255, 255, 0.56);
     width: 0.125rem;
-    height: ${({height}) => height}px;
+    height: ${({ height }) => height}px;
     position: absolute;
-    top: ${({top}) => top}px;
+    top: ${({ top }) => top}px;
     right: 0;
     border-radius: 0.125rem;
 `;
-
-const TopText = styled.div`
+const GradientScrollTopText = styled.div`
     top: -2rem;
     font-size: 0.75rem;
     left: 100%;
@@ -52,7 +53,7 @@ const TopText = styled.div`
     opacity: 0.8;
 `;
 
-const BottomText = styled.div`
+const GradientScrollBottomText = styled.div`
     bottom: -2rem;
     font-size: 0.75rem;
     left: 100%;
@@ -60,23 +61,25 @@ const BottomText = styled.div`
     opacity: 0.8;
 `;
 
-export const GradientScroll: React.FC<{ hsl: number[], colors: string[], scrollHeight: number, scrollTop: number, height: number }> = ({hsl, colors, scrollHeight, scrollTop, height}) => {
+export const GradientScroll: React.FC<{
+    hsl: number[];
+    colors: string[];
+    scrollHeight: number;
+    scrollTop: number;
+    height: number;
+}> = ({ hsl, colors, scrollHeight, scrollTop, height }) => {
     const heightAllScroll = height - 200;
     const heightScroll = heightAllScroll / (scrollHeight / height);
     const topScroll = heightAllScroll * (scrollTop / scrollHeight);
 
     return (
-        <Container>
-            <ContainerScroll>
-                <TopText>
-                    H:{hsl[0]}
-                </TopText>
-                <AllScroll colors={colors} />
-                <Scroll top={topScroll} height={heightScroll} />
-                <BottomText>
-                    H:{hsl[hsl.length - 1]}
-                </BottomText>
-            </ContainerScroll>
-        </Container>
+        <GradientScrollContainer>
+            <GradientScrollContainerScroll>
+                <GradientScrollTopText>H:{hsl[0]}</GradientScrollTopText>
+                <GradientScrollHeight colors={colors} />
+                <GradientScrollScroll top={topScroll} height={heightScroll} />
+                <GradientScrollBottomText>H:{hsl[hsl.length - 1]}</GradientScrollBottomText>
+            </GradientScrollContainerScroll>
+        </GradientScrollContainer>
     );
 };
