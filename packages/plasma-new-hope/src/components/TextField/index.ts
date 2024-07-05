24b47e59d2c89cb8e49eb704b41b21046e2984f4
Krivonos Aleksandr <yzn-yfv@mail.ru>
2024-07-05 11:28:54 +0300

    feat(plasma-new-hope): refactor textfield label placement

--- packages/plasma-new-hope/src/components/TextField/index.ts
+++ packages/plasma-new-hope/src/components/TextField/index.ts
@@ -1,6 +1,3 @@
-export { labelPlacement_inner as textField_labelPlacement_inner } from './variations/_label-placement/inner';
-export { labelPlacement_outer as textField_labelPlacement_outer } from './variations/_label-placement/outer';
-
 export { textFieldRoot, textFieldConfig } from './TextField';
 export { tokens as textFieldTokens } from './TextField.tokens';
 export type { TextFieldProps } from './TextField.types';
