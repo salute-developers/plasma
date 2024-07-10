f255b5a6fd3baf0901b0678c91743f9ed21f7494
Neretin Artem <hawktrike@gmail.com>
2024-07-10 18:44:45 +0500

    chore(sdds-dfa-docs): Add `sdds-dfa-docs`

---
+++ website/sdds-dfa-docs/src/theme/ReactLiveScope/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as Icons from '@salutejs/plasma-icons';
+import * as Tokens from '@salutejs/sdds-themes/tokens';
+import * as Package from '@salutejs/sdds-dfa';
+import { Filler } from '@salutejs/plasma-docs-ui';
+import styled from 'styled-components';
+
+// Add react-live imports you need here
+const ReactLiveScope = {
+    React,
+    Filler,
+    styled,
+    ...Icons,
+    ...Tokens,
+    ...Package,
+    ...React,
+};
+
+export default ReactLiveScope;
