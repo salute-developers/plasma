8f0bb25097a2c443f7eefce03f5285668719d9d4
Vladislav Denisov <deni.vladislav@gmail.com>
2024-07-19 14:44:32 +0300

    fix(plasma-new-hope): fix Portal PropsTable error

--- packages/plasma-new-hope/src/components/Portal/Portal.tsx
+++ packages/plasma-new-hope/src/components/Portal/Portal.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
 import ReactDOM from 'react-dom';
 
 import { PortalProps } from './Portal.types';
 
-/*
+/**
  * Вспомогательный компонент. Используется в Popup, Popover.
  * Представляет собой ReactDOM.createPortal() в форме компонента.
  */
-export const Portal = ({ children, container, disabled = false }: PortalProps) => {
+export const Portal: FC<PortalProps> = ({ children, container, disabled = false }) => {
     const portalContainer = typeof container === 'function' ? container() : container;
 
     return (
