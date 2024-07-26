78b32f39b4b537b344992d19944ce82e27845e06
Alexander Lobyntsev <yakutoc@gmail.com>
2024-07-26 14:16:00 +0700

    chore(caldera-online): delete package

--- packages/caldera-online/.storybook/decoratorToast.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { Decorator } from '@storybook/react';
-import { ToastProvider } from '../src/components/Toast';
-
-export const withToast: Decorator = (Story) => (
-    <ToastProvider>
-        <Story />
-    </ToastProvider>
-);
--- packages/caldera-online/src/components/Dropdown/index.ts
+++
@@ -1,3 +0,0 @@
-export { Dropdown } from './Dropdown';
-
-export type { DropdownProps } from '@salutejs/plasma-new-hope/styled-components';
