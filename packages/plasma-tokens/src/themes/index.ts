3f3aa68454a00dd4ffc92046162386067d6d53b1
iljs <38344415+iljs@users.noreply.github.com>
2024-08-13 18:10:56 +0300

    feat(plasma-tokens): Add theme testilya

--- packages/plasma-tokens/src/themes/index.ts
+++ packages/plasma-tokens/src/themes/index.ts
@@ -32,6 +32,8 @@ export { sdds_serv__dark, darkSdds_serv } from './sdds_serv__dark';
 export { sdds_serv__light, lightSdds_serv } from './sdds_serv__light';
 export { stylesSalute__dark, darkStylesSalute } from './stylesSalute__dark';
 export { stylesSalute__light, lightStylesSalute } from './stylesSalute__light';
+export { testilya__dark, darkTestilya } from './testilya__dark';
+export { testilya__light, lightTestilya } from './testilya__light';
 export { salutejs_sber__dark, darkSber } from './salutejs_sber__dark';
 export { salutejs_eva__dark, darkEva } from './salutejs_eva__dark';
 export { salutejs_joy__dark, darkJoy } from './salutejs_joy__dark';
