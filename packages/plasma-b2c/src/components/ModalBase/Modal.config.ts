584755852b04a70e521a7f7fdd5b37ef1fce6aa2
Кудрявцев Вадим Анатольевич <vanakudryavtsev@sberbank.ru>
2024-08-15 14:15:15 +0300

    fix: update ModalBase modalOverlayColor token

--- packages/plasma-b2c/src/components/ModalBase/Modal.config.ts
+++ packages/plasma-b2c/src/components/ModalBase/Modal.config.ts
@@ -8,7 +8,7 @@ export const config = {
         view: {
             default: css`
                 ${modalTokens.modalOverlayWithBlurColor}: rgba(35, 35, 35, 0.2);
-                ${modalTokens.modalOverlayColor}: var(--plasma-colors-overlay-soft);
+                ${modalTokens.modalOverlayColor}: var(--overlay-soft);
             `,
         },
     },
