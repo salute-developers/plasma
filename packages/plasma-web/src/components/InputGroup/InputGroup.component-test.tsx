dcb0047ca9012e1e376ccd385ed8d06b5786ff44
Alexandr Krivonos <yzn-yfv@mail.ru>
2024-09-02 16:25:50 +0300

    feat(plasma-b2c/web): add InputGroup with tests

--- packages/plasma-web/src/components/InputGroup/InputGroup.component-test.tsx
+++ packages/plasma-web/src/components/InputGroup/InputGroup.component-test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import type { FC, PropsWithChildren } from 'react';
+import { standard as standardTypo } from '@salutejs/plasma-typo';
+import { createGlobalStyle } from 'styled-components';
 import { mount, CypressTestDecorator, getComponent, PadMe } from '@salutejs/plasma-cy-utils';
+
+const StandardTypoStyle = createGlobalStyle(standardTypo);
 
 describe('plasma-web: InputGroup', () => {
     const InputGroup = getComponent('InputGroup');
     const TextField = getComponent('TextField');
+
+    const CypressTestDecoratorWithTypo: FC<PropsWithChildren> = ({ children }) => (
+        <CypressTestDecorator>
+            <StandardTypoStyle />
+            {children}
+        </CypressTestDecorator>
+    );
 
     const getTexFields = () => [
         <TextField size="l" placeholder="Size 'l'" />,
@@ -14,7 +26,7 @@ describe('plasma-web: InputGroup', () => {
 
     it('_size', () => {
         mount(
-            <CypressTestDecorator>
+            <CypressTestDecoratorWithTypo>
                 <InputGroup size="l">{getTexFields()}</InputGroup>
                 <PadMe />
                 <InputGroup size="m">{getTexFields()}</InputGroup>
@@ -23,7 +35,7 @@ describe('plasma-web: InputGroup', () => {
                 <PadMe />
                 <InputGroup size="xs">{getTexFields()}</InputGroup>
                 <PadMe />
-            </CypressTestDecorator>,
+            </CypressTestDecoratorWithTypo>,
         );
 
         cy.viewport(1366, 768);
@@ -32,11 +44,11 @@ describe('plasma-web: InputGroup', () => {
 
     it('_orientation', () => {
         mount(
-            <CypressTestDecorator>
+            <CypressTestDecoratorWithTypo>
                 <InputGroup orientation="horizontal">{getTexFields()}</InputGroup>
                 <PadMe />
                 <InputGroup orientation="vertical">{getTexFields()}</InputGroup>
-            </CypressTestDecorator>,
+            </CypressTestDecoratorWithTypo>,
         );
 
         cy.viewport(1366, 768);
@@ -45,22 +57,22 @@ describe('plasma-web: InputGroup', () => {
 
     it('_orientation: single text field', () => {
         mount(
-            <CypressTestDecorator>
+            <CypressTestDecoratorWithTypo>
                 <InputGroup orientation="horizontal">
-                    <TextField placegolder="Horizontal" />
+                    <TextField placeholder="Horizontal" />
                 </InputGroup>
                 <PadMe />
                 <InputGroup orientation="vertical">
-                    <TextField placegolder="Vertical" />
+                    <TextField placeholder="Vertical" />
                 </InputGroup>
-            </CypressTestDecorator>,
+            </CypressTestDecoratorWithTypo>,
         );
         cy.matchImageSnapshot();
     });
 
     it('_orientation: stretching - filled', () => {
         mount(
-            <CypressTestDecorator>
+            <CypressTestDecoratorWithTypo>
                 <InputGroup orientation="horizontal" stretching="filled">
                     {getTexFields()}
                 </InputGroup>
@@ -68,7 +80,7 @@ describe('plasma-web: InputGroup', () => {
                 <InputGroup orientation="vertical" stretching="filled">
                     {getTexFields()}
                 </InputGroup>
-            </CypressTestDecorator>,
+            </CypressTestDecoratorWithTypo>,
         );
 
         cy.viewport(1366, 768);
@@ -77,7 +89,7 @@ describe('plasma-web: InputGroup', () => {
 
     it('_shape', () => {
         mount(
-            <CypressTestDecorator>
+            <CypressTestDecoratorWithTypo>
                 <InputGroup shape="segmented" gap="none">
                     {getTexFields()}
                 </InputGroup>
@@ -94,7 +106,7 @@ describe('plasma-web: InputGroup', () => {
                     {getTexFields()}
                 </InputGroup>
                 <PadMe />
-            </CypressTestDecorator>,
+            </CypressTestDecoratorWithTypo>,
         );
 
         cy.viewport(1366, 768);
@@ -103,13 +115,13 @@ describe('plasma-web: InputGroup', () => {
 
     it('custom text fields', () => {
         mount(
-            <CypressTestDecorator>
+            <CypressTestDecoratorWithTypo>
                 <InputGroup isCommonInputStyles={false}>{getTexFields()}</InputGroup>
                 <PadMe />
                 <InputGroup isCommonInputStyles={false} orientation="vertical">
                     {getTexFields()}
                 </InputGroup>
-            </CypressTestDecorator>,
+            </CypressTestDecoratorWithTypo>,
         );
 
         cy.viewport(1366, 768);
