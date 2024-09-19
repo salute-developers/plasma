d8ca1c44f7dc25d2493bacba4e2df4f8db679d86
Alexandr Krivonos <yzn-yfv@mail.ru>
2024-09-19 14:36:56 +0300

    feat(sdds-*): add ChipGroup component

---
+++ packages/sdds-cs/src/components/ChipGroup/ChipGroup.config.ts
@@ -0,0 +1,70 @@
+import { css, chipGroupTokens as tokens } from '@salutejs/plasma-new-hope/styled-components';
+
+export const config = {
+    defaults: {
+        view: 'default',
+        size: 's',
+    },
+    variations: {
+        view: {
+            default: css`
+                ${tokens.chipColor}: var(--inverse-text-primary);
+                ${tokens.chipBackground}: var(--surface-solid-default);
+                ${tokens.chipColorHover}: var(--inverse-text-primary);
+                ${tokens.chipBackgroundHover}: var(--surface-solid-default);
+                ${tokens.chipColorActive}: var(--inverse-text-primary);
+                ${tokens.chipBackgroundActive}: var(--surface-solid-default);
+            `,
+            secondary: css`
+                ${tokens.chipColor}: var(--text-primary);
+                ${tokens.chipBackground}: var(--surface-solid-primary);
+                ${tokens.chipColorHover}: var(--text-primary);
+                ${tokens.chipBackgroundHover}: var(--surface-solid-primary);
+                ${tokens.chipColorActive}: var(--text-primary);
+                ${tokens.chipBackgroundActive}: var(--surface-solid-primary);
+            `,
+            accent: css`
+                ${tokens.chipColor}: var(--on-dark-text-primary);
+                ${tokens.chipBackground}: var(--surface-accent);
+                ${tokens.chipColorHover}: var(--on-dark-text-primary);
+                ${tokens.chipBackgroundHover}: var(--surface-accent);
+                ${tokens.chipColorActive}: var(--on-dark-text-primary);
+                ${tokens.chipBackgroundActive}: var(--surface-accent);
+            `,
+        },
+        size: {
+            s: css`
+                ${tokens.chipBorderRadius}: 0.5rem;
+                ${tokens.chipWidth}: auto;
+                ${tokens.chipHeight}: 2.5rem;
+                ${tokens.chipPaddingRight}: 1rem;
+                ${tokens.chipPaddingLeft}: 1rem;
+
+                ${tokens.chipFontFamily}: var(--plasma-typo-body-m-font-family);
+                ${tokens.chipFontSize}: var(--plasma-typo-body-m-font-size);
+                ${tokens.chipFontStyle}: var(--plasma-typo-body-m-font-style);
+                ${tokens.chipFontWeight}: var(--plasma-typo-body-m-font-weight);
+                ${tokens.chipLetterSpacing}: var(--plasma-typo-body-m-letter-spacing);
+                ${tokens.chipLineHeight}: var(--plasma-typo-body-m-line-height);
+
+                ${tokens.chipLeftContentMarginLeft}: -0.25rem;
+                ${tokens.chipLeftContentMarginRight}: 0.25rem;
+                ${tokens.chipClearContentMarginLeft}: 0.5rem;
+                ${tokens.chipClearContentMarginRight}: -0.25rem;
+
+                ${tokens.chipScaleHover}: 1.02;
+                ${tokens.chipScaleActive}: 0.98;
+
+                ${tokens.chipCloseIconSize}: 1.5rem;
+            `,
+        },
+        gap: {
+            dense: css`
+                ${tokens.chipGroupItemsGap}: 0.125rem;
+            `,
+            wide: css`
+                ${tokens.chipGroupItemsGap}: 0.5rem;
+            `,
+        },
+    },
+};
