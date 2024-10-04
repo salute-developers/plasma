eeb8aa9275671e40341a81d80471be77bb423216
Vladislav Denisov <deni.vladislav@gmail.com>
2024-10-04 10:11:48 +0300

    fix(plasma-b2c, plasma-web): fix Attach tests

--- packages/plasma-b2c/src/components/Attach/Attach.component-test.tsx
+++ packages/plasma-b2c/src/components/Attach/Attach.component-test.tsx
@@ -7,6 +7,14 @@ import { createGlobalStyle } from 'styled-components';
 const StandardTypoStyle = createGlobalStyle(standardTypo);
 
 const FIXTURE_PATH = 'images/placeholder.png';
+const cellRootSelector = '.cell-root';
+const inputSelector = 'input';
+
+const sizes = ['xs', 's', 'm', 'l'];
+const views = ['default', 'accent', 'secondary', 'success', 'warning', 'critical', 'clear'];
+const buttonTypes = ['button', 'iconButton'];
+const flows = ['horizontal', 'vertical', 'auto'];
+const filenameTruncationWidths = ['400px', '350px', '300px', '180px', '0px'];
 
 describe('plasma-b2c: Attach', () => {
     const Attach = getComponent('Attach');
@@ -30,41 +38,32 @@ describe('plasma-b2c: Attach', () => {
     it('_size', () => {
         mount(
             <CypressTestDecoratorWithTypo>
-                <Attach size="xs" />
-                <PadMe />
-                <Attach size="s" />
-                <PadMe />
-                <Attach size="m" />
-                <PadMe />
-                <Attach size="l" />
+                {sizes.map((size) => (
+                    <>
+                        <Attach size={size} />
+                        <PadMe />
+                    </>
+                ))}
             </CypressTestDecoratorWithTypo>,
         );
-        cy.get('input').each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100);
+        cy.get(inputSelector).each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
+        cy.get(cellRootSelector).should('have.length', sizes.length);
+
         cy.matchImageSnapshot();
     });
 
     it('_view', () => {
         mount(
             <CypressTestDecoratorWithTypo>
-                <Attach view="default" size="s" />
-                <PadMe />
-                <Attach view="accent" size="s" />
-                <PadMe />
-                <Attach view="secondary" size="s" />
-                <PadMe />
-                <Attach view="success" size="s" />
-                <PadMe />
-                <Attach view="warning" size="s" />
-                <PadMe />
-                <Attach view="critical" size="s" />
-                <PadMe />
-                <Attach view="clear" size="s" />
+                {views.map((view) => (
+                    <>
+                        <Attach view={view} size="s" />
+                        <PadMe />
+                    </>
+                ))}
             </CypressTestDecoratorWithTypo>,
         );
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100);
+
         cy.matchImageSnapshot();
     });
 
@@ -74,59 +73,60 @@ describe('plasma-b2c: Attach', () => {
                 <Attach hasAttachment={false} />
             </CypressTestDecoratorWithTypo>,
         );
-        cy.get('input').attachFile(FIXTURE_PATH);
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100);
+        cy.get(inputSelector).attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('not.exist');
+
         cy.matchImageSnapshot();
     });
 
     it('_buttonType', () => {
         mount(
             <CypressTestDecoratorWithTypo>
-                <Attach style={{ width: '400px' }} buttonType="button" />
-                <PadMe />
-                <Attach style={{ width: '400px' }} buttonType="iconButton" />
+                {buttonTypes.map((buttonType) => (
+                    <>
+                        <Attach style={{ width: '400px' }} buttonType={buttonType} />
+                        <PadMe />
+                    </>
+                ))}
             </CypressTestDecoratorWithTypo>,
         );
-        cy.get('input').each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100);
+        cy.get(inputSelector).each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
+        cy.get(cellRootSelector).should('have.length', buttonTypes.length);
+
         cy.matchImageSnapshot();
     });
 
     it('_flow', () => {
         mount(
             <CypressTestDecoratorWithTypo>
-                <Attach flow="horizontal" />
-                <PadMe />
-                <Attach flow="vertical" />
-                <PadMe />
-                <Attach flow="auto" />
+                {flows.map((flow) => (
+                    <>
+                        <Attach flow={flow} />
+                        <PadMe />
+                    </>
+                ))}
             </CypressTestDecoratorWithTypo>,
         );
-        cy.get('input').each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100);
+        cy.get(inputSelector).each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
+        cy.get(cellRootSelector).should('have.length', flows.length);
+
         cy.matchImageSnapshot();
     });
 
     it('filenameTruncation flow:auto', () => {
         mount(
             <CypressTestDecoratorWithTypo>
-                <Attach style={{ width: '400px' }} text="width: 400px" />
-                <PadMe />
-                <Attach style={{ width: '350px' }} text="width: 350px" />
-                <PadMe />
-                <Attach style={{ width: '300px' }} text="width: 300px" />
-                <PadMe />
-                <Attach style={{ width: '180px' }} text="width: 180px" />
-                <PadMe />
-                <Attach style={{ width: '0px' }} text="width: 0px" />
+                {filenameTruncationWidths.map((filenameTruncationWidth) => (
+                    <>
+                        <Attach style={{ width: filenameTruncationWidth }} text={`width: ${filenameTruncationWidth}`} />
+                        <PadMe />
+                    </>
+                ))}
             </CypressTestDecoratorWithTypo>,
         );
-        cy.get('input').each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100);
+        cy.get(inputSelector).each(($element) => cy.wrap($element).attachFile(FIXTURE_PATH));
+        cy.get(cellRootSelector).should('have.length', filenameTruncationWidths.length);
+
         cy.matchImageSnapshot();
     });
 });
