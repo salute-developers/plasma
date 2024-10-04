488a1798c6be7cd4fb55090818c746a2335d2d16
Dima Shugaev <shuga2704@gmail.com>
2024-10-04 11:55:08 +0300

    feat(plasma-new-hope): Got rid of styled-components imports in new-hope

--- packages/plasma-new-hope/src/components/Breadcrumbs/ui/BreadcrumbShorter/BreadcrumbShorter.styles.ts
+++ packages/plasma-new-hope/src/components/Breadcrumbs/ui/BreadcrumbShorter/BreadcrumbShorter.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { styled } from '@linaria/react';
 
 import { addFocus } from '../../../../mixins';
 import { tokens } from '../../Breadcrumbs.tokens';
