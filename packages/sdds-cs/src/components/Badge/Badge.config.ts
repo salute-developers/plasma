86df51beaa8a7beeccb00fcc7549494b126c185b
Alexander Lobyntsev <yakutoc@gmail.com>
2024-10-09 15:25:09 +0700

    fix(sdds-cs): change padding{Right,Left} to 8px [Badge]

--- packages/sdds-cs/src/components/Badge/Badge.config.ts
+++ packages/sdds-cs/src/components/Badge/Badge.config.ts
@@ -39,8 +39,8 @@ export const config = {
             s: css`
                 ${badgeTokens.borderRadius}: 0.375rem;
                 ${badgeTokens.height}: 1.5rem;
-                ${badgeTokens.paddingRight}: 0.5625rem;
-                ${badgeTokens.paddingLeft}: 0.5625rem;
+                ${badgeTokens.paddingRight}: 0.5rem;
+                ${badgeTokens.paddingLeft}: 0.5rem;
 
                 ${badgeTokens.fontFamily}: var(--plasma-typo-body-s-font-family);
                 ${badgeTokens.fontSize}: var(--plasma-typo-body-s-font-size);
