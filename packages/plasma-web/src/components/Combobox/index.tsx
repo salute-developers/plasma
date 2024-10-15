9a4d211c0d2b4cd5a367858a3d4fec5b58c38196
Dima Shugaev <shuga2704@gmail.com>
2024-10-15 13:00:01 +0300

    feat(plasma-web, plasma-b2c, sdds): Interlayer in Combobox according to the new generic

--- packages/plasma-web/src/components/Combobox/index.tsx
+++ packages/plasma-web/src/components/Combobox/index.tsx
@@ -1,5 +1,7 @@
 import React, { forwardRef, ComponentProps } from 'react';
+import type { ItemOption, ComboboxProps } from '@salutejs/plasma-new-hope';
 
+import { config } from './Combobox.config';
 import { Combobox as ComboboxOld } from './Legacy';
 import { Combobox as ComboboxNew } from './Combobox';
 
@@ -7,17 +9,27 @@ export { ComboboxDivider, ComboboxFooter, ComboboxGroup, ComboboxHeader, Combobo
 
 export type { ComboboxOldProps as ComboboxProps } from '@salutejs/plasma-new-hope/styled-components';
 
+function fixedForwardRef<T, P = {}>(
+    render: (props: P, ref: React.Ref<T>) => React.ReactElement | null,
+): (props: P & React.RefAttributes<T>) => React.ReactElement | null {
+    return forwardRef(render as any) as any;
+}
+
+type PropsFromConfig = keyof typeof config['variations'];
+
 type PropsOld = ComponentProps<typeof ComboboxOld> & { items?: never };
-type PropsNew = ComponentProps<typeof ComboboxNew>;
 
-type CommonProps = PropsOld | PropsNew;
+type PropsNew<T extends ItemOption> = Omit<ComboboxProps<T>, PropsFromConfig> &
+    Pick<ComponentProps<typeof ComboboxNew>, PropsFromConfig>;
 
-const Combobox = forwardRef<HTMLInputElement, CommonProps>((props, ref) => {
+type CommonProps<T extends ItemOption> = PropsOld | PropsNew<T>;
+
+const ComboboxComponent = <T extends ItemOption>(props: CommonProps<T>, ref: React.ForwardedRef<HTMLInputElement>) => {
     if (props.items) {
-        return <ComboboxNew ref={ref} {...props} />;
+        return <ComboboxNew ref={ref} {...(props as any)} />;
     }
 
     return <ComboboxOld ref={ref} {...props} />;
-});
+};
 
-export { Combobox };
+export const Combobox = fixedForwardRef(ComboboxComponent);
