0a1499b5fac32ae54d06493f19feb0612f3264eb
Neretin Artem <hawktrike@gmail.com>
2024-10-17 17:17:38 +0500

    feat(plasma-new-hope): Add `ViewComponent` to example and add import themes from `plasma-themes`

--- packages/plasma-new-hope/.storybook/preview.ts
+++ packages/plasma-new-hope/.storybook/preview.ts
@@ -1,9 +1,15 @@
 import type { Preview } from '@storybook/react';
+import { withViewContainer } from './decoratorViewContainer';
 
 const LIGHT_THEME = 'light';
 const DARK_THEME = 'dark';
 
+const DEFAULT_MODE = 'default';
+const ON_DARK_MODE = 'onDark';
+const ON_LIGHT_MODE = 'onLight';
+
 const preview: Preview = {
+    decorators: [withViewContainer],
     parameters: {
         docs: {
             source: { type: 'code', language: 'tsx' },
@@ -16,6 +22,14 @@ const preview: Preview = {
             toolbar: {
                 title: 'Theme',
                 items: [LIGHT_THEME, DARK_THEME],
+            },
+        },
+        viewContainer: {
+            description: 'ViewContainer mode for components',
+            defaultValue: DEFAULT_MODE,
+            toolbar: {
+                title: 'ViewContainer',
+                items: [DEFAULT_MODE, ON_DARK_MODE, ON_LIGHT_MODE],
             },
         },
     },
