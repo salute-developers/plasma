fc7d5c29f9ddc2467800c71fb352fdca5ee3a1e9
Ilya <illja.by4ek@gmail.com>
2024-10-24 01:03:15 +0300

    fix(new-hope): add new tokens to combobox

--- packages/plasma-new-hope/src/components/Combobox/ComboboxNew/Combobox.styles.ts
+++ packages/plasma-new-hope/src/components/Combobox/ComboboxNew/Combobox.styles.ts
@@ -38,7 +38,7 @@ export const Ul = styled.ul<{
 
 export const IconArrowWrapper = styled.div<{ disabled: boolean }>`
     line-height: 0;
-    color: var(${constants.disclosureIconColor});
+    color: var(${tokens.disclosureIconColor}, var(${constants.disclosureIconColor}));
     cursor: ${({ disabled }) => (disabled ? 'inherit' : 'pointer')};
 
     .${classes.arrowInverse} {
@@ -48,7 +48,9 @@ export const IconArrowWrapper = styled.div<{ disabled: boolean }>`
     &:hover,
     &:active {
         color: ${({ disabled }) =>
-            disabled ? `var(${constants.disclosureIconColor})` : `var(${constants.disclosureIconColorHover})`};
+            disabled
+                ? `var(${tokens.disclosureIconColor}, var(${constants.disclosureIconColor}))`
+                : `var(${tokens.disclosureIconColorHover}, var(${constants.disclosureIconColorHover}))`};
     }
 `;
 
