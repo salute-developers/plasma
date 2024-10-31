da126221f0250f115592f066c86cde90bf2fa18b
Neretin Artem <hawktrike@gmail.com>
2024-10-31 16:52:50 +0500

    fix(plasma-new-hope): Change background-color property to background for `Button`

--- packages/plasma-new-hope/src/components/Button/variations/_view/base.ts
+++ packages/plasma-new-hope/src/components/Button/variations/_view/base.ts
@@ -4,22 +4,22 @@ import { tokens, classes } from '../../Button.tokens';
 
 export const base = css`
     color: var(${tokens.buttonColor});
-    background-color: var(${tokens.buttonBackgroundColor});
+    background: var(${tokens.buttonBackgroundColor});
 
     &.${classes.buttonLoading} {
-        background-color: var(${tokens.buttonLoadingBackgroundColor});
+        background: var(${tokens.buttonLoadingBackgroundColor});
     }
 
     :hover {
         color: var(${tokens.buttonColorHover}, var(${tokens.buttonColor}));
-        background-color: var(${tokens.buttonBackgroundColorHover}, var(${tokens.buttonBackgroundColor}));
+        background: var(${tokens.buttonBackgroundColorHover}, var(${tokens.buttonBackgroundColor}));
 
         scale: var(${tokens.buttonScaleHover});
     }
 
     :active {
         color: var(${tokens.buttonColorActive}, var(${tokens.buttonColor}));
-        background-color: var(${tokens.buttonBackgroundColorActive}, var(${tokens.buttonBackgroundColor}));
+        background: var(${tokens.buttonBackgroundColorActive}, var(${tokens.buttonBackgroundColor}));
 
         scale: var(${tokens.buttonScaleActive});
     }
