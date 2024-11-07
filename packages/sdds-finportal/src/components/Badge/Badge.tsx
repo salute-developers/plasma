2cbf9beb12574a27ee85bb97bfeb2c18a48c3e81
Alexander Lobyntsev <yakutoc@gmail.com>
2024-11-07 11:48:59 +0700

    chore(finportal): add draft config [Badge]

--- packages/sdds-finportal/src/components/Badge/Badge.tsx
+++ packages/sdds-finportal/src/components/Badge/Badge.tsx
@@ -1,8 +1,12 @@
 import { badgeConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
 
-import { config } from './Badge.config';
+import { IS_DRAFT } from '../../helpers/isDraft';
 
-const mergedConfig = mergeConfig(badgeConfig, config);
+import { config } from './Badge.config';
+// INFO: После всех изменений draft станет основной конфигурацией
+import { config as draft } from './Badge.config.draft';
+
+const mergedConfig = mergeConfig(badgeConfig, IS_DRAFT() ? draft : config);
 
 /**
  * Компонент Badge.
