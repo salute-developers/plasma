4b69d673217dee43a25186ac91b8f666c9c474d3
Ilya <illja.by4ek@gmail.com>
2024-11-15 16:55:27 +0300

    fix(new-hope): edit select native ui component in combobox

--- packages/plasma-new-hope/src/components/Combobox/ComboboxNew/ui/SelectNative/SelectNative.tsx
+++ packages/plasma-new-hope/src/components/Combobox/ComboboxNew/ui/SelectNative/SelectNative.tsx
@@ -1,24 +1,27 @@
 import React, { ChangeEvent, forwardRef, useEffect, useRef } from 'react';
 import { useForkRef } from '@salutejs/plasma-core';
 
-import { createEvent } from '../../../../../utils/syntheticEvent';
+import { createEvent } from '../../../../../utils';
 import { ComboboxProps } from '../../Combobox.types';
 
-import { SelectHidden } from './Form.styles';
+import { SelectHidden } from './SelectNative.styles';
 
 type Props = Pick<ComboboxProps, 'name' | 'value' | 'multiple'> & {
     onChange: (value: ChangeEvent<HTMLSelectElement> | null) => void;
 };
 
-export const Form = forwardRef<HTMLSelectElement, Props>(({ name, multiple, value, onChange }, ref) => {
+export const SelectNative = forwardRef<HTMLInputElement, Props>(({ name, multiple, value, onChange }, ref) => {
     const values = (multiple ? value : [value]) as string[];
-    const selectRef = useRef<HTMLSelectElement | null>(null);
-    const forkRef = useForkRef(selectRef, ref);
+    const selectRef = useRef<HTMLSelectElement>(null);
+    const forkRef = useForkRef(selectRef, ref as any);
 
     useEffect(() => {
         const event = createEvent(selectRef);
-        onChange && onChange(event);
+        if (onChange) {
+            onChange(event);
+        }
     }, [values]);
+
     return (
         <>
             <SelectHidden ref={forkRef} multiple={multiple} name={name} hidden value={multiple ? values : values[0]}>
