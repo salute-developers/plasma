6df3c388b32a927e67a543cc035f4f8d1a1c228d
Vladislav Denisov <deni.vladislav@gmail.com>
2024-11-22 17:33:05 +0300

    fix(): fixed typo 'typograpy' ->  folder name

--- packages/plasma-new-hope/src/examples/plasma_b2c/components/Editable/Editable.ts
+++ packages/plasma-new-hope/src/examples/plasma_b2c/components/Editable/Editable.ts
@@ -1,9 +1,9 @@
 import { editableConfig } from '../../../../components/Editable';
 import { component, mergeConfig } from '../../../../engines';
-import { Body } from '../../../typograpy/components/Body/Body';
-import { Dspl } from '../../../typograpy/components/Dspl/Dspl';
-import { Heading } from '../../../typograpy/components/Heading/Heading';
-import { Text } from '../../../typograpy/components/Text/Text';
+import { Body } from '../../../typography/components/Body/Body';
+import { Dspl } from '../../../typography/components/Dspl/Dspl';
+import { Heading } from '../../../typography/components/Heading/Heading';
+import { Text } from '../../../typography/components/Text/Text';
 
 import { config } from './Editable.config';
 
