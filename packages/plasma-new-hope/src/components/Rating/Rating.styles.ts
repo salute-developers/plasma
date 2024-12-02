5931dda155ab994487e3c2dc96e72b304ac8d7bb
TitanKuzmich <yzn-yfv@mail.ru>
2024-12-02 17:20:23 +0300

    feat(plasma-new-hope): add Rating component

---
+++ packages/plasma-new-hope/src/components/Rating/Rating.styles.ts
@@ -0,0 +1,33 @@
+import { css } from '@linaria/core';
+import { styled } from '@linaria/react';
+
+export const base = css`
+    display: inline-flex;
+    flex-direction: column;
+`;
+
+export const Score = styled.div``;
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: start;
+    flex-direction: column;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    width: fit-content;
+`;
+
+export const StarsWrapper = styled.div`
+    display: flex;
+`;
+
+export const Star = styled.div`
+    display: flex;
+`;
+
+export const HelperText = styled.span`
+    word-break: break-word;
+`;
