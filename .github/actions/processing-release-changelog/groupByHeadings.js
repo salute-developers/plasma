e66d51a3a55d63b7aedb0291b74e610b18c504e2
Alexander Lobyntsev <yakutoc@gmail.com>
2024-12-04 14:08:06 +0700

    Revert "ci: processing changelog 2 [no ci]"
    This reverts commit 4115e5f819f97867e7f973b3fdf8c4eb9b090618.

--- .github/actions/processing-release-changelog/groupByHeadings.js
+++ .github/actions/processing-release-changelog/groupByHeadings.js
@@ -1,36 +1,17 @@
 export function groupByHeadings(tree) {
     const groups = new Map();
-    const h2List = new Set();
+    let currentHeading = null;
 
-    let currentGroup = null;
-    let currentNodes = [];
-
+    // Группируем узлы по заголовкам
     for (const node of tree.children) {
         if (node.type === 'heading' && node.depth === 2) {
-            // Сохраняем предыдущую группу
-            if (currentGroup) {
-                const data = [...(groups.get(currentGroup) || []), ...currentNodes];
-
-                groups.set(currentGroup, data);
-            }
-
-            const headingValue = node.children[0].value;
-
-            currentGroup = headingValue;
-
-            // Сохраняем заголовок только если он встречается впервые
-            currentNodes = !h2List.has(headingValue) ? [node] : [];
-            h2List.add(headingValue);
-        } else if (currentGroup) {
-            currentNodes.push(node);
+            currentHeading = node.children[0].value;
+            // При первом появлении заголовка создаем массив с ним
+            groups.set(currentHeading, [node]);
+        } else if (currentHeading) {
+            // Добавляем узлы к текущей группе
+            groups.get(currentHeading).push(node);
         }
-    }
-
-    // Сохраняем последнюю группу
-    if (currentGroup && currentNodes.length) {
-        const data = [...(groups.get(currentGroup) || []), ...currentNodes];
-
-        groups.set(currentGroup, data);
     }
 
     return {
