56b5cf3e3e4bc8c08e0a89a6601d03ebe9e0b9d7
TitanKuzmich <yzn-yfv@mail.ru>
2024-12-04 18:25:08 +0300

    feat(plasma-sb-utils): add color token helpers

--- utils/plasma-sb-utils/src/helpers/index.ts
+++ utils/plasma-sb-utils/src/helpers/index.ts
@@ -1,3 +1,6 @@
 export { disableProps } from './disableProps';
 export { flattenPalette } from './flattenPalette';
 export { extractCanvasThemeColors, extractWebThemeColors } from './themeColors';
+export { getOpacityFromHex, getGroupedTokens } from './colorTokens';
+
+export type { GroupedTokens } from './colorTokens';
