e744fd4eb5c291b554718a41d4ea203acb20369c
IgorYar <ihteyar2@yandex.ru>
2024-12-06 11:33:47 +0500

    test(plasma-b2c): update tests for new testing model

--- packages/plasma-b2c/src/components/Attach/Attach.update-test.component-test.tsx
+++ packages/plasma-b2c/src/components/Attach/Attach.update-test.component-test.tsx
@@ -21,10 +21,140 @@ describe('plasma-b2c: Attach', () => {
         </CypressTestDecorator>
     );
 
-    it('[PLASMA-T1296] Attach: flow=horizontal', () => {
+    it('[PLASMA-T1542] Attach: size=l, view=default, enableContentLeft, buttonText', () => {
         mount(
             <CypressTestDecoratorWithTypo>
-                <Attach flow="horizontal" />
+                <Attach view="default" size="l" contentLeft={<Icon />} />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1543] Attach: size=m, view=accent, enableContentRight, buttonValue', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach text="" view="accent" size="m" value="buttonValue" contentRight={<Icon />} />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1544] Attach: size=s, view=secondary, enableContentLeft, enableContentRight, buttonText, buttonValue', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="secondary" size="s" value="buttonValue" contentLeft={<Icon />} contentRight={<Icon />} />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1545] Attach: size=xs, view=clear', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="clear" size="xs" />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1546] Attach: view=success, hasAttachment=false', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="success" size="l" hasAttachment={false} />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('not.be.exist');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1547] Attach: view=warning, flow=horizontal, fileFormat=all', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="warning" size="l" flow="horizontal" fileFormat="all" />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1548] Attach: view=critical, flow=vertical', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="critical" size="l" flow="vertical" fileFormat="all" />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1549] Attach: view=dark, flow=auto, width=500px', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="dark" size="l" flow="auto" style={{ width: '500px' }} />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1550] Attach: view=black, width=250px', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="black" size="l" style={{ width: '250px' }} />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1551] Attach: view=white, width=0', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="white" size="l" style={{ width: '0px' }} />
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('input').attachFile(FIXTURE_PATH);
+        cy.get(cellRootSelector).should('be.visible');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1552] Attach: buttonType=iconButton', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Attach view="default" size="l" buttonType="iconButton" />
             </CypressTestDecoratorWithTypo>,
         );
 
@@ -49,161 +179,10 @@ describe('plasma-b2c: Attach', () => {
         cy.matchImageSnapshot();
     });
 
-    it('[PLASMA-T1298] Attach: flow=vertical', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach flow="vertical" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').attachFile(FIXTURE_PATH);
-        cy.get(cellRootSelector).should('be.visible');
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1299] Attach: hasAttachment=false', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach hasAttachment={false} />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').attachFile(FIXTURE_PATH);
-        cy.get(cellRootSelector).should('not.be.exist');
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1301] flow=auto, filenameTruncation', () => {
+    it('[PLASMA-T1301] filenameTruncation', () => {
         mount(
             <CypressTestDecoratorWithTypo>
                 <Attach style={{ width: '0px' }} text="width: 0px" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').attachFile(FIXTURE_PATH);
-        cy.get(cellRootSelector).should('be.visible');
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1304] Attach: view=accent, size=m', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="accent" size="m" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').attachFile(FIXTURE_PATH);
-        cy.get(cellRootSelector).should('be.visible');
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1305] Attach: view=default, size=l', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="default" size="l" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').attachFile(FIXTURE_PATH);
-        cy.get(cellRootSelector).should('be.visible');
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1306] Attach: view=secondary, size=s', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="secondary" size="s" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').attachFile(FIXTURE_PATH);
-        cy.get(cellRootSelector).should('be.visible');
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1307] Attach: view=clear, size=xs', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="clear" size="xs" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').attachFile(FIXTURE_PATH);
-        cy.get(cellRootSelector).should('be.visible');
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1308] Attach: view=success, square', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="success" square />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1309] Attach: view=warning, enableContentLeft', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="warning" contentLeft={<Icon />} />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1310] Attach: view=critical, enableContentRight', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="critical" contentRight={<Icon />} />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1311] Attach: view=dark, with buttonValue', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="dark" value="buttonValue" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1312] Attach: view=black', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="black" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1313] Attach: view=white', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="white" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('[PLASMA-T1315] Attach: view=accent, size=m, buttonType=iconButton', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Attach view="accent" size="m" buttonType="iconButton" />
             </CypressTestDecoratorWithTypo>,
         );
 
