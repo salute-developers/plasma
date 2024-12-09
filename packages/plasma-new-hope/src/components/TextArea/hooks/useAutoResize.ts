58d4f97cd66bcc97a8cd296e6256c54f7d007bad
Neretin Artem <hawktrike@gmail.com>
2024-12-09 15:51:42 +0300

    fix(plasma-new-hope): Remove ref from deps in TextArea component

--- packages/plasma-new-hope/src/components/TextArea/hooks/useAutoResize.ts
+++ packages/plasma-new-hope/src/components/TextArea/hooks/useAutoResize.ts
@@ -9,11 +9,12 @@ export const useAutoResize = <T extends HTMLTextAreaElement>(
     minAuto?: number,
     maxAuto?: number,
     resize?: string,
+    hiddenRef?: MutableRefObject<T | null>,
 ) => {
     const previousHeight = useRef<number | undefined>();
 
     useEffect(() => {
-        if (active && ref && ref.current) {
+        if (active && ref && ref.current && hiddenRef && hiddenRef.current) {
             // проверка на пользовательский resize (вручную)
             const height = ref.current.clientHeight / ROOT_FONT_SIZE;
             if (
@@ -30,9 +31,9 @@ export const useAutoResize = <T extends HTMLTextAreaElement>(
             const lineHeightInRem = lineHeight / ROOT_FONT_SIZE;
 
             const minAutoHeight = minAuto ? minAuto * lineHeightInRem : 0;
-            ref.current.style.height = `${minAutoHeight}rem`;
+            hiddenRef.current.style.height = `${minAutoHeight}rem`;
 
-            const lines = Math.floor(ref.current.scrollHeight / lineHeight);
+            const lines = Math.floor(hiddenRef.current.scrollHeight / lineHeight);
             const newScrollHeight = lines * lineHeightInRem;
 
             const maxAutoHeight = maxAuto ? maxAuto * lineHeightInRem : newScrollHeight;
@@ -42,5 +43,5 @@ export const useAutoResize = <T extends HTMLTextAreaElement>(
             ref.current.style.height = `${newHeight}rem`;
             previousHeight.current = newHeight;
         }
-    }, [ref, resize, active, value, minAuto, maxAuto]);
+    }, [resize, active, value, minAuto, maxAuto]);
 };
