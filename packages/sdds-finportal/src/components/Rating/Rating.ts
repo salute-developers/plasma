0aa2b8109a0854ab4f6d9b718847b678ec43c668
TitanKuzmich <yzn-yfv@mail.ru>
2024-12-11 14:01:24 +0300

    feat(sdds-*): add Rating component

--- packages/sdds-finportal/src/components/Rating/Rating.ts
+++
@@ -1,7 +0,0 @@
-import { ratingConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
-
-import { config } from './Rating.config';
-
-const mergedConfig = mergeConfig(ratingConfig, config);
-
-export const Rating = component(mergedConfig);
