c0cb321c8033d2dc6dd7c8e7d0600ab06885f49c
Ilya <illja.by4ek@gmail.com>
2024-12-17 02:10:16 +0300

    feat(new-hope): add icon xxs to button story

--- packages/plasma-new-hope/src/components/_Icon/Icons/IconMic.tsx
+++ packages/plasma-new-hope/src/components/_Icon/Icons/IconMic.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { Mic } from '../Icon.assets/Mic';
 import { IconRoot, IconProps } from '../IconRoot';
 
-export const IconMic: React.FC<IconProps> = ({ size = 's', color, className }) => {
-    return <IconRoot className={className} size={size} color={color} icon={Mic} />;
+export const IconMic: React.FC<IconProps> = ({ size = 's', color, className, sizeCustomValue }) => {
+    return <IconRoot className={className} sizeCustomValue={sizeCustomValue} size={size} color={color} icon={Mic} />;
 };
