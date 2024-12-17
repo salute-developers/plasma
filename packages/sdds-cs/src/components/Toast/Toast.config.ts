e53cd1ed56099fba0fc7d21646a6b7ddf65d629a
Ilya <illja.by4ek@gmail.com>
2024-12-18 01:24:20 +0300

    fix(sdds-cs): edit toast config

--- packages/sdds-cs/src/components/Toast/Toast.config.ts
+++ packages/sdds-cs/src/components/Toast/Toast.config.ts
@@ -19,22 +19,22 @@ export const config = {
                 ${toastTokens.color}: var(--on-dark-text-primary);
                 ${toastTokens.background}: var(--on-dark-surface-solid-primary-brightness);
 
-                ${toastTokens.closeIconColor}: var(--on-dark-text-secondary);
-                ${toastTokens.closeIconColorOnHover}: var(--on-dark-text-secondary);
+                ${toastTokens.closeIconColor}: var(--on-dark-text-accent);
+                ${toastTokens.closeIconColorOnHover}: var(--on-dark-text-accent-hover);
             `,
             light: css`
                 ${toastTokens.color}: var(--on-light-text-primary);
                 ${toastTokens.background}: var(--on-light-surface-solid-primary-brightness);
 
-                ${toastTokens.closeIconColor}: var(--on-light-text-secondary);
-                ${toastTokens.closeIconColorOnHover}: var(--on-light-text-secondary);
+                ${toastTokens.closeIconColor}: var(--on-light-text-accent);
+                ${toastTokens.closeIconColorOnHover}: var(--on-light-text-accent-hover);
             `,
         },
         size: {
             s: css`
                 ${toastTokens.borderRadius}: 0.75rem;
                 ${toastTokens.maxWidth}: calc(100vw - 5rem);
-                ${toastTokens.padding}: 0.5rem 0.5rem;
+                ${toastTokens.padding}: 0.5rem 0.75rem;
 
                 ${toastTokens.fontFamily}: var(--plasma-typo-body-m-font-family);
                 ${toastTokens.fontSize}: var(--plasma-typo-body-m-font-size);
@@ -43,7 +43,7 @@ export const config = {
                 ${toastTokens.letterSpacing}: var(--plasma-typo-body-m-letter-spacing);
                 ${toastTokens.lineHeight}: var(--plasma-typo-body-m-line-height);
 
-                ${toastTokens.leftContentMargin}: -0.0625rem 0.375rem -0.0625rem 0rem;
+                ${toastTokens.leftContentMargin}: -0.0625rem 0.5rem -0.0625rem 0rem;
                 ${toastTokens.closeIconMargin}: -0.0625rem 0.125rem -0.0625rem 0.75rem;
             `,
         },
@@ -58,7 +58,7 @@ export const config = {
                 ${toastTokens.pilledBorderRadius}: 1.5rem;
 
                 ${toastTokens.leftContentMargin}: -0.0625rem 0.375rem -0.0625rem 0rem;
-                ${toastTokens.closeIconMargin}: -0.0625rem 0rem -0.0625rem 0.375rem;
+                ${toastTokens.closeIconMargin}: -0.0625rem 0.25rem -0.0625rem 0.375rem;
             `,
         },
     },
