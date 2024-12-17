afc3dcd853f7d453179c76e2a5a4bed3ab99bc13
Neretin Artem <hawktrike@gmail.com>
2024-12-17 14:27:49 +0700

    feat(plasma-new-hope): Update `Dropdown` and `IconRoot` components

--- packages/plasma-new-hope/src/components/Dropdown/ui/DropdownItem/DropdownItem.tsx
+++ packages/plasma-new-hope/src/components/Dropdown/ui/DropdownItem/DropdownItem.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, FC, useContext } from 'react';
 
 import { classes } from '../../Dropdown.tokens';
 import { cx } from '../../../../utils';
-import { IconDisclosureRight } from '../../../_Icon';
 import { Context } from '../../Dropdown.context';
 import { getItemId } from '../../utils';
 
@@ -13,6 +12,7 @@ import {
     CellWrapper,
     StyledCell,
     RenderItemWrapper,
+    StyledIconDisclosureRight,
 } from './DropdownItem.styles';
 import type { DropdownItemProps } from './DropdownItem.type';
 
@@ -135,7 +135,7 @@ export const DropdownItem: FC<DropdownItemProps> = ({
 
                 {item.items && hasArrow && (
                     <DisclosureIconWrapper>
-                        <IconDisclosureRight size={disclosureIconSize} color="inherit" />
+                        <StyledIconDisclosureRight size={disclosureIconSize} color="inherit" />
                     </DisclosureIconWrapper>
                 )}
             </Wrapper>
