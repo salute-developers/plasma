229d947f21101d193d1f9bce35922ce94907e0ec
Neretin Artem <hawktrike@gmail.com>
2024-12-17 14:30:46 +0700

    feat(sdds-insol): Update configs in `Dropdown` and `Cell` components

--- packages/sdds-insol/src/components/Dropdown/Dropdown.config.ts
+++ packages/sdds-insol/src/components/Dropdown/Dropdown.config.ts
@@ -7,57 +7,21 @@ export const config = {
     },
     variations: {
         size: {
-            l: css`
+            xl: css`
                 ${tokens.padding}: 0.125rem;
                 ${tokens.width}: 17.5rem;
-                ${tokens.borderRadius}: 0.875rem;
+                ${tokens.borderRadius}: 1rem;
 
                 ${tokens.itemHeight}: 1.5rem;
-                ${tokens.itemPadding}: 1rem 1.125rem;
-                ${tokens.itemPaddingTight}: 0.75rem 1.125rem;
-                ${tokens.itemBorderRadius}: 0.75rem;
+                ${tokens.itemPadding}: 1.25rem;
+                ${tokens.itemPaddingTight}: 1rem 1.25rem;
+                ${tokens.itemGap}: 0.625rem;
+                ${tokens.itemGapTight}: 0.625rem;
 
                 ${tokens.dividerMarginTop}: 0.5rem;
                 ${tokens.dividerMarginRight}: 1.125rem;
                 ${tokens.dividerMarginBottom}: 0.5rem;
                 ${tokens.dividerMarginLeft}: 1.125rem;
-                ${tokens.dividerMarginTopTight}: 0.375rem;
-                ${tokens.dividerMarginBottomTight}: 0.375rem;
-
-                ${tokens.itemFontFamily}: var(--plasma-typo-body-l-font-family);
-                ${tokens.itemFontSize}: var(--plasma-typo-body-l-font-size);
-                ${tokens.itemFontStyle}: var(--plasma-typo-body-l-font-style);
-                ${tokens.itemFontWeightBold}: var(--plasma-typo-body-l-font-weight);
-                ${tokens.itemFontLetterSpacing}: var(--plasma-typo-body-l-letter-spacing);
-                ${tokens.itemFontLineHeight}: var(--plasma-typo-body-l-line-height);
-
-                ${tokens.cellPadding}: 0rem;
-                ${tokens.cellPaddingLeftContent}: 0rem;
-                ${tokens.cellPaddingContent}: 0rem;
-                ${tokens.cellPaddingRightContent}: 0rem;
-                ${tokens.cellTextboxGap}: 0.125rem;
-                ${tokens.cellGap}: 0.375rem;
-                ${tokens.cellTitleFontFamily}: var(--plasma-typo-body-l-font-family);
-                ${tokens.cellTitleFontSize}: var(--plasma-typo-body-l-font-size);
-                ${tokens.cellTitleFontStyle}: var(--plasma-typo-body-l-font-style);
-                ${tokens.cellTitleFontWeight}: var(--plasma-typo-body-l-font-weight);
-                ${tokens.cellTitleLetterSpacing}: var(--plasma-typo-body-l-letter-spacing);
-                ${tokens.cellTitleLineHeight}: var(--plasma-typo-body-l-line-height);
-            `,
-            m: css`
-                ${tokens.padding}: 0.125rem;
-                ${tokens.width}: 15rem;
-                ${tokens.borderRadius}: 0.75rem;
-
-                ${tokens.itemHeight}: 1.5rem;
-                ${tokens.itemPadding}: 0.75rem 0.875rem;
-                ${tokens.itemPaddingTight}: 0.5rem 0.875rem;
-                ${tokens.itemBorderRadius}: 0.625rem;
-
-                ${tokens.dividerMarginTop}: 0.375rem;
-                ${tokens.dividerMarginRight}: 0.875rem;
-                ${tokens.dividerMarginBottom}: 0.375rem;
-                ${tokens.dividerMarginLeft}: 0.875rem;
                 ${tokens.dividerMarginTopTight}: 0.375rem;
                 ${tokens.dividerMarginBottomTight}: 0.375rem;
 
@@ -67,13 +31,14 @@ export const config = {
                 ${tokens.itemFontWeightBold}: var(--plasma-typo-body-m-font-weight);
                 ${tokens.itemFontLetterSpacing}: var(--plasma-typo-body-m-letter-spacing);
                 ${tokens.itemFontLineHeight}: var(--plasma-typo-body-m-line-height);
+                ${tokens.itemDisclosureIconSize}: 1.5rem;
 
                 ${tokens.cellPadding}: 0rem;
                 ${tokens.cellPaddingLeftContent}: 0rem;
                 ${tokens.cellPaddingContent}: 0rem;
                 ${tokens.cellPaddingRightContent}: 0rem;
                 ${tokens.cellTextboxGap}: 0.125rem;
-                ${tokens.cellGap}: 0.375rem;
+                ${tokens.cellGap}: 0.625rem;
                 ${tokens.cellTitleFontFamily}: var(--plasma-typo-body-m-font-family);
                 ${tokens.cellTitleFontSize}: var(--plasma-typo-body-m-font-size);
                 ${tokens.cellTitleFontStyle}: var(--plasma-typo-body-m-font-style);
@@ -81,15 +46,94 @@ export const config = {
                 ${tokens.cellTitleLetterSpacing}: var(--plasma-typo-body-m-letter-spacing);
                 ${tokens.cellTitleLineHeight}: var(--plasma-typo-body-m-line-height);
             `,
+            l: css`
+                ${tokens.padding}: 0.125rem;
+                ${tokens.width}: 17.5rem;
+                ${tokens.borderRadius}: 0.875rem;
+
+                ${tokens.itemHeight}: 1.5rem;
+                ${tokens.itemPadding}: 1rem;
+                ${tokens.itemPaddingTight}: 0.75rem 1rem;
+                ${tokens.itemGap}: 0.625rem;
+                ${tokens.itemGapTight}: 0.625rem;
+
+                ${tokens.dividerMarginTop}: 0.5rem;
+                ${tokens.dividerMarginRight}: 1.125rem;
+                ${tokens.dividerMarginBottom}: 0.5rem;
+                ${tokens.dividerMarginLeft}: 1.125rem;
+                ${tokens.dividerMarginTopTight}: 0.375rem;
+                ${tokens.dividerMarginBottomTight}: 0.375rem;
+
+                ${tokens.itemFontFamily}: var(--plasma-typo-body-s-font-family);
+                ${tokens.itemFontSize}: var(--plasma-typo-body-s-font-size);
+                ${tokens.itemFontStyle}: var(--plasma-typo-body-s-font-style);
+                ${tokens.itemFontWeightBold}: var(--plasma-typo-body-s-font-weight);
+                ${tokens.itemFontLetterSpacing}: var(--plasma-typo-body-s-letter-spacing);
+                ${tokens.itemFontLineHeight}: var(--plasma-typo-body-s-line-height);
+                ${tokens.itemDisclosureIconSize}: 1.5rem;
+
+                ${tokens.cellPadding}: 0rem;
+                ${tokens.cellPaddingLeftContent}: 0rem;
+                ${tokens.cellPaddingContent}: 0rem;
+                ${tokens.cellPaddingRightContent}: 0rem;
+                ${tokens.cellTextboxGap}: 0.125rem;
+                ${tokens.cellGap}: 0.625rem;
+                ${tokens.cellTitleFontFamily}: var(--plasma-typo-body-s-font-family);
+                ${tokens.cellTitleFontSize}: var(--plasma-typo-body-s-font-size);
+                ${tokens.cellTitleFontStyle}: var(--plasma-typo-body-s-font-style);
+                ${tokens.cellTitleFontWeight}: var(--plasma-typo-body-s-font-weight);
+                ${tokens.cellTitleLetterSpacing}: var(--plasma-typo-body-s-letter-spacing);
+                ${tokens.cellTitleLineHeight}: var(--plasma-typo-body-s-line-height);
+            `,
+            m: css`
+                ${tokens.padding}: 0.125rem;
+                ${tokens.width}: 15rem;
+                ${tokens.borderRadius}: 0.75rem;
+
+                ${tokens.itemHeight}: 1.25rem;
+                ${tokens.itemPadding}: 0.875rem;
+                ${tokens.itemPaddingTight}: 0.625rem 0.875rem;
+                ${tokens.itemGap}: 0.625rem;
+                ${tokens.itemGapTight}: 0.625rem;
+
+                ${tokens.dividerMarginTop}: 0.375rem;
+                ${tokens.dividerMarginRight}: 0.875rem;
+                ${tokens.dividerMarginBottom}: 0.375rem;
+                ${tokens.dividerMarginLeft}: 0.875rem;
+                ${tokens.dividerMarginTopTight}: 0.375rem;
+                ${tokens.dividerMarginBottomTight}: 0.375rem;
+
+                ${tokens.itemFontFamily}: var(--plasma-typo-body-xs-font-family);
+                ${tokens.itemFontSize}: var(--plasma-typo-body-xs-font-size);
+                ${tokens.itemFontStyle}: var(--plasma-typo-body-xs-font-style);
+                ${tokens.itemFontWeightBold}: var(--plasma-typo-body-xs-font-weight);
+                ${tokens.itemFontLetterSpacing}: var(--plasma-typo-body-xs-letter-spacing);
+                ${tokens.itemFontLineHeight}: var(--plasma-typo-body-xs-line-height);
+                ${tokens.itemDisclosureIconSize}: 1.25rem;
+
+                ${tokens.cellPadding}: 0rem;
+                ${tokens.cellPaddingLeftContent}: 0rem;
+                ${tokens.cellPaddingContent}: 0rem;
+                ${tokens.cellPaddingRightContent}: 0rem;
+                ${tokens.cellTextboxGap}: 0.125rem;
+                ${tokens.cellGap}: 0.625rem;
+                ${tokens.cellTitleFontFamily}: var(--plasma-typo-body-xs-font-family);
+                ${tokens.cellTitleFontSize}: var(--plasma-typo-body-xs-font-size);
+                ${tokens.cellTitleFontStyle}: var(--plasma-typo-body-xs-font-style);
+                ${tokens.cellTitleFontWeight}: var(--plasma-typo-body-xs-font-weight);
+                ${tokens.cellTitleLetterSpacing}: var(--plasma-typo-body-xs-letter-spacing);
+                ${tokens.cellTitleLineHeight}: var(--plasma-typo-body-xs-line-height);
+            `,
             s: css`
                 ${tokens.padding}: 0.125rem;
                 ${tokens.width}: 12.5rem;
                 ${tokens.borderRadius}: 0.625rem;
 
-                ${tokens.itemHeight}: 1.5rem;
-                ${tokens.itemPadding}: 0.5rem 0.75rem;
-                ${tokens.itemPaddingTight}: 0.25rem 0.75rem;
-                ${tokens.itemBorderRadius}: 0.5rem;
+                ${tokens.itemHeight}: 1rem;
+                ${tokens.itemPadding}: 0.75rem 0.625rem;
+                ${tokens.itemPaddingTight}: 0.5rem 0.75rem;
+                ${tokens.itemGap}: 0.5rem;
+                ${tokens.itemGapTight}: 0.25rem;
 
                 ${tokens.dividerMarginTop}: 0.375rem;
                 ${tokens.dividerMarginRight}: 0.75rem;
@@ -98,25 +142,26 @@ export const config = {
                 ${tokens.dividerMarginTopTight}: 0.25rem;
                 ${tokens.dividerMarginBottomTight}: 0.25rem;
 
-                ${tokens.itemFontFamily}: var(--plasma-typo-body-s-font-family);
-                ${tokens.itemFontSize}: var(--plasma-typo-body-s-font-size);
-                ${tokens.itemFontStyle}: var(--plasma-typo-body-s-font-style);
-                ${tokens.itemFontWeightBold}: var(--plasma-typo-body-s-font-weight);
-                ${tokens.itemFontLetterSpacing}: var(--plasma-typo-body-s-letter-spacing);
-                ${tokens.itemFontLineHeight}: var(--plasma-typo-body-s-line-height);
+                ${tokens.itemFontFamily}: var(--plasma-typo-body-xs-font-family);
+                ${tokens.itemFontSize}: var(--plasma-typo-body-xs-font-size);
+                ${tokens.itemFontStyle}: var(--plasma-typo-body-xs-font-style);
+                ${tokens.itemFontWeightBold}: var(--plasma-typo-body-xs-font-weight);
+                ${tokens.itemFontLetterSpacing}: var(--plasma-typo-body-xs-letter-spacing);
+                ${tokens.itemFontLineHeight}: var(--plasma-typo-body-xs-line-height);
+                ${tokens.itemDisclosureIconSize}: 1rem;
 
                 ${tokens.cellPadding}: 0rem;
                 ${tokens.cellPaddingLeftContent}: 0rem;
                 ${tokens.cellPaddingContent}: 0rem;
                 ${tokens.cellPaddingRightContent}: 0rem;
                 ${tokens.cellTextboxGap}: 0.125rem;
-                ${tokens.cellGap}: 0.375rem;
-                ${tokens.cellTitleFontFamily}: var(--plasma-typo-body-s-font-family);
-                ${tokens.cellTitleFontSize}: var(--plasma-typo-body-s-font-size);
-                ${tokens.cellTitleFontStyle}: var(--plasma-typo-body-s-font-style);
-                ${tokens.cellTitleFontWeight}: var(--plasma-typo-body-s-font-weight);
-                ${tokens.cellTitleLetterSpacing}: var(--plasma-typo-body-s-letter-spacing);
-                ${tokens.cellTitleLineHeight}: var(--plasma-typo-body-s-line-height);
+                ${tokens.cellGap}: 0.5rem;
+                ${tokens.cellTitleFontFamily}: var(--plasma-typo-body-xs-font-family);
+                ${tokens.cellTitleFontSize}: var(--plasma-typo-body-xs-font-size);
+                ${tokens.cellTitleFontStyle}: var(--plasma-typo-body-xs-font-style);
+                ${tokens.cellTitleFontWeight}: var(--plasma-typo-body-xs-font-weight);
+                ${tokens.cellTitleLetterSpacing}: var(--plasma-typo-body-xs-letter-spacing);
+                ${tokens.cellTitleLineHeight}: var(--plasma-typo-body-xs-line-height);
             `,
             xs: css`
                 ${tokens.padding}: 0.125rem;
@@ -126,7 +171,8 @@ export const config = {
                 ${tokens.itemHeight}: 1rem;
                 ${tokens.itemPadding}: 0.5rem;
                 ${tokens.itemPaddingTight}: 0.25rem 0.5rem;
-                ${tokens.itemBorderRadius}: 0.375rem;
+                ${tokens.itemGap}: 0.375rem;
+                ${tokens.itemGapTight}: 0.375rem;
 
                 ${tokens.dividerMarginTop}: 0.25rem;
                 ${tokens.dividerMarginRight}: 0.5rem;
@@ -141,13 +187,14 @@ export const config = {
                 ${tokens.itemFontWeightBold}: var(--plasma-typo-body-xs-font-weight);
                 ${tokens.itemFontLetterSpacing}: var(--plasma-typo-body-xs-letter-spacing);
                 ${tokens.itemFontLineHeight}: var(--plasma-typo-body-xs-line-height);
+                ${tokens.itemDisclosureIconSize}: 1rem;
 
                 ${tokens.cellPadding}: 0rem;
                 ${tokens.cellPaddingLeftContent}: 0rem;
                 ${tokens.cellPaddingContent}: 0rem;
                 ${tokens.cellPaddingRightContent}: 0rem;
                 ${tokens.cellTextboxGap}: 0.125rem;
-                ${tokens.cellGap}: 0.25rem;
+                ${tokens.cellGap}: 0.375rem;
                 ${tokens.cellTitleFontFamily}: var(--plasma-typo-body-xs-font-family);
                 ${tokens.cellTitleFontSize}: var(--plasma-typo-body-xs-font-size);
                 ${tokens.cellTitleFontStyle}: var(--plasma-typo-body-xs-font-style);
@@ -158,6 +205,7 @@ export const config = {
         },
         view: {
             default: css`
+                ${tokens.itemBackgroundHover}: var(--surface-transparent-secondary);
                 ${tokens.dividerColor}: var(--surface-transparent-tertiary);
             `,
         },
