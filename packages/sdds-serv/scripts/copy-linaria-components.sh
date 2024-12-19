536be964369ac59062db77d4823c61f3524ca54c
TitanKuzmich <yzn-yfv@mail.ru>
2024-12-19 14:52:30 +0300

    feat(sdds-serv): fix typings path

--- packages/sdds-serv/scripts/copy-linaria-components.sh
+++ packages/sdds-serv/scripts/copy-linaria-components.sh
@@ -9,7 +9,7 @@ touch src-css/index.d.ts
 for component in $components; do
     cp -R src/components/$component src-css/components/;
     grep -E "\<$component\>" src/index.ts >> src-css/index.ts
-    echo "export * from '../components/$component';" >> css/index.d.ts;
+    echo "export * from '../components/$component';" >> src-css/index.d.ts;
     
 done;
 
