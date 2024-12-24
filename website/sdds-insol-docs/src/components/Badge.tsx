00841e48454cedc228c3ab0d2ee404e9cca058e9
Ilya <illja.by4ek@gmail.com>
2024-12-24 16:10:56 +0300

    feat(docs): add badge for only sc components

---
+++ website/sdds-insol-docs/src/components/Badge.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import type { FC } from 'react';
+import { Badge, Tooltip, BodyXS } from '@salutejs/sdds-insol';
+
+export const DocsBadge: FC<{ title: string; desctiption: string }> = ({ title, desctiption }) => (
+    <div
+        style={{
+            marginTop: '-22px',
+            marginBottom: '16px',
+        }}
+    >
+        <Tooltip
+            target={
+                <Badge size="m" view="accent">
+                    <BodyXS bold>{title}</BodyXS>
+                </Badge>
+            }
+            text={desctiption}
+            placement="right-start"
+            trigger="hover"
+            hoverTimeout={500}
+            maxWidth="400px"
+            hasArrow={false}
+            style={{
+                verticalAlign: 'middle',
+            }}
+        />
+    </div>
+);
