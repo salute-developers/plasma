48bb98bf04d42b608b8609541209aa5298c73385
TitanKuzmich <yzn-yfv@mail.ru>
2025-01-09 16:21:03 +0300

    feat(plasma-new-hope): add extra content to Avatar

--- packages/plasma-new-hope/src/components/Avatar/Avatar.types.ts
+++ packages/plasma-new-hope/src/components/Avatar/Avatar.types.ts
@@ -1,8 +1,48 @@
 import { HTMLAttributes } from 'react';
 
+import type { CustomCounterProps } from '../Counter/Counter.types';
+import type { CustomBadgeProps } from '../Badge/Badge.types';
+
+type Only<T, U> = {
+    [P in keyof T]: T[P];
+} &
+    {
+        [P in keyof U]?: never;
+    };
+
+type OneOf<T, U> = Only<T, U> | Only<U, T>;
+
+type ExtraBadgeProps = {
+    /**
+     * Вид Badge
+     * @default
+     * default
+     */
+    badgeView?: string;
+    /**
+     * Компонент c округлым border-radius
+     */
+    pilled?: boolean;
+} & Omit<CustomBadgeProps, 'size' | 'view' | 'maxWidth'>;
+
+type ExtraCounterProps = {
+    /**
+     * Вид Counter
+     * @default
+     * default
+     */
+    counterView?: string;
+    /**
+     * Отображаемое число. Не может быть меньше 1.
+     */
+    count?: number;
+} & Omit<CustomCounterProps, 'size' | 'view' | 'count'>;
+
+type ExtraProps = OneOf<ExtraBadgeProps, ExtraCounterProps>;
+
 type CustomAvatarProps = {
     // Размер аватара
-    size: 'xxl' | 'l' | 'm' | 's' | 'fit';
+    size?: string;
     // ФИО человека
     name?: string;
     // Ссылка на фотографию
@@ -21,7 +61,19 @@ type CustomAvatarProps = {
      * { active: 'Активен', inactive: 'Неактивен' }
      */
     statusLabels?: StatusLabels;
-};
+    /**
+     * Включить отображение дополнительного элемента
+     */
+    hasExtra?: boolean;
+    /**
+     * Тип дополнительного элемента
+     */
+    type?: 'badge' | 'counter';
+    /**
+     * Расположение дополнительного элемента
+     */
+    extraPlacement?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+} & ExtraProps;
 
 export type StatusLabels = Record<'active' | 'inactive', string>;
 
