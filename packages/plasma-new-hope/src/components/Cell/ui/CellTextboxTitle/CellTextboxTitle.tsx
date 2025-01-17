fb2d86c9a4c760c28723d6e4ea35e97f83683bb4
Vladislav Denisov <deni.vladislav@gmail.com>
2025-01-17 12:30:31 +0300

    fix(plasma-new-hope): CellTextbox components props fixed

--- packages/plasma-new-hope/src/components/Cell/ui/CellTextboxTitle/CellTextboxTitle.tsx
+++ packages/plasma-new-hope/src/components/Cell/ui/CellTextboxTitle/CellTextboxTitle.tsx
@@ -1,8 +1,14 @@
 import React, { HTMLAttributes } from 'react';
 
+import { cx } from '../../../../utils';
+
 import { StyledTextboxTitle } from './CellTextboxTitle.styles';
 
-export const CellTextboxTitle: React.FC<HTMLAttributes<HTMLDivElement>> = (props) => {
+export const CellTextboxTitle: React.FC<HTMLAttributes<HTMLDivElement>> = ({ children, className, ...rest }) => {
     // TODO: #1548
-    return <StyledTextboxTitle className="cell-textbox-title">{props.children}</StyledTextboxTitle>;
+    return (
+        <StyledTextboxTitle className={cx('cell-textbox-title', className)} {...rest}>
+            {children}
+        </StyledTextboxTitle>
+    );
 };
