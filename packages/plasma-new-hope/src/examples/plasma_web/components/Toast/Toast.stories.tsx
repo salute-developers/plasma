2176bbef6e4a54ff13c82ea6779043b29c497a74
Vladislav Denisov <deni.vladislav@gmail.com>
2025-01-23 15:17:02 +0300

    feat(plasma-new-hope): positive and negative views added; dark and light view marked as deprecated

--- packages/plasma-new-hope/src/examples/plasma_web/components/Toast/Toast.stories.tsx
+++ packages/plasma-new-hope/src/examples/plasma_web/components/Toast/Toast.stories.tsx
@@ -4,21 +4,39 @@ import { action } from '@storybook/addon-actions';
 import styled from 'styled-components';
 import { disableProps } from '@salutejs/plasma-sb-utils';
 
-import { toastConfig } from '../../../../components/Toast';
-import { mergeConfig } from '../../../../engines';
-import { WithTheme, argTypesFromConfig } from '../../../_helpers';
+import { WithTheme } from '../../../_helpers';
 import { Button } from '../Button/Button';
 import { PopupProvider } from '../Popup/Popup';
 import { addNotification } from '../../../../../src/components/Notification';
 import { NotificationsProvider } from '../Notification/Notification';
 import { Modal } from '../Modal/Modal';
 
-import { config } from './Toast.config';
 import { Toast, ToastController, ToastProvider, useToast } from './Toast';
+
+const views = ['default', 'positive', 'negative'];
+const closeIconTypes = ['default', 'thin'];
 
 const meta: Meta<typeof ToastController> = {
     title: 'web/Overlay/Toast',
     decorators: [WithTheme],
+    argTypes: {
+        view: {
+            options: views,
+            control: {
+                type: 'select',
+            },
+        },
+        closeIconType: {
+            options: closeIconTypes,
+            control: {
+                type: 'select',
+            },
+        },
+        textColor: {
+            control: 'color',
+        },
+        ...disableProps(['size']),
+    },
 };
 
 export default meta;
@@ -61,20 +79,13 @@ const Container = styled.div`
 export const ToastComponent: StoryComponent = {
     args: {
         text: 'Текст всплывающего уведомления',
+        textColor: undefined,
         view: 'default',
         closeIconType: 'default',
         size: 'm',
         hasClose: true,
         enableContentLeft: true,
         pilled: false,
-    },
-    argTypes: {
-        closeIconType: {
-            options: ['default', 'thin'],
-            control: {
-                type: 'select',
-            },
-        },
     },
     render: ({ enableContentLeft, ...args }) => (
         <ToastContainer>
@@ -94,6 +105,8 @@ const StoryLiveDemo = ({
     size,
     hasClose,
     enableContentLeft,
+    closeIconType,
+    textColor,
 }: StoryProps) => {
     const { showToast, hideToast } = useToast();
     const contentLeft = enableContentLeft && <BellIcon width="1rem" height="1rem" />;
@@ -114,6 +127,8 @@ const StoryLiveDemo = ({
                         view,
                         size,
                         hasClose,
+                        closeIconType,
+                        textColor,
                         onHide: action('onHide'),
                         onShow: action('onShow'),
                     });
@@ -141,7 +156,6 @@ export const LiveDemo: Story = {
                 type: 'inline-radio',
             },
         },
-        ...argTypesFromConfig(mergeConfig(toastConfig, config)),
         ...disableProps(['role', 'onShow', 'onHide', 'contentLeft']),
     },
     parameters: {
@@ -167,6 +181,8 @@ const StoryComplex = ({
     size,
     hasClose,
     enableContentLeft,
+    closeIconType,
+    textColor,
 }: StoryProps) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -185,6 +201,8 @@ const StoryComplex = ({
             view,
             size,
             hasClose,
+            closeIconType,
+            textColor,
             onHide: action('onHide'),
             onShow: action('onShow'),
         });
