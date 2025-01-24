bb739651718a0cd480abdd803b9f788b4ceabc49
TitanKuzmich <yzn-yfv@mail.ru>
2025-01-24 15:32:27 +0300

    feat(plasma-*): add Header sizes to configs to Accordion

--- packages/plasma-web/src/components/Accordion/Accordion.config.ts
+++ packages/plasma-web/src/components/Accordion/Accordion.config.ts
@@ -45,8 +45,8 @@ export const config = {
                 ${accordionTokens.accordionItemIconColor}: var(--text-primary);
                 ${accordionTokens.accordionItemFocus}: var(--surface-accent);
                 ${accordionTokens.accordionBackground}: var(--surface-clear);
-                ${accordionTokens.accordionItemBorder}: 0.125rem solid var(--surface-transparent-secondary);
-                ${accordionTokens.accordionItemBorderBottom}: 0.125rem solid var(--surface-transparent-secondary);
+                ${accordionTokens.accordionItemBorder}: 0.063rem solid var(--surface-transparent-secondary);
+                ${accordionTokens.accordionItemBorderBottom}: 0.063rem solid var(--surface-transparent-secondary);
                 ${accordionTokens.accordionItemPaddingHorizontalLeft}: var(${accordionTokens.accordionItemPaddingHorizontal});
             `,
             clear: css`
@@ -61,7 +61,7 @@ export const config = {
                 ${accordionTokens.accordionItemBorderRadius}: 0rem !important;
                 ${accordionTokens.accordionBackground}: var(--surface-clear);
                 ${accordionTokens.accordionItemBorder}: none;
-                ${accordionTokens.accordionItemBorderBottom}: 0.125rem solid var(--surface-solid-tertiary);
+                ${accordionTokens.accordionItemBorderBottom}: 0.063rem solid var(--surface-solid-tertiary);
                 ${accordionTokens.accordionItemPaddingHorizontalLeft}: 0;
             `,
         },
@@ -145,6 +145,88 @@ export const config = {
                 ${accordionTokens.accordionItemTextFontWeight}: var(--plasma-typo-body-xs-font-weight);
                 ${accordionTokens.accordionItemTextLetterSpacing}: var(--plasma-typo-body-xs-letter-spacing);
                 ${accordionTokens.accordionItemTextLineHeight}: var(--plasma-typo-body-xs-line-height);
+            `,
+            h2: css`
+                ${accordionTokens.accordionItemPaddingVertical}: 1rem;
+                ${accordionTokens.accordionItemPaddingHorizontal}: 1.25rem;
+                ${accordionTokens.accordionItemGap}: 0.25rem;
+                ${accordionTokens.accordionItemBorderRadius}: 0.75rem;
+                ${accordionTokens.accordionItemIconSize}: 1.5rem;
+
+                ${accordionTokens.accordionItemTitleFontFamily}: var(--plasma-typo-h2-font-family);
+                ${accordionTokens.accordionItemTitleFontSize}: var(--plasma-typo-h2-font-size);
+                ${accordionTokens.accordionItemTitleFontStyle}: var(--plasma-typo-h2-font-style);
+                ${accordionTokens.accordionItemTitleFontWeight}: var(--plasma-typo-h2-bold-font-weight);
+                ${accordionTokens.accordionItemTitleLetterSpacing}: var(--plasma-typo-h2-letter-spacing);
+                ${accordionTokens.accordionItemTitleLineHeight}: var(--plasma-typo-h2-line-height);
+
+                ${accordionTokens.accordionItemTextFontFamily}: var(--plasma-typo-body-l-font-family);
+                ${accordionTokens.accordionItemTextFontSize}: var(--plasma-typo-body-l-font-size);
+                ${accordionTokens.accordionItemTextFontStyle}: var(--plasma-typo-body-l-font-style);
+                ${accordionTokens.accordionItemTextFontWeight}: var(--plasma-typo-body-l-font-weight);
+                ${accordionTokens.accordionItemTextLetterSpacing}: var(--plasma-typo-body-l-letter-spacing);
+                ${accordionTokens.accordionItemTextLineHeight}: var(--plasma-typo-body-l-line-height);
+            `,
+            h3: css`
+                ${accordionTokens.accordionItemPaddingVertical}: 0.875rem;
+                ${accordionTokens.accordionItemPaddingHorizontal}: 1.125rem;
+                ${accordionTokens.accordionItemGap}: 0.25rem;
+                ${accordionTokens.accordionItemBorderRadius}: 0.75rem;
+                ${accordionTokens.accordionItemIconSize}: 1.5rem;
+
+                ${accordionTokens.accordionItemTitleFontFamily}: var(--plasma-typo-h3-font-family);
+                ${accordionTokens.accordionItemTitleFontSize}: var(--plasma-typo-h3-font-size);
+                ${accordionTokens.accordionItemTitleFontStyle}: var(--plasma-typo-h3-font-style);
+                ${accordionTokens.accordionItemTitleFontWeight}: var(--plasma-typo-h3-bold-font-weight);
+                ${accordionTokens.accordionItemTitleLetterSpacing}: var(--plasma-typo-h3-letter-spacing);
+                ${accordionTokens.accordionItemTitleLineHeight}: var(--plasma-typo-h3-line-height);
+
+                ${accordionTokens.accordionItemTextFontFamily}: var(--plasma-typo-body-l-font-family);
+                ${accordionTokens.accordionItemTextFontSize}: var(--plasma-typo-body-l-font-size);
+                ${accordionTokens.accordionItemTextFontStyle}: var(--plasma-typo-body-l-font-style);
+                ${accordionTokens.accordionItemTextFontWeight}: var(--plasma-typo-body-l-font-weight);
+                ${accordionTokens.accordionItemTextLetterSpacing}: var(--plasma-typo-body-l-letter-spacing);
+                ${accordionTokens.accordionItemTextLineHeight}: var(--plasma-typo-body-l-line-height);
+            `,
+            h4: css`
+                ${accordionTokens.accordionItemPaddingVertical}: 0.688rem;
+                ${accordionTokens.accordionItemPaddingHorizontal}: 0.875rem;
+                ${accordionTokens.accordionItemGap}: 0.25rem;
+                ${accordionTokens.accordionItemBorderRadius}: 0.625rem;
+
+                ${accordionTokens.accordionItemTitleFontFamily}: var(--plasma-typo-h4-font-family);
+                ${accordionTokens.accordionItemTitleFontSize}: var(--plasma-typo-h4-font-size);
+                ${accordionTokens.accordionItemTitleFontStyle}: var(--plasma-typo-h4-font-style);
+                ${accordionTokens.accordionItemTitleFontWeight}: var(--plasma-typo-h4-bold-font-weight);
+                ${accordionTokens.accordionItemTitleLetterSpacing}: var(--plasma-typo-h4-letter-spacing);
+                ${accordionTokens.accordionItemTitleLineHeight}: var(--plasma-typo-h4-line-height);
+
+                ${accordionTokens.accordionItemTextFontFamily}: var(--plasma-typo-body-m-font-family);
+                ${accordionTokens.accordionItemTextFontSize}: var(--plasma-typo-body-m-font-size);
+                ${accordionTokens.accordionItemTextFontStyle}: var(--plasma-typo-body-m-font-style);
+                ${accordionTokens.accordionItemTextFontWeight}: var(--plasma-typo-body-m-font-weight);
+                ${accordionTokens.accordionItemTextLetterSpacing}: var(--plasma-typo-body-m-letter-spacing);
+                ${accordionTokens.accordionItemTextLineHeight}: var(--plasma-typo-body-m-line-height);
+            `,
+            h5: css`
+                ${accordionTokens.accordionItemPaddingVertical}: 0.5rem;
+                ${accordionTokens.accordionItemPaddingHorizontal}: 0.75rem;
+                ${accordionTokens.accordionItemGap}: 0.25rem;
+                ${accordionTokens.accordionItemBorderRadius}: 0.5rem;
+
+                ${accordionTokens.accordionItemTitleFontFamily}: var(--plasma-typo-h5-font-family);
+                ${accordionTokens.accordionItemTitleFontSize}: var(--plasma-typo-h5-font-size);
+                ${accordionTokens.accordionItemTitleFontStyle}: var(--plasma-typo-h5-font-style);
+                ${accordionTokens.accordionItemTitleFontWeight}: var(--plasma-typo-h5-bold-font-weight);
+                ${accordionTokens.accordionItemTitleLetterSpacing}: var(--plasma-typo-h5-letter-spacing);
+                ${accordionTokens.accordionItemTitleLineHeight}: var(--plasma-typo-h5-line-height);
+
+                ${accordionTokens.accordionItemTextFontFamily}: var(--plasma-typo-body-m-font-family);
+                ${accordionTokens.accordionItemTextFontSize}: var(--plasma-typo-body-m-font-size);
+                ${accordionTokens.accordionItemTextFontStyle}: var(--plasma-typo-body-m-font-style);
+                ${accordionTokens.accordionItemTextFontWeight}: var(--plasma-typo-body-m-font-weight);
+                ${accordionTokens.accordionItemTextLetterSpacing}: var(--plasma-typo-body-m-letter-spacing);
+                ${accordionTokens.accordionItemTextLineHeight}: var(--plasma-typo-body-m-line-height);
             `,
         },
         stretching: {
