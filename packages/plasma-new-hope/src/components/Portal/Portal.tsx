75a6b8d609763465775438f53432d3d7776906e8
TitanKuzmich <yzn-yfv@mail.ru>
2025-01-29 18:22:49 +0300

    feat(plasma-new-hope): fix Portal logic and TextField Hint size

--- packages/plasma-new-hope/src/components/Portal/Portal.tsx
+++ packages/plasma-new-hope/src/components/Portal/Portal.tsx
@@ -1,8 +1,6 @@
 import React, { FC } from 'react';
 import ReactDOM from 'react-dom';
 
-import { canUseDOM } from '../../utils';
-
 import { PortalProps } from './Portal.types';
 
 /**
@@ -10,16 +8,7 @@ import { PortalProps } from './Portal.types';
  * Представляет собой ReactDOM.createPortal() в форме компонента.
  */
 export const Portal: FC<PortalProps> = ({ children, container, disabled = false }) => {
-    if (!canUseDOM) {
-        return null;
-    }
-
     const portalContainer = typeof container === 'function' ? container() : container;
 
-    return (
-        <>
-            {(disabled || !portalContainer) && children}
-            {!disabled && portalContainer && ReactDOM.createPortal(children, portalContainer)}
-        </>
-    );
+    return <>{disabled || !portalContainer ? children : ReactDOM.createPortal(children, portalContainer)}</>;
 };
