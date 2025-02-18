cfea3c6806c0655aa7c67da00f6ff6fd7150bdd0
Neretin Artem <hawktrike@gmail.com>
2025-02-18 18:10:56 +0500

    feat(plasma-*,sdds-*): Add `applyPaper` mixin

--- packages/sdds-cs/src/mixins/index.ts
+++ packages/sdds-cs/src/mixins/index.ts
@@ -1,1 +1,6 @@
+import { createApplyPaperMixin } from '@salutejs/plasma-new-hope/styled-components';
+import * as allTokens from '@salutejs/sdds-themes/tokens/sdds_cs';
+
 export { addFocus, mediaQuery } from '@salutejs/plasma-new-hope/styled-components';
+
+export const applyPaper = createApplyPaperMixin(allTokens);
