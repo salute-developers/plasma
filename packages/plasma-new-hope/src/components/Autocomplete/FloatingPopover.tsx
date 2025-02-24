e288adebef2cb0c04c9a208fb79a6f4cdd556f06
Dima Shugaev <50920576+shuga2704@users.noreply.github.com>
2025-02-24 19:44:11 +0300

    feat(plasma-new-hope): new architecture in Autocomplete

---
+++ packages/plasma-new-hope/src/components/Autocomplete/FloatingPopover.tsx
@@ -0,0 +1,75 @@
+import { flip, shift, size, useFloating, FloatingPortal } from '@floating-ui/react';
+import React, { forwardRef } from 'react';
+import { safeUseId } from '@salutejs/plasma-core';
+
+import type { FloatingPopoverProps } from './Autocomplete.types';
+
+const FloatingPopover = forwardRef<HTMLDivElement, FloatingPopoverProps>(
+    ({ target, children, opened, portal, listWidth }, ref) => {
+        const { refs, floatingStyles } = useFloating({
+            placement: 'bottom-start',
+            open: opened,
+            middleware: [
+                flip({ fallbackAxisSideDirection: 'end' }),
+                shift(),
+                size({
+                    apply({ rects, elements }) {
+                        Object.assign(elements.floating.style, {
+                            width: listWidth || `${rects.reference.width}px`,
+                        });
+                    },
+                }),
+            ],
+        });
+
+        const wrappedId = safeUseId();
+
+        // Проверка на target. Это может быть как ReactNode, так и функция, в которую пробрасывается ref.
+        // Это нужно для более тонкой настройки reference-элемента, вокруг которого и будет позиционироваться выпадашка.
+        // Пример: когда в Textfield под инпутом находится helperText (или еще что-либо),
+        // но выпадающий список должен позиционироваться непосредственно возле самого инпута.
+        const isTargetAsFunction = typeof target === 'function';
+
+        return (
+            <div ref={ref} id={wrappedId} style={{ position: 'relative' }}>
+                <div ref={isTargetAsFunction ? undefined : refs.setReference}>
+                    {typeof target === 'function' ? target(refs.setReference as any) : target}
+                </div>
+
+                {opened && (
+                    // root - принимает ref контейнера портала.
+                    // id - если есть портал - не используется, если портала нет - подставляется 'wrappedId'.
+                    <FloatingPortal {...getFloatingPortalProps(portal, wrappedId)}>
+                        <div ref={refs.setFloating} style={{ ...floatingStyles, zIndex: 1000 }}>
+                            {children}
+                        </div>
+                    </FloatingPortal>
+                )}
+            </div>
+        );
+    },
+);
+
+type FloatingPortalReturnedProps = {
+    root?: React.RefObject<HTMLElement>;
+    id?: string;
+};
+
+// root - принимает ref контейнера портала.
+// id - если есть портал - не используется, если портала нет - подставляется 'wrappedId'.
+const getFloatingPortalProps = (
+    portal: FloatingPopoverProps['portal'],
+    wrappedId: string,
+): FloatingPortalReturnedProps => {
+    if (!portal) {
+        return { id: wrappedId };
+    }
+
+    if (typeof portal === 'string') {
+        return { id: portal };
+    }
+
+    return { root: portal };
+};
+
+export { FloatingPopover };
