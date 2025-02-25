4eccc786c9132cd6333b1e14a0a80580928a44d4
IgorYar <ihteyar2@yandex.ru>
2025-02-25 10:59:16 +0500

    test(plasma-b2c): update tests for Counter, Segment, Accordion, DatePicker

---
+++ packages/plasma-b2c/src/components/Accordion/Accordion.update-test.component-test.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import type { FC, PropsWithChildren } from 'react';
+import { CypressTestDecorator, getComponent, mount } from '@salutejs/plasma-cy-utils';
+import { standard as standardTypo } from '@salutejs/plasma-typo';
+import { createGlobalStyle } from 'styled-components';
+
+const StandardTypoStyle = createGlobalStyle(standardTypo);
+
+describe('plasma-b2c: Accordion', () => {
+    const Accordion = getComponent('Accordion');
+    const AccordionItem = getComponent('AccordionItem');
+
+    const title = 'Как оплатить заправку бонусами СберСпасибо?';
+    const body =
+        'После указания деталей заправки нажмите кнопку «К оплате». Откроется окно оплаты, где вы сможете списать бонусы и оплатить ими до 99% стоимости топливa';
+
+    const CypressTestDecoratorWithTypo: FC<PropsWithChildren> = ({ children }) => (
+        <CypressTestDecorator>
+            <StandardTypoStyle />
+            {children}
+        </CypressTestDecorator>
+    );
+
+    it('[PLASMA-T1708] Accordion: size=l, view=default, stretching=filled, type=arrow', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Accordion size="l" view="default" stretching="filled">
+                    <AccordionItem type="arrow" title={title}>
+                        {body}
+                    </AccordionItem>
+                    <AccordionItem type="arrow" title={title}>
+                        {body}
+                    </AccordionItem>
+                    <AccordionItem type="arrow" title={title}>
+                        {body}
+                    </AccordionItem>
+                </Accordion>
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('.accordion-root').last().trigger('click', { waitForAnimations: true });
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1709] Accordion: size=m, view=secondary, stretching=fixed, type=sign, alignWithTitle', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Accordion size="m" view="secondary" stretching="fixed">
+                    <AccordionItem type="sign" alignWithTitle title={title}>
+                        {body}
+                    </AccordionItem>
+                    <AccordionItem type="sign" alignWithTitle title={title}>
+                        {body}
+                    </AccordionItem>
+                    <AccordionItem type="sign" alignWithTitle title={title}>
+                        {body}
+                    </AccordionItem>
+                </Accordion>
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('.accordion-root').last().trigger('click', { waitForAnimations: true });
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1710] Accordion: size=s, view=clear, type=clear', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Accordion size="s" view="clear">
+                    <AccordionItem type="clear" title={title}>
+                        {body}
+                    </AccordionItem>
+                    <AccordionItem type="clear" title={title}>
+                        {body}
+                    </AccordionItem>
+                    <AccordionItem type="clear" title={title}>
+                        {body}
+                    </AccordionItem>
+                </Accordion>
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('.accordion-root').last().trigger('click', { waitForAnimations: true });
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1711] Accordion: size=xs, view=outlined', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Accordion size="xs" view="outlined">
+                    <AccordionItem title={title}>{body}</AccordionItem>
+                    <AccordionItem title={title}>{body}</AccordionItem>
+                    <AccordionItem title={title}>{body}</AccordionItem>
+                </Accordion>
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.get('.accordion-root').last().trigger('click', { waitForAnimations: true });
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1712] Accordion: disabled', () => {
+        mount(
+            <CypressTestDecoratorWithTypo>
+                <Accordion size="l" disabled>
+                    <AccordionItem title={title}>{body}</AccordionItem>
+                    <AccordionItem title={title}>{body}</AccordionItem>
+                    <AccordionItem title={title}>{body}</AccordionItem>
+                </Accordion>
+            </CypressTestDecoratorWithTypo>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+});
---
+++ packages/plasma-b2c/src/components/Counter/Counter.update-test.component-test.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { mount, CypressTestDecorator, getComponent } from '@salutejs/plasma-cy-utils';
+
+describe('plasma-b2c: Counter', () => {
+    const Counter = getComponent('Counter');
+
+    it('[PLASMA-T1724] Counter: view=default, size=l', () => {
+        mount(
+            <CypressTestDecorator>
+                <Counter count={123} maxCount={200} view="default" size="l" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1725] Counter: view=accent, size=m, maxCount=2', () => {
+        mount(
+            <CypressTestDecorator>
+                <Counter count={123} maxCount={2} view="accent" size="m" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1726] Counter: view=positive, size=s, high count value', () => {
+        mount(
+            <CypressTestDecorator>
+                <Counter count={999999999999999} maxCount={999999999999999} view="positive" size="s" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1727] Counter: view=warning, size=xs', () => {
+        mount(
+            <CypressTestDecorator>
+                <Counter count={123} maxCount={200} view="warning" size="xs" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1728] Counter: view=negative, size=xxs', () => {
+        mount(
+            <CypressTestDecorator>
+                <Counter count={123} maxCount={200} view="negative" size="xxs" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('[PLASMA-T1965] Counter: negative count', () => {
+        mount(
+            <CypressTestDecorator>
+                <Counter count={-1} maxCount={200} view="default" size="l" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+});
