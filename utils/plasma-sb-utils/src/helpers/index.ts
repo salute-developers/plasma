f71af16e656749a06ce9056fb3c248b84d50ad3d
TitanKuzmich <yzn-yfv@mail.ru>
2025-03-13 11:59:59 +0300

    feat(plasma-sb-utils): refactor colorTokens utils

--- utils/plasma-sb-utils/src/helpers/index.ts
+++ utils/plasma-sb-utils/src/helpers/index.ts
@@ -3,5 +3,6 @@ export { flattenPalette } from './flattenPalette';
 export { extractCanvasThemeColors, extractWebThemeColors } from './themeColors';
 export { getOpacityFromHex, getGroupedTokens } from './colorTokens';
 export { getConfigVariations } from './getConfigVariations';
+export { upperFirstLetter } from './colorTokens';
 
-export type { GroupedTokens } from './colorTokens';
+export type { GroupedTokens, TokenData } from './colorTokens';
