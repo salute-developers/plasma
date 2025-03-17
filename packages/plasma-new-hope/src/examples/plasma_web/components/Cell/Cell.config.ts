c356621e2059da9b71ff736aef34500f24f58441
Ilya <illja.by4ek@gmail.com>
2025-03-18 02:32:04 +0300

    fix(*): edit gap in cell

--- packages/plasma-new-hope/src/examples/plasma_web/components/Cell/Cell.config.ts
+++ packages/plasma-new-hope/src/examples/plasma_web/components/Cell/Cell.config.ts
@@ -55,7 +55,7 @@ export const config = {
                 ${cellTokens.cellPaddingRightContent}: 0rem;
 
                 ${cellTokens.cellTextboxGap}: 0.125rem;
-                ${cellTokens.cellGap}: 0.375rem;
+                ${cellTokens.cellGap}: 0.5rem;
 
                 ${cellTokens.cellLabelFontFamily}: var(--plasma-typo-body-s-font-family);
                 ${cellTokens.cellLabelFontSize}: var(--plasma-typo-body-s-font-size);
@@ -119,7 +119,7 @@ export const config = {
                 ${cellTokens.cellPaddingRightContent}: 0rem;
 
                 ${cellTokens.cellTextboxGap}: 0.125rem;
-                ${cellTokens.cellGap}: 0.25rem;
+                ${cellTokens.cellGap}: 0.375rem;
                 ${cellTokens.cellGap}: 0.375rem;
 
                 ${cellTokens.cellLabelFontFamily}: var(--plasma-typo-body-xs-font-family);
