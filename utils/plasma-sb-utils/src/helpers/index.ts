bce2ffdca5d2b9ca47978788c09766a6363e6a5a
TitanKuzmich <yzn-yfv@mail.ru>
2025-03-19 22:17:10 +0300

    feat(plasma-sb-utils): add typography tokens utils

--- utils/plasma-sb-utils/src/helpers/index.ts
+++ utils/plasma-sb-utils/src/helpers/index.ts
@@ -1,10 +1,11 @@
 export { disableProps } from './disableProps';
 export { flattenPalette } from './flattenPalette';
 export { extractCanvasThemeColors, extractWebThemeColors } from './themeColors';
-export { getOpacityFromHex, getGroupedTokens } from './colorTokens';
 export { getGroupedCornerRadiusTokens, getGroupedSpacingTokens } from './numberTokens';
+export { getOpacityFromHex, getGroupedTokens, upperFirstLetter } from './colorTokens';
+export { getGroupedTypographyTokens, typographyPangrams, typographyToCssMap } from './typographyTokens';
 export { getConfigVariations } from './getConfigVariations';
-export { upperFirstLetter } from './colorTokens';
 
 export type { GroupedTokens, TokenData } from './colorTokens';
 export type { GroupedNumberTokens } from './numberTokens';
+export type { TypographyStructure, TypographyProperties, Breakpoint, TypographyWeight } from './typographyTokens';
