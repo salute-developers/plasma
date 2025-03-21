500396883e35287c0960b0b31a18c71889b9be70
Alexander Lobyntsev <yakutoc@gmail.com>
2025-03-21 11:17:33 +0700

    chore(plasma-giga): bump `linaria@6.2.0`

--- packages/plasma-giga/rollup.config.mjs
+++ packages/plasma-giga/rollup.config.mjs
@@ -4,7 +4,7 @@ import { createFilter } from '@rollup/pluginutils'
 
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-import linaria from '@linaria/rollup';
+import linaria from '@wyw-in-js/rollup';
 import { babel } from '@rollup/plugin-babel';
 
 import styles from "@ironkinoko/rollup-plugin-styles";
