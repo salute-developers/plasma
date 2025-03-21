18deff43d7d8a2440ed551d3c0595cd3565cc0fb
Alexander Lobyntsev <yakutoc@gmail.com>
2025-03-21 13:40:47 +0700

    feat(sdds-cs): processing isLoading state

--- packages/sdds-cs/src/components/Button/Button.tsx
+++ packages/sdds-cs/src/components/Button/Button.tsx
@@ -1,13 +1,42 @@
-import { buttonConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
+import React, { ComponentProps, useCallback, MouseEvent } from 'react';
+import {
+    buttonConfig,
+    component,
+    mergeConfig,
+    buttonClasses,
+    buttonTokens,
+} from '@salutejs/plasma-new-hope/styled-components';
+import styled from 'styled-components';
 
 import { config } from './Button.config';
 
 const mergedConfig = mergeConfig(buttonConfig, config);
 const ButtonComponent = component(mergedConfig);
 
-export type ButtonProps = typeof ButtonComponent;
+export type ButtonProps = ComponentProps<typeof ButtonComponent>;
+
+// INFO: Данное поведение запрошено продуктом/командой `sdds-cs`
+const StyledButton = styled(ButtonComponent)`
+    &&.${String(buttonClasses.buttonLoading)} {
+        color: var(${buttonTokens.buttonColorActive}, var(${buttonTokens.buttonColor}));
+        background: var(${buttonTokens.buttonBackgroundColorActive}, var(${buttonTokens.buttonBackgroundColor}));
+
+        pointer-events: none;
+    }
+`;
 
 /**
  * Кнопка.
  */
-export const Button = ButtonComponent;
+export const Button = ({ onClick, isLoading, ...props }: ButtonProps) => {
+    const handleClick = useCallback(
+        (event: MouseEvent<HTMLButtonElement>) => {
+            if (!isLoading) {
+                onClick?.(event);
+            }
+        },
+        [isLoading, onClick],
+    );
+
+    return <StyledButton onClick={handleClick} isLoading={isLoading} {...props} />;
+};
