56a5cc1d6bbd12e709257ecdbaa091d98ef0f54d
Neretin Artem <hawktrike@gmail.com>
2025-04-01 15:22:18 +0500

    chore(plasma-new-hope): Remove examples of old typography

--- packages/plasma-new-hope/src/examples/typography/components/Old/Subtitle/Subtitle.ts
+++
@@ -1,8 +0,0 @@
-import { subtitleConfig } from '../../../../../components/Typography';
-import { component, mergeConfig } from '../../../../../engines';
-
-import { config } from './Subtitle.config';
-
-const mergedConfig = mergeConfig(subtitleConfig, config);
-
-export const Subtitle = component(mergedConfig);
