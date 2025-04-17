4212cc4c3dcd5477b7d7aae138f14a2c5cc0ff51
Dima Shugaev <50920576+shuga2704@users.noreply.github.com>
2025-04-17 20:05:25 +0300

    feat(plasma, sdds): export ComboboxItemOption everywhere

--- packages/plasma-web/src/components/Combobox/index.ts
+++ packages/plasma-web/src/components/Combobox/index.ts
@@ -1,5 +1,8 @@
 export { ComboboxDivider, ComboboxFooter, ComboboxGroup, ComboboxHeader, ComboboxItem } from './Legacy';
 
-export type { ComboboxOldProps as ComboboxProps } from '@salutejs/plasma-new-hope/styled-components';
+export type {
+    ComboboxOldProps as ComboboxProps,
+    ComboboxItemOption,
+} from '@salutejs/plasma-new-hope/styled-components';
 
 export { Combobox } from './Combobox';
--- packages/sdds-finportal/src/components/Combobox/Combobox.tsx
+++ packages/sdds-finportal/src/components/Combobox/Combobox.tsx
@@ -4,7 +4,7 @@ import {
     mergeConfig,
     fixedForwardRef,
 } from '@salutejs/plasma-new-hope/styled-components';
-import type { ItemOption, ComboboxProps, DistributiveOmit, DistributivePick } from '@salutejs/plasma-new-hope';
+import type { ComboboxItemOption, ComboboxProps, DistributiveOmit, DistributivePick } from '@salutejs/plasma-new-hope';
 import React, { ComponentProps } from 'react';
 
 import { config } from './Combobox.config';
@@ -14,10 +14,13 @@ const ComboboxNew = component(mergedConfig);
 
 type PropsFromConfig = keyof typeof config['variations'];
 
-export type Props<T extends ItemOption> = DistributiveOmit<ComboboxProps<T>, PropsFromConfig> &
+export type Props<T extends ComboboxItemOption> = DistributiveOmit<ComboboxProps<T>, PropsFromConfig> &
     DistributivePick<ComponentProps<typeof ComboboxNew>, PropsFromConfig>;
 
-const ComboboxComponent = <T extends ItemOption>(props: Props<T>, ref: React.ForwardedRef<HTMLInputElement>) => {
+const ComboboxComponent = <T extends ComboboxItemOption>(
+    props: Props<T>,
+    ref: React.ForwardedRef<HTMLInputElement>,
+) => {
     return <ComboboxNew ref={ref} {...(props as any)} />;
 };
 
