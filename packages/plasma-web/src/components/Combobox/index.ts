d7c924441e1ca1507f82d153b2dd7353c0d701e0
Dima Shugaev <50920576+shuga2704@users.noreply.github.com>
2025-04-17 20:05:25 +0300

    feat(plasma, sdds): export ComboboxItemOption everywhere

--- packages/plasma-web/src/components/Combobox/index.ts
+++ packages/plasma-web/src/components/Combobox/index.ts
@@ -3,3 +3,4 @@ export { ComboboxDivider, ComboboxFooter, ComboboxGroup, ComboboxHeader, Combobo
 export type { ComboboxOldProps as ComboboxProps } from '@salutejs/plasma-new-hope/styled-components';
 
 export { Combobox } from './Combobox';
+export type { ComboboxItemOption } from './Combobox';
