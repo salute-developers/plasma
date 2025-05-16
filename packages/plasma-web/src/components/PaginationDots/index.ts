6d813679aa9530431e5cccffa77bc5ce3486777a
Dima Shugaev <50920576+shuga2704@users.noreply.github.com>
2025-05-16 15:35:14 +0300

    feat(plasma): move PaginationDots to the new-hope

--- packages/plasma-web/src/components/PaginationDots/index.ts
+++ packages/plasma-web/src/components/PaginationDots/index.ts
@@ -1,7 +1,7 @@
-export { SmartPaginationDots } from '@salutejs/plasma-hope';
+export { SmartPaginationDots } from '@salutejs/plasma-new-hope/styled-components';
 export type { SmartPaginationDotsProps } from '@salutejs/plasma-hope';
 
-export { PaginationDots } from '@salutejs/plasma-hope';
+export { PaginationDots } from '@salutejs/plasma-new-hope/styled-components';
 
-export { PaginationDot } from '@salutejs/plasma-hope';
+export { PaginationDot } from '@salutejs/plasma-new-hope/styled-components';
 export type { PaginationDotProps } from '@salutejs/plasma-hope';
