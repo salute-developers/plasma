f2484a4bf5bc1320b0235623d9cc66ccbc1039c2
Alexander Lobyntsev <yakutoc@gmail.com>
2025-05-19 14:01:46 +0700

    chore(sdds-scan): init package

---
+++ packages/sdds-scan/src/components/ViewContainer/ViewContainer.config.ts
@@ -0,0 +1,15 @@
+import { css } from '@salutejs/plasma-new-hope/styled-components';
+import { viewContainer } from '@salutejs/sdds-themes/tokens/sdds_scan';
+
+export const config = {
+    variations: {
+        view: {
+            onDark: css`
+                ${viewContainer.dark}
+            `,
+            onLight: css`
+                ${viewContainer.light}
+            `,
+        },
+    },
+};
