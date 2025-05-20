bf65a630340e51c3d476decf09771c2c6de9a943
TitanKuzmich <yzn-yfv@mail.ru>
2025-05-20 12:01:25 +0300

    feat(sdds-*): add CodeField component

---
+++ packages/sdds-netology/src/components/CodeField/index.ts
@@ -0,0 +1,8 @@
+export { CodeField } from './CodeField';
+export {
+    codeFieldTokens,
+    codeFieldClasses,
+    ONLY_DIGITS_PATTERN,
+    ONLY_CHARS_PATTERN,
+    ONLY_DIGITS_AND_CHARS_PATTERN,
+} from '@salutejs/plasma-new-hope/styled-components';
