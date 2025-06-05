4d3c4f16be9333127911b29748f0011921d8d3e5
TitanKuzmich <yzn-yfv@mail.ru>
2025-06-05 16:31:46 +0300

    feat(plasma-cy-utils): update Cypress to 10.11.0

--- utils/plasma-cy-utils/lib/CypressHelpers.d.ts
+++ utils/plasma-cy-utils/lib/CypressHelpers.d.ts
@@ -1,17 +1,5 @@
-import React, { PropsWithChildren } from 'react';
 import { mount as cyMount } from '@cypress/react';
-export declare const getComponent: <T = {
-    children?: React.ReactNode;
-}>(componentName: string) => React.FC<T>;
-interface CYTDec {
-    noSSR?: boolean;
-}
-export declare const CypressTestDecorator: React.FC<PropsWithChildren<CYTDec>>;
-export declare const PadMe: import("styled-components").StyledComponent<"div", any, {}, never>;
-export declare const SpaceMe: import("styled-components").StyledComponent<"span", any, {}, never>;
-export declare const withNoAnimation: <P extends {}>(Comp: React.FC<P>) => React.FC<P>;
-export declare const mount: typeof cyMount;
-export declare const mountLegacyMode: typeof cyMount;
+import type { resourceOrOption } from './Portal';
 declare global {
     export namespace Cypress {
         interface Chainable {
@@ -19,12 +7,5 @@ declare global {
         }
     }
 }
-export declare type resourceOrOption = string | {
-    timeout?: number;
-};
-interface PortalProps {
-    id: string;
-}
-export declare const Portal: React.FC<PropsWithChildren<PortalProps>>;
-export {};
-//# sourceMappingURL=CypressHelpers.d.ts.map
+export declare const mount: typeof cyMount;
+export declare const mountLegacyMode: typeof cyMount;
