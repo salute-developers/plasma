f8e99e2fc4d17179c2f79ac3e940c91742d26c44
TitanKuzmich <yzn-yfv@mail.ru>
2025-06-17 10:11:47 +0300

    feat(plasma-giga): add tests form M to R

---
+++ packages/plasma-giga/src/components/NumberFormat/NumberFormat.component-test.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { mount, CypressTestDecorator, getComponent, PadMe } from '@salutejs/plasma-cy-utils';
+
+import { NumberFormat as NumberFormatGIGA } from '.';
+
+describe('plasma-giga: NumberFormat', () => {
+    const NumberFormat = getComponent('NumberFormat') as typeof NumberFormatGIGA;
+
+    it('empty', () => {
+        mount(
+            <CypressTestDecorator>
+                <NumberFormat size="l" label="Числовой формат" textAfter="₽" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it(':thousandSeparator,decimalSeparator', () => {
+        mount(
+            <CypressTestDecorator>
+                <NumberFormat
+                    size="l"
+                    label="Числовой формат"
+                    textAfter="₽"
+                    thousandSeparator=" "
+                    decimalSeparator="."
+                    value={123456789}
+                />
+                <PadMe />
+                <NumberFormat
+                    size="l"
+                    label="Числовой формат"
+                    textAfter="₽"
+                    thousandSeparator="/"
+                    decimalSeparator=","
+                    value={123456789}
+                />
+                <PadMe />
+                <NumberFormat
+                    size="l"
+                    label="Числовой формат"
+                    textAfter="₽"
+                    thousandSeparator=","
+                    decimalSeparator="$"
+                    value={123456789}
+                />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it(':decimalScale', () => {
+        mount(
+            <CypressTestDecorator>
+                <NumberFormat
+                    size="l"
+                    label="Числовой формат"
+                    textAfter="₽"
+                    thousandSeparator=" "
+                    decimalSeparator="."
+                    decimalScale={3}
+                />
+            </CypressTestDecorator>,
+        );
+
+        cy.get('input').click().type('123456.6789');
+        cy.get('input').should('have.value', '123 456.678');
+
+        cy.matchImageSnapshot();
+    });
+
+    it(':fixedDecimalScale', () => {
+        mount(
+            <CypressTestDecorator>
+                <NumberFormat
+                    size="l"
+                    label="Числовой формат"
+                    textAfter="₽"
+                    thousandSeparator=" "
+                    decimalSeparator="."
+                    decimalScale={4}
+                    fixedDecimalScale
+                />
+            </CypressTestDecorator>,
+        );
+
+        cy.get('input').click().type('1234569');
+        cy.get('input').should('have.value', '1 234 569.0000');
+
+        cy.matchImageSnapshot();
+    });
+
+    it(':allowNegative, allowLeadingZeros', () => {
+        mount(
+            <CypressTestDecorator>
+                <NumberFormat
+                    size="l"
+                    label="Числовой формат"
+                    textAfter="₽"
+                    thousandSeparator=" "
+                    allowNegative
+                    allowLeadingZeros
+                />
+            </CypressTestDecorator>,
+        );
+
+        cy.get('input').click().type('-000123');
+        cy.get('input').should('have.value', '-000123');
+
+        cy.matchImageSnapshot();
+    });
+
+    it('remove extra zero', () => {
+        mount(
+            <CypressTestDecorator>
+                <NumberFormat size="l" label="Числовой формат" textAfter="₽" thousandSeparator=" " />
+            </CypressTestDecorator>,
+        );
+
+        cy.get('input').click().type('00.123');
+        cy.get('input').should('have.value', '00.123');
+        cy.get('body').click();
+        cy.get('input').should('have.value', '0.123');
+
+        cy.matchImageSnapshot();
+    });
+});
---
+++ packages/plasma-giga/src/components/Popup/Popup.component-test.tsx
@@ -0,0 +1,192 @@
+import React from 'react';
+import styled from 'styled-components';
+import { mount, CypressTestDecorator, getComponent } from '@salutejs/plasma-cy-utils';
+
+const Content = styled.div`
+    background: #f0f0f0;
+    padding: 1rem;
+`;
+
+const OtherContent = styled.div`
+    width: 50%;
+    height: 50%;
+    background: #e1e1e1;
+    position: absolute;
+
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 1rem;
+
+    bottom: 0rem;
+    right: 0;
+`;
+
+describe('plasma-giga: Popup', () => {
+    const PopupProvider = getComponent('PopupProvider');
+    const Popup = getComponent('Popup');
+    const Button = getComponent('Button');
+    const H3 = getComponent('H3');
+
+    function Demo({ open = false, placement }: { open?: boolean; placement?: string }) {
+        const [isOpen, setIsOpen] = React.useState(open);
+
+        return (
+            <>
+                <Button text="Open popup" onClick={() => setIsOpen(true)} />
+                <Popup opened={isOpen} onClose={() => setIsOpen(false)} placement={placement}>
+                    <Content id="popup-content">
+                        <H3>Popup</H3>
+                        <Button text="Close" onClick={() => setIsOpen(false)} />
+                    </Content>
+                </Popup>
+            </>
+        );
+    }
+
+    function Placement({ placement, offset }: { placement?: string; offset?: [number, number] }) {
+        return (
+            <>
+                <Popup opened placement={placement} offset={offset}>
+                    <Content>
+                        <H3>{placement}</H3>
+                        <Button text="Close" />
+                    </Content>
+                </Popup>
+            </>
+        );
+    }
+
+    function Frame() {
+        const [isOpenA, setIsOpenA] = React.useState(false);
+        const [isOpenB, setIsOpenB] = React.useState(false);
+
+        const ref = React.useRef<HTMLDivElement>(null);
+        return (
+            <>
+                <Button text="Open popup A" onClick={() => setIsOpenA(true)} />
+                <Button text="Open popup B" onClick={() => setIsOpenB(true)} />
+                <Popup opened={isOpenA} frame={ref}>
+                    <Content>
+                        <H3>Popup A</H3>
+                        <Button text="Close A" onClick={() => setIsOpenA(false)} />
+                    </Content>
+                </Popup>
+                <OtherContent ref={ref}>
+                    <H3>Frame</H3>
+                </OtherContent>
+                <Popup opened={isOpenB}>
+                    <Content>
+                        <H3>Popup B</H3>
+                        <Button text="Close B" onClick={() => setIsOpenB(false)} />
+                    </Content>
+                </Popup>
+            </>
+        );
+    }
+
+    it('simple', () => {
+        mount(
+            <CypressTestDecorator>
+                <PopupProvider>
+                    <Demo />
+                </PopupProvider>
+            </CypressTestDecorator>,
+        );
+
+        cy.get('button').click();
+
+        cy.matchImageSnapshot();
+    });
+
+    it('close', () => {
+        mount(
+            <CypressTestDecorator>
+                <PopupProvider>
+                    <Demo />
+                </PopupProvider>
+            </CypressTestDecorator>,
+        );
+
+        cy.get('button').click();
+        cy.get('#popup-content').should('be.visible');
+        cy.get('button').contains('Close').click();
+        cy.get('.popup-base-root').should('not.exist');
+    });
+
+    it('placement basic', () => {
+        mount(
+            <CypressTestDecorator>
+                <PopupProvider>
+                    <Placement placement="center" />
+                    <Placement placement="right" />
+                    <Placement placement="left" />
+                    <Placement placement="bottom" />
+                    <Placement placement="top" />
+                </PopupProvider>
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('placement combination', () => {
+        mount(
+            <CypressTestDecorator>
+                <PopupProvider>
+                    <Placement placement="bottom-right" />
+                    <Placement placement="bottom-left" />
+                    <Placement placement="top-right" />
+                    <Placement placement="top-left" />
+                </PopupProvider>
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('placement combination + offset', () => {
+        mount(
+            <CypressTestDecorator>
+                <PopupProvider>
+                    <Placement placement="bottom-right" offset={[1, 1]} />
+                    <Placement placement="bottom-left" offset={[1, 1]} />
+                    <Placement placement="top-right" offset={[1, 1]} />
+                    <Placement placement="top-left" offset={[1, 1]} />
+                </PopupProvider>
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('frame', () => {
+        mount(
+            <CypressTestDecorator>
+                <PopupProvider>
+                    <Frame />
+                </PopupProvider>
+            </CypressTestDecorator>,
+        );
+
+        cy.get('button').contains('Open popup A').click();
+        cy.get('button').contains('Open popup B').click();
+        cy.matchImageSnapshot();
+    });
+
+    it('prop: data-attrs', () => {
+        mount(
+            <CypressTestDecorator>
+                <PopupProvider>
+                    <Popup opened data-testid="test-data-id">
+                        <Content>
+                            <H3>Popup</H3>
+                        </Content>
+                    </Popup>
+                </PopupProvider>
+            </CypressTestDecorator>,
+        );
+
+        cy.get('.popup-base-root').should('have.attr', 'data-testid', 'test-data-id');
+    });
+});
