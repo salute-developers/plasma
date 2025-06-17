78238a6241681701cc20864faaa589c375868bf9
Dima Shugaev <50920576+shuga2704@users.noreply.github.com>
2025-06-18 00:04:47 +0300

    feat(plasma, sdds): add SWC builder

--- packages/plasma-new-hope/.babelrc.js
+++ packages/plasma-new-hope/.babelrc.js
@@ -1,61 +1,19 @@
-const namespace = process.env.SC_NAMESPACE;
-
 const plugins = [
     '@babel/plugin-proposal-class-properties',
     'babel-plugin-annotate-pure-calls',
     '@babel/plugin-transform-react-constant-elements',
+    [
+        'module-resolver',
+        {
+            root: ['./src'],
+            alias: {
+                src: './src',
+            },
+        },
+    ],
 ];
+
 const ignore = ['**/*.d.ts', '**/helpers', '**/__helpers', '**/*.stories.tsx'];
-
-if (process.env.EMOTION) {
-    plugins.push([
-        '@emotion/babel-plugin',
-        {
-            autoLabel: 'always',
-            labelFormat: `${namespace}__[local]`,
-        },
-    ]);
-
-    plugins.push([
-        'module-resolver',
-        {
-            'root': ['./src'],
-            alias: {
-                src: './src-emotion'
-            }
-        }
-    ]);
-} else if (process.env.SC) {
-    plugins.push([
-        'babel-plugin-styled-components',
-        {
-            displayName: false,
-            namespace,
-        },
-    ]);
-
-    plugins.push([
-        'module-resolver',
-        {
-            'root': ['./src'],
-            alias: {
-                src: './src-sc'
-            }
-        }
-    ]);
-} else {
-    plugins.push([
-        'module-resolver',
-        {
-            'root': ['./src'],
-            alias: {
-                src: './src'
-            }
-        }
-    ])
-}
-
-process.env.COVERAGE && plugins.push(['babel-plugin-istanbul']);
 
 module.exports = {
     env: {
