7381d1f53a9e6ad631842a57a69372509bcc4d00
TitanKuzmich <yzn-yfv@mail.ru>
2025-07-02 13:05:04 +0300

    docs: fix background token overwrite

--- website/sdds-cs-docs/docusaurus.config.js
+++ website/sdds-cs-docs/docusaurus.config.js
@@ -74,7 +74,7 @@ module.exports = {
 
             // Hides the switch in the navbar
             // Useful if you want to support a single color mode
-            disableSwitch: true,
+            disableSwitch: false,
 
             // Whether to use the prefers-color-scheme media-query,
             // using user system preferences, instead of the hardcoded defaultMode.
