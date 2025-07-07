928eb26d6c732d741555154cd9f9bea6816db194
Ilya <illja.by4ek@gmail.com>
2025-07-07 07:14:12 +0300

    feat(sdds-cs): add view secondary

--- packages/sdds-cs/src/components/Tooltip/Tooltip.config.ts
+++ packages/sdds-cs/src/components/Tooltip/Tooltip.config.ts
@@ -30,15 +30,21 @@ export const config = {
                 ${tooltipTokens.arrowMaskImage}: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6c3ZnPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggY2xpcC1ydWxlPSJldmVub2RkIiBkPSJtMCw5Ljg1bDE2LDBjLTQuNDEsMCAtOCwyLjY5IC04LDZjMCwtMy4zMSAtMy41OSwtNiAtOCwtNnoiIGZpbGw9IiMxNzE3MTciIGZpbGwtcnVsZT0iZXZlbm9kZCIgaWQ9IlRhaWwiLz4KPC9zdmc+");
                 ${tooltipTokens.arrowHeight}: 0.375rem;
                 ${tooltipTokens.arrowEdgeMargin}: 0.5625rem;
-                ${tooltipTokens.arrowBackground}: var(--surface-solid-card-brightness);
             `,
         },
         view: {
             // TODO заменить тень на токен https://github.com/salute-developers/plasma/issues/1131
             default: css`
-                ${tooltipTokens.backgroundColor}: var(--surface-solid-card-brightness);
+                ${tooltipTokens.backgroundColor}: var(--inverse-surface-solid-card);
+                ${tooltipTokens.arrowBackground}: var(--inverse-surface-solid-card);
                 ${tooltipTokens.boxShadow}: 0px 4px 12px 0px rgba(0, 0, 0, 0.16),0px 1px 4px 0px rgba(0, 0, 0, 0.08);
-                ${tooltipTokens.color}: var(--text-primary);
+                ${tooltipTokens.color}: var(--inverse-text-primary);
+            `,
+            secondary: css`
+                ${tooltipTokens.backgroundColor}: var(--on-dark-surface-solid-secondary);
+                ${tooltipTokens.arrowBackground}: var(--on-dark-surface-solid-secondary);
+                ${tooltipTokens.boxShadow}: 0px 4px 12px 0px rgba(0, 0, 0, 0.16),0px 1px 4px 0px rgba(0, 0, 0, 0.08);
+                ${tooltipTokens.color}: var(--inverse-text-primary);
             `,
         },
     },
