067dede282c905182162d011862184916d53bd20
Ilya <illja.by4ek@gmail.com>
2025-07-16 22:16:00 +0300

    feat(*): add view container to storybook

--- packages/plasma-b2c/.storybook/decoratorThemes.tsx
+++ packages/plasma-b2c/.storybook/decoratorThemes.tsx
@@ -6,6 +6,8 @@ import { plasma_b2c__light, plasma_b2c__dark } from '@salutejs/plasma-themes';
 import { link, linkHover, linkActive } from '@salutejs/plasma-tokens-web';
 import { standard as standardTypo, compatible as compatibleTypo } from '@salutejs/plasma-typo';
 
+import { ViewContainer } from '../src/components/ViewContainer/ViewContainer';
+
 /* stylelint-disable */
 const DocumentStyle = createGlobalStyle`
     html:root {
@@ -36,22 +38,55 @@ const themes = {
     dark: createGlobalStyle(plasma_b2c__dark),
 };
 
+type ViewType = {
+    style?: object;
+    view?: 'onDark' | 'onLight';
+};
+
+const viewMap: Record<string, ViewType> = {
+    default: {
+        style: undefined,
+        view: undefined,
+    },
+    onDark: {
+        style: {
+            background: '#1a1a1a',
+            color: 'white',
+            margin: '3rem',
+            'border-radius': '1rem',
+        },
+        view: 'onDark',
+    },
+    onLight: {
+        style: {
+            background: '#ededed',
+            color: 'black',
+            margin: '3rem',
+            'border-radius': '1rem',
+        },
+        view: 'onLight',
+    },
+};
+
 export const withTheme = (Story, context) => {
     const Theme = themes[context.globals.theme];
+    const viewContainerType = viewMap[context.globals.viewContainer];
 
     return (
-        <>
-            {context.globals.typoVersion === 'standard' ? (
-                <>
-                    <TypoStyle breakWord={context.globals.breakWord === 'yes'} />
-                    <CompatibleTypoStyle />
-                </>
-            ) : (
-                <OldTypo />
-            )}
-            <Theme />
-            <DocumentStyle />
-            <Story {...context} />
-        </>
+        <div style={viewContainerType.style}>
+            <ViewContainer view={viewContainerType.view}>
+                {context.globals.typoVersion === 'standard' ? (
+                    <>
+                        <TypoStyle breakWord={context.globals.breakWord === 'yes'} />
+                        <CompatibleTypoStyle />
+                    </>
+                ) : (
+                    <OldTypo />
+                )}
+                <Theme />
+                <DocumentStyle />
+                <Story {...context} />
+            </ViewContainer>
+        </div>
     );
 };
