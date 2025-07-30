4a76948285d7f188f69538955374622a6e1b76d3
TitanKuzmich <yzn-yfv@mail.ru>
2025-07-30 15:05:57 +0300

    feat(sdds-*): update configs and stories for Chip

--- packages/sdds-cs/src/components/Chip/Chip.stories.tsx
+++ packages/sdds-cs/src/components/Chip/Chip.stories.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { InSpacingDecorator, disableProps } from '@salutejs/plasma-sb-utils';
+import { InSpacingDecorator, disableProps, getConfigVariations } from '@salutejs/plasma-sb-utils';
+
+import { config } from './Chip.config';
 
 import { Chip } from '.';
 
-const views = ['default', 'secondary', 'accent'];
-const sizes = ['s', 'xs'];
-
 const onClear = action('onClear');
+
+const { views, sizes } = getConfigVariations(config);
+
+const iconSizeMapper = {
+    s: '1.5rem',
+    xs: '1rem',
+};
 
 const meta: Meta<typeof Chip> = {
     title: 'Data Display/Chip',
-    component: Chip,
     decorators: [InSpacingDecorator],
+    component: Chip,
+    args: {
+        text: 'Hello',
+        view: 'default',
+        size: 's',
+        hasClear: true,
+        disabled: false,
+        focused: true,
+        pilled: false,
+        onClear,
+    },
     argTypes: {
         view: {
             options: views,
@@ -42,52 +58,57 @@ const meta: Meta<typeof Chip> = {
 
 export default meta;
 
-type Story = StoryObj<typeof Chip>;
+type StoryPropsDefault = ComponentProps<typeof Chip> & {
+    enableContentRight: boolean;
+    enableContentLeft: boolean;
+};
 
-const TrashIcon = (props) => (
-    <svg width="100%" viewBox="0 0 24 24" fill="none" color="inherit" {...props}>
+const BellIcon = (props) => (
+    <svg width="100%" viewBox="0 0 24 24" fill="none" {...props}>
         <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M8 5.5A2.5 2.5 0 0110.5 3h3A2.5 2.5 0 0116 5.5V6h-2v-.5a.5.5 0 00-.5-.5h-3a.5.5 0 00-.5.5V6H8v-.5zM4 8a1 1 0 011-1h14a1 1 0 110 2h-1v9.5a2.5 2.5 0 01-2.5 2.5h-7A2.5 2.5 0 016 18.5V9H5a1 1 0 01-1-1z"
+            d="M11.501 21.28c1.088 0 1.978-.889 1.978-1.977H9.524c0 1.088.88 1.978 1.977 1.978zm5.933-5.932v-4.944c0-3.035-1.622-5.576-4.45-6.248v-.673c0-.82-.662-1.483-1.483-1.483-.82 0-1.483.662-1.483 1.483v.672c-2.838.673-4.45 3.204-4.45 6.25v4.943l-1.275 1.276c-.623.623-.188 1.69.692 1.69h13.022c.88 0 1.325-1.067.702-1.69l-1.275-1.276z"
             fill="currentColor"
         />
     </svg>
 );
 
-export const Default: Story = {
+const StoryDefault = ({ enableContentLeft, enableContentRight, size, ...rest }: StoryPropsDefault) => {
+    return (
+        <Chip
+            contentLeft={
+                enableContentLeft ? (
+                    <BellIcon
+                        width={iconSizeMapper[size as keyof typeof iconSizeMapper]}
+                        height={iconSizeMapper[size as keyof typeof iconSizeMapper]}
+                    />
+                ) : undefined
+            }
+            contentRight={
+                enableContentRight ? (
+                    <BellIcon
+                        width={iconSizeMapper[size as keyof typeof iconSizeMapper]}
+                        height={iconSizeMapper[size as keyof typeof iconSizeMapper]}
+                    />
+                ) : undefined
+            }
+            size={size}
+            {...rest}
+        />
+    );
+};
+
+export const Default: StoryObj<StoryPropsDefault> = {
     args: {
-        text: 'This is chip',
-        view: 'default',
-        size: 's',
-        hasClear: true,
-        focused: true,
-        onClear,
+        enableContentLeft: false,
+        enableContentRight: false,
     },
+    render: (args) => <StoryDefault {...args} />,
 };
 
-export const WithIcon: Story = {
-    args: { ...Default.args },
-    render: (args) => {
-        const iconSizeMapper = {
-            s: '1.5rem',
-            xs: '1rem',
-        };
-        const iconSize = args.size || 's';
-
-        return (
-            <Chip
-                contentLeft={<TrashIcon width={iconSizeMapper[iconSize]} height={iconSizeMapper[iconSize]} />}
-                {...args}
-            />
-        );
-    },
-};
-
-export const LongText: Story = {
+export const LongText: StoryObj<StoryPropsDefault> = {
     args: {
         ...Default.args,
         text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
     },
-    render: (args) => <Chip style={{ width: '10rem' }} {...args} />,
+    render: (args) => <StoryDefault style={{ width: '10rem' }} {...args} />,
 };
