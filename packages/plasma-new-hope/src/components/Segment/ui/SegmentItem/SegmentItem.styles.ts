ab845d5ef6b94e525a146c42c39efcee983b8f49
TitanKuzmich <yzn-yfv@mail.ru>
2025-07-30 10:34:14 +0300

    feat(plasma-new-hope): add truncate logic to Segment

--- packages/plasma-new-hope/src/components/Segment/ui/SegmentItem/SegmentItem.styles.ts
+++ packages/plasma-new-hope/src/components/Segment/ui/SegmentItem/SegmentItem.styles.ts
@@ -4,6 +4,41 @@ import { css } from '@linaria/core';
 import { addFocus, applyEllipsis } from '../../../../mixins';
 import { classes, tokens } from '../../tokens';
 
+export const StyledContent = styled.div<{
+    hasRightMargin?: boolean;
+    hasLeftMargin?: boolean;
+    isSecondary?: boolean;
+}>`
+    display: inline-block;
+    width: fit-content;
+    padding: var(${tokens.itemContentPadding});
+    ${applyEllipsis()};
+
+    margin-right: ${({ hasRightMargin }) => (hasRightMargin ? `var(${tokens.itemIconMargin})` : '0')};
+    margin-left: ${({ hasLeftMargin }) => (hasLeftMargin ? `var(${tokens.itemIconMargin})` : '0')};
+`;
+
+export const RightContent = styled.div`
+    display: flex;
+    margin-left: var(${tokens.itemIconMargin});
+    overflow: hidden;
+
+    color: var(${tokens.itemColor});
+
+    &.${classes.segmentAdditionalText} {
+        display: inline-block;
+        color: var(${tokens.itemAdditionalColor});
+        ${applyEllipsis()};
+    }
+`;
+
+export const LeftContent = styled.div`
+    display: flex;
+    margin-right: var(${tokens.itemIconMargin});
+
+    color: inherit;
+`;
+
 export const base = css`
     position: relative;
     display: inline-flex;
@@ -11,6 +46,7 @@ export const base = css`
     justify-content: center;
     box-sizing: border-box;
     min-width: 0;
+    white-space: nowrap;
 
     appearance: none;
     border: none;
@@ -39,39 +75,11 @@ export const base = css`
             }
         `,
     })};
-`;
 
-export const StyledContent = styled.div<{
-    hasRightMargin?: boolean;
-    hasLeftMargin?: boolean;
-    isSecondary?: boolean;
-}>`
-    display: inline-block;
-    width: fit-content;
-    padding: var(${tokens.itemContentPadding});
-    ${applyEllipsis()};
-
-    margin-right: ${({ hasRightMargin }) => (hasRightMargin ? `var(${tokens.itemIconMargin})` : '0')};
-    margin-left: ${({ hasLeftMargin }) => (hasLeftMargin ? `var(${tokens.itemIconMargin})` : '0')};
-`;
-
-export const RightContent = styled.div`
-    display: flex;
-    margin-left: var(${tokens.itemIconMargin});
-    overflow: hidden;
-
-    color: var(${tokens.itemColor});
-
-    &.${classes.segmentAdditionalText} {
-        text-overflow: ellipsis;
-        overflow: hidden;
-        color: var(${tokens.itemAdditionalColor});
+    &.${classes.segmentTruncate} {
+        ${StyledContent} {
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
     }
-`;
-
-export const LeftContent = styled.div`
-    display: flex;
-    margin-right: var(${tokens.itemIconMargin});
-
-    color: inherit;
 `;
