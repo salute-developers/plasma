7b87d5a4928ea15073f3c4face949a9d58700f92
TitanKuzmich <yzn-yfv@mail.ru>
2025-08-08 12:30:28 +0300

    feat: add numberFormatter func for NumberFormat

--- packages/plasma-new-hope/src/components/NumberFormat/index.ts
+++ packages/plasma-new-hope/src/components/NumberFormat/index.ts
@@ -1,3 +1,4 @@
+export { numericFormatter as numberFormatter } from 'react-number-format';
 export { composeNumberFormat } from './NumberFormat';
 
 export type { CustomNumberFormatProps, CustomNumericFormatProps } from './NumberFormat.types';
