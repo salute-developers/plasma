c17017ceea0c6ce29a91ad933d6eaafc5ab75417
Mikhail Lapshin <mvalapshin@sberbank.ru>
2025-08-08 22:54:18 +0300

    feat(): refactor styles and collapse sections

--- packages/plasma-new-hope/src/components/NavigationDrawer/ui/MenuItem/MenuItem.styles.ts
+++ packages/plasma-new-hope/src/components/NavigationDrawer/ui/MenuItem/MenuItem.styles.ts
@@ -1,31 +1,54 @@
-import { css } from '@linaria/core';
+import { counterConfig, counterTokens } from 'src/components/Counter';
+import { styled } from '@linaria/react';
+import { component, mergeConfig } from 'src/engines';
+import { indicatorTokens, indicatorConfig } from 'src/components/Indicator';
 
 import { tokens } from '../../NavigationDrawer.tokens';
 
-export const menuItemStyles = css`
-    padding: var(${tokens.menuItemPadding});
+const mergedCounterConfig = mergeConfig(counterConfig);
+const Counter = component(mergedCounterConfig);
+
+const indicatorMergedConfig = mergeConfig(indicatorConfig);
+const Indicator: React.FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = component(
+    indicatorMergedConfig,
+) as never;
+
+export const ExtraCounter = styled(Counter)`
+    margin: auto;
+
+    ${counterTokens.background}: var(${tokens.counterBackground});
+    ${counterTokens.color}: var(${tokens.counterColor});
+    ${counterTokens.borderRadius}: var(${tokens.counterBorderRadius});
+    ${counterTokens.height}: var(${tokens.counterHeight});
+    ${counterTokens.padding}: var(${tokens.counterPadding});
+    ${counterTokens.fontFamily}: var(${tokens.counterFontFamily});
+    ${counterTokens.fontSize}: var(${tokens.counterFontSize});
+    ${counterTokens.fontStyle}: var(${tokens.counterFontStyle});
+    ${counterTokens.fontWeight}: var(${tokens.counterFontWeight});
+    ${counterTokens.letterSpacing}: var(${tokens.counterLetterSpacing});
+    ${counterTokens.lineHeight}: var(${tokens.counterLineHeight});
+`;
+
+export const StyledIndicator = styled(Indicator)`
+    margin: auto;
+
+    ${indicatorTokens.size}: var(${tokens.menuItemIndicatorSize});
+    ${indicatorTokens.color}: var(${tokens.menuItemIndicatorColor});
+`;
+
+export const IconWrapper = styled.div`
+    position: relative;
+    width: var(${tokens.menuItemIconContainerSize});
+    height: var(${tokens.menuItemIconContainerSize});
     display: flex;
-    gap: var(${tokens.menuItemGap});
-    align-items: center;
-    cursor: pointer;
-    border-radius: var(${tokens.menuItemBorderRadius});
-    line-height: 1;
-
-    &:hover {
-        background: var(${tokens.menuItemHoverColor});
-    }
 `;
 
-export const selectedStyles = css`
-    background: var(${tokens.menuItemSelectedColor});
+export const Icon = styled.div`
+    margin: auto;
 `;
 
-export const disabledStyles = css`
-    cursor: not-allowed;
-    opacity: var(${tokens.menuItemDisabledOpacity});
-`;
-
-export const linkStyles = css`
-    text-decoration: none;
-    color: inherit;
+export const RightContentWrapper = styled.div`
+    position: relative;
+    height: var(${tokens.menuItemIconContainerSize});
+    display: flex;
 `;
