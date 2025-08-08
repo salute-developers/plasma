2953076de6319e1ce0c4ce62bac4c8e8a9ee6148
Mikhail Lapshin <mvalapshin@sberbank.ru>
2025-08-08 22:54:18 +0300

    feat(): refactor styles and collapse sections

--- packages/plasma-new-hope/src/components/NavigationDrawer/NavigationDrawer.styles.ts
+++ packages/plasma-new-hope/src/components/NavigationDrawer/NavigationDrawer.styles.ts
@@ -1,30 +1,103 @@
 import { css } from '@linaria/core';
+import { styled } from '@linaria/react';
 
-import { tokens } from './NavigationDrawer.tokens';
+import { classes, tokens } from './NavigationDrawer.tokens';
 
 export const base = css`
     display: flex;
     box-sizing: border-box;
     height: 100%;
     position: relative;
+
+    .${classes.navigationDrawerMenuItemSelected} {
+        background: var(${tokens.menuItemSelectedColor});
+    }
+
+    .${classes.navigationDrawerMenuItemDisabled} {
+        cursor: not-allowed;
+        opacity: var(${tokens.menuItemDisabledOpacity});
+    }
+
+    .${classes.navigationDrawerMenuLink} {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .${classes.navigationDrawerMenuItem} {
+        padding: var(${tokens.menuItemPadding});
+        display: flex;
+        gap: var(${tokens.menuItemGap});
+        align-items: center;
+        cursor: pointer;
+        border-radius: var(${tokens.menuItemBorderRadius});
+        line-height: 1;
+        font-size: var(${tokens.menuItemFontSize});
+
+        &:hover {
+            background: var(${tokens.menuItemHoverColor});
+        }
+    }
+
+    .${classes.navigationDrawerMenuItemWithoutContentLeft} {
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .${classes.navigationDrawerMenuIndicator} {
+        position: absolute;
+        top: -6px;
+        right: -6px;
+    }
+
+    .${classes.navigationDrawerMenuCounter} {
+        position: absolute;
+        top: -10px;
+        right: -10px;
+    }
+
+    .${classes.navigationDrawerSidebar} {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        overflow-y: auto;
+
+        height: var(${tokens.sidebarHeight});
+        padding: var(${tokens.sidebarPadding});
+        min-width: var(${tokens.sidebarWidth});
+        max-width: var(${tokens.sidebarWidth});
+        box-shadow: var(${tokens.sidebarBoxShadow});
+        background: var(${tokens.sidebarBackground});
+    }
+
+    .${classes.navigationDrawerSidebarClosed} {
+        min-width: var(${tokens.sidebarWidthClosed});
+        max-width: var(${tokens.sidebarWidthClosed});
+    }
+
+    .${classes.navigationDrawerSidebarOverlay} {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1;
+    }
+
+    .${classes.navigationDrawerContentOverlay} {
+        margin-left: calc(var(${tokens.sidebarWidthClosed}) + 24px);
+    }
 `;
 
-export const contentStyles = css`
+export const Content = styled.div`
     width: 100%;
     height: 100%;
 `;
 
-export const contentOverlayStyles = css`
-    margin-left: calc(var(${tokens.sidebarWidthClosed}) + 24px);
-`;
-
-export const overlayStyles = css`
+export const Overlay = styled.div`
     position: absolute;
     top: 0;
     left: calc(var(${tokens.sidebarWidth}) + 24px);
+    width: calc(100% - 250px + 8px);
     z-index: 1;
     background-color: var(${tokens.overlayBackground});
     height: 100%;
-    width: 100%;
     cursor: pointer;
 `;
--- packages/plasma-new-hope/src/components/NavigationDrawer/ui/MenuItem/MenuItem.tsx
+++ packages/plasma-new-hope/src/components/NavigationDrawer/ui/MenuItem/MenuItem.tsx
@@ -1,76 +1,60 @@
 import React from 'react';
 import { MenuItemProps } from 'src/components/NavigationDrawer/ui/MenuItem/MenuItem.types';
-import { cx } from 'src/utils';
+import cls from 'classnames';
 import {
-    disabledStyles,
-    linkStyles,
-    menuItemStyles,
-    selectedStyles,
+    ExtraCounter,
+    IconWrapper,
+    Icon,
+    StyledIndicator,
+    RightContentWrapper,
 } from 'src/components/NavigationDrawer/ui/MenuItem/MenuItem.styles';
-import { component, mergeConfig } from 'src/engines';
-import { indicatorConfig } from 'src/components/Indicator/Indicator';
-import { styled } from '@linaria/react';
-import { indicatorTokens } from 'src/components/Indicator';
-import { counterConfig } from 'src/components/Counter';
-
-import { tokens } from '../../NavigationDrawer.tokens';
-
-const indicatorMergedConfig = mergeConfig(indicatorConfig);
-
-const Indicator: React.FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = component(
-    indicatorMergedConfig,
-) as never;
-
-const counterMergedConfig = mergeConfig(counterConfig);
-
-const Counter: React.FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = component(counterMergedConfig) as never;
-
-const IconWrapper = styled.div`
-    position: relative;
-    width: var(${tokens.menuItemIconSize});
-    height: var(${tokens.menuItemIconSize});
-    display: flex;
-`;
-
-const Icon = styled.div`
-    margin: auto;
-`;
-
-const StyledIndicator = styled(Indicator)`
-    position: absolute;
-    top: -4px;
-    right: -4px;
-    ${indicatorTokens.size}: var(${tokens.menuItemIndicatorSize});
-    ${indicatorTokens.color}: var(${tokens.menuItemIndicatorColor});
-`;
+import { classes } from 'src/components/NavigationDrawer/NavigationDrawer.tokens';
 
 export const MenuItem: React.FC<MenuItemProps> = (props) => {
     const { action, disabled, selected, icon, withContentLeft, label, isOpened, hasIndicator, counter } = props;
 
-    const onClick = action && typeof action !== 'string' && disabled ? action : undefined;
+    const onClick = action && typeof action === 'function' && !disabled ? action : undefined;
 
-    const menuClasses = cx(menuItemStyles, selected && selectedStyles, disabled && disabledStyles);
+    const menuClasses = cls(
+        classes.navigationDrawerMenuItem,
+        selected && classes.navigationDrawerMenuItemSelected,
+        disabled && classes.navigationDrawerMenuItemDisabled,
+        !withContentLeft && classes.navigationDrawerMenuItemWithoutContentLeft,
+    );
 
-    const menuItemContent = (
+    const indicators = (
         <>
-            {icon && withContentLeft && (
-                <IconWrapper>
-                    <Icon>{icon}</Icon>
-                    {hasIndicator && <StyledIndicator />}
-                    {counter !== undefined && <Counter>{counter}</Counter>}
-                </IconWrapper>
+            {hasIndicator && (
+                <StyledIndicator className={cls(withContentLeft && classes.navigationDrawerMenuIndicator)} />
             )}
-            {isOpened && label}
+            {counter !== undefined && (
+                <ExtraCounter className={cls(withContentLeft && classes.navigationDrawerMenuCounter)} count={counter} />
+            )}
         </>
     );
 
-    return typeof action === 'string' ? (
-        <a className={cx(linkStyles, menuClasses)} href={action} rel="noopener noreferrer">
-            {menuItemContent}
-        </a>
+    const menuItemContent = withContentLeft ? (
+        <>
+            <IconWrapper>
+                <Icon>{icon}</Icon>
+                {indicators}
+            </IconWrapper>
+            {isOpened && label}
+        </>
     ) : (
+        <>
+            {label}
+            <RightContentWrapper>{indicators}</RightContentWrapper>
+        </>
+    );
+
+    return typeof action === 'function' ? (
         <div className={menuClasses} onClick={onClick}>
             {menuItemContent}
         </div>
+    ) : (
+        <a className={cls(classes.navigationDrawerMenuLink, menuClasses)} href={action} rel="noopener noreferrer">
+            {menuItemContent}
+        </a>
     );
 };
