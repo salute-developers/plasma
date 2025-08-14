61281ee051f43bc0a471afed763e403e08d05abb
Alexander Lobyntsev <yakutoc@gmail.com>
2025-08-14 14:52:43 +0300

    chore(sdds-finai): add docs

---
+++ website/sdds-finai-docs/docusaurus.config.js
@@ -0,0 +1,202 @@
+import { themes } from 'prism-react-renderer';
+import path from 'path';
+import { withCustomConfig } from 'react-docgen-typescript';
+import fg from 'fast-glob';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pckgJson = require('./package.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const versionsArchived = require('./versionsArchived.json');
+
+const { VERSION_NAME, PREFIX } = process.env;
+const defaultUrl = PREFIX ? `/${PREFIX}/sdds-finai/` : '/sdds-finai/';
+const baseUrl = VERSION_NAME ? `/versions/${VERSION_NAME}/` : defaultUrl;
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+const config = {
+    title: 'sdds-finai',
+    tagline: 'Дизайн-система для разработки современных приложений.',
+    url: 'https://plasma.sberdevices.ru/',
+    baseUrl,
+    onBrokenLinks: 'warn',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'images/favicon.png',
+    organizationName: 'Salute-Developers',
+    trailingSlash: true,
+    themes: ['@docusaurus/theme-live-codeblock'],
+    future: {
+        experimental_faster: true,
+    },
+    themeConfig: {
+        navbar: {
+            title: 'SDDS-FINAI',
+            items: [
+                {
+                    type: 'docsVersionDropdown',
+                    position: 'right',
+                    dropdownActiveClassDisabled: false,
+                    dropdownItemsAfter: [
+                        ...Object.entries(versionsArchived).map(([versionName, versionUrl]) => ({
+                            label: versionName,
+                            href: versionUrl,
+                        })),
+                    ],
+                },
+                {
+                    href: 'https://github.com/salute-developers/plasma',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'light',
+            links: [
+                {
+                    title: 'Сообщество',
+                    items: [
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/salute-developers/plasma',
+                        },
+                    ],
+                },
+            ],
+        },
+        prism: {
+            theme: themes.github,
+            darkTheme: themes.dracula,
+        },
+        colorMode: {
+            // "light" | "dark"
+            defaultMode: 'light',
+
+            // Hides the switch in the navbar
+            // Useful if you want to support a single color mode
+            disableSwitch: false,
+
+            // Whether to use the prefers-color-scheme media-query,
+            // using user system preferences, instead of the hardcoded defaultMode.
+            respectPrefersColorScheme: true,
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/salute-developers/plasma/blob/master/website/sdds-finai-docs/',
+                    versions: {
+                        current: {
+                            label: pckgJson.dependencies['@salutejs/sdds-finai'],
+                            path: '',
+                        },
+                    },
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
+    ],
+    plugins: [
+        function docgenPlugin() {
+            // Плагин генерации документации
+            return {
+                name: 'docusaurus-plugin-react-docgen-typescript',
+                async loadContent() {
+                    // Загрузка компонентов для документации
+                    return withCustomConfig('./tsconfig.json', {
+                        shouldExtractLiteralValuesFromEnum: true,
+                        shouldRemoveUndefinedFromOptional: true,
+                        propFilter: () => {
+                            // Функция обрезки типов
+                            return true;
+                        },
+                    }).parse(
+                        await fg([
+                            '../../packages/sdds-finai/src/**/*.{ts,tsx}',
+                            '!../../packages/sdds-finai/src/**/*.test.*',
+                        ]),
+                    );
+                },
+                configureWebpack(config) {
+                    return {
+                        resolve: {
+                            alias: {
+                                '@docgen': path.join(
+                                    config.resolve.alias['@generated'],
+                                    'docusaurus-plugin-react-docgen-typescript',
+                                    'default',
+                                ),
+                            },
+                        },
+                    };
+                },
+                async contentLoaded({ content, actions }) {
+                    // Генерация массива типов
+                    const names = [];
+                    content
+                        .filter((module) => {
+                            const result =
+                                !names.includes(module.displayName) &&
+                                /^[A-Z]/.test(module.displayName) &&
+                                (module.props || module.description) &&
+                                module.displayName !== 'Default';
+
+                            if (result) {
+                                names.push(module.displayName);
+                                return true;
+                            }
+
+                            return false;
+                        })
+                        .forEach(async (component) =>
+                            actions.createData(
+                                `${component.displayName}.json`,
+                                JSON.stringify({
+                                    props: Object.entries(component.props).reduce(
+                                        (acc, [key, { declarations, ...rest }]) => ({
+                                            ...acc,
+                                            [key]: rest,
+                                        }),
+                                        {},
+                                    ),
+                                    description: component.description,
+                                }),
+                            ),
+                        );
+                },
+            };
+        },
+        function aliasPlugin() {
+            return {
+                name: 'docusaurus-plugin-aliases',
+                configureWebpack() {
+                    return {
+                        resolve: {
+                            symlinks: false,
+                            alias: {
+                                react: path.resolve(__dirname, 'node_modules', 'react'),
+                                'react-dom': path.resolve(__dirname, 'node_modules', 'react-dom'),
+                                'styled-components': path.resolve(__dirname, 'node_modules', 'styled-components'),
+                            },
+                        },
+                    };
+                },
+            };
+        },
+    ],
+    markdown: {
+        format: 'detect',
+        mdx1Compat: {
+            comments: true,
+            admonitions: true,
+            headingIds: true,
+        },
+    },
+};
+
+export default config;
