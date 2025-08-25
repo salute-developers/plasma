01b54cd4ab43216169a96d1a7947aebb16ab3de3
Alexander Lobyntsev <yakutoc@gmail.com>
2025-08-25 12:47:40 +0300

    chore(sdds-cs): update storybook `8.6.14`

--- packages/sdds-cs/.storybook/main.ts
+++ packages/sdds-cs/.storybook/main.ts
@@ -8,7 +8,7 @@ const storyMap = {
     emotion: ['../src-emotion/**/*.stories.tsx'],
 };
 
-const stories = ['../README.stories.mdx'];
+const stories = ['../README.mdx'];
 
 if (USE_EMOTION_COMPONENTS) {
     stories.push(...storyMap['emotion']);
@@ -31,6 +31,9 @@ const config: StorybookConfig = {
     docs: {
         autodocs: false,
         defaultName: 'Docs',
+    },
+    typescript: {
+        reactDocgen: false,
     },
     async viteFinal(config) {
         return mergeConfig(config, {
