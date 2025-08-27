b23c3f59da11dc6cb134d78e6558b05e2a17158c
Alexander Lobyntsev <yakutoc@gmail.com>
2025-08-27 09:34:58 +0300

    chore(button): create button common stories

---
+++ utils/plasma-sb-utils/src/components/Button/fixtures.ts
@@ -0,0 +1,14 @@
+export const stretchingValues = ['auto', 'filled', 'fixed'];
+
+export const pinValues = [
+    '',
+    'square-square',
+    'square-clear',
+    'clear-square',
+    'clear-clear',
+    'clear-circle',
+    'circle-clear',
+    'circle-circle',
+];
+
+export const contentPlacingValues = ['default', 'relaxed'];
