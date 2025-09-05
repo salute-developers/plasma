d42467cb2f73e91cec9a49065917f528489f6b63
TitanKuzmich <yzn-yfv@mail.ru>
2025-09-05 11:32:49 +0300

    feat(plasma-new-hope): fix Pagination perPage list width

--- packages/plasma-new-hope/src/components/Pagination/Pagination.types.ts
+++ packages/plasma-new-hope/src/components/Pagination/Pagination.types.ts
@@ -8,112 +8,112 @@ import type { AsProps, NumericRange, CreateArrayWithLengthX } from '../../types'
 export type PaginationTypes = 'compact' | 'default';
 
 export type CustomPaginationProps = {
-    /*
+    /**
      * Вид
      */
     view?: string;
 
-    /*
+    /**
      *  Вид выбранной кнопки
      */
     viewCurrentPage?: string;
 
-    /*
+    /**
      *  Левый контент
      */
     leftContent?: ReactNode;
 
-    /*
+    /**
      *  Правый контент
      */
     rightContent?: ReactNode;
 
-    /*
+    /**
      *  Тип обычный или компактный
      */
     type?: PaginationTypes;
 
-    /*
+    /**
      * Размер
      */
     size?: string;
 
-    /*
+    /**
      * Количество страниц
      */
     count?: number;
 
-    /*
+    /**
      * Выбранная страница
      */
     value?: number;
 
-    /*
+    /**
      * Выбранная страница по умолчанию
      */
     defaultValue?: number;
 
-    /*
+    /**
      * Выбор страницы через Input
      */
     hasQuickJump?: boolean;
 
-    /*
+    /**
      * Выбор количества результатов на странице
      */
     hasPerPage?: boolean;
 
-    /*
+    /**
      * Выбранное количество результатов на странице
      */
     perPage?: number;
 
-    /*
+    /**
      * Выбранное количество результатов на странице по умолчанию
      */
     defaultPerPage?: number;
 
-    /*
+    /**
      * Список результатов на странице
      */
     perPageList?: number[];
 
-    /*
+    /**
      * Заблокированные страницы
      */
     disabledPages?: string[];
 
-    /*
+    /**
      * Количество PageButtons
      */
     slots?: NumericRange<CreateArrayWithLengthX<7>, 15>;
 
-    /*
+    /**
      * Скругление кнопки
      */
     pilled?: boolean;
 
-    /*
+    /**
      * Равная ширина и высота кнопки
      */
     square?: boolean;
 
-    /*
+    /**
      * Авто изменение стиля кнопок в ButtonGroup
      */
     isCommonButtonStyles?: boolean;
 
-    /*
+    /**
      * Placeholder TextField'a для быстрого прыжка на страницу
      */
     placeholderQuickJump?: string;
 
-    /*
+    /**
      * Текст для быстрого прыжка на страницу
      */
     textQuickJump?: string;
 
-    /*
+    /**
      * Текст для выбора кол-ва результатов на странице
      */
     textPerPage?: string;
@@ -124,15 +124,15 @@ export type CustomPaginationProps = {
      */
     listWidth?: CSSProperties['width'];
 
-    /*
+    /**
      * Функция которая исполняeтся при изменении `page`, `perPage`
      */
     onChange?: (page?: number, perPage?: number) => void;
-    /*
+    /**
      * @deprecated - использовать onChange
      */
     onChangePageValue?: (page?: number) => void;
-    /*
+    /**
      * @deprecated - использовать onChange
      */
     onChangePerPageValue?: (perPage?: number) => void;
