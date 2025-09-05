2090b135cd5fa712e86d2b639cba27106382a78e
Dima Shugaev <shuga2704@gmail.com>
2025-09-05 14:11:59 +0300

    feat(*): add generic in Autocomplete

--- packages/sdds-cs/src/components/Autocomplete/Autocomplete.ts
+++
@@ -1,6 +0,0 @@
-import { autocompleteConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
-
-import { config } from './Autocomplete.config';
-
-const mergedConfig = mergeConfig(autocompleteConfig, config);
-export const Autocomplete = component(mergedConfig);
