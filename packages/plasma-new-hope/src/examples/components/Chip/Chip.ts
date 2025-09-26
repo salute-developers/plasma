7700783cd0bfe11cf5ad6128402dec942fcc16e9
Ilya <illja.by4ek@gmail.com>
2025-09-26 08:29:39 +0300

    feat(insol): add chip appearence

--- packages/plasma-new-hope/src/examples/components/Chip/Chip.ts
+++ packages/plasma-new-hope/src/examples/components/Chip/Chip.ts
@@ -1,8 +1,23 @@
+import { createConditionalComponent } from 'src/utils/createConditionalComponent';
+import { ComponentProps } from 'react';
+
 import { chipConfig } from '../../../components/Chip';
 import { component, mergeConfig } from '../../../engines';
 
 import { config } from './Chip.config';
+import { config as clearConfig } from './Chip.transparent.config';
 
-const mergedConfig = mergeConfig(chipConfig, config);
+const mergedConfigDefault = mergeConfig(chipConfig, config);
+export const ChipDefault = component(mergedConfigDefault);
 
-export const Chip = component(mergedConfig);
+const mergedConfigTransparent = mergeConfig(chipConfig, clearConfig);
+export const ChipTransparent = component(mergedConfigTransparent);
+
+export type ChipProps = ComponentProps<typeof ChipDefault>;
+
+export const Chip = createConditionalComponent<ChipProps>(ChipDefault, [
+    {
+        conditions: { prop: 'appearance', value: 'transparent' },
+        component: ChipTransparent,
+    },
+]);
