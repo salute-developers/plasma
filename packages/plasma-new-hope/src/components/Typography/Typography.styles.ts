b98d9bc3eb316ab568dd40c87aee7eef41507ded
Alexander Lobyntsev <yakutoc@gmail.com>
2025-10-24 10:58:10 +0300

    fix(typography): reset default padding/margin/border

--- packages/plasma-new-hope/src/components/Typography/Typography.styles.ts
+++ packages/plasma-new-hope/src/components/Typography/Typography.styles.ts
@@ -5,12 +5,17 @@ import { applyHyphens, applyHyphensNormal } from '../../mixins';
 import { classes, tokens } from './tokens';
 
 export const base = css`
+    margin: 0;
+    padding: 0;
+
     font-family: var(${tokens.typoFontFamily});
     font-size: var(${tokens.typoFontSize});
     font-style: var(${tokens.typoFontStyle});
     letter-spacing: var(${tokens.typoFontLetterSpacing});
     line-height: var(${tokens.typoFontLineHeight});
     font-weight: var(${tokens.typoFontWeight});
+
+    border: 0;
 
     &.${classes.typoBold} {
         font-weight: var(${tokens.typoFontWeightBold});
