77c99bb513899622872de52793b1836203c4c275
TitanKuzmich <yzn-yfv@mail.ru>
2025-11-12 15:43:17 +0300

    feat(plasma-cy-utils): update helpers box-sizing

--- utils/plasma-cy-utils/lib/StyleHelpers.js
+++ utils/plasma-cy-utils/lib/StyleHelpers.js
@@ -9,8 +9,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.withNoAnimation = exports.SpaceMe = exports.PadMe = void 0;
 var styled_components_1 = __importDefault(require("styled-components"));
-exports.PadMe = styled_components_1.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n    padding: 5px;\n"], ["\n    padding: 5px;\n"])));
-exports.SpaceMe = styled_components_1.default.span(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n    padding: 5px;\n"], ["\n    padding: 5px;\n"])));
+exports.PadMe = styled_components_1.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n    padding: 5px;\n    box-sizing: content-box;\n"], ["\n    padding: 5px;\n    box-sizing: content-box;\n"])));
+exports.SpaceMe = styled_components_1.default.span(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n    padding: 5px;\n    box-sizing: content-box;\n"], ["\n    padding: 5px;\n    box-sizing: content-box;\n"])));
 var withNoAnimation = function (Comp) {
     return (0, styled_components_1.default)(Comp)(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n        animation: none !important;\n        /* stylelint-disable-next-line selector-max-universal */\n        & * {\n            animation: none !important;\n        }\n    "], ["\n        animation: none !important;\n        /* stylelint-disable-next-line selector-max-universal */\n        & * {\n            animation: none !important;\n        }\n    "])));
 };
