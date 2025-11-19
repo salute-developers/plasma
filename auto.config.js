f6c22d169c8b7f6af1deb785db05b867429793e1
Alexander Lobyntsev <yakutoc@gmail.com>
2025-11-19 08:51:40 +0300

    ci(npm): migrate to OIDC

--- auto.config.js
+++ auto.config.js
@@ -3,11 +3,6 @@ const npmOptions = {
     commitNextVersion: true,
 };
 
-const releasedOptions = {
-    includeBotPrs: true,
-    message: '🚀 This %TYPE is included in version: %VERSION 🚀',
-};
-
 /** Auto configuration */
 module.exports = function rc() {
     const plugins = [['npm', npmOptions], 'conventional-commits'];
