43e94405447e02d462cfb860ac1cbbc7cef6271e
Alexander Lobyntsev <yakutoc@gmail.com>
2025-12-02 11:40:55 +0300

    chore(platform-ai): avatar

--- packages/sdds-platform-ai/src/components/Avatar/Avatar.stories.tsx
+++ packages/sdds-platform-ai/src/components/Avatar/Avatar.stories.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import type { ComponentProps } from 'react';
 import type { StoryObj, Meta } from '@storybook/react';
-import { disableProps } from '@salutejs/plasma-sb-utils';
+import { disableProps, getConfigVariations } from '@salutejs/plasma-sb-utils';
 
 import { Avatar } from './Avatar';
+import { config } from './Avatar.config';
+
+const { views, sizes } = getConfigVariations(config);
 
 const extraPlacements = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
 const extraType = ['', 'badge', 'counter'];
@@ -14,8 +17,8 @@ const meta: Meta<typeof Avatar> = {
     title: 'Data Display/Avatar',
     component: Avatar,
     argTypes: {
-        view: { control: 'inline-radio', options: ['default'] },
-        size: { control: 'select', options: ['xxl', 'l', 'm', 's', 'fit'] },
+        view: { control: 'select', options: views },
+        size: { control: 'select', options: sizes },
         status: { control: 'select', options: ['active', 'inactive'] },
         type: {
             control: 'select',
