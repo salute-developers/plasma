3db8bfb6d8c395b92ba16dd53c61da6fd24ac105
Alexander Lobyntsev <yakutoc@gmail.com>
2025-12-02 12:02:48 +0300

    chore(platform-ai): chip

--- packages/sdds-platform-ai/src/components/Chip/Chip.config.tsx
+++ packages/sdds-platform-ai/src/components/Chip/Chip.config.tsx
@@ -100,28 +100,6 @@ export const config = {
             `,
         },
         size: {
-            l: css`
-                ${chipTokens.borderRadius}: 0.75rem;
-                ${chipTokens.pilledBorderRadius}: 1.5rem;
-                ${chipTokens.width}: auto;
-                ${chipTokens.height}: 3rem;
-                ${chipTokens.padding}: 0 1rem;
-
-                ${chipTokens.fontFamily}: var(--plasma-typo-body-l-font-family);
-                ${chipTokens.fontSize}: var(--plasma-typo-body-l-font-size);
-                ${chipTokens.fontStyle}: var(--plasma-typo-body-l-font-style);
-                ${chipTokens.fontWeight}: var(--plasma-typo-body-l-font-weight);
-                ${chipTokens.letterSpacing}: var(--plasma-typo-body-l-letter-spacing);
-                ${chipTokens.lineHeight}: var(--plasma-typo-body-l-line-height);
-
-                ${chipTokens.leftContentMarginLeft}: -0.125rem;
-                ${chipTokens.leftContentMarginRight}: 0.5rem;
-                ${chipTokens.rightContentMarginLeft}: 0.5rem;
-                ${chipTokens.rightContentMarginRight}: -0.125rem;
-                ${chipTokens.clearContentMarginLeft}: 0.625rem;
-                ${chipTokens.clearContentMarginRight}: -0.25rem;
-                ${chipTokens.closeIconSize}: 1.5rem;
-            `,
             m: css`
                 ${chipTokens.borderRadius}: 0.625rem;
                 ${chipTokens.pilledBorderRadius}: 1.25rem;
@@ -143,50 +121,6 @@ export const config = {
                 ${chipTokens.clearContentMarginLeft}: 0.5rem;
                 ${chipTokens.clearContentMarginRight}: -0.25rem;
                 ${chipTokens.closeIconSize}: 1.25rem;
-            `,
-            s: css`
-                ${chipTokens.borderRadius}: 0.5rem;
-                ${chipTokens.pilledBorderRadius}: 1rem;
-                ${chipTokens.width}: auto;
-                ${chipTokens.height}: 2rem;
-                ${chipTokens.padding}: 0 0.875rem;
-
-                ${chipTokens.fontFamily}: var(--plasma-typo-body-s-font-family);
-                ${chipTokens.fontSize}: var(--plasma-typo-body-s-font-size);
-                ${chipTokens.fontStyle}: var(--plasma-typo-body-s-font-style);
-                ${chipTokens.fontWeight}: var(--plasma-typo-body-s-font-weight);
-                ${chipTokens.letterSpacing}: var(--plasma-typo-body-s-letter-spacing);
-                ${chipTokens.lineHeight}: var(--plasma-typo-body-s-line-height);
-
-                ${chipTokens.leftContentMarginLeft}: -0.125rem;
-                ${chipTokens.leftContentMarginRight}: 0.25rem;
-                ${chipTokens.rightContentMarginLeft}: 0.25rem;
-                ${chipTokens.rightContentMarginRight}: -0.125rem;
-                ${chipTokens.clearContentMarginLeft}: 0.375rem;
-                ${chipTokens.clearContentMarginRight}: -0.25rem;
-                ${chipTokens.closeIconSize}: 1rem;
-            `,
-            xs: css`
-                ${chipTokens.borderRadius}: 0.375rem;
-                ${chipTokens.pilledBorderRadius}: 0.75rem;
-                ${chipTokens.width}: auto;
-                ${chipTokens.height}: 1.5rem;
-                ${chipTokens.padding}: 0 0.625rem;
-
-                ${chipTokens.fontFamily}: var(--plasma-typo-body-xs-font-family);
-                ${chipTokens.fontSize}: var(--plasma-typo-body-xs-font-size);
-                ${chipTokens.fontStyle}: var(--plasma-typo-body-xs-font-style);
-                ${chipTokens.fontWeight}: var(--plasma-typo-body-xs-font-weight);
-                ${chipTokens.letterSpacing}: var(--plasma-typo-body-xs-letter-spacing);
-                ${chipTokens.lineHeight}: var(--plasma-typo-body-xs-line-height);
-
-                ${chipTokens.leftContentMarginLeft}: -0.125rem;
-                ${chipTokens.leftContentMarginRight}: 0.25rem;
-                ${chipTokens.rightContentMarginLeft}: 0.25rem;
-                ${chipTokens.rightContentMarginRight}: -0.125rem;
-                ${chipTokens.clearContentMarginLeft}: 0.25rem;
-                ${chipTokens.clearContentMarginRight}: -0.25rem;
-                ${chipTokens.closeIconSize}: 0.75rem;
             `,
             xxs: css`
                 ${chipTokens.borderRadius}: 0.375rem;
