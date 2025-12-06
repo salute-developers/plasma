116d46a35f301429277ecc1cb5e0f21c429472ae
Dima Shugaev <shuga2704@gmail.com>
2025-12-06 13:41:27 +0300

    feat(): update

--- packages/plasma-new-hope/.storybook/manager.js
+++ packages/plasma-new-hope/.storybook/manager.js
@@ -1,6 +1,30 @@
-import { addons } from 'storybook/manager-api';
+import { addons, types } from 'storybook/manager-api';
 import theme from './theme';
+import React from 'react';
 
 addons.setConfig({
     theme,
+});
+
+addons.register('theme-switcher', (api) => {
+    addons.add('theme-switcher/tool', {
+        title: 'Theme Switcher',
+        type: types.TOOL,
+        match: ({ viewMode }) => viewMode === 'story',
+        render: () => {
+            const isDark = localStorage.getItem('sbTheme') === 'dark';
+
+            return (
+                <button
+                    onClick={() => {
+                        const newTheme = isDark ? 'light' : 'dark';
+                        localStorage.setItem('sbTheme', newTheme);
+                        location.reload(); // Storybook needs refresh to apply UI theme
+                    }}
+                >
+                    {isDark ? '🌞 Light' : '🌚 Dark'}
+                </button>
+            );
+        },
+    });
 });
