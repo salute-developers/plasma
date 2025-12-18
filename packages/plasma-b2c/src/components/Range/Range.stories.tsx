d6776407af139ce5833cb4260f7c3a3c77584ebf
TitanKuzmich <yzn-yfv@mail.ru>
2025-12-18 12:42:20 +0300

    feat(plasma-*): add clear Range config & tests

--- packages/plasma-b2c/src/components/Range/Range.stories.tsx
+++ packages/plasma-b2c/src/components/Range/Range.stories.tsx
@@ -1,18 +1,25 @@
 import React, { ComponentProps } from 'react';
 import type { Meta } from '@storybook/react-vite';
 import { getRangeStories } from '@salutejs/plasma-sb-utils';
+import styled from 'styled-components';
 
 import { IconButton } from '../IconButton';
+import { EmbedIconButton } from '../EmbedIconButton';
 
 import { Range } from './Range';
 import { config } from './Range.config';
 
 type RangeProps = ComponentProps<typeof Range>;
 
+const StyledEmbedIconButton = styled(EmbedIconButton)`
+    margin-right: 0;
+`;
+
 const { meta: META, Default } = getRangeStories({
     component: Range,
     componentConfig: config,
     iconButton: IconButton,
+    embedIconButton: StyledEmbedIconButton,
 });
 
 const meta: Meta<RangeProps> = {
