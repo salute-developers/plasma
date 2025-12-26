e7625e1c7387f316addee06e0e736d10f74bbc42
Krivonos Aleksandr <yzn-yfv@mail.ru>
2025-12-26 14:15:11 +0300

    feat(plasma-*): add clear for DatePicker

--- packages/plasma-b2c/src/components/DatePicker/DatePicker.ts
+++ packages/plasma-b2c/src/components/DatePicker/DatePicker.ts
@@ -3,12 +3,34 @@ import {
     datePickerRangeConfig,
     component,
     mergeConfig,
+    createConditionalComponent,
 } from '@salutejs/plasma-new-hope/styled-components';
 
 import { config } from './DatePicker.config';
+import { config as configClear } from './DatePicker.clear.config';
 
-const mergedConfig = mergeConfig(datePickerConfig, config);
-export const DatePicker = component(mergedConfig);
+const mergedConfigDefault = mergeConfig(datePickerConfig, config);
+export const DatePickerDefault = component(mergedConfigDefault);
 
-const mergedRangeConfig = mergeConfig(datePickerRangeConfig, config);
-export const DatePickerRange = component(mergedRangeConfig);
+const mergedConfigClear = mergeConfig(datePickerConfig, configClear);
+export const DatePickerClear = component(mergedConfigClear);
+
+export const DatePicker = createConditionalComponent(DatePickerDefault, [
+    {
+        conditions: { prop: 'appearance', value: 'clear' },
+        component: DatePickerClear,
+    },
+]);
+
+const mergedRangeConfigDefault = mergeConfig(datePickerRangeConfig, config);
+export const DatePickerRangeDefault = component(mergedRangeConfigDefault);
+
+const mergedRangeConfigClear = mergeConfig(datePickerRangeConfig, configClear);
+export const DatePickerRangeClear = component(mergedRangeConfigClear);
+
+export const DatePickerRange = createConditionalComponent(DatePickerRangeDefault, [
+    {
+        conditions: { prop: 'appearance', value: 'clear' },
+        component: DatePickerRangeClear,
+    },
+]);
