5f2554438a9900544723097968ad2eedca84bbfe
Alexander Lobyntsev <yakutoc@gmail.com>
2026-01-27 16:45:07 +0300

    fix(ListItem): handle disabled state into onClick callback

--- packages/plasma-new-hope/src/components/List/ui/ListItem.tsx
+++ packages/plasma-new-hope/src/components/List/ui/ListItem.tsx
@@ -9,10 +9,19 @@ export const ListItem: React.FC<HTMLAttributes<HTMLLIElement> & ListItemProps> =
     children,
     contentRight,
     disabled,
+    onClick,
     ...rest
 }) => {
+    const handleClick = (event: React.MouseEvent<HTMLLIElement>) => {
+        if (disabled || !onClick) {
+            return;
+        }
+
+        onClick(event);
+    };
+
     return (
-        <StyledListItem className={disabled ? classes.disabledListItem : ''} {...rest}>
+        <StyledListItem className={disabled ? classes.disabledListItem : ''} onClick={handleClick} {...rest}>
             <CellItem contentRight={contentRight}>{children}</CellItem>
         </StyledListItem>
     );
