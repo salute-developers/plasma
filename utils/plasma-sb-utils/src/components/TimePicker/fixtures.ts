16e4b81cb2d38b11da00a9b1f6ffadb1aab7250e
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-01-28 13:59:40 +0300

    feat: refactor TimePicker stories

---
+++ utils/plasma-sb-utils/src/components/TimePicker/fixtures.ts
@@ -0,0 +1,5 @@
+export const labelPlacements = ['outer', 'inner'];
+export const columnsQuantities = [2, 3];
+export const dropdownAligns = ['left', 'right'];
+export const dropdownWidths = ['fixed', 'fullWidth', '10rem', '35rem', '50%', '100%'];
+export const requiredPlacements = ['left', 'right'];
