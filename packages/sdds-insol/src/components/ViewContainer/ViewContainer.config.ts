43e240b91ec08525362c7dcf5f126a98269699cc
Alexander Lobyntsev <yakutoc@gmail.com>
2026-01-29 18:07:39 +0300

    chore(insol-next): use `sdds_insol_next` tokens

--- packages/sdds-insol/src/components/ViewContainer/ViewContainer.config.ts
+++ packages/sdds-insol/src/components/ViewContainer/ViewContainer.config.ts
@@ -1,7 +1,7 @@
 import { css } from '@salutejs/plasma-new-hope/styled-components';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { viewContainer } from '@salutejs/sdds-themes/es/tokens/sdds_insol';
+import { viewContainer } from '@salutejs/sdds-themes/es/tokens/sdds_insol_next';
 
 export const config = {
     variations: {
