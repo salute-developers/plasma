b9bd88880d509bc8950436c96b448bbc3d86b226
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-02-02 15:21:50 +0300

    feat: update TabItem disabled style

--- packages/plasma-new-hope/src/components/Tabs/ui/vertical/VerticalTabItem/variations/_disabled/base.ts
+++ packages/plasma-new-hope/src/components/Tabs/ui/vertical/VerticalTabItem/variations/_disabled/base.ts
@@ -1,19 +1,29 @@
 import { css } from '@linaria/core';
 
 import { classes, tokens } from '../../../../../tokens';
+import { TabItemValue } from '../../VerticalTabItem.styles';
 
 export const base = css`
     &[disabled] {
         cursor: not-allowed;
+        opacity: var(${tokens.disabledOpacity});
 
         &:hover {
             color: var(${tokens.itemColor});
             background-color: var(${tokens.itemBackgroundColor});
+
+            ${TabItemValue} {
+                color: var(${tokens.itemValueColor});
+            }
         }
 
         &.${classes.selectedTabsItem}:hover {
             color: var(${tokens.itemSelectedColor});
             background-color: var(${tokens.itemSelectedBackgroundColor});
+
+            ${TabItemValue} {
+                color: var(${tokens.itemSelectedValueColor});
+            }
         }
     }
 `;
