905d7fb3bce44944595311bce7fe06391b5d547e
Dima Shugaev <shuga2704@gmail.com>
2026-02-05 16:21:02 +0300

    feat(*): Spinner size correction

---
+++ utils/plasma-sb-utils/src/components/Tree/meta.ts
@@ -0,0 +1,80 @@
+import { disableProps, InSpacingDecorator } from '../..';
+
+type CreateMetaProps = {
+    component: any;
+    componentConfig: any;
+    defaultArgs?: {};
+    additionalArgTypes?: {};
+    disablePropsList?: string[];
+};
+
+const arrowPlacements = ['left', 'right'];
+const mode = ['default', 'radio'];
+const variant = ['normal', 'tight'];
+
+export const createMeta = ({
+    component,
+    componentConfig,
+    defaultArgs = {},
+    additionalArgTypes = {},
+    disablePropsList = [],
+}: CreateMetaProps) => {
+    return {
+        decorators: [InSpacingDecorator],
+        component,
+        argTypes: {
+            size: {
+                control: 'select',
+                options: componentConfig.sizes,
+            },
+            checkable: {
+                control: 'boolean',
+            },
+            multiple: {
+                control: 'boolean',
+            },
+            arrowPlacement: {
+                options: arrowPlacements,
+                control: {
+                    type: 'inline-radio',
+                },
+            },
+            defaultExpandAll: {
+                control: 'boolean',
+            },
+            fullWidthItemSelection: {
+                control: 'boolean',
+            },
+            height: {
+                control: 'number',
+            },
+            hasIcon: {
+                control: 'boolean',
+            },
+            mode: {
+                control: 'select',
+                options: mode,
+            },
+            variant: {
+                control: 'select',
+                options: variant,
+            },
+            ...additionalArgTypes,
+            ...disableProps([...disablePropsList]),
+        },
+        args: {
+            size: 'm',
+            fullWidthItemSelection: false,
+            singleLine: true,
+            checkable: false,
+            multiple: false,
+            arrowPlacement: 'left',
+            defaultExpandAll: true,
+            height: undefined,
+            hasIcon: true,
+            mode: 'default',
+            variant: 'normal',
+            ...defaultArgs,
+        },
+    };
+};
