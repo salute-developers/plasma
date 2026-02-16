a1a77e61237d8adaa13d335a3426b7a5c1f7e5c4
Alexander Lobyntsev <yakutoc@gmail.com>
2026-02-16 16:23:55 +0300

    feat(platform_ai): add theme [PLASMA-6541]

--- packages/plasma-tokens/src/themes/index.ts
+++ packages/plasma-tokens/src/themes/index.ts
@@ -52,6 +52,8 @@ export { sdds_insol__dark, darkSdds_insol } from './sdds_insol__dark';
 export { sdds_insol__light, lightSdds_insol } from './sdds_insol__light';
 export { sdds_os__dark, darkSdds_os } from './sdds_os__dark';
 export { sdds_os__light, lightSdds_os } from './sdds_os__light';
+export { sdds_platform_ai__dark, darkSdds_platform_ai } from './sdds_platform_ai__dark';
+export { sdds_platform_ai__light, lightSdds_platform_ai } from './sdds_platform_ai__light';
 export { sdds_procom__dark, darkSdds_procom } from './sdds_procom__dark';
 export { sdds_procom__light, lightSdds_procom } from './sdds_procom__light';
 export { sdds_scan__dark, darkSdds_scan } from './sdds_scan__dark';
