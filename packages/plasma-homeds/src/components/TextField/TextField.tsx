88ace69787155cf3de0e2d4a8c5850b22617fdfa
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-02-24 14:11:10 +0300

    feat(plasma-homeds): update TextField config

--- packages/plasma-homeds/src/components/TextField/TextField.tsx
+++ packages/plasma-homeds/src/components/TextField/TextField.tsx
@@ -1,22 +1,6 @@
-import {
-    textFieldConfig,
-    component,
-    mergeConfig,
-    createConditionalComponent,
-} from '@salutejs/plasma-new-hope/styled-components';
+import { textFieldConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
 
 import { config } from './TextField.config';
-import { config as clearConfig } from './TextField.clear.config';
 
-const mergedConfigDefault = mergeConfig(textFieldConfig, config);
-export const TextFieldDefault = component(mergedConfigDefault);
-
-const mergedConfigClear = mergeConfig(textFieldConfig, clearConfig);
-export const TextFieldClear = component(mergedConfigClear);
-
-export const TextField = createConditionalComponent(TextFieldDefault, [
-    {
-        conditions: { prop: 'appearance', value: 'clear' },
-        component: TextFieldClear,
-    },
-]);
+const mergedConfig = mergeConfig(textFieldConfig, config);
+export const TextField = component(mergedConfig);
