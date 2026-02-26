0a1e581869e692585a6032df2f2eb34e8b82626b
Alexander Lobyntsev <yakutoc@gmail.com>
2026-02-26 15:12:02 +0300

    fix(skeleton): accepted changelog 30.12.2025 [PLASMA-6740]

--- packages/sdds-cs/src/components/Skeleton/LineSkeleton.config.ts
+++ packages/sdds-cs/src/components/Skeleton/LineSkeleton.config.ts
@@ -8,12 +8,12 @@ export const config = {
     variations: {
         view: {
             default: css`
-                ${skeletonTokens.gradientColor}: var(--plasma-colors-skeleton-gradient, var(--surface-skeleton-gradient));
+                ${skeletonTokens.gradientColor}: var(--plasma-colors-skeleton-gradient, linear-gradient(270.00deg, rgba(35, 41, 45, 0.06),rgba(35, 41, 45, 0.05) 10%,rgba(35, 41, 45, 0.04) 20%,rgba(35, 41, 45, 0.03) 25%,rgba(35, 41, 45, 0.02) 30%,rgba(35, 41, 45, 0.01) 40%,rgba(35, 41, 45, 0) 50%,rgba(35, 41, 45, 0.01) 60%,rgba(35, 41, 45, 0.02) 70%,rgba(35, 41, 45, 0.03) 75%,rgba(35, 41, 45, 0.04) 80%,rgba(35, 41, 45, 0.05) 90%,rgba(35, 41, 45, 0.06) 100%));
                 ${skeletonTokens.fadeInColor}: var(--surface-transparent-secondary);
                 ${skeletonTokens.fadeOutColor}: var(--surface-transparent-tertiary);
             `,
             lighter: css`
-                ${skeletonTokens.gradientColor}: var(--plasma-colors-skeleton-gradient-lighter, var(--surface-skeleton-deep-gradient));
+                ${skeletonTokens.gradientColor}: var(--plasma-colors-skeleton-gradient-lighter, linear-gradient(90.00deg, rgba(35, 41, 45, 0.24),rgba(8, 8, 8, 0.22) 5%,rgba(35, 41, 45, 0.21) 10%,rgba(35, 41, 45, 0.19) 16.527%,rgba(35, 41, 45, 0.18) 20%,rgba(35, 41, 45, 0.16) 25%,rgba(35, 41, 45, 0.14) 30%,rgba(35, 41, 45, 0.13) 35%,rgba(35, 41, 45, 0.11) 40%,rgba(35, 41, 45, 0.1) 45%,rgba(35, 41, 45, 0.08) 50%,rgba(35, 41, 45, 0.1) 55%,rgba(35, 41, 45, 0.11) 60%,rgba(35, 41, 45, 0.13) 65%,rgba(35, 41, 45, 0.14) 70%,rgba(35, 41, 45, 0.16) 75%,rgba(35, 41, 45, 0.18) 80%,rgba(35, 41, 45, 0.19) 85%,rgba(35, 41, 45, 0.21) 90%,rgba(35, 41, 45, 0.22) 95%,rgba(35, 41, 45, 0.24) 100%));
                 ${skeletonTokens.fadeInColor}: var(--surface-transparent-secondary);
                 ${skeletonTokens.fadeOutColor}: var(--surface-transparent-tertiary);
             `,
