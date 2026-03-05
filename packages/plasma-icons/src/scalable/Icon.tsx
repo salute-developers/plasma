bc58476816b41d24d096797bcdfe610dcc6d8f84
Alexander Lobyntsev <yakutoc@gmail.com>
2026-03-05 06:39:13 +0300

    feat(plasma-icons): Add icon `Vibration`

--- packages/plasma-icons/src/scalable/Icon.tsx
+++ packages/plasma-icons/src/scalable/Icon.tsx
@@ -686,6 +686,7 @@ export const iconSectionsSet = {
         dropsGroupFill: 'DropsGroupFill',
         gas: 'Gas',
         smoke: 'Smoke',
+        vibration: 'Vibration',
     },
     Toggle: {
         bellDashFill: 'BellDashFill',
