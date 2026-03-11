1c72e3b9e324fd96c72add9258ab4dfea46fb779
Alexander Lobyntsev <yakutoc@gmail.com>
2026-03-11 09:02:14 +0300

    fix(useOverflow): add cleanup function [PLASMA-6815]

--- packages/plasma-new-hope/src/components/Sheet/hooks/useOverflow.ts
+++ packages/plasma-new-hope/src/components/Sheet/hooks/useOverflow.ts
@@ -1,19 +1,20 @@
-import { useEffect, useRef } from 'react';
-import { canUseDOM } from 'src/utils';
+import { useEffect } from 'react';
 
 import { SheetHookArgs } from '../Sheet.types';
 
 export const useOverflow = ({ opened }: SheetHookArgs) => {
-    const overflow = useRef<string>(canUseDOM ? document.body.style.overflowY : 'initial');
-
-    // linaria не поддерживает динамический global
+    // INFO: Linaria не поддерживает динамический global
     useEffect(() => {
-        if (opened) {
-            overflow.current = document.body.style.overflowY;
-            document.body.style.overflowY = 'hidden';
+        if (!opened) {
             return;
         }
 
-        document.body.style.overflowY = overflow.current;
+        const prev = document.body.style.overflowY;
+
+        document.body.style.overflowY = 'hidden';
+
+        return () => {
+            document.body.style.overflowY = prev;
+        };
     }, [opened]);
 };
