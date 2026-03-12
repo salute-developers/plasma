8fc0f724ab19b691104ba404d4394f88fa4f1ad7
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-03-12 13:03:11 +0300

    feat: migrate TextField tests to new-hope

--- packages/plasma-b2c/src/components/TextField/TextField.component-test.tsx
+++ packages/plasma-b2c/src/components/TextField/TextField.component-test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { FC, PropsWithChildren } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { standard as standardTypo } from '@salutejs/plasma-typo';
-import { mount, CypressTestDecorator, getComponent, SpaceMe } from '@salutejs/plasma-cy-utils';
+import { mount, CypressTestDecorator, getComponent, SpaceMe, PadMe } from '@salutejs/plasma-cy-utils';
 import { IconSleep, IconEye } from '@salutejs/plasma-icons';
 
 import { TextField as TextFieldB2C } from '.';
@@ -20,33 +20,6 @@ describe('plasma-b2c: TextField', () => {
             {children}
         </CypressTestDecorator>
     );
-
-    function Demo({ maxLength }) {
-        const [value, setValue] = React.useState('');
-
-        return (
-            <TextField
-                size="m"
-                maxLength={maxLength}
-                placeholder="Placeholder"
-                onChange={(e) => {
-                    setValue(e.target.value);
-                }}
-                value={value}
-                helperText="Helper text"
-            />
-        );
-    }
-
-    it('default', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField size="m" value="Value" placeholder="Placeholder" helperText="Helper text" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
 
     it('_size', () => {
         mount(
@@ -120,149 +93,6 @@ describe('plasma-b2c: TextField', () => {
                 <TextField appearance="clear" hasDivider size="m" placeholder="Placeholder" status="warning" />
                 <SpaceMe />
                 <TextField appearance="clear" hasDivider size="m" placeholder="Placeholder" status="error" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('_maxLength', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <Demo maxLength={10} />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input').type('More then ten symbols');
-
-        cy.matchImageSnapshot();
-    });
-
-    it(':empty', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField size="m" placeholder="Placeholder" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it(':focused', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField size="m" value="Value" placeholder="Placeholder" helperText="Helper text" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.get('input:first').focus();
-        cy.matchImageSnapshot();
-    });
-
-    it(':disabled', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField size="m" value="Value" placeholder="Placeholder" helperText="Helper text" disabled />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it(':readOnly', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField size="m" value="Value" placeholder="Placeholder" helperText="Helper text" readOnly />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('content', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField
-                    size="m"
-                    value="Value"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    contentLeft={<IconSleep color="inherit" size="s" />}
-                />
-                <SpaceMe />
-                <TextField
-                    size="m"
-                    value="Value"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    contentLeft={<IconSleep color="inherit" size="s" />}
-                    contentRight={<IconEye color="inherit" size="s" />}
-                />
-                <SpaceMe />
-                <TextField
-                    size="m"
-                    value="Value"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    contentRight={<IconEye color="inherit" size="s" />}
-                />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('_defaultValue', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField size="m" defaultValue="Default value" placeholder="Placeholder" helperText="Helper text" />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    it('content: with label', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField
-                    size="l"
-                    value="Value"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    label="Label"
-                />
-                <TextField
-                    size="l"
-                    value="Value"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    label="Label"
-                    contentLeft={<IconSleep color="inherit" size="s" />}
-                />
-                <SpaceMe />
-                <TextField
-                    size="l"
-                    value="Value"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    label="Label"
-                    contentLeft={<IconSleep color="inherit" size="s" />}
-                    contentRight={<IconEye color="inherit" size="s" />}
-                />
-                <SpaceMe />
-                <TextField
-                    size="l"
-                    value="Value"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    label="Label"
-                    contentRight={<IconEye color="inherit" size="s" />}
-                />
             </CypressTestDecoratorWithTypo>,
         );
 
@@ -444,284 +274,32 @@ describe('plasma-b2c: TextField', () => {
         cy.get('#test').matchImageSnapshot('empty');
     });
 
-    it('_enumerationType:chip, _chipView, _chipValidator', () => {
-        cy.viewport(500, 778);
-
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField
-                    size="l"
-                    label="Label"
-                    enumerationType="chip"
-                    chips={['Value 1', 'Value 2']}
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                />
-                <SpaceMe />
-                <TextField
-                    size="l"
-                    label="Label"
-                    value="place values"
-                    enumerationType="chip"
-                    chips={['Value 1', 'Value 2']}
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                />
-                <SpaceMe />
-                <TextField
-                    size="l"
-                    label="Label"
-                    enumerationType="chip"
-                    chips={['Value 1', 'Value 2']}
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    chipView="accent"
-                />
-                <SpaceMe />
-                <TextField
-                    size="l"
-                    label="Label"
-                    enumerationType="chip"
-                    chips={['Value 1', 'Value 2']}
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    chipView="positive"
-                />
-                <SpaceMe />
-                <TextField
-                    size="l"
-                    label="Label"
-                    enumerationType="chip"
-                    chips={['Value 1', 'Value 2']}
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    chipView="negative"
-                />
-                <SpaceMe />
-                <TextField
-                    size="l"
-                    label="Label"
-                    enumerationType="chip"
-                    chips={['Value 1', 'Value 2', 'Value 3']}
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                    chipView="warning"
-                    chipValidator={(value) => (value === 'Value 1' ? { view: 'negative' } : {})}
-                />
-                <SpaceMe />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    describe('_required', () => {
-        const sizes = ['xs', 's', 'm', 'l'] as const;
-
-        const cases = [
-            { labelPlacement: 'outer' },
-            { requiredPlacement: 'left', labelPlacement: 'outer' },
-            { labelPlacement: 'inner', view: 'innerLabel' },
-            { requiredPlacement: 'left', labelPlacement: 'inner', view: 'innerLabel' },
-        ];
-
-        sizes.forEach((size) => {
-            it(`_size:${size}`, () => {
-                mount(
-                    <CypressTestDecoratorWithTypo>
-                        {cases.map((props) => (
-                            <div style={{ margin: '0 1rem' }}>
-                                <TextField
-                                    value="Value"
-                                    placeholder="Placeholder"
-                                    label="Title"
-                                    size={size}
-                                    required
-                                    hasRequiredIndicator
-                                    {...props}
-                                />
-                                <SpaceMe />
-                            </div>
-                        ))}
-                    </CypressTestDecoratorWithTypo>,
-                );
-
-                cy.matchImageSnapshot();
-            });
-        });
-
-        it('required attribute', () => {
-            mount(
-                <CypressTestDecoratorWithTypo>
-                    <TextField id="required" value="Value" placeholder="Placeholder" label="Title" required />
-                    <TextField
-                        id="falseRequired"
-                        value="Value"
-                        placeholder="Placeholder"
-                        label="Title"
-                        required={false}
-                    />
-                    <TextField id="notRequired" value="Value" placeholder="Placeholder" label="Title" />
-                </CypressTestDecoratorWithTypo>,
-            );
-
-            cy.get('#required').should('have.attr', 'required');
-            cy.get('#falseRequired').should('not.have.attr', 'required');
-            cy.get('#notRequired').should('not.have.attr', 'required');
-        });
-
-        it('required with chips', () => {
-            mount(
-                <CypressTestDecoratorWithTypo>
-                    <TextField enumerationType="chip" id="textfield" placeholder="Placeholder" label="Title" required />
-                </CypressTestDecoratorWithTypo>,
-            );
-
-            cy.get('#textfield').should('have.attr', 'required');
-            cy.get('#textfield').type('some value{enter}');
-            cy.get('#textfield').should('not.have.attr', 'required');
-            cy.get('#textfield').type('{backspace}');
-            cy.get('#textfield').should('have.attr', 'required');
-        });
-    });
-
-    it('chipType', () => {
-        mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField
-                    size="l"
-                    label="Label"
-                    enumerationType="chip"
-                    chips={['Value 1', 'Value 2']}
-                    chipType="text"
-                    placeholder="Placeholder"
-                    helperText="Helper text"
-                    animatedHint="label"
-                />
-            </CypressTestDecoratorWithTypo>,
-        );
-
-        cy.matchImageSnapshot();
-    });
-
-    describe('with hint', () => {
-        const sizes = ['xs', 's', 'm', 'l'] as const;
-
+    it('with hint', () => {
         const cases = [
             { labelPlacement: 'outer' },
             { labelPlacement: 'inner', view: 'innerLabel' },
             { labelPlacement: 'inner', view: 'innerLabel', hintTargetPlacement: 'inner' },
         ] as const;
 
-        sizes.forEach((size) => {
-            it(`_size:${size}`, () => {
-                mount(
-                    <CypressTestDecoratorWithTypo>
-                        {cases.map((props) => (
-                            <div style={{ margin: '0 3rem' }}>
-                                <TextField
-                                    value="Value"
-                                    placeholder="Placeholder"
-                                    label="Title"
-                                    size={size}
-                                    hintText="Подсказка к полю"
-                                    hintTrigger="click"
-                                    {...props}
-                                />
-                                <SpaceMe />
-                            </div>
-                        ))}
-                    </CypressTestDecoratorWithTypo>,
-                );
-
-                cy.get('.popover-wrapper').first().click();
-
-                cy.matchImageSnapshot();
-            });
-        });
-    });
-});
-
-describe('plasma-b2c: TextField keyboard navigation', () => {
-    const TextField = getComponent('TextField') as typeof TextFieldB2C;
-
-    const CypressTestDecoratorWithTypo: FC<PropsWithChildren> = ({ children }) => (
-        <CypressTestDecorator>
-            <StandardTypoStyle />
-            {children}
-        </CypressTestDecorator>
-    );
-
-    it('chips', () => {
         mount(
-            <CypressTestDecoratorWithTypo>
-                <TextField size="m" placeholder="Placeholder" enumerationType="chip" helperText="Helper text" />
-            </CypressTestDecoratorWithTypo>,
+            <>
+                {cases.map((props) => (
+                    <div style={{ margin: '0 3rem' }}>
+                        <TextField
+                            value="Value"
+                            placeholder="Placeholder"
+                            label="Title"
+                            hintText="Подсказка к полю"
+                            hintTrigger="click"
+                            {...props}
+                        />
+                        <PadMe />
+                    </div>
+                ))}
+            </>,
         );
 
-        cy.get('body').tab();
-
-        // добавляем chips
-        cy.get('input').type('Hello,{enter} Plasma!{enter} Have fun{enter}');
-
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200);
-        cy.get('.has-chips').find('button').should('have.length', 3);
-
-        // фокус на первом чипе слева от инпута по ArrowLeft
-        cy.get('input').type('{leftarrow}');
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100);
-        cy.get('.has-chips').find('button').eq(2).should('be.focused');
-
-        // фокус на первом чипе по ArrowLeft
-        cy.focused()
-            .trigger('keydown', { key: 'ArrowLeft' })
-            .trigger('keydown', { key: 'ArrowLeft' })
-            .trigger('keydown', { key: 'ArrowLeft' });
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200);
-        cy.get('.has-chips').find('button').eq(0).should('be.focused');
-
-        // фокус на инпуте по ArrowRight до конца
-        cy.focused()
-            .trigger('keydown', { key: 'ArrowRight' })
-            .trigger('keydown', { key: 'ArrowRight' })
-            .trigger('keydown', { key: 'ArrowRight' });
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200);
-        cy.get('input').should('be.focused');
-
-        // удалён последний чип по Backspace из фокуса на инпуте
-        cy.focused().trigger('keydown', { key: 'Backspace' });
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200);
-        cy.get('.has-chips').find('button').should('have.length', 2);
-
-        // фокус на первом чипе по ArrowLeft
-        cy.focused().trigger('keydown', { key: 'ArrowLeft' }).trigger('keydown', { key: 'ArrowLeft' });
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200);
-        cy.get('.has-chips').find('button').eq(0).should('be.focused');
-
-        // удалён первый чип по Backspace
-        cy.focused().trigger('keydown', { key: 'Backspace' });
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200);
-        cy.get('.has-chips').find('button').should('have.length', 1);
-
-        // фокус на инпуте и ввод текста
-        cy.get('input').focus().type('Hello!!!');
-
-        // удаление части текста
-        cy.focused().type('{backspace}{backspace}{backspace}');
-        cy.get('input').should('have.value', 'Hello');
+        cy.get('.popover-wrapper').first().click();
 
         cy.matchImageSnapshot();
     });
