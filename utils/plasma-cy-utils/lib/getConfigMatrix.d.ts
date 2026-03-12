4026d062ce44961b179925de32a38c741b054147
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-03-12 11:12:40 +0300

    chore: tune getBaseVisualTests

--- utils/plasma-cy-utils/lib/getConfigMatrix.d.ts
+++ utils/plasma-cy-utils/lib/getConfigMatrix.d.ts
@@ -1,5 +1,5 @@
 type GetConfigMatrixReturn = {
     [key: string]: string;
 };
-export declare const getConfigMatrix: <T = {}>(config: T, options?: string[]) => GetConfigMatrixReturn[];
+export declare const getConfigMatrix: <T = {}>(config: T, options?: string[], excludePropsValues?: Record<string, string[]>) => GetConfigMatrixReturn[];
 export {};
