1f662bde5bf29de1d5e5e561d1d2985c11ad6b3d
Alexander Lobyntsev <yakutoc@gmail.com>
2026-03-13 20:13:00 +0300

    feat(core): handle extraBold font-weight

--- packages/plasma-new-hope/src/components/Typography/Typography.tsx
+++ packages/plasma-new-hope/src/components/Typography/Typography.tsx
@@ -16,8 +16,9 @@ export const typographyRootCompose = (defaultArgs?: { defaultBold?: boolean; typ
             children,
             breakWord,
             noWrap,
-            bold = defaultArgs?.defaultBold,
             medium,
+            bold = defaultArgs?.defaultBold,
+            extraBold,
             color,
             isNumeric,
             isItalic,
@@ -31,6 +32,14 @@ export const typographyRootCompose = (defaultArgs?: { defaultBold?: boolean; typ
         const isHeading = defaultArgs?.type === 'heading';
         const ariaHeadingLevel = isHeading ? size?.split('')[1] : null;
 
+        // Порядок определяет приоритет: extraBold и medium перебивают дефолтный bold
+        // TODO: Нужно менять поведение, не должен быть bold по-умолчанию
+        const fontWeightClass = [
+            extraBold && classes.typoExtraBold,
+            medium && classes.typoMedium,
+            bold && classes.typoBold,
+        ].find(Boolean);
+
         return (
             <Root
                 size={size}
@@ -38,8 +47,7 @@ export const typographyRootCompose = (defaultArgs?: { defaultBold?: boolean; typ
                 className={cx(
                     noWrap && classes.typoWithNoWrap,
                     breakWord && classes.typoWithBreakWord,
-                    bold && classes.typoBold,
-                    medium && classes.typoMedium,
+                    fontWeightClass,
                     isNumeric && classes.typoIsNumeric,
                     isItalic && classes.typoIsItalic,
                     className,
--- packages/plasma-new-hope/src/components/Typography/tokens.ts
+++ packages/plasma-new-hope/src/components/Typography/tokens.ts
@@ -1,8 +1,9 @@
 export const classes = {
     typoWithNoWrap: 'with-no-wrap',
     typoWithBreakWord: 'with-break-word',
-    typoBold: 'typography-bold',
     typoMedium: 'typography-medium',
+    typoBold: 'typography-bold',
+    typoExtraBold: 'typography-extra-bold',
     typoIsItalic: 'typography-is-italic',
     typoIsNumeric: 'typography-is-numeric',
 };
@@ -12,8 +13,9 @@ export const tokens = {
     typoFontSize: '--typo-font-size',
     typoFontStyle: '--typo-font-style',
     typoFontWeight: '--typo-font-weight',
-    typoFontWeightBold: '--typo-font-weight-bold',
     typoFontWeightMedium: '--typo-font-weight-medium',
+    typoFontWeightBold: '--typo-font-weight-bold',
+    typoFontWeightExtraBold: '--typo-font-weight-extra-bold',
     typoFontLetterSpacing: '--typo-font-letter-spacing',
     typoFontLineHeight: '--typo-font-line-height',
 };
