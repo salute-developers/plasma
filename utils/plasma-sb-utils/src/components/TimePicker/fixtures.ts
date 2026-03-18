128b006e91a342184e70776fc6b37bb661b40bb7
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-03-18 15:43:16 +0300

    feat(plasma-new-hope): refactor TimePicker popover mechanism

--- utils/plasma-sb-utils/src/components/TimePicker/fixtures.ts
+++ utils/plasma-sb-utils/src/components/TimePicker/fixtures.ts
@@ -1,5 +1,18 @@
 export const labelPlacements = ['outer', 'inner'];
 export const columnsQuantities = [2, 3];
-export const dropdownAligns = ['left', 'right'];
 export const dropdownWidths = ['fixed', 'fullWidth', '10rem', '35rem', '50%', '100%'];
 export const requiredPlacements = ['left', 'right'];
+export const placements = [
+    'top',
+    'top-start',
+    'top-end',
+    'right',
+    'right-start',
+    'right-end',
+    'bottom',
+    'bottom-start',
+    'bottom-end',
+    'left',
+    'left-start',
+    'left-end',
+];
