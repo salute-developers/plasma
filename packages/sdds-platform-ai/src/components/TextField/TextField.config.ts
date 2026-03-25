e381c378fcb0ba840daf1350b3cfe411af7e88dd
Alexander Lobyntsev <yakutoc@gmail.com>
2026-03-25 07:55:27 +0300

    fix(platform-ai): fix caret color to `text- primary`

--- packages/sdds-platform-ai/src/components/TextField/TextField.config.ts
+++ packages/sdds-platform-ai/src/components/TextField/TextField.config.ts
@@ -21,7 +21,7 @@ export const config = {
                 ${tokens.backgroundColor}: var(--surface-transparent-primary);
                 ${tokens.backgroundColorHover}: var(--surface-transparent-primary-hover);
                 ${tokens.backgroundColorFocus}: var(--surface-transparent-secondary);
-                ${tokens.caretColor}: var(--text-accent);
+                ${tokens.caretColor}: var(--text-primary);
                 ${tokens.textBeforeColor}: var(--text-tertiary);
                 ${tokens.textAfterColor}: var(--text-tertiary);
                 ${tokens.labelColor}: var(--text-primary);
