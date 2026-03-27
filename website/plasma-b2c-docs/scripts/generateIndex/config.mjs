334d101eb39716e60069f95d81c9029caf48a7d0
Dima Shugaev <shuga2704@gmail.com>
2026-03-27 09:43:58 +0300

    feat(sdds-cs-docs): add mcp

--- website/plasma-b2c-docs/scripts/generateIndex/config.mjs
+++ website/plasma-b2c-docs/scripts/generateIndex/config.mjs
@@ -2,10 +2,10 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 
 const config = {
-    baseUrl: 'https://plasma.sberdevices.ru/giga/',
+    baseUrl: 'https://plasma.sberdevices.ru/b2c/',
     pathToDocs: path.join(dirname(fileURLToPath(import.meta.url)), '../../docs'),
     exclude: ['_examples', '_theme-switch', '_theme-turn-on', '_utils', 'tokens.mdx'],
-    productId: 'plasma-giga',
+    productId: 'plasma-b2c',
     qdrantConfig: {
         collectionName: 'plasma-docs',
         payloadSchema: ['metadata.productId', 'metadata.source.url'],
