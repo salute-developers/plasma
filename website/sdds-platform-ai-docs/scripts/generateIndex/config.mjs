c5c1d5cc08c01adeb881bafc0fc70cef9b5c403a
Dima Shugaev <shuga2704@gmail.com>
2026-03-27 11:03:42 +0300

    feat(sdds-platform-ai): add mcp

---
+++ website/sdds-platform-ai-docs/scripts/generateIndex/config.mjs
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const config = {
+    baseUrl: 'https://plasma.sberdevices.ru/sdds-platform-ai/',
+    pathToDocs: path.join(dirname(fileURLToPath(import.meta.url)), '../../docs'),
+    exclude: ['_examples', '_theme-switch', '_theme-turn-on', '_utils', 'tokens.mdx'],
+    productId: 'sdds-platform-ai',
+    qdrantConfig: {
+        collectionName: 'plasma-docs',
+        payloadSchema: ['metadata.productId', 'metadata.source.url'],
+    },
+};
+
+export default config;
