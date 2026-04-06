828745d0efa35a37ae2adb63f92815bf5b919cbf
Anton Mikhailov <antonmih006@gmail.com>
2026-04-06 12:39:49 +0300

    fix(sdds-finai): fix Segment config

--- packages/sdds-finai/src/components/Segment/SegmentItem.config.ts
+++ packages/sdds-finai/src/components/Segment/SegmentItem.config.ts
@@ -2,13 +2,13 @@ import { css, segmentTokens } from '@salutejs/plasma-new-hope/styled-components'
 
 export const config = {
     defaults: {
-        view: 'clear',
+        view: 'none',
         size: 'xs',
     },
     variations: {
         view: {
-            clear: css`
-                ${segmentTokens.itemColor}: var(--text-secondary);
+            none: css`
+                ${segmentTokens.itemColor}: var(--text-primary);
                 ${segmentTokens.itemBackgroundColor}: transparent;
                 ${segmentTokens.itemColorHover}: var(--text-primary-hover);
                 ${segmentTokens.itemBackgroundColorHover}: transparent;
@@ -39,7 +39,7 @@ export const config = {
 
                 ${segmentTokens.groupFilledBackgroundColor}: var(--text-accent);
             `,
-            default: css`
+            primary: css`
                 ${segmentTokens.itemColor}: var(--text-primary);
                 ${segmentTokens.itemBackgroundColor}: transparent;
                 ${segmentTokens.itemColorHover}: var(--text-primary-hover);
@@ -52,22 +52,6 @@ export const config = {
                 ${segmentTokens.itemSelectedBackgroundColorHover}: var(--surface-solid-default);
                 ${segmentTokens.itemSelectedAdditionalColor}: var(--inverse-text-secondary);
                 ${segmentTokens.itemSelectedAdditionalColorHover}: var(--inverse-text-secondary);
-
-                ${segmentTokens.groupFilledBackgroundColor}: var(--text-accent);
-            `,
-            accent: css`
-                ${segmentTokens.itemColor}: var(--text-primary);
-                ${segmentTokens.itemBackgroundColor}: transparent;
-                ${segmentTokens.itemColorHover}: var(--text-primary-hover);
-                ${segmentTokens.itemBackgroundColorHover}: transparent;
-                ${segmentTokens.itemAdditionalColor}: var(--text-secondary);
-                ${segmentTokens.itemAdditionalColorHover}: var(--text-secondary);
-                ${segmentTokens.itemSelectedColor}: var(--on-dark-text-primary);
-                ${segmentTokens.itemSelectedBackgroundColor}: var(--surface-accent);
-                ${segmentTokens.itemSelectedColorHover}: var(--on-dark-text-primary-hover);
-                ${segmentTokens.itemSelectedBackgroundColorHover}: var(--surface-accent);
-                ${segmentTokens.itemSelectedAdditionalColor}: var(--on-dark-text-secondary);
-                ${segmentTokens.itemSelectedAdditionalColorHover}: var(--on-dark-text-secondary);
 
                 ${segmentTokens.groupFilledBackgroundColor}: var(--text-accent);
             `,
@@ -106,57 +90,6 @@ export const config = {
                 ${segmentTokens.fontWeight}: var(--plasma-typo-body-s-bold-font-weight);
                 ${segmentTokens.letterSpacing}: var(--plasma-typo-body-s-letter-spacing);
                 ${segmentTokens.lineHeight}: var(--plasma-typo-body-s-line-height);
-            `,
-            m: css`
-                ${segmentTokens.itemBorderRadius}: 0.75rem;
-                ${segmentTokens.itemWidth}: auto;
-                ${segmentTokens.itemHeight}: 3rem;
-                ${segmentTokens.itemPadding}: 0.75rem 1.25rem;
-                ${segmentTokens.itemPilledPadding}: 0.75rem 0.75rem;
-                ${segmentTokens.itemContentPadding}: 0.125rem;
-                ${segmentTokens.itemIconMargin}: 0.25rem;
-                ${segmentTokens.itemMarginLeft}: 0rem;
-
-                ${segmentTokens.fontFamily}: var(--plasma-typo-body-m-font-family);
-                ${segmentTokens.fontSize}: var(--plasma-typo-body-m-font-size);
-                ${segmentTokens.fontStyle}: var(--plasma-typo-body-m-font-style);
-                ${segmentTokens.fontWeight}: var(--plasma-typo-body-m-bold-font-weight);
-                ${segmentTokens.letterSpacing}: var(--plasma-typo-body-m-letter-spacing);
-                ${segmentTokens.lineHeight}: var(--plasma-typo-body-m-line-height);
-            `,
-            l: css`
-                ${segmentTokens.itemBorderRadius}: 0.875rem;
-                ${segmentTokens.itemWidth}: auto;
-                ${segmentTokens.itemHeight}: 3.5rem;
-                ${segmentTokens.itemPadding}: 1rem 1.5rem; /* 1.625 */
-                ${segmentTokens.itemPilledPadding}: 1rem 1rem;
-                ${segmentTokens.itemContentPadding}: 0.0625rem 0.125rem;
-                ${segmentTokens.itemIconMargin}: 0.375rem;
-                ${segmentTokens.itemMarginLeft}: 0;
-
-                ${segmentTokens.fontFamily}: var(--plasma-typo-body-l-font-family);
-                ${segmentTokens.fontSize}: var(--plasma-typo-body-l-font-size);
-                ${segmentTokens.fontStyle}: var(--plasma-typo-body-l-font-style);
-                ${segmentTokens.fontWeight}: var(--plasma-typo-body-l-bold-font-weight);
-                ${segmentTokens.letterSpacing}: var(--plasma-typo-body-l-letter-spacing);
-                ${segmentTokens.lineHeight}: var(--plasma-typo-body-l-line-height);
-            `,
-            xl: css`
-                ${segmentTokens.itemBorderRadius}: 1rem;
-                ${segmentTokens.itemWidth}: auto;
-                ${segmentTokens.itemHeight}: 4rem;
-                ${segmentTokens.itemPadding}: 1.25rem 1.75rem;
-                ${segmentTokens.itemPilledPadding}: 1.25rem;
-                ${segmentTokens.itemContentPadding}: 0.0625rem 0.125rem;
-                ${segmentTokens.itemIconMargin}: 0.375rem;
-                ${segmentTokens.itemMarginLeft}: 0;
-
-                ${segmentTokens.fontFamily}: var(--plasma-typo-body-l-font-family);
-                ${segmentTokens.fontSize}: var(--plasma-typo-body-l-font-size);
-                ${segmentTokens.fontStyle}: var(--plasma-typo-body-l-font-style);
-                ${segmentTokens.fontWeight}: var(--plasma-typo-body-l-bold-font-weight);
-                ${segmentTokens.letterSpacing}: var(--plasma-typo-body-l-letter-spacing);
-                ${segmentTokens.lineHeight}: var(--plasma-typo-body-l-line-height);
             `,
         },
         disabled: {
