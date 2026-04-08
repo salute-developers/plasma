0f68ca544e3653e27c134b71b0bf078f6de51b49
Alexander Lobyntsev <yakutoc@gmail.com>
2026-04-08 08:22:35 +0300

    fix(date-*): fix import DatePicker, TimePicker

--- packages/sdds-platform-ai/src/index.ts
+++ packages/sdds-platform-ai/src/index.ts
@@ -32,6 +32,8 @@ export * from './components/Segment';
 export * from './components/Switch';
 export * from './components/Slider';
 export * from './components/DateTimePicker';
+export * from './components/DatePicker';
+export * from './components/TimePicker';
 
 // components group Data Entry (upload)
 export * from './components/Attach';
