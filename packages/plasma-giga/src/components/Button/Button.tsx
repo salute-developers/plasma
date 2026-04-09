63556b87335a20d7391ef26a6dc728c26d56420e
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-04-09 16:01:54 +0300

    feat: add MakePolymorphic to Button

--- packages/plasma-giga/src/components/Button/Button.tsx
+++ packages/plasma-giga/src/components/Button/Button.tsx
@@ -1,13 +1,14 @@
+import { ComponentProps } from 'react';
 import { buttonConfig, component, mergeConfig } from '@salutejs/plasma-new-hope/styled-components';
+import { makePolymorphic } from '@salutejs/plasma-new-hope';
 
 import { config } from './Button.config';
 
 const mergedConfig = mergeConfig(buttonConfig, config);
 const ButtonComponent = component(mergedConfig);
 
-export type ButtonProps = typeof ButtonComponent;
-
 /**
  * Кнопка.
  */
-export const Button = ButtonComponent;
+export const Button = makePolymorphic(ButtonComponent, 'button');
+export type ButtonProps = ComponentProps<typeof ButtonComponent>;
