a06b78060fe7c63d3b5d0f1ef54d0f724a0aca57
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-04-09 13:00:39 +0300

    feat: refactor Chip stories

--- packages/plasma-new-hope/src/examples/components/Chip/Chip.stories.tsx
+++ packages/plasma-new-hope/src/examples/components/Chip/Chip.stories.tsx
@@ -1,102 +1,22 @@
-import React, { ComponentProps } from 'react';
-import { action } from 'storybook/actions';
-import { disableProps, getConfigVariations } from '@salutejs/plasma-sb-utils';
-import type { Meta, StoryObj } from '@storybook/react-vite';
-
-import { IconMic } from '../../../components/_Icon';
-import { WithTheme } from '../../_helpers';
+import type { ComponentProps } from 'react';
+import type { Meta } from '@storybook/react-vite';
+import { getChipStories } from '@salutejs/plasma-sb-utils';
 
 import { Chip } from './Chip';
 import { config } from './Chip.config';
 
-const onClear = action('onClear');
+type ChipProps = ComponentProps<typeof Chip>;
 
-const { views, sizes } = getConfigVariations(config);
-
-const iconSizeMapper = {
-    l: '1.5rem',
-    m: '1.5rem',
-    s: '1rem',
-    xs: '0.75rem',
-    xxs: '0.75rem',
-};
-
-const meta: Meta<typeof Chip> = {
-    title: 'Data Display/Chip',
-    decorators: [WithTheme],
+const { meta: META, Default } = getChipStories({
     component: Chip,
-    args: {
-        text: 'Hello',
-        view: 'default',
-        size: 'm',
-        appearance: 'default',
-        hasClear: true,
-        disabled: false,
-        focused: true,
-        pilled: false,
-        onClear,
-    },
-    argTypes: {
-        view: {
-            options: views,
-            control: {
-                type: 'select',
-            },
-        },
-        size: {
-            options: sizes,
-            control: {
-                type: 'select',
-            },
-        },
-        appearance: {
-            options: ['default', 'transparent'],
-            control: {
-                type: 'select',
-            },
-        },
-        ...disableProps(['readOnly', 'onClear', 'contentLeft']),
-    },
+    componentConfig: config,
+});
+
+const meta: Meta<ChipProps> = {
+    ...META,
+    title: 'Data Display/Chip',
 };
 
 export default meta;
 
-type StoryPropsDefault = ComponentProps<typeof Chip> & {
-    enableContentRight: boolean;
-    enableContentLeft: boolean;
-};
-
-const StoryDefault = ({ enableContentLeft, enableContentRight, size, ...rest }: StoryPropsDefault) => {
-    return (
-        <Chip
-            contentLeft={
-                enableContentLeft ? (
-                    <IconMic sizeCustomValue={iconSizeMapper[size as keyof typeof iconSizeMapper]} color="inherit" />
-                ) : undefined
-            }
-            contentRight={
-                enableContentRight ? (
-                    <IconMic sizeCustomValue={iconSizeMapper[size as keyof typeof iconSizeMapper]} color="inherit" />
-                ) : undefined
-            }
-            size={size}
-            {...rest}
-        />
-    );
-};
-
-export const Default: StoryObj<StoryPropsDefault> = {
-    args: {
-        enableContentLeft: false,
-        enableContentRight: false,
-    },
-    render: (args) => <StoryDefault {...args} />,
-};
-
-export const LongText: StoryObj<StoryPropsDefault> = {
-    args: {
-        ...Default.args,
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    },
-    render: (args) => <StoryDefault style={{ width: '10rem' }} {...args} />,
-};
+export { Default };
