d451b169468f36b4f79277ddb056d6976565cc3c
Alexander Lobyntsev <yakutoc@gmail.com>
2026-04-10 09:16:42 +0300

    chore(sb-utils): update storybook

--- utils/plasma-sb-utils/src/components/TimePicker/stories.tsx
+++ utils/plasma-sb-utils/src/components/TimePicker/stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { action } from '@storybook/addon-actions';
+import { action } from 'storybook/actions';
 import { IconPlasma, IconSearch } from '@salutejs/plasma-icons';
 
 const onToggle = action('onToggle');
