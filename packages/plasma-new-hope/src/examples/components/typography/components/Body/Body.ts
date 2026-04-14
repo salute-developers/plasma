31011882c1bf5c5215700ffde1a283272a4202c1
Neretin Artem <hawktrike@gmail.com>
2026-04-14 11:59:02 +0500

    feat(plasma-new-hope): Add polymorphic as and ref prop support

--- packages/plasma-new-hope/src/examples/components/typography/components/Body/Body.ts
+++ packages/plasma-new-hope/src/examples/components/typography/components/Body/Body.ts
@@ -1,8 +1,8 @@
-import { bodyConfig } from '../../../../../components/Typography';
+import { bodyConfig, typographyComponent } from '../../../../../components/Typography';
 import { component, mergeConfig } from '../../../../../engines';
 
 import { config } from './Body.config';
 
 const mergedConfig = mergeConfig(bodyConfig, config);
 
-export const Body = component(mergedConfig);
+export const Body = typographyComponent(component(mergedConfig));
