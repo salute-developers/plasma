6d420666bfa2f62c0316e784717464bcbbd1fdc7
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-05-22 12:04:51 +0300

    feat: refacto Skeleton tests

--- packages/plasma-new-hope/src/components/Skeleton/hoc/withSkeleton.styles.ts
+++ packages/plasma-new-hope/src/components/Skeleton/hoc/withSkeleton.styles.ts
@@ -1,4 +1,4 @@
-import { styled } from '@linaria/react';
+import styled from 'styled-components';
 import { applySkeletonPulseGradient, applySkeletonShimmerGradient } from 'src/mixins';
 
 import { classes, privateTokens } from '../tokens';
