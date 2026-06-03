87aad91bfc5a3e8172c220a4de507950fe78221e
Askar Khasanshin <subglowlamp@gmail.com>
2026-06-03 16:13:28 +0300

    feat(sbcom): added apperence for textfield

--- packages/sdds-sbcom/src/components/TextField/TextField.tsx
+++ packages/sdds-sbcom/src/components/TextField/TextField.tsx
@@ -7,6 +7,7 @@ import {
 
 import { config } from './TextField.config';
 import { config as clearConfig } from './TextField.clear.config';
+import { config as searchConfig } from './TextField.search.config';
 
 const mergedConfigDefault = mergeConfig(textFieldConfig, config);
 export const TextFieldDefault = component(mergedConfigDefault);
@@ -14,7 +15,11 @@ export const TextFieldDefault = component(mergedConfigDefault);
 const mergedConfigClear = mergeConfig(textFieldConfig, clearConfig);
 export const TextFieldClear = component(mergedConfigClear);
 
+const mergedConfigSearch = mergeConfig(textFieldConfig, searchConfig);
+export const TextFieldSearch = component(mergedConfigSearch);
+
 export const TextField = createConditionalComponent({
     default: TextFieldDefault,
     clear: TextFieldClear,
+    search: TextFieldSearch,
 });
