a7c4f0e54168d4977e361905c94664af551633bd
Vasiliy <Yeti.or@gmail.com>
2026-06-12 17:34:13 +0300

    refactor(plasma-hope): replace Typography imports with plasma-core; delete Typography (T4)

--- packages/plasma-hope/src/components/Typography/Subtitle/index.ts
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-import { subtitle } from '../../../tokens';
-
-export const Subtitle = styled.div({ overflowWrap: 'break-word', hyphens: 'auto', ...subtitle });
