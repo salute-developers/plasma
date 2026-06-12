a828bbc56e86ac56bcfdc83eb3df172dce86fcab
Vasiliy <Yeti.or@gmail.com>
2026-06-12 17:33:49 +0300

    refactor(plasma-hope): delete unused Badge, Cell, List, Price, Switch, Slider (T2)

--- packages/plasma-hope/src/components/Badge/index.ts
+++
@@ -1,5 +0,0 @@
-export { badgeSizes } from '@salutejs/plasma-core';
-export type { BadgeSize } from '@salutejs/plasma-core';
-
-export { Badge, badgeViews } from './Badge';
-export type { BadgeProps, BadgeView } from './Badge';
