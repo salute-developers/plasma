c584658f35f94bfb45c9fde20ec2842a70aeb3eb
Vasiliy <Yeti.or@gmail.com>
2026-06-12 17:34:21 +0300

    refactor(plasma-hope): use plasma-core Button in ModalView; delete hope Button (T5)

--- packages/plasma-hope/src/components/Button/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button';
-export type { ButtonProps, ButtonSizes, Design } from './types';
-
-export { buttonViews } from './Button.props';
-export type { ButtonView } from './Button.props';
