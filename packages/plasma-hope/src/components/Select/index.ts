c767d67ed957c742d13c2e8e49b8ce279c2fabd8
Vasiliy <Yeti.or@gmail.com>
2026-06-12 17:34:05 +0300

    refactor(plasma-hope): delete unused Editable, Dropdown, Select (T3)

--- packages/plasma-hope/src/components/Select/index.ts
+++
@@ -1,5 +0,0 @@
-export { Select } from './Select';
-export type { SelectProps } from './Select';
-export type { SelectRefElement } from './SelectButton';
-
-export { SelectGroup } from './SelectGroup';
