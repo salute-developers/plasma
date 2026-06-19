fdfbb3343843593c599eae55125849fd9089d8fc
Dima Shugaev <shuga2704@gmail.com>
2026-06-19 09:55:57 +0300

    feat(): mcp for bizcom

---
+++ website/sdds-bizcom-docs/scripts/generateIndex/config.mjs
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const config = {
+    baseUrl: 'https://plasma.sberdevices.ru/sdds-bizcom/',
+    pathToDocs: path.join(dirname(fileURLToPath(import.meta.url)), '../../docs'),
+    exclude: ['_examples', '_theme-switch', '_theme-turn-on', '_utils'],
+    productId: 'sdds-bizcom',
+    qdrantConfig: {
+        collectionName: 'plasma-docs',
+        payloadSchema: ['metadata.productId', 'metadata.source.url'],
+    },
+};
+
+export default config;
