848eb6b05c991e856615f1a42a27cdb9e3dbe73f
Neretin Artem <hawktrike@gmail.com>
2026-07-01 15:20:53 +0500

    fix(plasma-new-hope): Fix `Note` component

--- packages/plasma-new-hope/src/components/Note/Note.tokens.ts
+++ packages/plasma-new-hope/src/components/Note/Note.tokens.ts
@@ -20,7 +20,7 @@ export const tokens = {
     contentBeforeAlignSelf: '--plasma-note-content-before-align-self',
 
     padding: '--plasma-note-padding',
-    paddingBottomWithActionContent: '--plasma-note-padding-with-action-content',
+    paddingBottomWithActionContent: '--plasma-note-padding-bottom-with-action-content',
     paddingScalable: '--plasma-note-padding-scalable',
     borderRadius: '--plasma-note-border-radius',
     gap: '--plasma-note-gap',
