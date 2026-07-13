540567ba514a59d4d3a475168f8a257572880a16
Alexander Lobyntsev <yakutoc@gmail.com>
2026-07-13 13:30:29 +0300

    chore(sb-utils): use disabledToolsParameters

--- packages/plasma-b2c/.storybook/preview.ts
+++ packages/plasma-b2c/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/react-vite';
 import { withTheme, DEFAULT_MODE, ON_DARK_MODE, ON_LIGHT_MODE } from './decoratorThemes';
 import { withToast } from './decoratorToast';
-import { withReactStrictMode, reactStrictModePreviewOption } from '@salutejs/plasma-sb-utils';
+import { withReactStrictMode, reactStrictModePreviewOption, disabledToolsParameters } from '@salutejs/plasma-sb-utils';
 
 // Workaround: to make VoiceOver read russian text properly
 if (typeof document !== 'undefined') {
@@ -48,6 +48,7 @@ const preview: Preview = {
         viewContainer: DEFAULT_MODE,
     },
     parameters: {
+        ...disabledToolsParameters,
         options: {
             storySort: {
                 method: 'alphabetical',
--- packages/plasma-new-hope/.storybook/preview.ts
+++ packages/plasma-new-hope/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/react-vite';
 // @ts-ignore
 import { withViewContainer } from './decoratorViewContainer';
-import { withReactStrictMode, reactStrictModePreviewOption } from '@salutejs/plasma-sb-utils';
+import { withReactStrictMode, reactStrictModePreviewOption, disabledToolsParameters } from '@salutejs/plasma-sb-utils';
 
 const LIGHT_THEME = 'light';
 const DARK_THEME = 'dark';
@@ -13,6 +13,7 @@ const ON_LIGHT_MODE = 'onLight';
 const preview: Preview = {
     decorators: [withViewContainer, withReactStrictMode],
     parameters: {
+        ...disabledToolsParameters,
         docs: {
             source: { type: 'code', language: 'tsx' },
         },
