f9c3f73769291eb2a4dffd945df657508084a11e
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-07-20 15:32:57 +0300

    feat: refactor Rating stories

--- packages/sdds-cs/src/components/Rating/Rating.stories.tsx
+++ packages/sdds-cs/src/components/Rating/Rating.stories.tsx
@@ -1,125 +1,25 @@
-import React, { ComponentProps } from 'react';
-import type { StoryObj, Meta } from '@storybook/react-vite';
-import { InSpacingDecorator, disableProps, getConfigVariations } from '@salutejs/plasma-sb-utils';
-import { IconKeyFill, IconKeyOutline, IconLockFill } from '@salutejs/plasma-icons';
-import { ratingClasses } from '@salutejs/plasma-new-hope/emotion';
+import type { ComponentProps } from 'react';
+import type { Meta } from '@storybook/react-vite';
+import { getRatingStories } from '@salutejs/plasma-sb-utils';
 
 import { Rating } from './Rating';
 import { config } from './Rating.config';
 
-const { views, sizes } = getConfigVariations(config);
-const scorePrecision = [1, 2];
-const valuePlacements = ['before', 'after'];
-const iconsCount = [1, 5, 10];
-const helperTextStretching = ['fixed', 'filled'];
+type RatingProps = ComponentProps<typeof Rating>;
 
-const meta: Meta<typeof Rating> = {
-    title: 'Data Display/Rating',
+const { meta: META, Default, CustomIcons } = getRatingStories({
     component: Rating,
-    decorators: [InSpacingDecorator],
-    argTypes: {
-        view: {
-            options: views,
-            control: {
-                type: 'select',
-            },
-        },
-        size: {
-            options: sizes,
-            control: {
-                type: 'select',
-            },
-        },
-        value: {
-            control: {
-                type: 'number',
-            },
-        },
-        precision: {
-            options: scorePrecision,
-            control: {
-                type: 'inline-radio',
-            },
-            if: { arg: 'hasValue', truthy: true },
-        },
-        valuePlacement: {
-            options: valuePlacements,
-            control: {
-                type: 'inline-radio',
-            },
-            if: { arg: 'hasValue', truthy: true },
-        },
-        hasIcons: {
-            control: {
-                type: 'boolean',
-            },
-            if: { arg: 'hasValue', truthy: true },
-        },
-        iconQuantity: {
-            options: iconsCount,
-            control: {
-                type: 'inline-radio',
-            },
-        },
-        helperTextStretching: {
-            options: helperTextStretching,
-            control: {
-                type: 'inline-radio',
-            },
-            if: { arg: 'helperText', neq: '' },
-        },
-        ...disableProps(['iconSlot', 'iconSlotOutline', 'iconSlotHalf']),
+    componentConfig: config,
+    defaultArgs: {
+        size: 's',
     },
+});
+
+const meta: Meta<RatingProps> = {
+    ...META,
+    title: 'Data Display/Rating',
 };
 
 export default meta;
 
-type StoryPropsDefault = ComponentProps<typeof Rating> & {
-    hasValue?: boolean;
-};
-
-export const Default: StoryObj<StoryPropsDefault> = {
-    args: {
-        view: 'default',
-        size: 's',
-        hasValue: true,
-        value: 3.8,
-        precision: 1,
-        valuePlacement: 'before',
-        hasIcons: true,
-        iconQuantity: 5,
-        helperText: 'Helper text',
-        helperTextStretching: 'filled',
-    },
-    render: ({ hasIcons, hasValue, ...args }) => (
-        <Rating hasIcons={hasValue ? hasIcons : true} hasValue={hasValue} {...args} />
-    ),
-};
-
-export const CustomIcons: StoryObj<StoryPropsDefault> = {
-    argTypes: {
-        ...disableProps(['size', 'view']),
-    },
-    args: {
-        view: 'default',
-        size: 's',
-        hasValue: true,
-        value: 3.8,
-        precision: 1,
-        valuePlacement: 'before',
-        hasIcons: true,
-        iconQuantity: 5,
-        helperText: 'Helper text',
-        helperTextStretching: 'filled',
-    },
-    render: ({ hasIcons, hasValue, ...args }) => (
-        <Rating
-            hasIcons={hasValue ? hasIcons : true}
-            hasValue={hasValue}
-            iconSlot={<IconKeyFill size="s" className={ratingClasses.customIconSizing} />}
-            iconSlotOutline={<IconLockFill size="s" className={ratingClasses.customIconSizing} />}
-            iconSlotHalf={<IconKeyOutline size="s" className={ratingClasses.customIconSizing} />}
-            {...args}
-        />
-    ),
-};
+export { Default, CustomIcons };
