3e7123c44f5b31fe0a06dca32ddb88ae61e62cc5
Anton Mikhailov <antonmih006@gmail.com>
2026-07-27 14:34:05 +0300

    fix(sdds-cs): align Beta Tooltip with legacy Tooltip [PLASMA-7780]

--- packages/sdds-cs/src/components/_beta/Tooltip/Tooltip.config.ts
+++ packages/sdds-cs/src/components/_beta/Tooltip/Tooltip.config.ts
@@ -1,47 +1,41 @@
 import {
     bodyS,
-    bodyXS,
-    inverseSurfaceSolidCardBrightness,
+    inverseSurfaceSolidCard,
     inverseTextPrimary,
-    shadowDownHardM,
+    onDarkSurfaceSolidSecondary,
+    shadowDownHardS,
 } from '@salutejs/sdds-themes/tokens/sdds_cs';
 import { css, _beta_tooltipTokens as tokens } from '@salutejs/plasma-new-hope/emotion';
 
 export const config = {
     defaults: {
         view: 'default',
-        size: 'm',
+        size: 's',
     },
     variations: {
         view: {
             default: css`
-                ${tokens.backgroundColor}: ${inverseSurfaceSolidCardBrightness};
-                ${tokens.boxShadow}: ${shadowDownHardM};
+                ${tokens.backgroundColor}: ${inverseSurfaceSolidCard};
+                ${tokens.boxShadow}: ${shadowDownHardS};
+                ${tokens.color}: ${inverseTextPrimary};
+            `,
+            secondary: css`
+                ${tokens.backgroundColor}: ${onDarkSurfaceSolidSecondary};
+                ${tokens.boxShadow}: ${shadowDownHardS};
                 ${tokens.color}: ${inverseTextPrimary};
             `,
         },
         size: {
-            m: css`
-                ${tokens.borderRadius}: 0.625rem;
-                ${tokens.padding}: 0.75rem 0.875rem;
-                ${tokens.gap}: 0.375rem;
+            s: css`
+                ${tokens.borderRadius}: 0.5rem;
+                ${tokens.padding}: 0.5rem 0.75rem;
+                ${tokens.gap}: 0.25rem;
                 ${tokens.fontFamily}: ${bodyS.fontFamily};
                 ${tokens.fontSize}: ${bodyS.fontSize};
                 ${tokens.fontStyle}: ${bodyS.fontStyle};
                 ${tokens.fontWeight}: ${bodyS.fontWeight};
                 ${tokens.letterSpacing}: ${bodyS.letterSpacing};
                 ${tokens.lineHeight}: ${bodyS.lineHeight};
-            `,
-            s: css`
-                ${tokens.borderRadius}: 0.5rem;
-                ${tokens.padding}: 0.5rem 0.75rem;
-                ${tokens.gap}: 0.25rem;
-                ${tokens.fontFamily}: ${bodyXS.fontFamily};
-                ${tokens.fontSize}: ${bodyXS.fontSize};
-                ${tokens.fontStyle}: ${bodyXS.fontStyle};
-                ${tokens.fontWeight}: ${bodyXS.fontWeight};
-                ${tokens.letterSpacing}: ${bodyXS.letterSpacing};
-                ${tokens.lineHeight}: ${bodyXS.lineHeight};
             `,
         },
     },
