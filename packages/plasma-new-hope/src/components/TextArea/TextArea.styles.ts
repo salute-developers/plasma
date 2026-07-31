cf361a50199c8cd6021247fbec1b8a92dccddf37
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-07-31 11:02:43 +0300

    feat(new-hope): add z-index for requiredIndicator in TextArea

--- packages/plasma-new-hope/src/components/TextArea/TextArea.styles.ts
+++ packages/plasma-new-hope/src/components/TextArea/TextArea.styles.ts
@@ -381,6 +381,7 @@ export const StyledPlaceholder = styled.label<{
 export const StyledIndicator = styled.div`
     position: absolute;
     border-radius: 50%;
+    z-index: 1;
 
     background-color: var(${tokens.indicatorColor});
 
