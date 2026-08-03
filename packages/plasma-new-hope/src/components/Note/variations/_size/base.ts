cd57c704b4d4764de6dc3c844fe24eafbcb66bfe
Vladimir Mytnik <vova@mytnik.net>
2026-08-03 21:01:58 +0800

    feat(new-hope): align self token for ContentBefore and fixed content

--- packages/plasma-new-hope/src/components/Note/variations/_size/base.ts
+++ packages/plasma-new-hope/src/components/Note/variations/_size/base.ts
@@ -38,6 +38,7 @@ export const base = css`
         width: var(${tokens.fixedContentBeforeWidth});
         height: var(${tokens.fixedContentBeforeHeight});
         padding: var(${tokens.fixedContentBeforePadding});
+        align-self: var(${tokens.fixedContentBeforeAlignSelf});
         box-sizing: border-box;
     }
 
