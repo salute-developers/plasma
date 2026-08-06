bc0fbaab9b10f00e70459e45e6d0cdbc735f649a
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-08-06 10:47:10 +0300

    feat: update common libs file

--- scripts/cy-cli.js
+++ scripts/cy-cli.js
@@ -8,43 +8,9 @@
 const inquirer = require('inquirer');
 const { spawnSync } = require('child_process');
 
+const { PACKAGES, PACKAGE_MAP } = require('./lib/final-packages');
+
 const prompt = inquirer.prompt || inquirer.default?.prompt || inquirer.createPromptModule?.();
-
-const PACKAGES = [
-    'b2c',
-    'giga',
-    'web',
-    'cs',
-    'insol',
-    'insol-next',
-    'netology',
-    'bizcom',
-    'serv',
-    'scan',
-    'os',
-    'platform-ai',
-    'finai',
-    'dfa',
-    'homeds',
-];
-
-const PACKAGE_MAP = {
-    b2c: 'plasma-b2c',
-    giga: 'plasma-giga',
-    web: 'plasma-web',
-    cs: 'sdds-cs',
-    insol: 'sdds-insol',
-    'insol-next': 'sdds-insol-next',
-    netology: 'sdds-netology',
-    bizcom: 'sdds-bizcom',
-    serv: 'sdds-serv',
-    scan: 'sdds-scan',
-    os: 'sdds-os',
-    'platform-ai': 'sdds-platform-ai',
-    finai: 'sdds-finai',
-    dfa: 'sdds-dfa',
-    homeds: 'plasma-homeds',
-};
 
 async function main() {
     const { action } = await prompt([
