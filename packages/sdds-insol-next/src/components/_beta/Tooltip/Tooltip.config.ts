e3d1c83cc9e3c9e1370601bb53053fd93b94eceb
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-08-06 15:17:13 +0300

    feat(sdds-insol-next): return beta comps

---
+++ packages/sdds-insol-next/src/components/_beta/Tooltip/Tooltip.config.ts
@@ -0,0 +1,36 @@
+import { css, _beta_tooltipTokens as tokens } from '@salutejs/plasma-new-hope/styled-components';
+import {
+    bodyS,
+    shadowDownHardM,
+    surfaceSolidCardBrightness,
+    textPrimary,
+} from '@salutejs/sdds-themes/tokens/sdds_insol_next';
+
+export const config = {
+    defaults: {
+        view: 'default',
+        size: 'm',
+    },
+    variations: {
+        view: {
+            default: css`
+                ${tokens.backgroundColor}: ${surfaceSolidCardBrightness};
+                ${tokens.boxShadow}: ${shadowDownHardM};
+                ${tokens.color}: ${textPrimary};
+            `,
+        },
+        size: {
+            m: css`
+                ${tokens.borderRadius}: 0.625rem;
+                ${tokens.padding}: 0.6875rem 0.875rem;
+                ${tokens.gap}: 0.375rem;
+                ${tokens.fontFamily}: ${bodyS.fontFamily};
+                ${tokens.fontSize}: ${bodyS.fontSize};
+                ${tokens.fontStyle}: ${bodyS.fontStyle};
+                ${tokens.fontWeight}: ${bodyS.fontWeight};
+                ${tokens.letterSpacing}: ${bodyS.letterSpacing};
+                ${tokens.lineHeight}: ${bodyS.lineHeight};
+            `,
+        },
+    },
+};
