505bd7bcdbb130dc19678844decf6c0729491a02
Igor Yaroslavtsev <ihteyar2@yandex.ru>
2026-08-11 13:18:41 +0500

    test(core): migrate tests for Link

--- packages/sdds-scan/src/index.ts
+++ packages/sdds-scan/src/index.ts
@@ -13,6 +13,7 @@ export * from './components/ProgressBarCircular';
 // Navigation
 export * from './components/Breadcrumbs';
 export * from './components/Dropdown';
+export * from './components/Link';
 export * from './components/Pagination';
 export * from './components/Tabs';
 
@@ -86,7 +87,6 @@ export * from './components/Flow';
 
 // INFO: Disabled components
 // export * from './components/Grid';
-// export * from './components/Link';
 // export * from './components/Portal';
 // export * from './components/Price';
 // export * from './components/Sheet';
