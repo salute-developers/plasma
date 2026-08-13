a6ba4d8c0de04079b266cd5890777fb6eb29d0a7
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-08-13 12:01:56 +0300

    feat(plasma-new-hope): add formatting to NumberInput

--- utils/plasma-sb-utils/src/components/NumberInput/stories.tsx
+++ utils/plasma-sb-utils/src/components/NumberInput/stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { action } from 'storybook/actions';
 
 const onChange = action('onChange');
@@ -6,7 +6,11 @@ const onDecrement = action('onDecrement');
 const onIncrement = action('onIncrement');
 
 export const createDefaultStory = (NumberInput: any) => (args: any) => {
-    const [value, setValue] = useState<number | string | undefined>(5);
+    const [value, setValue] = useState<number | string | undefined>(args.value);
+
+    useEffect(() => {
+        setValue(args.value);
+    }, [args.value]);
 
     const handleChange = (_: any, newValue: number | string | undefined) => {
         setValue(newValue);
