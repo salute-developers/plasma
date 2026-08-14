a81e83f54c4001a7e32a97d896823dc28646d461
Dima Shugaev <shuga2704@gmail.com>
2026-08-14 10:32:03 +0300

    feat(sdds-icons): gradient support

--- packages/sdds-icons/src/createIcon24.tsx
+++ packages/sdds-icons/src/createIcon24.tsx
@@ -1,17 +1,5 @@
-import React, { forwardRef } from 'react';
-import type { ComponentPropsWithoutRef, ReactNode } from 'react';
+import type { ReactNode } from 'react';
 
-export const createIcon24 = (content: ReactNode) =>
-    forwardRef<SVGSVGElement, ComponentPropsWithoutRef<'svg'>>((props, ref) => (
-        <svg
-            ref={ref}
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            {...props}
-        >
-            {content}
-        </svg>
-    ));
+import { createIcon } from './createIcon.js';
+
+export const createIcon24 = (content: ReactNode) => createIcon(content, 24);
--- packages/sdds-icons/src/createIcon36.tsx
+++ packages/sdds-icons/src/createIcon36.tsx
@@ -1,17 +1,5 @@
-import React, { forwardRef } from 'react';
-import type { ComponentPropsWithoutRef, ReactNode } from 'react';
+import type { ReactNode } from 'react';
 
-export const createIcon36 = (content: ReactNode) =>
-    forwardRef<SVGSVGElement, ComponentPropsWithoutRef<'svg'>>((props, ref) => (
-        <svg
-            ref={ref}
-            width={36}
-            height={36}
-            viewBox="0 0 36 36"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            {...props}
-        >
-            {content}
-        </svg>
-    ));
+import { createIcon } from './createIcon.js';
+
+export const createIcon36 = (content: ReactNode) => createIcon(content, 36);
