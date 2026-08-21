9624770ed3e9ef8d3d809c3f76c4ed38448140c0
Anton Mikhailov <antonmih006@gmail.com>
2026-08-21 14:33:55 +0300

    fix: handle gradient token states

--- packages/themes/core-themes/src/creators/createShadowTokens.ts
+++ packages/themes/core-themes/src/creators/createShadowTokens.ts
@@ -12,7 +12,7 @@ const getCSSVariable = (tokenName: string, value: string[]) => {
     return `  --shadow-${tokenName}: ${value.join(', ')};`;
 };
 
-const getShadow = (shadow: any, tokens: TokenType[], direction: 'down' | 'up', isJS = false) =>
+const getShadow = (shadow: any, tokens: TokenType[], direction: 'down' | 'up' | 'gradient', isJS = false) =>
     tokens
         .filter((token) => token.tags[0] === direction)
         .map((token) => {
@@ -39,7 +39,13 @@ export const createShadowTokens = (shadow: any, tokens?: Array<TokenType>, isJS 
         };
     }
 
-    const shadows = `${getShadow(shadow, tokens, 'down', isJS)}\n${getShadow(shadow, tokens, 'up', isJS)}`;
+    const shadows = [
+        getShadow(shadow, tokens, 'down', isJS),
+        getShadow(shadow, tokens, 'up', isJS),
+        getShadow(shadow, tokens, 'gradient', isJS),
+    ]
+        .filter(Boolean)
+        .join('\n');
 
     return {
         dark: shadows,
