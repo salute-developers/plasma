1118dbde1420ea9cc8dd1264c39a018ab8bfb9fe
Krivonos Aleksandr <yzn-yfv@mail.ru>
2026-08-24 12:59:13 +0300

    feat: refactor Drawer stories

--- packages/plasma-new-hope/src/examples/components/Drawer/Drawer.stories.tsx
+++ packages/plasma-new-hope/src/examples/components/Drawer/Drawer.stories.tsx
@@ -1,228 +1,43 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import type { ComponentProps } from 'react';
-import type { StoryObj, Meta } from '@storybook/react-vite';
+import type { Meta } from '@storybook/react-vite';
+import { getDrawerStories } from '@salutejs/plasma-sb-utils';
 
 import { Button } from '../Button/Button';
-import { WithTheme } from '../../_helpers';
 import { Heading } from '../typography/components/Heading/Heading';
-import { IconDone } from '../../../components/_Icon';
 import { SSRProvider } from '../../../components/SSRProvider';
 import { PopupProvider } from '../Popup/Popup';
-import type { ClosePlacementType } from '../../../components/Drawer';
 
 import { Drawer, DrawerContent, DrawerFooter, DrawerHeader } from './Drawer';
+import { config } from './Drawer.config';
 
-const meta: Meta<typeof Drawer> = {
-    title: 'Overlay/Drawer',
-    decorators: [WithTheme],
-    argTypes: {
-        placement: {
-            options: ['top', 'bottom', 'right', 'left'],
-            control: {
-                type: 'select',
-            },
-            table: { defaultValue: { summary: 'center' } },
-        },
-        offsetX: {
-            control: {
-                type: 'number',
-            },
-            table: { defaultValue: { summary: 0 } },
-        },
-        offsetY: {
-            control: {
-                type: 'number',
-            },
-            table: { defaultValue: { summary: 0 } },
-        },
-        closeOnEsc: {
-            control: {
-                type: 'boolean',
-            },
-            table: { defaultValue: { summary: true } },
-        },
-        closeOnOverlayClick: {
-            control: {
-                type: 'boolean',
-            },
-            table: { defaultValue: { summary: true } },
-        },
-        withBlur: {
-            control: {
-                type: 'boolean',
-            },
-            table: { defaultValue: { summary: false } },
-        },
-        showHeader: {
-            control: {
-                type: 'boolean',
-            },
-            table: { defaultValue: { summary: true } },
-        },
-        showFooter: {
-            control: {
-                type: 'boolean',
-            },
-            table: { defaultValue: { summary: true } },
-        },
-        showActions: {
-            control: {
-                type: 'boolean',
-            },
-            table: { defaultValue: { summary: true } },
-        },
-        closePlacement: {
-            options: ['right', 'left'],
-            control: {
-                type: 'select',
-            },
-            table: { defaultValue: { summary: 'right' } },
-        },
-        borderRadius: {
-            options: ['none', 'default'],
-            control: {
-                type: 'select',
-            },
-        },
-        customBackgroundColor: {
-            control: {
-                type: 'color',
-            },
-        },
-        customContentBackgroundColor: {
-            control: {
-                type: 'color',
-            },
-        },
+type DrawerProps = ComponentProps<typeof Drawer>;
+type HeadingProps = ComponentProps<typeof Heading>;
+
+const H2 = (props: HeadingProps) => <Heading {...props} size="h2" />;
+const H3 = (props: HeadingProps) => <Heading {...props} size="h3" />;
+
+const { meta: META, Default } = getDrawerStories({
+    component: Drawer,
+    componentConfig: config,
+    additionalComponents: {
+        Button,
+        DrawerContent,
+        DrawerFooter,
+        DrawerHeader,
+        H2,
+        H3,
+        PopupProvider,
+        SSRProvider,
     },
+    frame: 'theme-root',
+});
+
+const meta: Meta<DrawerProps> = {
+    ...META,
+    title: 'Overlay/Drawer',
 };
 
 export default meta;
 
-type StoryDrawerProps = ComponentProps<typeof Drawer> & {
-    placement: string;
-    offsetX: number;
-    offsetY: number;
-    closeOnEsc: boolean;
-    closeOnOverlayClick: boolean;
-    showHeader: boolean;
-    showFooter: boolean;
-    showActions: boolean;
-    closePlacement: string;
-    hasClose: boolean;
-    asModal: boolean;
-};
-
-const StyledWrapper = styled.div`
-    height: 100vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-`;
-
-const StyledContentWrapper = styled.div`
-    height: 80%;
-    display: flex;
-    flex-grow: 1;
-`;
-
-const StyledContent = styled.div`
-    background: #a8a875;
-    flex-grow: 1;
-    height: 100%;
-`;
-
-const StyledSection = styled.div`
-    background: #808080;
-    min-height: 20%;
-`;
-
-const StyledIconButton = styled(Button)`
-    position: relative;
-    width: 1.5rem;
-    height: 1.5rem;
-`;
-
-const StoryDrawerDemo = ({
-    showHeader,
-    showFooter,
-    showActions,
-    hasClose,
-    closePlacement,
-    offsetX,
-    offsetY,
-    ...rest
-}: StoryDrawerProps) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    return (
-        <SSRProvider>
-            <PopupProvider>
-                <StyledWrapper>
-                    <StyledSection>
-                        <Button text="Открыть панель" onClick={() => setIsOpen(true)} />
-                    </StyledSection>
-                    <StyledContentWrapper>
-                        <Drawer
-                            frame="theme-root"
-                            opened={isOpen}
-                            offset={[offsetX, offsetY]}
-                            onClose={() => setIsOpen(false)}
-                            {...rest}
-                            overlayProps={{ background: 'red', left: '20%', right: '20%' }}
-                        >
-                            <DrawerHeader
-                                hasClose={hasClose}
-                                closePlacement={closePlacement as ClosePlacementType}
-                                actions={
-                                    showActions && (
-                                        <StyledIconButton size="s" view="clear">
-                                            <IconDone size="s" color="inherit" />
-                                        </StyledIconButton>
-                                    )
-                                }
-                                onClose={() => setIsOpen(false)}
-                            >
-                                {showHeader && <Heading size="h3">Header</Heading>}
-                            </DrawerHeader>
-                            <DrawerContent>Content</DrawerContent>
-                            {showFooter && (
-                                <DrawerFooter>
-                                    <Heading size="h3">Footer</Heading>
-                                </DrawerFooter>
-                            )}
-                        </Drawer>
-                        <StyledContent>
-                            <Heading style={{ margin: '2rem' }} size="h2">
-                                Some basic content
-                            </Heading>
-                        </StyledContent>
-                    </StyledContentWrapper>
-                    <StyledSection />
-                </StyledWrapper>
-            </PopupProvider>
-        </SSRProvider>
-    );
-};
-
-export const DrawerDemo: StoryObj<StoryDrawerProps> = {
-    args: {
-        placement: 'right',
-        withBlur: false,
-        closeOnEsc: true,
-        closeOnOverlayClick: true,
-        offsetX: 0,
-        offsetY: 0,
-        showHeader: true,
-        showFooter: true,
-        showActions: true,
-        hasClose: true,
-        asModal: true,
-        closePlacement: 'right',
-        width: '50vw',
-        height: '100dvh',
-        borderRadius: 'none',
-    },
-    render: (args) => <StoryDrawerDemo {...args} />,
-};
+export { Default };
